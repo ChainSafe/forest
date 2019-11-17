c2fc90f53b84e5c650ec5467f894bbb8f03b047a
austinabell <austinabell8@gmail.com>
2019-11-17 14:33:05 -0500

    Clean up linting issues

--- blockchain/message_pool/src/pool.rs
+++ blockchain/message_pool/src/pool.rs
@@ -3,7 +3,7 @@ use message::SignedMessage;
 trait PoolValidator {
     fn validate(&self, msg: SignedMessage);
 }
-
+#[allow(dead_code)]
 // MessagePoolConfig holds all configuration options related to nodes message pool (mpool).
 pub struct MessagePoolConfig {
     // max_pool_size is the maximum number of pending messages will will allow in the message pool at any time
@@ -11,7 +11,7 @@ pub struct MessagePoolConfig {
     // max_nonce_gap is the maximum nonce of a message past the last received on chain
     max_nonce_gap: u64,
 }
-
+#[allow(dead_code)]
 // Pool keeps an unordered, de-duplicated set of Messages and supports removal by CID.
 // By 'de-duplicated' we mean that insertion of a message by cid that already
 // exists is a nop. We use a Pool to store all messages received by this node
@@ -21,39 +21,39 @@ pub struct MessagePoolConfig {
 // Pool is safe for concurrent access.
 pub struct Pool {
     cfg: MessagePoolConfig,
-    validator: PoolValidator,
+    validator: dyn PoolValidator,
     // pending CID messages
     // addressNonce
 }
-
+#[allow(dead_code)]
 struct TimedMessage {
     // message
     added_at: u64,
 }
-
+#[allow(dead_code)]
 fn new_default_message_pool_config() -> MessagePoolConfig {
     return MessagePoolConfig {
         max_pool_size: 10000,
         max_nonce_gap: 100,
     };
 }
-
+#[allow(dead_code)]
 // new_pool constructs a new Pool.
 fn new_pool() -> &'static str {
     return "Pool{ cfg: cfg, validator: validator}";
 }
-
-struct addressNonce {
+#[allow(dead_code)]
+struct AddressNonce {
     //addr  address
     nonce: u64,
 }
-
-fn new_address_nonce(msg: SignedMessage) -> &'static str {
+#[allow(dead_code)]
+fn new_address_nonce(_msg: SignedMessage) -> &'static str {
     return "return addressNonce";
 }
-
+#[allow(dead_code)]
 // Add adds a message to the pool, tagged with the block height at which it was received.
 // Does nothing if the message is already in the pool.
-fn add(msg: SignedMessage, height: u64) -> &'static str {
+fn add(_msg: SignedMessage, _height: u64) -> &'static str {
     return "add message to pool";
 }
