fefd4c2edde506a47d5fe6cb4e3690be555d5366
austinabell <austinabell8@gmail.com>
2019-11-17 16:14:03 -0500

    add back used pub types

--- vm/actor/src/actor_state.rs
+++ vm/actor/src/actor_state.rs
@@ -2,20 +2,20 @@ extern crate cid;
 use cid::Cid;
 
 #[allow(dead_code)]
-type UVarint = u64;
+pub type UVarint = u64;
 
 #[allow(dead_code)]
-type TokenAmount = UVarint;
+pub type TokenAmount = UVarint;
 #[allow(dead_code)]
-type CallSeqNum = UVarint;
+pub type CallSeqNum = UVarint;
 
 #[allow(dead_code)]
-type CodeCID = Cid;
+pub type CodeCID = Cid;
 #[allow(dead_code)]
-type ActorSubstateCID = Cid;
+pub type ActorSubstateCID = Cid;
 
 #[allow(dead_code)]
-struct ActorState {
+pub struct ActorState {
     code_cid: CodeCID,
     state: ActorSubstateCID,
     balance: TokenAmount,
