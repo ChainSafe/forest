98f2f34e01c950887e05b718b072c9571467a43a
Gregory Markou <gregorymarkou@gmail.com>
2019-11-19 12:18:18 -0500

    add basic cli

---
+++ node/cli/cli.rs
@@ -0,0 +1,26 @@
+use clap::{App, Arg, SubCommand};
+
+pub fn main() {
+    let app = App::new("Ferret")
+        .version(crate_version!())
+        .author("ChainSafe Systems <info@chainsafe.io>")
+        .about("Filecoin implementation in Rust")
+        /*
+         * Flags
+         */
+        .arg(
+            Arg::with_name("config")
+                .long("config")
+                .short("c")
+                .help("A toml file containing relevant configurations.")
+        )
+        .get_matches();
+
+    if app.is_present("Ferret") {
+        println!("Ferret was run!")
+    }
+
+    if let Some(ref config_file) = matches.value_of("config") {
+        println!("File path: {}", config_file);
+    }
+}
