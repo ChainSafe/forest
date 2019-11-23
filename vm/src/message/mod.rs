cd7e7d1000d83ad98fcc3d27bf322bca56e5fcfa
austinabell <austinabell8@gmail.com>
2019-11-23 14:16:16 -0500

    Implements basic message type and signature flow

--- vm/src/message/mod.rs
+++ vm/src/message/mod.rs
@@ -1,5 +1,41 @@
+mod errors;
+mod signature;
 mod signed_message;
-mod unsigned_message;
+mod signer;
 
+pub use errors::*;
+pub use signature::*;
 pub use signed_message::*;
-pub use unsigned_message::*;
+pub use signer::*;
+
+use crate::address::Address;
+use num_bigint::BigUint;
+
+/// VM message type which includes all data needed for a state transition
+#[derive(PartialEq, Clone)]
+pub struct Message {
+    pub(crate) from: Address,
+    pub(crate) to: Address,
+
+    pub(crate) nonce: u64,
+
+    pub(crate) value: BigUint,
+
+    pub(crate) method: u64,
+    pub(crate) params: Vec<u8>,
+
+    pub(crate) gas_price: BigUint, // change these to big int
+    pub(crate) gas_limit: BigUint,
+}
+
+impl Message {
+    // Marshalling and unmarshalling
+    pub fn unmarshall_cbor(&mut self, _bz: &mut [u8]) -> Result<(), Error> {
+        // TODO
+        Err(Error::DecodingError)
+    }
+    pub fn marshall_cbor(&self) -> Result<Vec<u8>, Error> {
+        // TODO
+        Err(Error::EncodingError)
+    }
+}
