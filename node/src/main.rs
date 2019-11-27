34fbfb9ccdb58f47fefb954200226ec44535ab97
Gregory Markou <gregorymarkou@gmail.com>
2019-11-27 03:25:32 -0500

    baisc file writing and reading

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,7 +1,12 @@
 mod cli;
+mod utils;
 
 use cli::cli;
+use utils::{write_libp2p_id, get_libp2p_id};
 
 fn main() {
     cli();
+    write_libp2p_id("5");
+    // let c = get_libp2p_id();
+    // println!("{:?}", c);
 }
---
+++ node/src/utils/mod.rs
@@ -0,0 +1,52 @@
+extern crate dirs;
+
+use std::fs::{File, create_dir_all};
+use std::path::Path;
+use std::io::{Result, prelude::*};
+use dirs::home_dir;
+
+// Generic methods
+
+/// Writes a &str to a specified file.
+/// TODO Do we assume the file exists?
+fn write_string_to_file(message: &str, path: String) -> Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(&message.as_bytes())?;
+    Ok(())
+}
+
+fn get_home_dir() -> String {
+    // Will return correct path for windows, linux, and osx
+    home_dir().unwrap().to_str().unwrap().to_owned()
+}
+
+// libp2p specific methods
+
+fn get_libp2p_path() -> String {
+    // TODO remove hardcoded `file`
+    let file = "/ferret/libp2p";
+    format!("{:?}{}", get_home_dir(), file)
+}
+
+/// Stores the libp2p id to the Ferret directory
+pub fn write_libp2p_id(id: &str) {
+    let path = get_libp2p_path();
+    
+    // Create path if it doesn't exist
+    if !Path::new(&path).exists() {
+        // TODO result/error
+        // Use .expect()??
+        let _ = create_dir_all(Path::new(&path));
+    }
+    // TODO handle result somehow
+    let _ = write_string_to_file(id, get_libp2p_path());
+}
+
+/// Check if libp2p id exists in filesystem
+pub fn get_libp2p_id() -> Result<String> {
+    let mut file = File::open(get_libp2p_path())?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    // assert_eq!(contents, "Hello, world!");
+    Ok(contents)
+}
