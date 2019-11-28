1df2cc368f6458b2fdf6a0e05fabc54ce9daf3f6
Eric Tu <tu.eric@hotmail.com>
2019-11-28 14:25:26 -0500

    initial logger

---
+++ node/src/log/mod.rs
@@ -0,0 +1,12 @@
+use slog::*;
+use slog_async;
+use slog_term;
+use slog::Drain;
+
+pub fn setup_logging() -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(drain, o!())
+}
--- node/src/main.rs
+++ node/src/main.rs
@@ -1,7 +1,16 @@
 mod cli;
+mod log;
+use slog::*;
 
 use cli::cli;
-
+#[derive(Debug)]
+struct A {
+    a:i32,
+    b:String,
+}
 fn main() {
     cli();
+    let log = log::setup_logging();
+    info!(log, "Starting Ferret");
+
 }
