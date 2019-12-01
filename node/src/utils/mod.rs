2dee296d04ebe93ee3c7d79f0e0f5a7c6de4e89e
Gregory Markou <gregorymarkou@gmail.com>
2019-12-01 11:29:54 -0500

    move tests

--- node/src/utils/mod.rs
+++ node/src/utils/mod.rs
@@ -2,8 +2,11 @@
 
 extern crate dirs;
 
+#[cfg(test)]
+mod test;
+
 use dirs::home_dir;
-use std::fs::{create_dir_all, remove_dir_all, File};
+use std::fs::{create_dir_all, File};
 use std::io::{prelude::*, Result};
 use std::path::Path;
 
@@ -40,66 +43,4 @@ pub fn read_file(path: String) -> Result<String> {
 fn get_home_dir() -> String {
     // We will panic if we cannot determine a home directory.
     home_dir().unwrap().to_str().unwrap().to_owned()
-}
-
-//
-// Test helper function
-//
-
-// Please use with caution, remove_dir_all will completely delete a directory
-fn cleanup_file(path: &str) {
-    if let Err(e) = remove_dir_all(path) {
-        println!("cleanup_file() failed: {:?}", e);
-        panic!(false);
-    }
-}
-
-#[test]
-fn test_write_string_to_file() {
-    let path = "./test-write/";
-    let file = "test.txt";
-    match write_string_to_file("message", path, file) {
-        Ok(_) => cleanup_file(path),
-        Err(e) => {
-            println!("{:?}", e);
-            cleanup_file(path);
-            assert!(false);
-        }
-    }
-}
-
-#[test]
-fn test_write_string_to_file_nested_dir() {
-    let root = "./test_missing";
-    let path = format!("{}{}", root, "/test_write_string/");
-    match write_string_to_file("message", &path, "test-file") {
-        Ok(_) => cleanup_file(root),
-        Err(e) => {
-            println!("{:?}", e);
-            cleanup_file(root);
-            assert!(false);
-        }
-    }
-}
-
-#[test]
-fn test_read_file() {
-    let msg = "Hello World!";
-    let path = "./test_read_file/";
-    let file_name = "out.keystore";
-    match write_string_to_file(msg, path, file_name) {
-        Ok(_) => (),
-        Err(e) => assert!(false, e),
-    }
-    match read_file(format!("{}{}", path, file_name)) {
-        Ok(contents) => {
-            cleanup_file(path);
-            assert_eq!(contents, msg)
-        }
-        Err(e) => {
-            println!("{:?}", e);
-            cleanup_file(path);
-            assert!(false);
-        }
-    }
 }
