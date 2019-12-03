9fba7d98ddcaba222bb20be43ad4f83818eb05e7
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-12-03 11:37:19 -0600

    New Tipset w/ unit tests (#56)
    * Blockchain Interfaces WIP
    
    * Added multihash package to retrieve blake2b hash
    * Added sort_key method for Ticket struct
    * Added equals method for TipSetKeys struct type
    * WIP for Tipset new fn
    * WIP cid method for block type which would return CID of block
    
    * New Tipset fn WIP
    
    * Added new conditional checks as per spec update
    * WIP sorted and compare logic for ticket size
    * Fix cid method for blockHeader
    * WIP new fn tests
    
    * Added unit tests
    
    * Added unit tests for tipset methods
    
    * WIP tipset new fn
    
    * Added unit tests
    
    * Added unit tests for tipset methods
    
    * Added tipset new logic
    
    * Added new tipset logic, includes conditional checks and sorting based on ticket size
    * Added unit tests for tipset methods
    * Added cid fn although it is incomplete as we need cbor encoding
    
    * Fixed equals fn
    
    * Fixed equals fn check
    * Linted
    
    * Remove typo
    
    * Made requested changes
    * Updated multihash to specific version
    * Removed extern crate statement
    * Improved commenting
    * Updated sort statement
    * Improved unit test setup
    * Updated error messages for tipset condition checks
    
    * Made requested changes
    * Updated cid reference
    * Removed pub from mod.rs
    
    * Requested changes made
    * Updated error to handle String
    * Removed unnecessary import
    * Removed sort_key method for Ticket
    
    * Requested changes made
    * Removed dead code and unused variable tag
    
    * Remove reverse in sort
    
    * improved tests

--- blockchain/src/blocks/errors.rs
+++ blockchain/src/blocks/errors.rs
@@ -2,14 +2,14 @@ use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    UndefinedTipSet,
+    UndefinedTipSet(String),
     NoBlocks,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
-            Error::UndefinedTipSet => write!(f, "Undefined tipset"),
+            Error::UndefinedTipSet(ref msg) => write!(f, "Invalid tipset: {}", msg),
             Error::NoBlocks => write!(f, "No blocks for tipset"),
         }
     }
