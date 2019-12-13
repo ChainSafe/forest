2bb5c0accf23231cb99fe4baf261ebf371ea8ac2
Austin Abell <austinabell8@gmail.com>
2019-12-13 14:25:57 -0500

    remove ref keywords (#99)

--- blockchain/src/blocks/errors.rs
+++ blockchain/src/blocks/errors.rs
@@ -8,8 +8,8 @@ pub enum Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Error::UndefinedTipSet(ref msg) => write!(f, "Invalid tipset: {}", msg),
+        match self {
+            Error::UndefinedTipSet(msg) => write!(f, "Invalid tipset: {}", msg),
             Error::NoBlocks => write!(f, "No blocks for tipset"),
         }
     }
