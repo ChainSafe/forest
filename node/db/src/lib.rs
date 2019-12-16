92e070f14c317788157bb8f2cb336526410504ed
Gregory Markou <gregorymarkou@gmail.com>
2019-12-16 09:47:59 -0500

    more generic

--- node/db/src/lib.rs
+++ node/db/src/lib.rs
@@ -6,7 +6,10 @@ use std::io;
 pub trait database {
     fn open(&self) -> Result<(), io::Error>;
     fn close(&self) -> Result<(), io::Error>;
-    fn get(&self, key: String) -> Result<vec<u8>, io::Error>;
-    fn put(&self, key: String, value: vec<u8>) -> Result<vec<u8>, io:Error>;
-    fn delete(&self, key: String) -> Result<vec<u8>, io:Error>;
+    fn get<K: AsRef<[u8]>>(&self, key: K) -> Result<vec<u8>, io::Error>;
+    fn delete<K: AsRef<[u8]>>(&self, key: K) -> Result<(), io:Error>;
+    fn put<K, V>(&self, key: K, value: V) -> Result<(), io:Error>
+    where 
+        K: AsRef<[u8]>,
+        V: AsRef<[u8]>;
 }
