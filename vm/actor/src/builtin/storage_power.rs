eace8d8176c4899e0bed54d8a334dbbadec9a6ed
Austin Abell <austinabell8@gmail.com>
2020-01-07 08:46:10 +1300

    Storage Power Actor framework (#129)
    * Setup storage power actor framework
    
    * add license

---
+++ vm/actor/src/builtin/storage_power.rs
@@ -0,0 +1,72 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
+use num_bigint::BigUint;
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+use runtime::{arg_end, ActorCode, Runtime};
+use vm::{ExitCode, InvocOutput, MethodNum, MethodParams, SysCode, METHOD_CONSTRUCTOR};
+
+/// State of storage power actor
+pub struct StoragePowerActorState {
+    // TODO add power tables on finished spec
+    _total_storage: BigUint,
+}
+
+/// Method definitions for Storage Power Actor
+#[derive(FromPrimitive)]
+pub enum StoragePowerMethod {
+    /// Constructor for Storage Power Actor
+    Constructor = METHOD_CONSTRUCTOR,
+    // TODO add other methods on finished spec
+    /// Gets the total storage for the network
+    GetTotalStorage = 5,
+}
+
+impl StoragePowerMethod {
+    /// from_method_num converts a method number into an StoragePowerMethod enum
+    fn from_method_num(m: MethodNum) -> Option<StoragePowerMethod> {
+        FromPrimitive::from_i32(m.into())
+    }
+}
+
+#[derive(Clone)]
+pub struct StoragePowerActorCode;
+impl StoragePowerActorCode {
+    /// Constructor for StoragePower actor
+    fn constructor(_rt: &dyn Runtime) -> InvocOutput {
+        // TODO
+        unimplemented!();
+    }
+    /// Withdraw available funds from StoragePower map
+    fn get_total_storage(rt: &dyn Runtime) -> InvocOutput {
+        // TODO get actor state from storage and use as output
+        let result = BigUint::from(0 as u32).to_bytes_be();
+        rt.value_return(result)
+    }
+}
+
+impl ActorCode for StoragePowerActorCode {
+    fn invoke_method(
+        &self,
+        rt: &dyn Runtime,
+        method: MethodNum,
+        params: &MethodParams,
+    ) -> InvocOutput {
+        match StoragePowerMethod::from_method_num(method) {
+            // TODO determine parameters for each method on finished spec
+            Some(StoragePowerMethod::Constructor) => Self::constructor(rt),
+            Some(StoragePowerMethod::GetTotalStorage) => {
+                arg_end(params, rt);
+                Self::get_total_storage(rt)
+            }
+            _ => {
+                rt.abort(
+                    ExitCode::SystemErrorCode(SysCode::InvalidMethod),
+                    "Invalid method",
+                );
+                unreachable!();
+            }
+        }
+    }
+}
