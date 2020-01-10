aece8e6e44351bc9794aed1420934a5cb5988759
austinabell <austinabell8@gmail.com>
2020-01-10 14:58:07 -0500

    Add missing headers

--- node/db/tests/db_utils/mod.rs
+++ node/db/tests/db_utils/mod.rs
@@ -1,3 +1,6 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0
+
 // Taken from
 // https://github.com/rust-rocksdb/rust-rocksdb/blob/master/tests/util/mod.rs
 use rocksdb::{Options, DB};
