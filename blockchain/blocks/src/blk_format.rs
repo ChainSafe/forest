b1f8a35cabd8f42bb44854564e126ee01e22c1fc
Dustin Brickwood <dustinbrickwood204@gmail.com>
2020-01-11 11:00:40 -0600

    Refactored to reflect changes

--- blockchain/blocks/src/blk_format.rs
+++ blockchain/blocks/src/blk_format.rs
@@ -1,12 +1,14 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0
 
+#![allow(dead_code)]
+
 use cid::{Cid, Codec, Version};
+use multihash::{encode, Hash};
 use std::fmt;
-use multihash::{Hash, encode};
 
 /// Block provides abstraction for blocks implementations.
-trait Block {
+pub trait Block {
     fn raw_data(&self) -> Vec<u8>;
     fn cid(&self) -> Cid;
     fn multihash(&self) -> Hash;
@@ -21,10 +23,9 @@ struct BasicBlock {
 impl BasicBlock {
     /// NewBlock creates a Block object from opaque data. It will hash the data.
     fn new(&self, data: Vec<u8>) -> Self {
-        // TODO
-        // replace SHA2256 with Blake2b when rust-cid updates multihash dependency
-        let h = encode(Hash::SHA2256, data).unwrap();
-        return Self {
+        // TODO replace SHA2256 with Blake2b when rust-cid updates multihash dependency
+        let h = encode(Hash::SHA2256, data.as_ref()).unwrap();
+        Self {
             cid: Cid::new(Codec::DagCBOR, Version::V1, &h),
             data,
         }
@@ -32,39 +33,31 @@ impl BasicBlock {
     /// NewBlockWithCid creates a new block when the hash of the data
     /// is already known.
     fn new_blk_with_cid(&self, data: Vec<u8>, cid: Cid) -> Self {
-        // TODO
-        // check cryptographic sum of buffer; see https://github.com/ipfs/go-block-format/blob/master/blocks.go#L45
+        // TODO check cryptographic sum of buffer; see https://github.com/ipfs/go-block-format/blob/master/blocks.go#L45
         // cid library does not have Sum method
         // go version https://github.com/multiformats/go-multihash/blob/master/sum.go#L36
-        return Self {
-            cid,
-            data
-        }
+        Self { cid, data }
     }
 }
 
 impl Block for BasicBlock {
     /// returns the block raw contents as a byte array
     fn raw_data(&self) -> Vec<u8> {
-        self.data
+        self.data.clone()
     }
     /// returns the content identifier of the block
     fn cid(&self) -> Cid {
-        self.cid
+        self.cid.clone()
     }
     /// returns the hash contained in the block CID
     fn multihash(&self) -> Hash {
-        self.cid.hash
+        self.cid.prefix().mh_type
     }
 }
 
 /// human-readable string representation of a block CID
 impl fmt::Display for BasicBlock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "block: {}",
-            self.cid
-        )
+        write!(f, "block: {}", self.cid)
     }
 }
