a9f8c9e47c83d3aea8d3dcf6ce5615faca8cf1c2
Dustin Brickwood <dustinbrickwood204@gmail.com>
2020-01-13 10:38:13 -0600

    move datastore to chainstore

--- blockchain/chain/src/store/chain_store.rs
+++ blockchain/chain/src/store/chain_store.rs
@@ -3,15 +3,17 @@
 
 use super::{Error, TipIndex};
 use blocks::Tipset;
+use db::RocksDb as Blockstore;
 use cid::Cid;
 use network::service::NetworkMessage;
 use num_bigint::BigUint;
 
 pub struct ChainStore {
-    // TODO add Blockstore
     // TODO add IPLD Store
     // TODO add StateTreeLoader
-    // TODO add access to datastore operations
+
+    // key-value datastore
+    _db: Blockstore,
 
     // genesis is the CID of the genesis block.
     _genesis: Cid,
@@ -31,7 +33,11 @@ impl ChainStore {
         // TODO
         Ok(BigUint::from(0 as u32))
     }
-    pub fn persist_headers(&self, headers: Vec<BlockHeader>) {
+    pub fn persist_headers(&self, headers: Vec<BlockHeader>) -> Result<(), Error> {
+        // TODO serialize and put blocks into raw format
+        // TODO should be stored as
 
+        //self._db.exists(headers.)
+        self._db.bulk_write(headers.cid(), headers);
     }
 }
