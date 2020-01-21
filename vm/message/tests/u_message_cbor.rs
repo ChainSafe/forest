07dde7d961959e55c4c663dae0e5445bef0baa9d
austinabell <austinabell8@gmail.com>
2020-01-21 11:43:02 -0500

    Add remaining part of test

--- vm/message/tests/u_message_cbor.rs
+++ vm/message/tests/u_message_cbor.rs
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use address::Address;
-// use encoding::{from_slice, to_vec};
-use encoding::to_vec;
+use encoding::{from_slice, to_vec};
 use hex::decode;
 use message::UnsignedMessage;
 use num_bigint::BigUint;
@@ -11,7 +10,7 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::prelude::*;
 use std::str::FromStr;
-use vm::{MethodNum, TokenAmount};
+use vm::{MethodNum, Serialized, TokenAmount};
 
 #[derive(Debug, Deserialize)]
 struct MessageVector {
@@ -33,9 +32,7 @@ impl From<MessageVector> for UnsignedMessage {
             .sequence(vector.nonce)
             .value(TokenAmount::new(vector.value))
             .method_num(MethodNum::new(vector.method))
-            // .params(
-            //     from_slice(&decode(vector.params).unwrap()).expect("bytes to MethodParams failed"),
-            // )
+            .params(Serialized::new(decode(vector.params).unwrap()))
             .gas_limit(BigUint::from(vector.gas_limit))
             .gas_price(BigUint::from(vector.gas_price))
             .build()
@@ -52,8 +49,13 @@ struct TestVector {
 fn encode_assert_cbor(message: &UnsignedMessage, expected: &[u8]) {
     let enc_bz: Vec<u8> = to_vec(message).expect("cbor serialization failed");
 
-    // TODO remove only checking first bytes (after method param encoding format)
-    assert_eq!(&enc_bz[..81], &expected[..81]);
+    // Assert the message is encoded in same format
+    assert_eq!(enc_bz.as_slice(), expected);
+    // Assert decoding from those bytes goes back to unsigned message
+    assert_eq!(
+        &from_slice::<UnsignedMessage>(expected).expect("Should be able to deserialize cbor bytes"),
+        message
+    );
 }
 
 #[test]
