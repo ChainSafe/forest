2b798a3c2e49e1a9c33f2b6f911c1d0a6e780777
Dustin Brickwood <dustinbrickwood204@gmail.com>
2020-02-04 17:40:11 -0600

    Added serialize, deserailize to msgmeta

--- vm/message/src/lib.rs
+++ vm/message/src/lib.rs
@@ -11,8 +11,12 @@ pub use unsigned_message::*;
 
 use address::Address;
 use cid::Cid;
+use encoding::{
+    de::{self, Deserializer},
+    ser::{self, Serializer},
+};
 use num_bigint::BigUint;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use vm::{MethodNum, Serialized, TokenAmount};
 
 pub trait Message {
@@ -34,8 +38,33 @@ pub trait Message {
     fn gas_limit(&self) -> &BigUint;
 }
 
-#[derive(Serialize, Deserialize)]
 pub struct MsgMeta {
     pub bls_message_root: Cid,
     pub secp_message_root: Cid,
+}
+
+impl ser::Serialize for MsgMeta {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        (
+            self.bls_message_root.clone(),
+            self.secp_message_root.clone(),
+        )
+            .serialize(serializer)
+    }
+}
+
+impl<'de> de::Deserialize<'de> for MsgMeta {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let (bls_message_root, secp_message_root) = Deserialize::deserialize(deserializer)?;
+        Ok(Self {
+            bls_message_root,
+            secp_message_root,
+        })
+    }
 }
