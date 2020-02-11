bc9248f4c09cbfc01fe349b6f91866934945fc70
austinabell <austinabell8@gmail.com>
2020-02-11 10:03:43 -0500

    clean up tests

--- node/utils/tests/files.rs
+++ node/utils/tests/files.rs
@@ -8,47 +8,43 @@ use std::fs::remove_dir_all;
 
 // Please use with caution, remove_dir_all will completely delete a directory
 fn cleanup_file(path: &str) {
-    if let Err(e) = remove_dir_all(path) {
-        panic!("cleanup_file() path: {:?} failed: {:?}", path, e);
-    }
+    remove_dir_all(path).expect("cleanup_file() path: {:?} failed: {:?}");
 }
 
 #[test]
 fn write_to_file_to_path() {
-    let msg = "Hello World!".as_bytes().to_vec();
+    let msg = "Hello World!".as_bytes();
     let path = "./test-write/";
     let file = "test.txt";
     match write_to_file(&msg, &path, file) {
         Ok(_) => cleanup_file(path),
         Err(e) => {
             cleanup_file(path);
-            panic!("{:?}", e);
+            panic!("{}", e);
         }
     }
 }
 
 #[test]
 fn write_to_file_nested_dir() {
-    let msg = "Hello World!".as_bytes().to_vec();
+    let msg = "Hello World!".as_bytes();
     let root = "./test_missing";
     let path = format!("{}{}", root, "/test_write_string/");
     match write_to_file(&msg, &path, "test-file") {
         Ok(_) => cleanup_file(root),
         Err(e) => {
             cleanup_file(root);
-            panic!("{:?}", e);
+            panic!("{}", e);
         }
     }
 }
 
 #[test]
 fn read_from_file_vec() {
-    let msg = "Hello World!".as_bytes().to_vec();
+    let msg = "Hello World!".as_bytes();
     let path = "./test_read_file/";
     let file_name = "out.keystore";
-    if let Err(e) = write_to_file(&msg, &path, file_name) {
-        panic!(e);
-    }
+    write_to_file(&msg, &path, file_name).unwrap();
     match read_file_to_vec(&format!("{}{}", path, file_name)) {
         Ok(contents) => {
             cleanup_file(path);
@@ -56,7 +52,7 @@ fn read_from_file_vec() {
         }
         Err(e) => {
             cleanup_file(path);
-            panic!("{:?}", e);
+            panic!("{}", e);
         }
     }
 }
@@ -66,9 +62,7 @@ fn read_from_file_string() {
     let msg = "Hello World!";
     let path = "./test_string_read_file/";
     let file_name = "out.keystore";
-    if let Err(e) = write_to_file(&msg.as_bytes().to_vec(), &path, file_name) {
-        panic!(e);
-    }
+    write_to_file(&msg.as_bytes(), &path, file_name).unwrap();
     match read_file_to_string(&format!("{}{}", path, file_name)) {
         Ok(contents) => {
             cleanup_file(path);
@@ -76,7 +70,7 @@ fn read_from_file_string() {
         }
         Err(e) => {
             cleanup_file(path);
-            panic!("{:?}", e);
+            panic!("{}", e);
         }
     }
 }
@@ -106,10 +100,7 @@ fn read_from_toml() {
         github = 'xxxxxxxxxxxxxxxxx'
         travis = 'yyyyyyyyyyyyyyyyy'
     "#;
-    let config: Config = match read_toml(toml_str) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e),
-    };
+    let config: Config = read_toml(toml_str).unwrap();
 
     assert_eq!(config.ip, "127.0.0.1");
     assert_eq!(config.port, None);
