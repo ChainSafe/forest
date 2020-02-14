596fd1cb407efda62a7ef09ab045ed9183e5638e
austinabell <austinabell8@gmail.com>
2020-02-14 14:39:02 -0500

    Add deserialize default Cbor trait function

--- encoding/src/cbor.rs
+++ encoding/src/cbor.rs
@@ -2,15 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use super::errors::Error;
-use crate::{ser, to_vec};
+use crate::{de::DeserializeOwned, from_slice, ser::Serialize, to_vec};
 use cid::{multihash::Hash::Blake2b256, Cid};
 
-/// Implemented for types that are CBOR encodable
-pub trait Cbor: ser::Serialize {
+/// Cbor utility functions for serializable objects
+pub trait Cbor: Serialize + DeserializeOwned {
+    /// Marshalls cbor encodable object into cbor bytes
     fn marshal_cbor(&self) -> Result<Vec<u8>, Error> {
         Ok(to_vec(&self)?)
     }
-    /// returns the content identifier of the block
+
+    /// Unmarshals cbor encoded bytes to object
+    fn unmarshal_cbor_owned(bz: &[u8]) -> Result<Self, Error> {
+        Ok(from_slice(bz)?)
+    }
+
+    /// returns the content identifier of the raw block of data
     fn cid(&self) -> Result<Cid, Error> {
         Ok(Cid::from_bytes(&self.marshal_cbor()?, Blake2b256)?)
     }
