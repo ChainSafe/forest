1cdd5826cde7e420e80e8b04c7f7c5d33fa267e2
Eric Tu <tu.eric@hotmail.com>
2020-02-18 13:02:40 -0800

    use pretty_env_logger

--- forest/src/logger/mod.rs
+++ forest/src/logger/mod.rs
@@ -1,9 +1,12 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+
 pub(crate) fn setup_logger() {
-    let logger = femme::pretty::Logger::new();
-    async_log::Logger::wrap(logger, || 12)
-        .start(log::LevelFilter::Trace)
+    let logger = pretty_env_logger::formatted_timed_builder()
+        .filter(None, log::LevelFilter::Debug)
+        .build();
+    async_log::Logger::wrap(logger, || 0)
+        .start(log::LevelFilter::Debug)
         .unwrap();
 }
