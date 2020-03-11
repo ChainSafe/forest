adec48b3725f9701ecea2dc0dc9992953f806f3d
Eric Tu <tu.eric@hotmail.com>
2020-03-11 13:40:11 -0400

    fixed comments

--- vm/src/exit_code.rs
+++ vm/src/exit_code.rs
@@ -7,62 +7,62 @@
 pub enum ExitCode {
     Ok = 0,
 
-    // Indicates failure to find an actor in the state tree.
+    /// Indicates failure to find an actor in the state tree.
     SysErrActorNotFound = 1,
 
-    // Indicates failure to find the code for an actor.
+    /// Indicates failure to find the code for an actor.
     SysErrActorCodeNotFound = 2,
 
-    // Indicates failure to find a method in an actor.
+    /// Indicates failure to find a method in an actor.
     SysErrInvalidMethod = 3,
 
-    // Indicates syntactically invalid parameters for a method.
+    /// Indicates syntactically invalid parameters for a method.
     SysErrInvalidParameters = 4,
 
-    // Indicates a message sender has insufficient funds for a message's execution.
+    /// Indicates a message sender has insufficient funds for a message's execution.
     SysErrInsufficientFunds = 5,
 
-    // Indicates a message invocation out of sequence.
+    /// Indicates a message invocation out of sequence.
     SysErrInvalidCallSeqNum = 6,
 
-    // Indicates message execution (including subcalls) used more gas than the specified limit.
+    /// Indicates message execution (including subcalls) used more gas than the specified limit.
     SysErrOutOfGas = 7,
 
-    // Indicates a message execution is forbidden for the caller.
+    /// Indicates a message execution is forbidden for the caller.
     SysErrForbidden = 8,
 
-    // Indicates actor code performed a disallowed operation. Disallowed operations include:
-    // - mutating state outside of a state acquisition block
-    // - failing to invoke caller validation
-    // - aborting with a reserved exit code (including success or a system error).
+    /// Indicates actor code performed a disallowed operation. Disallowed operations include:
+    /// - mutating state outside of a state acquisition block
+    /// - failing to invoke caller validation
+    /// - aborting with a reserved exit code (including success or a system error).
     SysErrorIllegalActor = 9,
 
-    // Indicates an invalid argument passed to a runtime method.
+    /// Indicates an invalid argument passed to a runtime method.
     SysErrorIllegalArgument = 10,
 
-    // Indicates  an object failed to de/serialize for storage.
+    /// Indicates  an object failed to de/serialize for storage.
     SysErrSerialization = 11,
 
-    // Reserved exit codes, do not use.
+    /// Reserved exit codes, do not use.
     SysErrorReserved1 = 12,
     SysErrorReserved2 = 13,
     SysErrorReserved3 = 14,
 
-    // Indicates something broken within the VM.
+    /// Indicates something broken within the VM.
     SysErrInternal = 15,
 
     // -------Actor Error Codes-------
-    // Indicates a method parameter is invalid.
+    /// Indicates a method parameter is invalid.
     ErrIllegalArgument = 16,
-    // Indicates a requested resource does not exist.
+    /// Indicates a requested resource does not exist.
     ErrNotFound = 17,
-    // Indicates an action is disallowed.
+    /// Indicates an action is disallowed.
     ErrForbidden = 18,
-    // Indicates a balance of funds is insufficient.
+    /// Indicates a balance of funds is insufficient.
     ErrInsufficientFunds = 19,
-    // Indicates an actor's internal state is invalid.
+    /// Indicates an actor's internal state is invalid.
     ErrIllegalState = 20,
-    // Indicates de/serialization failure within actor code.
+    /// Indicates de/serialization failure within actor code.
     ErrSerialization = 21,
 
     ErrPlaceholder = 1000,
