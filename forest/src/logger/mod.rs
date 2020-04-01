0988f0d9431b7a3beed03ce24a616b836fec615f
austinabell <austinabell8@gmail.com>
2020-04-01 10:20:09 -0400

    Enable filter parsing from env and using a customizable logger

--- forest/src/logger/mod.rs
+++ forest/src/logger/mod.rs
@@ -1,11 +1,21 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use log::LevelFilter;
+
 pub(crate) fn setup_logger() {
-    let logger = pretty_env_logger::formatted_timed_builder()
-        .filter(None, log::LevelFilter::Info)
-        .build();
+    let mut logger_builder = pretty_env_logger::formatted_timed_builder();
+    if let Ok(s) = ::std::env::var("RUST_LOG") {
+        // Set log level based on filters if set
+        logger_builder.parse_filters(&s);
+    } else {
+        // If no ENV variable specified, default to info
+        logger_builder.filter(None, LevelFilter::Info);
+    }
+    let logger = logger_builder.build();
+
+    // Wrap Logger in async_log
     async_log::Logger::wrap(logger, || 0)
-        .start(log::LevelFilter::Info)
+        .start(LevelFilter::Trace)
         .unwrap();
 }
