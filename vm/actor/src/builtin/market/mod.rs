d30d396ee9e089bb1edf0f1e3ca2ae29dbcb36db
Austin Abell <austinabell8@gmail.com>
2020-04-07 16:00:33 -0400

    Market actor state (#330)
    * Implement market state
    
    * Added generate deal_id function for actor
    
    * Add messages to asserts in market state

---
+++ vm/actor/src/builtin/market/mod.rs
@@ -0,0 +1,10 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+mod deal;
+mod policy;
+mod state;
+
+pub use self::deal::*;
+use self::policy::*;
+pub use self::state::State;
