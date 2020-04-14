258a8c759ce0d801dabbf245d4a387a7e5a7b159
austinabell <austinabell8@gmail.com>
2020-04-14 18:45:25 -0400

    Gas syscalls test

--- vm/default_runtime/src/gas_syscalls.rs
+++ vm/default_runtime/src/gas_syscalls.rs
@@ -85,3 +85,92 @@ where
             .verify_consensus_fault(h1, h2, extra, earliest)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use runtime::{ConsensusFault, ConsensusFaultType, Syscalls};
+
+    #[derive(Copy, Debug, Clone)]
+    struct TestSyscalls;
+    impl Syscalls for TestSyscalls {
+        fn verify_signature(
+            &self,
+            _signature: &Signature,
+            _signer: &Address,
+            _plaintext: &[u8],
+        ) -> Result<(), ActorError> {
+            Ok(())
+        }
+        fn hash_blake2b(&self, _data: &[u8]) -> Result<[u8; 32], ActorError> {
+            Ok([0u8; 32])
+        }
+        fn compute_unsealed_sector_cid(
+            &self,
+            _reg: RegisteredProof,
+            _pieces: &[PieceInfo],
+        ) -> Result<Cid, ActorError> {
+            Ok(Default::default())
+        }
+        fn verify_seal(&self, _vi: &SealVerifyInfo) -> Result<(), ActorError> {
+            Ok(Default::default())
+        }
+        fn verify_post(&self, _vi: &PoStVerifyInfo) -> Result<(), ActorError> {
+            Ok(Default::default())
+        }
+        fn verify_consensus_fault(
+            &self,
+            _h1: &[u8],
+            _h2: &[u8],
+            _extra: &[u8],
+            _earliest: ChainEpoch,
+        ) -> Result<ConsensusFault, ActorError> {
+            Ok(ConsensusFault {
+                target: Address::new_id(0).unwrap(),
+                epoch: 0,
+                fault_type: ConsensusFaultType::DoubleForkMining,
+            })
+        }
+    }
+
+    #[test]
+    fn gas_syscalls() {
+        let gsys = GasSyscalls {
+            price_list: PriceList {
+                on_chain_message_base: 1,
+                on_chain_message_per_byte: 1,
+                on_chain_return_value_per_byte: 1,
+                hashing_base: 1,
+                hashing_per_byte: 1,
+                compute_unsealed_sector_cid_base: 1,
+                verify_seal_base: 1,
+                verify_post_base: 1,
+                verify_consensus_fault: 1,
+                ..Default::default()
+            },
+            gas: Rc::new(RefCell::new(GasTracker::new(20, 0))),
+            syscalls: TestSyscalls,
+        };
+
+        assert_eq!(gsys.gas.borrow().gas_used(), 0);
+        gsys.verify_signature(&Default::default(), &Default::default(), &[0u8])
+            .unwrap();
+        assert_eq!(gsys.gas.borrow().gas_used(), 5);
+
+        gsys.hash_blake2b(&[0u8]).unwrap();
+        assert_eq!(gsys.gas.borrow().gas_used(), 7);
+
+        gsys.compute_unsealed_sector_cid(Default::default(), &[])
+            .unwrap();
+        assert_eq!(gsys.gas.borrow().gas_used(), 8);
+
+        gsys.verify_seal(&Default::default()).unwrap();
+        assert_eq!(gsys.gas.borrow().gas_used(), 9);
+
+        gsys.verify_post(&Default::default()).unwrap();
+        assert_eq!(gsys.gas.borrow().gas_used(), 10);
+
+        gsys.verify_consensus_fault(&[], &[], &[], 0).unwrap();
+        assert_eq!(gsys.gas.borrow().gas_used(), 11);
+    }
+}
