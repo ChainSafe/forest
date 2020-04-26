66911a763bc13460dba07227d7e3c785fda8adf1
austinabell <austinabell8@gmail.com>
2020-04-26 12:03:34 -0400

    setup tests for functionality

--- ipld/blockstore/src/buffered.rs
+++ ipld/blockstore/src/buffered.rs
@@ -27,6 +27,7 @@ where
             write: Default::default(),
         }
     }
+    /// Flushes the buffered cache based on the root node
     pub fn flush(&mut self, _root: &Cid) -> Result<(), Box<dyn StdError>> {
         // TODO update this to only write over values connected to the root
         // This will be done by querying root in `self.write` and writing all values connected
@@ -37,6 +38,9 @@ where
 
         self.write = Default::default();
         Ok(())
+    }
+    fn write_recursive(&self, cid: &Cid) -> Result<(), Box<dyn StdError>> {
+        todo!()
     }
 }
 
@@ -117,15 +121,60 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use cid::multihash::Identity;
+    use cid::multihash::{Blake2b256, Identity};
+    use commcid::{commitment_to_cid, FilecoinMultihashCode};
+    use forest_ipld::Ipld;
+    use std::collections::BTreeMap;
 
     #[test]
     fn basic_buffered_store() {
         let mem = db::MemoryDB::default();
-        let buf_store = BufferedBlockStore::new(&mem);
+        let mut buf_store = BufferedBlockStore::new(&mem);
 
         let cid = buf_store.put(&8, Identity).unwrap();
         assert_eq!(mem.get::<u8>(&cid).unwrap(), None);
         assert_eq!(buf_store.get::<u8>(&cid).unwrap(), Some(8));
+
+        buf_store.flush(&cid).unwrap();
+        assert_eq!(buf_store.get::<u8>(&cid).unwrap(), Some(8));
+        assert_eq!(mem.get::<u8>(&cid).unwrap(), Some(8));
+    }
+
+    #[test]
+    fn buffered_store_with_links() {
+        let mem = db::MemoryDB::default();
+        let mut buf_store = BufferedBlockStore::new(&mem);
+        let str_val = "value";
+        let value = 8u8;
+        let arr_cid = buf_store.put(&(str_val, value), Blake2b256).unwrap();
+        let identity_cid = buf_store.put(&0u8, Identity).unwrap();
+
+        // Create map to insert into store
+        let mut map: BTreeMap<String, Ipld> = Default::default();
+        map.insert("array".to_owned(), Ipld::Link(arr_cid.clone()));
+        let sealed_comm_cid = commitment_to_cid(&[7u8; 32], FilecoinMultihashCode::SealedV1);
+        map.insert("sealed".to_owned(), Ipld::Link(sealed_comm_cid));
+        let unsealed_comm_cid = commitment_to_cid(&[5u8; 32], FilecoinMultihashCode::UnsealedV1);
+        map.insert("unsealed".to_owned(), Ipld::Link(unsealed_comm_cid));
+        map.insert("identity".to_owned(), Ipld::Link(identity_cid));
+        map.insert("value".to_owned(), Ipld::String(str_val.to_owned()));
+
+        // Make sure a block not connected to the root does not get written
+        let unconnected = buf_store.put(&27u8, Blake2b256).unwrap();
+
+        let map_cid = buf_store.put(&map, Blake2b256).unwrap();
+        assert_eq!(mem.get::<(String, u8)>(&arr_cid).unwrap(), None);
+        assert_eq!(mem.get::<Ipld>(&map_cid).unwrap(), None);
+        assert_eq!(buf_store.get::<u8>(&unconnected).unwrap(), Some(27u8));
+
+        // Flush and assert changes
+        buf_store.flush(&map_cid).unwrap();
+        assert_eq!(mem.get::<Ipld>(&map_cid).unwrap(), Some(Ipld::Map(map)));
+        assert_eq!(
+            mem.get::<(String, u8)>(&arr_cid).unwrap(),
+            Some((str_val.to_owned(), value))
+        );
+        // assert_eq!(mem.get::<u8>(&unconnected).unwrap(), None);
+        // assert_eq!(buf_store.get::<u8>(&unconnected).unwrap(), None);
     }
 }
