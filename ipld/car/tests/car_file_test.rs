5e78b9e0c62e67ae7f31618d4edc8e3379ea5c3d
Eric Tu <tu.eric@hotmail.com>
2020-04-28 19:55:01 -0400

    updated blockheader with post proof

--- ipld/car/tests/car_file_test.rs
+++ ipld/car/tests/car_file_test.rs
@@ -5,7 +5,6 @@ use db::MemoryDB;
 use forest_car::*;
 use std::fs::File;
 use std::io::BufReader;
-use blockstore::BlockStore;
 
 #[test]
 fn load_into_blockstore() {
@@ -13,7 +12,5 @@ fn load_into_blockstore() {
     let buf_reader = BufReader::new(file);
     let mut bs = MemoryDB::default();
 
-    let cids = load_car(&mut bs, buf_reader).unwrap();
-
-    println!("{:x?}", bs.get_bytes(&cids[0]).unwrap())
+    let _ = load_car(&mut bs, buf_reader).unwrap();
 }
