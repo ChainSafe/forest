8d2a4936103b066eb9ce8ff26c77d193044ecdfb
Jaden Foldesi <jfoldesi57@gmail.com>
2020-06-10 10:00:53 -0400

    Implement Wallet (#469)
    * initial wallet implementation
    
    * implemented wallet except generating new private key
    
    * license
    
    * implemented private key generation and refactor
    
    * reformatting code according to comments on pr
    
    * changed keystore to be a trait and refactored
    
    * cargo clippy
    
    * fixed issues using rust stable 1.44
    
    * fixed issues rust 1.44 issues
    
    * added wallet tests
    
    * added changes
    
    * Update key_management/src/keystore.rs
    
    Co-authored-by: Austin Abell <austinabell8@gmail.com>
    
    Co-authored-by: Austin Abell <austinabell8@gmail.com>

---
+++ key_management/src/lib.rs
@@ -0,0 +1,12 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+mod errors;
+mod keystore;
+mod wallet;
+mod wallet_helpers;
+
+pub use errors::*;
+pub use keystore::*;
+pub use wallet::*;
+pub use wallet_helpers::*;
---
+++ key_management/src/wallet_helpers.rs
@@ -0,0 +1,79 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use super::errors::Error;
+use address::Address;
+use bls_signatures::{PrivateKey as BlsPrivate, Serialize};
+use crypto::{Signature, SignatureType};
+use rand::rngs::OsRng;
+use secp256k1::{Message as SecpMessage, PublicKey as SecpPublic, SecretKey as SecpPrivate};
+
+/// Return the public key for a given private_key and SignatureType
+pub fn to_public(sig_type: SignatureType, private_key: &[u8]) -> Result<Vec<u8>, Error> {
+    match sig_type {
+        SignatureType::BLS => Ok(BlsPrivate::from_bytes(&private_key)
+            .map_err(|err| Error::Other(err.to_string()))?
+            .public_key()
+            .as_bytes()),
+        SignatureType::Secp256 => {
+            let private_key = SecpPrivate::parse_slice(private_key)
+                .map_err(|err| Error::Other(err.to_string()))?;
+            let public_key = SecpPublic::from_secret_key(&private_key);
+            Ok(public_key.serialize().to_vec())
+        }
+    }
+}
+
+/// Return a new Address that is of a given SignatureType and uses the supplied public_key
+pub fn new_address(sig_type: SignatureType, public_key: &[u8]) -> Result<Address, Error> {
+    match sig_type {
+        SignatureType::BLS => {
+            let addr = Address::new_bls(public_key).map_err(|err| Error::Other(err.to_string()))?;
+            Ok(addr)
+        }
+        SignatureType::Secp256 => {
+            let addr =
+                Address::new_secp256k1(public_key).map_err(|err| Error::Other(err.to_string()))?;
+            Ok(addr)
+        }
+    }
+}
+
+/// Sign takes in SignatureType, private key and message. Returns a Signature for that message
+pub fn sign(sig_type: SignatureType, private_key: &[u8], msg: &[u8]) -> Result<Signature, Error> {
+    match sig_type {
+        SignatureType::BLS => {
+            let priv_key =
+                BlsPrivate::from_bytes(private_key).map_err(|err| Error::Other(err.to_string()))?;
+            // this returns a signature from bls-signatures, so we need to convert this to a crypto signature
+            let sig = priv_key.sign(msg);
+            let crypto_sig = Signature::new_bls(sig.as_bytes());
+            Ok(crypto_sig)
+        }
+        SignatureType::Secp256 => {
+            let priv_key = SecpPrivate::parse_slice(private_key)
+                .map_err(|err| Error::Other(err.to_string()))?;
+            let message =
+                SecpMessage::parse_slice(msg).map_err(|err| Error::Other(err.to_string()))?;
+            // this returns a signature of secp256k1 type, next lines convert this sig to crypto signature type
+            let (sig, _) = secp256k1::sign(&message, &priv_key);
+            let crypto_sig = Signature::new_secp256k1(sig.serialize().to_vec());
+            Ok(crypto_sig)
+        }
+    }
+}
+
+/// Generate a new private key
+pub fn generate(sig_type: SignatureType) -> Result<Vec<u8>, Error> {
+    let rng = &mut OsRng::default();
+    match sig_type {
+        SignatureType::BLS => {
+            let key = BlsPrivate::generate(rng);
+            Ok(key.as_bytes())
+        }
+        SignatureType::Secp256 => {
+            let key = SecpPrivate::random(rng);
+            Ok(key.serialize().to_vec())
+        }
+    }
+}
