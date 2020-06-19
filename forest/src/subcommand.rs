0f4a5b224586413d2a4f91708a771e5d24361eea
austinabell <austinabell8@gmail.com>
2020-06-19 15:24:42 -0400

    Refactor CLI and setup fetch-params command

---
+++ forest/src/subcommand.rs
@@ -0,0 +1,9 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use super::cli::Subcommand;
+
+/// Process CLI subcommand
+pub(super) fn process(_command: Subcommand) {
+    todo!()
+}
