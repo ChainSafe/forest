2ce9745667dddd72e33c5468fa53df73c9d1a6c8
austinabell <austinabell8@gmail.com>
2020-07-08 15:42:12 -0400

    setup sync functions

--- node/rpc/src/lib.rs
+++ node/rpc/src/lib.rs
@@ -24,40 +24,30 @@ pub async fn start_rpc<DB: BlockStore + Send + Sync + 'static>(
     state: RpcState<DB>,
     rpc_endpoint: &str,
 ) {
+    use chain_api::*;
+    use sync_api::*;
     let rpc = Server::new()
         .with_data(Data::new(state))
         // Chain API
-        .with_method(
-            "Filecoin.ChainGetMessage",
-            chain_api::chain_get_message::<DB>,
-        )
-        .with_method("Filecoin.ChainGetObj", chain_api::chain_read_obj::<DB>)
-        .with_method("Filecoin.ChainHasObj", chain_api::chain_has_obj::<DB>)
-        .with_method(
-            "Filecoin.ChainGetBlockMessages",
-            chain_api::chain_block_messages::<DB>,
-        )
+        .with_method("Filecoin.ChainGetMessage", chain_get_message::<DB>)
+        .with_method("Filecoin.ChainGetObj", chain_read_obj::<DB>)
+        .with_method("Filecoin.ChainHasObj", chain_has_obj::<DB>)
+        .with_method("Filecoin.ChainGetBlockMessages", chain_block_messages::<DB>)
         .with_method(
             "Filecoin.ChainGetTipsetByHeight",
-            chain_api::chain_get_tipset_by_height::<DB>,
+            chain_get_tipset_by_height::<DB>,
         )
-        .with_method(
-            "Filecoin.ChainGetGenesis",
-            chain_api::chain_get_genesis::<DB>,
-        )
-        .with_method(
-            "Filecoin.ChainTipsetWeight",
-            chain_api::chain_tipset_weight::<DB>,
-        )
-        .with_method("Filecoin.ChainGetTipset", chain_api::chain_get_tipset::<DB>)
-        .with_method(
-            "Filecoin.GetRandomness",
-            chain_api::chain_get_randomness::<DB>,
-        )
-        .with_method("Filecoin.ChainGetBlock", chain_api::chain_get_block::<DB>)
-        .with_method("Filecoin.ChainHead", chain_api::chain_head::<DB>)
+        .with_method("Filecoin.ChainGetGenesis", chain_get_genesis::<DB>)
+        .with_method("Filecoin.ChainTipsetWeight", chain_tipset_weight::<DB>)
+        .with_method("Filecoin.ChainGetTipset", chain_get_tipset::<DB>)
+        .with_method("Filecoin.GetRandomness", chain_get_randomness::<DB>)
+        .with_method("Filecoin.ChainGetBlock", chain_get_block::<DB>)
+        .with_method("Filecoin.ChainHead", chain_head::<DB>)
         // Sync API
-        .with_method("Filecoin.SyncCheckBad", sync_api::check_bad::<DB>)
+        .with_method("Filecoin.SyncCheckBad", sync_check_bad::<DB>)
+        .with_method("Filecoin.SyncMarkBad", sync_mark_bad::<DB>)
+        .with_method("Filecoin.SyncState", sync_state::<DB>)
+        .with_method("Filecoin.SyncSubmitBlock", sync_submit_block::<DB>)
         // TODO add other sync methods
         .finish_unwrapped();
 
--- node/rpc/src/sync_api.rs
+++ node/rpc/src/sync_api.rs
@@ -2,14 +2,41 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use crate::RpcState;
+use blocks::header::json::BlockHeaderJson;
 use blockstore::BlockStore;
-use cid::json::vec::CidJsonVec;
+use cid::json::CidJson;
 
 use jsonrpc_v2::{Data, Error as JsonRpcError, Params};
 
-pub(crate) async fn check_bad<DB: BlockStore + Send + Sync + 'static>(
+/// Checks if a given block is marked as bad.
+pub(crate) async fn sync_check_bad<DB: BlockStore + Send + Sync + 'static>(
     _data: Data<RpcState<DB>>,
-    Params(_params): Params<(CidJsonVec,)>,
+    Params(_params): Params<(CidJson,)>,
 ) -> Result<String, JsonRpcError> {
+    todo!()
+}
+
+/// Marks a block as bad, meaning it will never be synced.
+pub(crate) async fn sync_mark_bad<DB: BlockStore + Send + Sync + 'static>(
+    _data: Data<RpcState<DB>>,
+    Params(_params): Params<(CidJson,)>,
+) -> Result<(), JsonRpcError> {
+    todo!()
+}
+
+// TODO SyncIncomingBlocks (requires websockets)
+
+/// Returns the current status of the ChainSync process.
+pub(crate) async fn sync_state<DB: BlockStore + Send + Sync + 'static>(
+    _data: Data<RpcState<DB>>,
+) -> Result<(), JsonRpcError> {
+    todo!()
+}
+
+/// Submits block to be sent through gossipsub.
+pub(crate) async fn sync_submit_block<DB: BlockStore + Send + Sync + 'static>(
+    _data: Data<RpcState<DB>>,
+    Params(_params): Params<(BlockHeaderJson,)>,
+) -> Result<(), JsonRpcError> {
     todo!()
 }
