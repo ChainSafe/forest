fd6a0d35edac36f1b761875a424158a7796504e5
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2020-07-21 10:18:12 -0400

    Update blockchain/state_manager/src/lib.rs
    Co-authored-by: Austin Abell <austinabell8@gmail.com>

--- blockchain/state_manager/src/lib.rs
+++ blockchain/state_manager/src/lib.rs
@@ -470,11 +470,6 @@ where
                             }),
                         );
                     }
-                    if let Ok(m_cid) = s.to_cid()
-                    {
-                        let error_msg = format!("found message with equal nonce as the one we are looking for (F:{:} n {:}, TS: {:} n{:})",cid,message.sequence(),m_cid,s.sequence());
-                        return Some(Err(Error::Other(error_msg)))
-                    }
                     let error_msg = format!("found message with equal nonce as the one we are looking for (F:{:} n {:}, TS: `Error Converting message to Cid` n{:})", cid, message.sequence(), s.sequence());
                     return Some(Err(Error::Other(error_msg)))
                 }
