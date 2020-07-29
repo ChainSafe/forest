6e8c3acfa92f73e5d728af1c203ba2713a8e763d
austinabell <austinabell8@gmail.com>
2020-07-29 14:55:35 -0400

    Update init actor

--- vm/actor/src/builtin/init/mod.rs
+++ vm/actor/src/builtin/init/mod.rs
@@ -1,13 +1,13 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-mod params;
 mod state;
+mod types;
 
-pub use self::params::*;
 pub use self::state::State;
+pub use self::types::*;
 use crate::{
-    make_map, ACCOUNT_ACTOR_CODE_ID, INIT_ACTOR_CODE_ID, MARKET_ACTOR_CODE_ID, MINER_ACTOR_CODE_ID,
+    make_map, MINER_ACTOR_CODE_ID, MULTISIG_ACTOR_CODE_ID, PAYCH_ACTOR_CODE_ID,
     POWER_ACTOR_CODE_ID, SYSTEM_ACTOR_ADDR,
 };
 use address::Address;
@@ -38,12 +38,9 @@ impl Actor {
         let sys_ref: &Address = &SYSTEM_ACTOR_ADDR;
         rt.validate_immediate_caller_is(std::iter::once(sys_ref))?;
         let mut empty_map = make_map(rt.store());
-        let root = empty_map.flush().map_err(|err| {
-            rt.abort(
-                ExitCode::ErrIllegalState,
-                format!("failed to construct state: {}", err),
-            )
-        })?;
+        let root = empty_map
+            .flush()
+            .map_err(|err| actor_error!(ErrIllegalState; "failed to construct state: {}", err))?;
 
         rt.create(&State::new(root, params.network_name))?;
 
@@ -59,7 +56,7 @@ impl Actor {
         rt.validate_immediate_caller_accept_any()?;
         let caller_code = rt
             .get_actor_code_cid(rt.message().caller())?
-            .ok_or_else(|| actor_error!(ErrForbidden; "No code for actor"))?;
+            .ok_or_else(|| actor_error!(fatal("No code for actor at {}", rt.message().caller())))?;
         if !can_exec(&caller_code, &params.code_cid) {
             return Err(actor_error!(ErrForbidden;
                     "called type {} cannot exec actor type {}",
@@ -77,9 +74,7 @@ impl Actor {
         // Store mapping of pubkey or actor address to actor ID
         let id_address: Address = rt.transaction::<State, _, _>(|s, rt| {
             s.map_address_to_new_id(rt.store(), &robust_address)
-                .map_err(|e| {
-                    ActorError::new(ExitCode::ErrIllegalState, format!("exec failed {}", e))
-                })
+                .map_err(|e| actor_error!(ErrIllegalState; "exec failed: {}", e))
         })??;
 
         // Create an empty actor
@@ -92,7 +87,7 @@ impl Actor {
             params.constructor_params,
             rt.message().value_received().clone(),
         )
-        .map_err(|err| rt.abort(err.exit_code(), "constructor failed"))?;
+        .map_err(|err| err.wrap("constructor failed"))?;
 
         Ok(ExecReturn {
             id_address,
@@ -121,24 +116,21 @@ impl ActorCode for Actor {
                 let res = Self::exec(rt, params.deserialize()?)?;
                 Ok(Serialized::serialize(res)?)
             }
-            _ => {
+            None => {
                 // Method number does not match available, abort in runtime
-                Err(rt.abort(ExitCode::SysErrInvalidMethod, "Invalid method"))
+                Err(actor_error!(SysErrInvalidMethod; "Invalid method"))
             }
         }
     }
 }
 
 fn can_exec(caller: &Cid, exec: &Cid) -> bool {
-    // TODO spec also checks for an undefined Cid, see if this should be supported
-    if exec == &*ACCOUNT_ACTOR_CODE_ID
-        || exec == &*INIT_ACTOR_CODE_ID
-        || exec == &*POWER_ACTOR_CODE_ID
-        || exec == &*MARKET_ACTOR_CODE_ID
-        || exec == &*MINER_ACTOR_CODE_ID
+    if (exec == &*MINER_ACTOR_CODE_ID && caller == &*POWER_ACTOR_CODE_ID)
+        || exec == &*MULTISIG_ACTOR_CODE_ID
+        || exec == &*PAYCH_ACTOR_CODE_ID
     {
-        exec == &*MINER_ACTOR_CODE_ID && caller == &*POWER_ACTOR_CODE_ID
-    } else {
         true
+    } else {
+        false
     }
 }
