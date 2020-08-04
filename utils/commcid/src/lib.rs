d0fc611db4a3b2dd1c4bea6e94137a0549cac8df
austinabell <austinabell8@gmail.com>
2020-08-04 12:30:34 -0400

    Update commcid codes and creation

--- utils/commcid/src/lib.rs
+++ utils/commcid/src/lib.rs
@@ -1,114 +1,123 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use cid::{multihash, Cid, Codec};
+use cid::{
+    multihash::{self, Code},
+    Cid, Codec,
+};
 use filecoin_proofs_api::Commitment;
-use num_derive::FromPrimitive;
-use num_traits::FromPrimitive;
 
-// Multicodec index that identifies a multihash type for Filecoin
-#[derive(PartialEq, Eq, Copy, Clone, FromPrimitive, Debug, Hash)]
-#[repr(u64)]
-pub enum FilecoinMultihashCode {
-    /// v1 hashing algorithm used in constructing merkleproofs of unsealed data
-    UnsealedV1 = 0xfc1,
-    /// v1 hashing algorithm used in constructing merkleproofs of sealed replicated data
-    SealedV1 = 0xfc2,
-    /// Reserved3 is reserved for future use
-    Reserved3 = 0xfc3,
-    /// Reserved4 is reserved for future use
-    Reserved4 = 0xfc4,
-    /// Reserved5 is reserved for future use
-    Reserved5 = 0xfc5,
-    /// Reserved6 is reserved for future use
-    Reserved6 = 0xfc6,
-    /// Reserved7 is reserved for future use
-    Reserved7 = 0xfc7,
-    /// Reserved8 is reserved for future use
-    Reserved8 = 0xfc8,
-    /// Reserved9 is reserved for future use
-    Reserved9 = 0xfc9,
-    /// Reserved10 is reserved for future use
-    Reserved10 = 0xfca,
-}
+/// Multihash code for Sha2 256 trunc254 padded used in data commitments.
+pub const SHA2_256_TRUNC254_PADDED: Code = Code::Custom(0x1012);
+/// Multihash code for Poseidon BLS replica commitments.
+pub const POSEIDON_BLS12_381_A1_FC1: Code = Code::Custom(0xb401);
 
-/// Converts a raw commitment hash to a CID
+/// CommitmentToCID converts a raw commitment hash to a CID
 /// by adding:
-/// - serialization type of raw
+/// - the given filecoin codec type
 /// - the given filecoin hash type
-pub fn commitment_to_cid(commitment: &Commitment, code: FilecoinMultihashCode) -> Cid {
-    let mh = multihash::wrap(multihash::Code::Custom(code as u64), commitment);
+pub fn commitment_to_cid(
+    mc: Codec,
+    mh: Code,
+    commitment: &Commitment,
+) -> Result<Cid, &'static str> {
+    validate_filecoin_cid_segments(mc, mh, commitment)?;
 
-    Cid::new_v1(Codec::Raw, mh)
+    let mh = multihash::wrap(mh, commitment);
+
+    Ok(Cid::new_v1(mc, mh))
 }
 
-/// cid_to_commitment extracts the raw data commitment from a CID
-/// assuming that it has the correct hashing function and
-/// serialization types
-pub fn cid_to_commitment(c: &Cid) -> Result<(Commitment, FilecoinMultihashCode), &'static str> {
-    if c.codec != Codec::Raw {
-        return Err("codec for all commitments is raw");
-    }
-
-    let code = match c.hash.algorithm() {
-        multihash::Code::Custom(code) => {
-            FromPrimitive::from_u64(code).ok_or("Invalid custom code")?
-        }
-        _ => return Err("Invalid Cid hash algorithm"),
-    };
+/// CIDToCommitment extracts the raw commitment bytes, the FilMultiCodec and
+/// FilMultiHash from a CID, after validating that the codec and hash type are
+/// consistent
+pub fn cid_to_commitment(c: &Cid) -> Result<(Codec, Code, Commitment), &'static str> {
+    validate_filecoin_cid_segments(c.codec, c.hash.algorithm(), c.hash.digest())?;
 
     let mut comm = Commitment::default();
     comm.copy_from_slice(c.hash.digest());
 
-    Ok((comm, code))
+    Ok((c.codec, c.hash.algorithm(), comm))
 }
 
-/// data_commitment_v1_to_cid converts a raw data commitment to a CID
+/// DataCommitmentV1ToCID converts a raw data commitment to a CID
 /// by adding:
-/// - serialization type of raw
-/// - hashing type of Filecoin unsealed hashing function v1 (0xfc2)
-pub fn data_commitment_v1_to_cid(comm_d: &Commitment) -> Cid {
-    commitment_to_cid(comm_d, FilecoinMultihashCode::UnsealedV1)
+/// - codec: cid.FilCommitmentUnsealed
+/// - hash type: multihash.SHA2_256_TRUNC254_PADDED
+pub fn data_commitment_v1_to_cid(comm_d: &Commitment) -> Result<Cid, &'static str> {
+    commitment_to_cid(
+        Codec::FilCommitmentUnsealed,
+        SHA2_256_TRUNC254_PADDED,
+        comm_d,
+    )
 }
 
 /// cid_to_data_commitment_v1 extracts the raw data commitment from a CID
 /// assuming that it has the correct hashing function and
 /// serialization types
 pub fn cid_to_data_commitment_v1(c: &Cid) -> Result<Commitment, &'static str> {
-    let (comm_d, code) = cid_to_commitment(c)?;
+    let (codec, _, comm_d) = cid_to_commitment(c)?;
 
-    if code != FilecoinMultihashCode::UnsealedV1 {
-        return Err("incorrect hashing function for data commitment");
+    if codec != Codec::FilCommitmentUnsealed {
+        return Err("data commitment codec must be Unsealed");
     }
 
     Ok(comm_d)
 }
 
-/// replica_commitment_v1_to_cid converts a raw data commitment to a CID
+/// ReplicaCommitmentV1ToCID converts a raw data commitment to a CID
 /// by adding:
-/// - serialization type of raw
-/// - hashing type of Filecoin sealed hashing function v1 (0xfc2)
-pub fn replica_commitment_v1_to_cid(comm_r: &Commitment) -> Cid {
-    commitment_to_cid(comm_r, FilecoinMultihashCode::SealedV1)
+/// - codec: cid.FilCommitmentSealed
+/// - hash type: multihash.POSEIDON_BLS12_381_A1_FC1
+pub fn replica_commitment_v1_to_cid(comm_r: &Commitment) -> Result<Cid, &'static str> {
+    commitment_to_cid(
+        Codec::FilCommitmentSealed,
+        POSEIDON_BLS12_381_A1_FC1,
+        comm_r,
+    )
 }
 
 /// cid_to_replica_commitment_v1 extracts the raw replica commitment from a CID
 /// assuming that it has the correct hashing function and
 /// serialization types
 pub fn cid_to_replica_commitment_v1(c: &Cid) -> Result<Commitment, &'static str> {
-    let (comm_r, hash) = cid_to_commitment(c)?;
+    let (codec, _, comm_r) = cid_to_commitment(c)?;
 
-    if hash != FilecoinMultihashCode::SealedV1 {
-        return Err("incorrect hashing function for data commitment");
+    if codec != Codec::FilCommitmentSealed {
+        return Err("data commitment codec must be Sealed");
     }
 
     Ok(comm_r)
 }
 
+/// ValidateFilecoinCidSegments returns an error if the provided CID parts
+/// conflict with each other.
+fn validate_filecoin_cid_segments(mc: Codec, mh: Code, comm_x: &[u8]) -> Result<(), &'static str> {
+    match mc {
+        Codec::FilCommitmentUnsealed => {
+            if mh != SHA2_256_TRUNC254_PADDED {
+                return Err("Incorrect hash function for unsealed commitment");
+            }
+        }
+        Codec::FilCommitmentSealed => {
+            if mh != POSEIDON_BLS12_381_A1_FC1 {
+                return Err("Incorrect hash function for sealed commitment");
+            }
+        }
+        _ => return Err("Invalid Codec, expected sealed or unsealed commitment codec"),
+    }
+
+    if comm_x.len() != 32 {
+        Err("commitments must be 32 bytes long")
+    } else {
+        Ok(())
+    }
+}
+
 /// piece_commitment_v1_to_cid converts a comm_p to a CID
 /// -- it is just a helper function that is equivalent to
 /// data_commitment_v1_to_cid.
-pub fn piece_commitment_v1_to_cid(comm_p: &Commitment) -> Cid {
+pub fn piece_commitment_v1_to_cid(comm_p: &Commitment) -> Result<Cid, &'static str> {
     data_commitment_v1_to_cid(comm_p)
 }
 
