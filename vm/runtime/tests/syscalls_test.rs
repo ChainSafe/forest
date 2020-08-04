68a8293171e667ef48a61540a644b3cd6282c8f4
austinabell <austinabell8@gmail.com>
2020-08-04 12:37:15 -0400

    Fix other reference in test

--- vm/runtime/tests/syscalls_test.rs
+++ vm/runtime/tests/syscalls_test.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use cid::{multihash, Cid, Codec};
-use commcid::FilecoinMultihashCode;
+use commcid::{POSEIDON_BLS12_381_A1_FC1, SHA2_256_TRUNC254_PADDED};
 use fil_types::{RegisteredSealProof, SealVerifyInfo};
 use interpreter::DefaultSyscalls;
 use runtime::*;
@@ -12,14 +12,8 @@ fn verify_seal_test() {
     let db = db::MemoryDB::default();
     let sys = DefaultSyscalls::new(&db);
     let data: &[u8; 32] = &[2; 32];
-    let mh_sealed = multihash::wrap(
-        multihash::Code::Custom(FilecoinMultihashCode::SealedV1 as u64),
-        data,
-    );
-    let mh_unsealed = multihash::wrap(
-        multihash::Code::Custom(FilecoinMultihashCode::UnsealedV1 as u64),
-        data,
-    );
+    let mh_sealed = multihash::wrap(POSEIDON_BLS12_381_A1_FC1, data);
+    let mh_unsealed = multihash::wrap(SHA2_256_TRUNC254_PADDED, data);
     let vi = SealVerifyInfo {
         registered_proof: RegisteredSealProof::StackedDRG64GiBV1,
         sector_id: Default::default(),
