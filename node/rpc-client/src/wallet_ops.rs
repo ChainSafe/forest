6a7e75995d70ade110c42624752f42d628ddfd02
Dustin Brickwood <dustinbrickwood204@gmail.com>
2020-08-06 09:53:57 -0500

    quick fix

--- node/rpc-client/src/wallet_ops.rs
+++ node/rpc-client/src/wallet_ops.rs
@@ -14,27 +14,27 @@ pub async fn new(
     client: &mut RawClient<HTC>,
     sig_type: SignatureType,
 ) -> Result<String, JsonRpcError> {
-    Ok(Filecoin::wallet_new(&mut client, sig_type).await?)
+    Ok(Filecoin::wallet_new(client, sig_type).await?)
 }
 
 /// Lists all the addresses in the wallet
 pub async fn list(client: &mut RawClient<HTC>) -> Result<Vec<String>, JsonRpcError> {
-    Ok(Filecoin::wallet_list(&mut client).await?)
+    Ok(Filecoin::wallet_list(client).await?)
 }
 
 /// Returns the balance of the given address at the current head of the chain
 pub async fn balance(client: &mut RawClient<HTC>, address: String) -> Result<String, JsonRpcError> {
-    Ok(Filecoin::wallet_balance(&mut client, address).await?)
+    Ok(Filecoin::wallet_balance(client, address).await?)
 }
 
 /// Marks the given address as as the default one
 pub async fn set_default(client: &mut RawClient<HTC>, address: String) -> Result<(), JsonRpcError> {
-    Ok(Filecoin::wallet_set_default(&mut client, address).await?)
+    Ok(Filecoin::wallet_set_default(client, address).await?)
 }
 
 /// Returns the address marked as default in the wallet
 pub async fn default(client: &mut RawClient<HTC>) -> Result<String, JsonRpcError> {
-    Ok(Filecoin::wallet_default(&mut client).await?)
+    Ok(Filecoin::wallet_default(client).await?)
 }
 
 /// Signs the given bytes using the given address
@@ -42,7 +42,7 @@ pub async fn sign(
     client: &mut RawClient<HTC>,
     params: (String, String),
 ) -> Result<SignatureJson, JsonRpcError> {
-    Ok(Filecoin::wallet_sign(&mut client, params).await?)
+    Ok(Filecoin::wallet_sign(client, params).await?)
 }
 
 /// Takes an address, a signature, and some bytes, and indicates whether the signature is valid.
@@ -51,7 +51,7 @@ pub async fn verify(
     client: &mut RawClient<HTC>,
     params: (String, String, SignatureJson),
 ) -> Result<bool, JsonRpcError> {
-    Ok(Filecoin::wallet_verify(&mut client, params).await?)
+    Ok(Filecoin::wallet_verify(client, params).await?)
 }
 
 /// Receives a KeyInfo, which includes a private key, and imports it into the wallet
@@ -59,7 +59,7 @@ pub async fn import(
     client: &mut RawClient<HTC>,
     key_info: KeyInfoJson,
 ) -> Result<String, JsonRpcError> {
-    Ok(Filecoin::wallet_import(&mut client, key_info).await?)
+    Ok(Filecoin::wallet_import(client, key_info).await?)
 }
 
 /// Returns the private key of an address in the wallet
@@ -67,5 +67,5 @@ pub async fn export(
     client: &mut RawClient<HTC>,
     address: String,
 ) -> Result<KeyInfoJson, JsonRpcError> {
-    Ok(Filecoin::wallet_export(&mut client, address).await?)
+    Ok(Filecoin::wallet_export(client, address).await?)
 }
