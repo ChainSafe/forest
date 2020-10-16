982738c2d2826ba18d08244ea875563c5dbe6cad
Jaden Foldesi <jfoldesi57@gmail.com>
2020-10-16 17:25:11 -0400

    Authorization Setup for Write Access on RPC Calls (#620)
    * fixed header parsing and finished rough impl
    
    * rm dup license
    
    * fixed jwt key gen
    
    * fixed bug and made requested changes
    
    * added dependancy for this current branch
    
    * removed extra clone
    
    * updated actions
    
    Co-authored-by: Ashanti Mutinta <ashantimutinta@gmail.com>

--- node/rpc-client/src/lib.rs
+++ node/rpc-client/src/lib.rs
@@ -1,8 +1,10 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+mod auth_ops;
 mod chain_ops;
 mod client;
 
+pub use self::auth_ops::*;
 pub use self::chain_ops::*;
 pub use self::client::*;
---
+++ utils/auth/src/lib.rs
@@ -0,0 +1,87 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use crypto::SignatureType;
+use jsonrpc_v2::Error as JsonRpcError;
+use jsonwebtoken::errors::Result as JWTResult;
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use wallet::KeyInfo;
+
+/// constant string that is used to identify the JWT secret key in KeyStore
+pub const JWT_IDENTIFIER: &str = "auth-jwt-private";
+/// Admin permissions
+pub const ADMIN: [&str; 4] = ["read", "write", "sign", "admin"];
+/// Signing permissions
+pub const SIGN: [&str; 3] = ["read", "write", "sign"];
+/// Writing permissions
+pub const WRITE: [&str; 2] = ["read", "write"];
+/// Reading permissions
+pub const READ: [&str; 1] = ["read"];
+/// All methods that require write permission
+pub const WRITE_ACCESS: [&str; 6] = [
+    "Filecoin.MpoolPush",
+    "Filecoin.WalletNew",
+    "Filecoin.WalletHas",
+    "Filecoin.WalletList",
+    "Filecoin.WalletDefaultAddress",
+    "Filecoin.WalletList",
+];
+
+/// Error Enum for Authentification
+#[derive(Debug, Error, Serialize, Deserialize)]
+pub enum Error {
+    /// Filecoin Method does not exist
+    #[error("Filecoin method does not exist")]
+    MethodParam,
+    /// Invalid permissions to use specified method
+    #[error("Incorrect permissions to access method")]
+    InvalidPermissions,
+    /// Missing authentication header
+    #[error("Missing authentication header")]
+    NoAuthHeader,
+    #[error("{0}")]
+    Other(String),
+}
+
+/// Claim struct for JWT Tokens
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    #[serde(rename = "Allow")]
+    allow: Vec<String>,
+}
+
+/// Create a new JWT Token
+pub fn create_token(perms: Vec<String>, key: &[u8]) -> JWTResult<String> {
+    let payload = Claims { allow: perms };
+    encode(&Header::default(), &payload, &EncodingKey::from_secret(key))
+}
+
+/// Verify JWT Token and return the allowed permissions from token
+pub fn verify_token(token: &str, key: &[u8]) -> JWTResult<Vec<String>> {
+    let mut validation = Validation::default();
+    validation.validate_exp = false;
+    let token = decode::<Claims>(token, &DecodingKey::from_secret(key), &validation)?;
+    Ok(token.claims.allow)
+}
+
+/// Check whether or not header has required permissions
+pub fn has_perms(header_raw: String, required: &str, key: &[u8]) -> Result<(), JsonRpcError> {
+    if header_raw.starts_with("Bearer: ") {
+        let token = header_raw.trim_start_matches("Bearer: ");
+        let perms = verify_token(token, key).map_err(|err| Error::Other(err.to_string()))?;
+        if !perms.contains(&required.to_string()) {
+            return Err(JsonRpcError::from(Error::InvalidPermissions));
+        }
+    }
+    Ok(())
+}
+
+pub fn generate_priv_key() -> KeyInfo {
+    let priv_key = rand::thread_rng().gen::<[u8; 32]>();
+    // TODO temp use of bls key as placeholder, need to update keyinfo to use string instead of keyinfo
+    // for key type
+    KeyInfo::new(SignatureType::BLS, priv_key.to_vec())
+}
