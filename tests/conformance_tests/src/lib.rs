eadbe61862cf0d5d3a2c06e7ecf72474225eeae8
austinabell <austinabell8@gmail.com>
2020-10-19 15:13:09 -0400

    reset feature usage

--- tests/conformance_tests/src/lib.rs
+++ tests/conformance_tests/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-// #![cfg(feature = "submodule_tests")]
+#![cfg(feature = "submodule_tests")]
 
 mod message;
 mod rand_replay;
