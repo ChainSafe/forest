253ba0df31531cdbfaae55487dc68361984dd226
DragonMural <rajarupans@gmail.com>
2020-10-21 00:52:21 -0400

    Lint fixes

--- blockchain/message_pool/src/lib.rs
+++ blockchain/message_pool/src/lib.rs
@@ -1,10 +1,10 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+pub mod block_prob;
 mod config;
 mod errors;
 mod msgpool;
-mod block_prob;
 pub use self::config::*;
 pub use self::errors::*;
 pub use self::msgpool::*;
