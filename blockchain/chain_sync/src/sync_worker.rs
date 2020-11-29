ChainSafe/forest

commit 56b4961f15d51a1436c4ea2026de996a0cd7df96

Rpc fixes and implementations (#841)
* fixes implementations

* fixes to key management

* Update blockchain/chain/src/store/chain_store.rs

Co-authored-by: Austin Abell <austinabell8@gmail.com>

* fixes to miner info

* cargo fmt

* changes to sync api

* revert some stuff in keystore

* remove println

* remove pritns in miner actor

* remove println in crypto

* fix suggestions in state mgr

* remove opt_vec in address

* lint

* remove get_power_raw

* remove lookback in chainstore

* comment

* lint

* remove drand generic constrint

* const undef

* serialize repr

* Refactor json usage and cleanup logic

* Fix actor dev deps

* Remove unused

* remove implied values on version

* Remove redundant mutex access

Co-authored-by: Austin Abell <austinabell8@gmail.com>
Co-authored-by: Eric Tu <tu.eric@hotmail.com>
Co-authored-by: Eric Tu <6364934+ec2@users.noreply.github.com>

blockchain/chain_sync/src/sync_worker.rs
@@ -445,17 +445,11 @@ where
             .map_err(|e| (*block_cid, e.into()))?;
 
         // Retrieve lookback tipset for validation.
-        let lbts = cs
-            .get_lookback_tipset_for_round(base_ts.clone(), block.header().epoch())
+        let (lbts, lbst) = sm
+            .get_lookback_tipset_for_round::<V>(base_ts.clone(), block.header().epoch())
             .await
             .map_err(|e| (*block_cid, e.into()))?;
 
-        let (lbst, _) = sm.tipset_state::<V>(&lbts).await.map_err(|e| {
-            (
-                *block_cid,
-                Error::Validation(format!("Could not update state: {}", e.to_string())),
-            )
-        })?;
         let lbst = Arc::new(lbst);
 
         let prev_beacon = cs

blockchain/state_manager/src/lib.rs
@@ -10,21 +10,26 @@ pub mod utils;
 mod vm_circ_supply;
 
 pub use self::errors::*;
+use crate::miner::CHAIN_FINALITY;
 use actor::*;
 use address::{Address, BLSPublicKey, Payload, Protocol, BLS_PUB_LEN};
 use async_log::span;
 use async_std::{sync::RwLock, task};
+use beacon::BeaconEntry;
+use beacon::{Beacon, Schedule, IGNORE_DRAND_VAR};
 use blockstore::BlockStore;
 use blockstore::BufferedBlockStore;
-use chain::{ChainStore, HeadChange};
+use chain::{draw_randomness, ChainStore, HeadChange};
 use chain_rand::ChainRand;
 use cid::Cid;
 use clock::ChainEpoch;
 use encoding::de::DeserializeOwned;
 use encoding::Cbor;
-use fil_types::{get_network_version_default, verifier::ProofVerifier};
+use fil_types::{get_network_version_default, verifier::ProofVerifier, NetworkVersion, Randomness};
+use fil_types::{SectorInfo, SectorSize};
 use flo_stream::Subscriber;
 use forest_blocks::{BlockHeader, Tipset, TipsetKeys};
+use forest_crypto::DomainSeparationTag;
 use futures::channel::oneshot;
 use futures::stream::{FuturesUnordered, StreamExt};
 use interpreter::{resolve_to_key_addr, ApplyRet, BlockMessages, Rand, VM};
@@ -34,6 +39,7 @@ use log::{debug, info, trace, warn};
 use message::{message_receipt, unsigned_message};
 use message::{ChainMessage, Message, MessageReceipt, UnsignedMessage};
 use num_bigint::{bigint_ser, BigInt};
+use num_traits::identities::Zero;
 use serde::{Deserialize, Serialize};
 use state_tree::StateTree;
 use std::collections::HashMap;
@@ -55,6 +61,10 @@ pub struct InvocResult {
     pub error: Option<String>,
 }
 
+struct VersionSpec {
+    network_version: NetworkVersion,
+    at_or_below: ChainEpoch,
+}
 // An alias Result that represents an InvocResult and an Error
 pub type StateCallResult = Result<InvocResult, Error>;
 
@@ -75,6 +85,8 @@ pub struct StateManager<DB> {
     /// of the state/receipt root.
     cache: RwLock<HashMap<TipsetKeys, Arc<RwLock<Option<CidPair>>>>>,
     subscriber: Option<Subscriber<HeadChange>>,
+    network_versions: Vec<VersionSpec>,
+    latest_version: NetworkVersion,
     genesis_info: GenesisInfoPair,
 }
 
@@ -87,6 +99,8 @@ where
             cs,
             cache: RwLock::new(HashMap::new()),
             subscriber: None,
+            network_versions: Vec::new(),
+            latest_version: NetworkVersion::V0,
             genesis_info: GenesisInfoPair::default(),
         }
     }
@@ -100,6 +114,8 @@ where
             cs,
             cache: RwLock::new(HashMap::new()),
             subscriber: Some(chain_subs),
+            network_versions: Vec::new(),
+            latest_version: NetworkVersion::V0,
             genesis_info: GenesisInfoPair::default(),
         }
     }
@@ -469,6 +485,180 @@ where
             .into_inner()
             .ok_or_else(|| Error::Other("message did not have a return".to_string()))?;
         Ok((out_mes, out_ret))
+    }
+
+    /// Gets lookback tipset for block validations.
+    pub async fn get_lookback_tipset_for_round<V>(
+        self: &Arc<Self>,
+        tipset: Arc<Tipset>,
+        round: ChainEpoch,
+    ) -> Result<(Arc<Tipset>, Cid), Error>
+    where
+        V: ProofVerifier,
+    {
+        let mut lbr: ChainEpoch = ChainEpoch::from(0);
+        let version = self
+            .network_versions
+            .iter()
+            .find(|s| round == s.at_or_below)
+            .map(|s| s.network_version)
+            .unwrap_or(self.latest_version);
+        let lb = if version <= NetworkVersion::V3 {
+            ChainEpoch::from(10)
+        } else {
+            CHAIN_FINALITY
+        };
+
+        if round > lb {
+            lbr = round - lb
+        }
+
+        if lbr >= tipset.epoch() {
+            let (st, _) = self
+                .tipset_state::<V>(&tipset)
+                .await
+                .map_err(|e| Error::Other(format!("Could execute tipset_state {:?}", e)))?;
+            return Ok((tipset, st));
+        }
+        let next_ts = self
+            .cs
+            .tipset_by_height(lbr, tipset.clone(), false)
+            .await
+            .map_err(|e| Error::Other(format!("Could not get tipset by height {:?}", e)))?;
+        if lbr == next_ts.epoch() {
+            return Err(Error::Other(format!(
+                "failed to find non-null tipset {:?} {} which is known to exist, found {:?} {}",
+                tipset.key(),
+                tipset.epoch(),
+                next_ts.key(),
+                next_ts.epoch()
+            )));
+        }
+        let lbts = self
+            .cs
+            .tipset_from_keys(tipset.parents())
+            .await
+            .map_err(|e| Error::Other(format!("Could not get tipset from keys {:?}", e)))?;
+        Ok((lbts, *next_ts.parent_state()))
+    }
+
+    fn eligible_to_mine<V: ProofVerifier>(
+        self: &Arc<Self>,
+        address: &Address,
+        base_tipset: &Tipset,
+        lookback_tipset: &Tipset,
+    ) -> Result<bool, Error> {
+        let hmp_result = self.miner_has_min_power(address, lookback_tipset);
+        let version = self
+            .network_versions
+            .iter()
+            .find(|s| base_tipset.epoch() == s.at_or_below)
+            .map(|s| s.network_version)
+            .unwrap_or(self.latest_version);
+
+        if version as u8 <= 3 {
+            if let Ok(hmp) = hmp_result {
+                return Ok(hmp);
+            }
+        }
+
+        let hmp = hmp_result?;
+
+        if !hmp {
+            return Ok(false);
+        }
+
+        let power_state: power::State =
+            self.load_actor_state(&STORAGE_POWER_ACTOR_ADDR, &base_tipset.parent_state())?;
+
+        let claim = power_state
+            .miner_power(self.blockstore(), &address)
+            .map_err(|e| {
+                Error::Other(format!(
+                    "Could not execute miner_power func for elligable to mine func : {:?}",
+                    e
+                ))
+            })?
+            .ok_or_else(|| Error::Other("Could not get claim".to_string()))?;
+        if claim.quality_adj_power <= BigInt::zero() {
+            return Ok(false);
+        }
+
+        if base_tipset.epoch() <= clock::EPOCH_UNDEFINED {
+            return Ok(false);
+        }
+
+        Ok(true)
+    }
+
+    /// gets associated miner base info based
+    pub async fn miner_get_base_info<V: ProofVerifier, B: Beacon>(
+        self: &Arc<Self>,
+        schedule: &Schedule<B>,
+        key: &TipsetKeys,
+        round: ChainEpoch,
+        address: Address,
+    ) -> Result<Option<MiningBaseInfo>, Box<dyn StdError>> {
+        let tipset = self.cs.tipset_from_keys(key).await?;
+        let prev = match self.cs.latest_beacon_entry(&tipset).await {
+            Ok(prev) => prev,
+            Err(err) => {
+                if std::env::var(IGNORE_DRAND_VAR)
+                    .map(|e| e == "1")
+                    .unwrap_or_default()
+                {
+                    return Err(Box::from(format!(
+                        "failed to get latest beacon entry: {:?}",
+                        err
+                    )));
+                }
+                beacon::BeaconEntry::default()
+            }
+        };
+        let beacon = schedule.beacon_for_epoch(round)?;
+        let entries = beacon::beacon_entries_for_block(beacon, round, &prev).await?;
+        let rbase = entries.iter().last().unwrap_or(&prev);
+        let (lbts, lbst) = self
+            .get_lookback_tipset_for_round::<V>(tipset.clone(), round)
+            .await?;
+        let state: miner::State = self.load_actor_state(&address, &lbst)?;
+
+        let buf = address.marshal_cbor()?;
+        let prand = draw_randomness(
+            rbase.data(),
+            DomainSeparationTag::WinningPoStChallengeSeed,
+            round,
+            &buf,
+        )?;
+
+        let sectors = self.get_sectors_for_winning_post::<V>(&lbst, &address, Randomness(prand))?;
+
+        if sectors.is_empty() {
+            return Ok(None);
+        }
+
+        let (mpow, tpow) = self.get_power(&lbst, &address)?;
+
+        let info = state.get_info(self.blockstore())?;
+
+        let (st, _) = self.tipset_state::<V>(&lbts).await?;
+        let state = StateTree::new_from_root(self.blockstore(), &st)
+            .map_err(|e| Error::State(e.to_string()))?;
+
+        let worker_key = resolve_to_key_addr(&state, self.blockstore(), &info.worker)?;
+
+        let elligable = self.eligible_to_mine::<V>(&address, &tipset.as_ref(), &lbts)?;
+
+        Ok(Some(MiningBaseInfo {
+            miner_power: Some(mpow.quality_adj_power),
+            network_power: Some(tpow.quality_adj_power),
+            sectors,
+            worker_key,
+            sector_size: info.sector_size,
+            prev_beacon_entry: prev,
+            beacon_entries: entries,
+            elligable_for_minning: elligable,
+        }))
     }
 
     pub async fn compute_tipset_state<V, CB: 'static>(
@@ -979,4 +1169,15 @@ where
         }
         Ok(())
     }
+}
+
+pub struct MiningBaseInfo {
+    pub miner_power: Option<TokenAmount>,
+    pub network_power: Option<TokenAmount>,
+    pub sectors: Vec<SectorInfo>,
+    pub worker_key: Address,
+    pub sector_size: SectorSize,
+    pub prev_beacon_entry: BeaconEntry,
+    pub beacon_entries: Vec<BeaconEntry>,
+    pub elligable_for_minning: bool,
 }

blockchain/beacon/src/lib.rs
@@ -13,7 +13,7 @@ use clock::ChainEpoch;
 use std::error::Error;
 
 pub async fn beacon_entries_for_block<B: Beacon>(
-    beacon: B,
+    beacon: &B,
     round: ChainEpoch,
     prev: &BeaconEntry,
 ) -> Result<Vec<BeaconEntry>, Box<dyn Error>> {

node/rpc/src/lib.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 mod auth_api;
+mod beacon_api;
 mod chain_api;
 mod common_api;
 mod gas_api;
@@ -10,7 +11,7 @@ mod state_api;
 mod sync_api;
 mod wallet_api;
 
-use crate::{common_api::version, state_api::*};
+use crate::{beacon_api::beacon_get_entry, common_api::version, state_api::*};
 use async_log::span;
 use async_std::net::{TcpListener, TcpStream};
 use async_std::sync::{Arc, RwLock, Sender};
@@ -19,9 +20,12 @@ use async_tungstenite::{
     tungstenite::handshake::server::Request, tungstenite::Message, WebSocketStream,
 };
 use auth::{has_perms, Error as AuthError, JWT_IDENTIFIER, WRITE_ACCESS};
+use beacon::{Beacon, Schedule};
 use blockstore::BlockStore;
+use chain::ChainStore;
 use chain::{headchange_json::HeadChangeJson, EventsPayload};
 use chain_sync::{BadBlockCache, SyncState};
+use fil_types::verifier::ProofVerifier;
 use flo_stream::{MessagePublisher, Publisher, Subscriber};
 use forest_libp2p::NetworkMessage;
 use futures::future;
@@ -51,10 +55,11 @@ struct StreamingData<'a> {
 }
 
 /// This is where you store persistant data, or at least access to stateful data.
-pub struct RpcState<DB, KS>
+pub struct RpcState<DB, KS, B>
 where
     DB: BlockStore + Send + Sync + 'static,
     KS: KeyStore + Send + Sync + 'static,
+    B: Beacon + Send + Sync + 'static,
 {
     pub state_manager: Arc<StateManager<DB>>,
     pub keystore: Arc<RwLock<KS>>,
@@ -64,12 +69,16 @@ where
     pub sync_state: Arc<RwLock<Vec<Arc<RwLock<SyncState>>>>>,
     pub network_send: Sender<NetworkMessage>,
     pub network_name: String,
+    pub chain_store: Arc<ChainStore<DB>>,
+    pub beacon: Schedule<B>,
 }
 
-pub async fn start_rpc<DB, KS>(state: RpcState<DB, KS>, rpc_endpoint: &str)
+pub async fn start_rpc<DB, KS, B, V>(state: RpcState<DB, KS, B>, rpc_endpoint: &str)
 where
     DB: BlockStore + Send + Sync + 'static,
     KS: KeyStore + Send + Sync + 'static,
+    B: Beacon + Send + Sync + 'static,
+    V: ProofVerifier + Send + Sync + 'static,
 {
     use auth_api::*;
     use chain_api::*;
@@ -81,181 +90,213 @@ where
     let rpc = Server::new()
         .with_data(Data::new(state))
         // Auth API
-        .with_method("Filecoin.AuthNew", auth_new::<DB, KS>, false)
-        .with_method("Filecoin.AuthVerify", auth_verify::<DB, KS>, false)
+        .with_method("Filecoin.AuthNew", auth_new::<DB, KS, B>, false)
+        .with_method("Filecoin.AuthVerify", auth_verify::<DB, KS, B>, false)
         // Chain API
         .with_method(
             "Filecoin.ChainGetMessage",
-            chain_api::chain_get_message::<DB, KS>,
+            chain_api::chain_get_message::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.ChainGetObj", chain_read_obj::<DB, KS>, false)
-        .with_method("Filecoin.ChainHasObj", chain_has_obj::<DB, KS>, false)
+        .with_method("Filecoin.ChainGetObj", chain_read_obj::<DB, KS, B>, false)
+        .with_method("Filecoin.ChainHasObj", chain_has_obj::<DB, KS, B>, false)
         .with_method(
             "Filecoin.ChainGetBlockMessages",
-            chain_block_messages::<DB, KS>,
+            chain_block_messages::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.ChainGetTipsetByHeight",
-            chain_get_tipset_by_height::<DB, KS>,
+            chain_get_tipset_by_height::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.ChainGetGenesis",
-            chain_get_genesis::<DB, KS>,
+            chain_get_genesis::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.ChainTipsetWeight",
-            chain_tipset_weight::<DB, KS>,
+            chain_tipset_weight::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.ChainGetTipset", chain_get_tipset::<DB, KS>, false)
+        .with_method(
+            "Filecoin.ChainGetTipset",
+            chain_get_tipset::<DB, KS, B>,
+            false,
+        )
         .with_method(
             "Filecoin.GetRandomness",
-            chain_get_randomness::<DB, KS>,
+            chain_get_randomness::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.ChainGetBlock",
-            chain_api::chain_get_block::<DB, KS>,
+            chain_api::chain_get_block::<DB, KS, B>,
             false,
         )
-        .with_method(CHAIN_NOTIFY_METHOD_NAME, chain_notify::<DB, KS>, true)
-        .with_method("Filecoin.ChainHead", chain_head::<DB, KS>, false)
+        .with_method(CHAIN_NOTIFY_METHOD_NAME, chain_notify::<DB, KS, B>, true)
+        .with_method("Filecoin.ChainHead", chain_head::<DB, KS, B>, false)
         // Message Pool API
         .with_method(
             "Filecoin.MpoolEstimateGasPrice",
-            estimate_gas_premium::<DB, KS>,
+            estimate_gas_premium::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.MpoolGetNonce",
-            mpool_get_sequence::<DB, KS>,
+            mpool_get_sequence::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.MpoolPending", mpool_pending::<DB, KS>, false)
-        .with_method("Filecoin.MpoolPush", mpool_push::<DB, KS>, false)
+        .with_method("Filecoin.MpoolPending", mpool_pending::<DB, KS, B>, false)
+        .with_method("Filecoin.MpoolPush", mpool_push::<DB, KS, B>, false)
         .with_method(
             "Filecoin.MpoolPushMessage",
-            mpool_push_message::<DB, KS>,
+            mpool_push_message::<DB, KS, B>,
             false,
         )
         // Sync API
-        .with_method("Filecoin.SyncCheckBad", sync_check_bad::<DB, KS>, false)
-        .with_method("Filecoin.SyncMarkBad", sync_mark_bad::<DB, KS>, false)
-        .with_method("Filecoin.SyncState", sync_state::<DB, KS>, false)
+        .with_method("Filecoin.SyncCheckBad", sync_check_bad::<DB, KS, B>, false)
+        .with_method("Filecoin.SyncMarkBad", sync_mark_bad::<DB, KS, B>, false)
+        .with_method("Filecoin.SyncState", sync_state::<DB, KS, B>, false)
         .with_method(
             "Filecoin.SyncSubmitBlock",
-            sync_submit_block::<DB, KS>,
+            sync_submit_block::<DB, KS, B>,
             false,
         )
         // Wallet API
-        .with_method("Filecoin.WalletBalance", wallet_balance::<DB, KS>, false)
+        .with_method("Filecoin.WalletBalance", wallet_balance::<DB, KS, B>, false)
         .with_method(
             "Filecoin.WalletDefaultAddress",
-            wallet_default_address::<DB, KS>,
+            wallet_default_address::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.WalletExport", wallet_export::<DB, KS>, false)
-        .with_method("Filecoin.WalletHas", wallet_has::<DB, KS>, false)
-        .with_method("Filecoin.WalletImport", wallet_import::<DB, KS>, false)
-        .with_method("Filecoin.WalletList", wallet_list::<DB, KS>, false)
-        .with_method("Filecoin.WalletNew", wallet_new::<DB, KS>, false)
+        .with_method("Filecoin.WalletExport", wallet_export::<DB, KS, B>, false)
+        .with_method("Filecoin.WalletHas", wallet_has::<DB, KS, B>, false)
+        .with_method("Filecoin.WalletImport", wallet_import::<DB, KS, B>, false)
+        .with_method("Filecoin.WalletList", wallet_list::<DB, KS, B>, false)
+        .with_method("Filecoin.WalletNew", wallet_new::<DB, KS, B>, false)
         .with_method(
             "Filecoin.WalletSetDefault",
-            wallet_set_default::<DB, KS>,
+            wallet_set_default::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.WalletSign", wallet_sign::<DB, KS>, false)
+        .with_method("Filecoin.WalletSign", wallet_sign::<DB, KS, B>, false)
         .with_method(
             "Filecoin.WalletSignMessage",
-            wallet_sign_message::<DB, KS>,
+            wallet_sign_message::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.WalletVerify", wallet_verify::<DB, KS>, false)
+        .with_method("Filecoin.WalletVerify", wallet_verify::<DB, KS, B>, false)
         // State API
         .with_method(
             "Filecoin.StateMinerSector",
-            state_miner_sector::<DB, KS>,
+            state_miner_sector::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.StateCall", state_call::<DB, KS>, false)
+        .with_method("Filecoin.StateCall", state_call::<DB, KS, B>, false)
         .with_method(
             "Filecoin.StateMinerDeadlines",
-            state_miner_deadlines::<DB, KS>,
+            state_miner_deadlines::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.StateSectorPrecommitInfo",
-            state_sector_precommit_info::<DB, KS>,
+            state_sector_precommit_info::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.StateSectorInfo",
-            state_sector_info::<DB, KS>,
+            state_sector_info::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.StateMinerProvingDeadline",
-            state_miner_proving_deadline::<DB, KS>,
+            state_miner_proving_deadline::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.StateMinerInfo", state_miner_info::<DB, KS>, false)
+        .with_method(
+            "Filecoin.StateMinerInfo",
+            state_miner_info::<DB, KS, B>,
+            false,
+        )
         .with_method(
             "Filecoin.StateMinerFaults",
-            state_miner_faults::<DB, KS>,
+            state_miner_faults::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.StateAllMinerFaults",
-            state_all_miner_faults::<DB, KS>,
+            state_all_miner_faults::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.StateMinerRecoveries",
-            state_miner_recoveries::<DB, KS>,
+            state_miner_recoveries::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.StateReplay", state_replay::<DB, KS>, false)
-        .with_method("Filecoin.StateGetActor", state_get_actor::<DB, KS>, false)
+        .with_method("Filecoin.StateReplay", state_replay::<DB, KS, B>, false)
+        .with_method(
+            "Filecoin.StateGetActor",
+            state_get_actor::<DB, KS, B>,
+            false,
+        )
         .with_method(
             "Filecoin.StateAccountKey",
-            state_account_key::<DB, KS>,
+            state_account_key::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.StateLookupId", state_lookup_id::<DB, KS>, false)
+        .with_method(
+            "Filecoin.StateLookupId",
+            state_lookup_id::<DB, KS, B>,
+            false,
+        )
         .with_method(
             "Filecoin.StateMartketBalance",
-            state_market_balance::<DB, KS>,
+            state_market_balance::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.StateGetReceipt",
-            state_get_receipt::<DB, KS>,
+            state_get_receipt::<DB, KS, B>,
             false,
         )
-        .with_method("Filecoin.StateWaitMsg", state_wait_msg::<DB, KS>, false)
-        .with_method("Filecoin.NetworkName", state_network_name::<DB, KS>, false)
+        .with_method("Filecoin.StateWaitMsg", state_wait_msg::<DB, KS, B>, false)
+        .with_method(
+            "Filecoin.NetworkName",
+            state_network_name::<DB, KS, B>,
+            false,
+        )
+        .with_method(
+            "Filecoin.MinerGetBaseInfo",
+            state_miner_get_base_info::<DB, KS, B, V>,
+            false,
+        )
+        .with_method("Filecoin.NetworkVersion", state_get_network_version, false)
         // Gas API
         .with_method(
             "Filecoin.GasEstimateGasLimit",
-            gas_estimate_gas_limit::<DB, KS>,
+            gas_estimate_gas_limit::<DB, KS, B, V>,
             false,
         )
         .with_method(
             "Filecoin.GasEstimateGasPremium",
-            gas_estimate_gas_premium::<DB, KS>,
+            gas_estimate_gas_premium::<DB, KS, B>,
             false,
         )
         .with_method(
             "Filecoin.GasEstimateFeeCap",
-            gas_estimate_fee_cap::<DB, KS>,
+            gas_estimate_fee_cap::<DB, KS, B>,
             false,
         )
         // Common
         .with_method("Filecoin.Version", version, false)
+        //beacon
+        .with_method(
+            "Filecoin.BeaconGetEntry",
+            beacon_get_entry::<DB, KS, B>,
+            false,
+        )
         .finish_unwrapped();
 
     let try_socket = TcpListener::bind(rpc_endpoint).await;
@@ -307,6 +348,7 @@ async fn handle_connection_and_log(
                 match message_result {
                     Ok(message) => {
                         let request_text = message.into_text().unwrap();
+                        info!("request senty {:?}", request_text.clone());
                         match serde_json::from_str(&request_text)
                             as Result<RequestObject, serde_json::Error>
                         {

commit 867bec1afdcd76c037fa326e5e10b04ad3f72b99

Fix syncing regressions from #841

blockchain/chain_sync/src/sync_worker.rs
@@ -594,10 +594,10 @@ where
                 ));
             }
 
-            let hp = sm_c.miner_has_min_power(h.miner_address(), &lbts)?;
+            let hp = sm_c.eligible_to_mine(h.miner_address(), &base_ts_clone, &lbts)?;
             if !hp {
                 return Err(Error::Validation(
-                    "Block's miner does not meet minimum power threshold".to_string(),
+                    "Block's miner is ineligible to mine".to_string(),
                 ));
             }
 

blockchain/state_manager/src/lib.rs
@@ -61,10 +61,6 @@ pub struct InvocResult {
     pub error: Option<String>,
 }
 
-struct VersionSpec {
-    network_version: NetworkVersion,
-    at_or_below: ChainEpoch,
-}
 // An alias Result that represents an InvocResult and an Error
 pub type StateCallResult = Result<InvocResult, Error>;
 
@@ -85,8 +81,6 @@ pub struct StateManager<DB> {
     /// of the state/receipt root.
     cache: RwLock<HashMap<TipsetKeys, Arc<RwLock<Option<CidPair>>>>>,
     subscriber: Option<Subscriber<HeadChange>>,
-    network_versions: Vec<VersionSpec>,
-    latest_version: NetworkVersion,
     genesis_info: GenesisInfoPair,
 }
 
@@ -99,8 +93,6 @@ where
             cs,
             cache: RwLock::new(HashMap::new()),
             subscriber: None,
-            network_versions: Vec::new(),
-            latest_version: NetworkVersion::V0,
             genesis_info: GenesisInfoPair::default(),
         }
     }
@@ -114,8 +106,6 @@ where
             cs,
             cache: RwLock::new(HashMap::new()),
             subscriber: Some(chain_subs),
-            network_versions: Vec::new(),
-            latest_version: NetworkVersion::V0,
             genesis_info: GenesisInfoPair::default(),
         }
     }
@@ -497,12 +487,7 @@ where
         V: ProofVerifier,
     {
         let mut lbr: ChainEpoch = ChainEpoch::from(0);
-        let version = self
-            .network_versions
-            .iter()
-            .find(|s| round == s.at_or_below)
-            .map(|s| s.network_version)
-            .unwrap_or(self.latest_version);
+        let version = get_network_version_default(round);
         let lb = if version <= NetworkVersion::V3 {
             ChainEpoch::from(10)
         } else {
@@ -513,6 +498,7 @@ where
             lbr = round - lb
         }
 
+        // More null blocks than lookback
         if lbr >= tipset.epoch() {
             let (st, _) = self
                 .tipset_state::<V>(&tipset)
@@ -520,12 +506,13 @@ where
                 .map_err(|e| Error::Other(format!("Could execute tipset_state {:?}", e)))?;
             return Ok((tipset, st));
         }
+
         let next_ts = self
             .cs
-            .tipset_by_height(lbr, tipset.clone(), false)
+            .tipset_by_height(lbr + 1, tipset.clone(), false)
             .await
             .map_err(|e| Error::Other(format!("Could not get tipset by height {:?}", e)))?;
-        if lbr == next_ts.epoch() {
+        if lbr > next_ts.epoch() {
             return Err(Error::Other(format!(
                 "failed to find non-null tipset {:?} {} which is known to exist, found {:?} {}",
                 tipset.key(),
@@ -536,33 +523,24 @@ where
         }
         let lbts = self
             .cs
-            .tipset_from_keys(tipset.parents())
+            .tipset_from_keys(next_ts.parents())
             .await
             .map_err(|e| Error::Other(format!("Could not get tipset from keys {:?}", e)))?;
         Ok((lbts, *next_ts.parent_state()))
     }
 
-    fn eligible_to_mine<V: ProofVerifier>(
+    pub fn eligible_to_mine(
         self: &Arc<Self>,
         address: &Address,
         base_tipset: &Tipset,
         lookback_tipset: &Tipset,
     ) -> Result<bool, Error> {
-        let hmp_result = self.miner_has_min_power(address, lookback_tipset);
-        let version = self
-            .network_versions
-            .iter()
-            .find(|s| base_tipset.epoch() == s.at_or_below)
-            .map(|s| s.network_version)
-            .unwrap_or(self.latest_version);
+        let hmp = self.miner_has_min_power(address, lookback_tipset)?;
+        let version = get_network_version_default(base_tipset.epoch());
 
-        if version as u8 <= 3 {
-            if let Ok(hmp) = hmp_result {
-                return Ok(hmp);
-            }
+        if version <= NetworkVersion::V3 {
+            return Ok(hmp);
         }
-
-        let hmp = hmp_result?;
 
         if !hmp {
             return Ok(false);
@@ -647,7 +625,7 @@ where
 
         let worker_key = resolve_to_key_addr(&state, self.blockstore(), &info.worker)?;
 
-        let elligable = self.eligible_to_mine::<V>(&address, &tipset.as_ref(), &lbts)?;
+        let elligable = self.eligible_to_mine(&address, &tipset.as_ref(), &lbts)?;
 
         Ok(Some(MiningBaseInfo {
             miner_power: Some(mpow.quality_adj_power),