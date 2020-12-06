260edb09030e7755fce68225984d3f8368a2d9ac
austinabell <austinabell8@gmail.com>
2020-12-06 15:47:52 -0500

    broken but wip because a lot of changes

---
+++ vm/actor_interface/src/lib.rs
@@ -0,0 +1,22 @@
+mod builtin;
+
+pub use self::builtin::*;
+use fil_types::NetworkVersion;
+
+pub enum ActorVersion {
+    V1,
+    V2,
+}
+
+impl From<NetworkVersion> for ActorVersion {
+    fn from(version: NetworkVersion) -> Self {
+        match version {
+            NetworkVersion::V0 | NetworkVersion::V1 | NetworkVersion::V2 | NetworkVersion::V3 => {
+                ActorVersion::V1
+            }
+            NetworkVersion::V4 | NetworkVersion::V5 | NetworkVersion::V6 | NetworkVersion::V7 => {
+                ActorVersion::V1
+            }
+        }
+    }
+}
