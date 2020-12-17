fd3eb4c649a6f6f4377f5a864c424e1749787b44
Eric Tu <tu.eric@hotmail.com>
2020-12-16 20:13:30 -0500

    destructure it

--- node/rpc/src/chain_api.rs
+++ node/rpc/src/chain_api.rs
@@ -290,7 +290,7 @@ where
     KS: KeyStore + Send + Sync + 'static,
     B: Beacon + Send + Sync + 'static,
 {
-    let (tsk, pers, epoch, entropy) = params;
+    let (TipsetKeysJson(tsk), pers, epoch, entropy) = params;
     Ok(data
         .state_manager
         .chain_store()
