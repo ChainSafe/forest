334551e13db13d2dbea7c11aea981c50af259043
Austin Abell <austinabell8@gmail.com>
2021-01-22 08:00:09 -0500

    Update consensus min power (#946)

--- vm/actor/src/builtin/sector.rs
+++ vm/actor/src/builtin/sector.rs
@@ -8,11 +8,10 @@ use fil_types::{RegisteredSealProof, StoragePower};
 pub fn consensus_miner_min_power(p: RegisteredSealProof) -> Result<StoragePower, String> {
     use RegisteredSealProof::*;
     match p {
-        StackedDRG2KiBV1 | StackedDRG2KiBV1P1 => Ok(StoragePower::from(0)),
-        StackedDRG512MiBV1 | StackedDRG512MiBV1P1 => Ok(StoragePower::from(16 << 20)),
-        StackedDRG8MiBV1 | StackedDRG8MiBV1P1 => Ok(StoragePower::from(1 << 30)),
-        StackedDRG32GiBV1 | StackedDRG32GiBV1P1 => Ok(StoragePower::from(10u64 << 40)),
-        StackedDRG64GiBV1 | StackedDRG64GiBV1P1 => Ok(StoragePower::from(20u64 << 40)),
+        // Specs actors defaults to other values, these are the mainnet values put in place
+        StackedDRG2KiBV1 | StackedDRG2KiBV1P1 | StackedDRG512MiBV1 | StackedDRG512MiBV1P1
+        | StackedDRG8MiBV1 | StackedDRG8MiBV1P1 | StackedDRG32GiBV1 | StackedDRG32GiBV1P1
+        | StackedDRG64GiBV1 | StackedDRG64GiBV1P1 => Ok(StoragePower::from(10u64 << 40)),
         Invalid(i) => Err(format!("unsupported proof type: {}", i)),
     }
 }
