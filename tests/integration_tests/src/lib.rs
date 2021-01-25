e5c3fca11152c5b011cfdc300c93e6f8dd226747
Hunter Trujillo <cryptoquick@pm.me>
2021-01-25 14:36:25 -0700

    A start on stdout processing

--- tests/integration_tests/src/lib.rs
+++ tests/integration_tests/src/lib.rs
@@ -1,33 +1,59 @@
-#![cfg(feature = "submodule_tests")]
+#![cfg(feature = "integration_tests")]
 
-use cmd_lib::run_cmd;
+use anyhow::{Error, Result};
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
 
-const CONFIGS_PATH: &str = "tests/integration_tests/configs";
+const CONFIGS_PATH: &str = "./configs";
 
 #[test]
-pub fn single_producer_multiple_consumer() {
+pub fn single_producer_multiple_consumer() -> Result<(), Error> {
   let forest_sp1_config = format!("{}/forest/single_producer_1.toml", CONFIGS_PATH);
-  let result = run_cmd!(|forest_sp1_config| forest --config "#forest_sp1_config")?;
 
-  assert_eq!(result, "some stdout assertion");
+  let mut output = Command::new("bash")
+    .arg("-C")
+    .arg(format!(
+      "../../target/release/forest --config {}",
+      forest_sp1_config
+    ))
+    .stdout(Stdio::piped())
+    .spawn()
+    .expect("forest failed to start");
+
+  let mut stdout = BufReader::new(output.stdout.as_mut().unwrap());
+  let mut line = String::new();
+
+  let mut reading = true;
+
+  while reading {
+    stdout.read_line(&mut line)?;
+
+    if line.contains("forest::daemon") {
+      assert_eq!(line, "some stdout assertion");
+
+      reading = false;
+    }
+  }
+
+  Ok(())
 }
 
 #[test]
 pub fn interop_bootstrap_sync() {
-  !unimplemented
+  unimplemented!();
 }
 
 #[test]
 pub fn interop_forest_producer() {
-  !unimplemented
+  unimplemented!();
 }
 
 #[test]
 pub fn interop_lotus_consumer() {
-  !unimplemented
+  unimplemented!();
 }
 
 #[test]
 pub fn interop_lotus_produce() {
-  !unimplemented
+  unimplemented!();
 }
