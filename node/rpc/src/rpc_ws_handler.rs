c0e0253bc82abf2c3888d5914d7b776c711b8713
Hunter Trujillo <cryptoquick@pm.me>
2021-03-17 16:17:50 -0600

    Refactor WS handler into separate task. Spawns a thread for every valid WS call. Improved error handling with fewer unwraps.

--- node/rpc/src/rpc_ws_handler.rs
+++ node/rpc/src/rpc_ws_handler.rs
@@ -3,6 +3,7 @@
 
 use async_std::sync::{Arc, Mutex};
 use crossbeam::atomic::AtomicCell;
+use futures::stream::SplitSink;
 use futures::{SinkExt, StreamExt};
 use log::{debug, error, info, warn};
 use tide_websockets::{Message, WebSocketConnection};
@@ -16,6 +17,89 @@ use crate::rpc_util::{
     call_rpc, call_rpc_str, get_error_str, RPC_METHOD_CHAIN_HEAD_SUB, RPC_METHOD_CHAIN_NOTIFY,
 };
 
+async fn rpc_ws_task<DB, KS, B>(
+    rpc_call: jsonrpc_v2::RequestObject,
+    rpc_server: JsonRpcServerState,
+    is_socket_active: Arc<AtomicCell<bool>>,
+    ws_sender: Arc<Mutex<SplitSink<WebSocketConnection, Message>>>,
+) -> Result<(), tide::Error> {
+    let call_method = rpc_call.method_ref();
+    let call_id = rpc_call.id_ref();
+
+    match call_method {
+        RPC_METHOD_CHAIN_NOTIFY => {
+            let request_id = match call_id {
+                Some(id) => id.to_owned(),
+                None => jsonrpc_v2::Id::Null,
+            };
+
+            let (subscription_response, subscription_id) = call_rpc::<i64>(
+                rpc_server.clone(),
+                jsonrpc_v2::RequestObject::request()
+                    .with_method(RPC_METHOD_CHAIN_HEAD_SUB)
+                    .with_id(request_id.clone())
+                    .finish(),
+            )
+            .await?;
+
+            ws_sender
+                .lock()
+                .await
+                .send(Message::Text(subscription_response))
+                .await?;
+
+            info!(
+                "RPC WS ChainNotify for subscription ID: {}",
+                subscription_id
+            );
+
+            while is_socket_active.load() {
+                let (_, event) = call_rpc::<SubscriptionHeadChange>(
+                    rpc_server.clone(),
+                    jsonrpc_v2::RequestObject::request()
+                        .with_method(RPC_METHOD_CHAIN_NOTIFY)
+                        .with_id(subscription_id)
+                        .finish(),
+                )
+                .await?;
+
+                debug!("RPC WS ChainNotify event: {:?}", event);
+
+                let event_response = StreamingData {
+                    json_rpc: "2.0",
+                    method: "xrpc.ch.val",
+                    params: event,
+                };
+
+                match ws_sender
+                    .lock()
+                    .await
+                    .send(Message::Text(serde_json::to_string(&event_response)?))
+                    .await
+                {
+                    Ok(_) => {
+                        info!(
+                            "New ChainNotify data sent via subscription ID: {}",
+                            subscription_id
+                        );
+                    }
+                    Err(msg) => {
+                        warn!("WS connection closed. {:?}", msg);
+                        is_socket_active.store(false);
+                    }
+                }
+            }
+        }
+        _ => {
+            info!("RPC WS called method: {}", call_method);
+            let response = call_rpc_str(rpc_server.clone(), rpc_call).await?;
+            ws_sender.lock().await.send(Message::Text(response)).await?;
+        }
+    }
+
+    Ok(())
+}
+
 pub async fn rpc_ws_handler<DB, KS, B>(
     request: tide::Request<JsonRpcServerState>,
     ws_stream: WebSocketConnection,
@@ -26,9 +110,9 @@ where
     B: Beacon + Send + Sync + 'static,
 {
     let rpc_server = request.state();
+    let socket_active = Arc::new(AtomicCell::new(true));
     let (ws_sender, mut ws_receiver) = ws_stream.split();
     let ws_sender = Arc::new(Mutex::new(ws_sender));
-    let socket_active = Arc::new(AtomicCell::new(true));
 
     info!("Accepted WS connection!");
 
@@ -42,102 +126,61 @@ where
                 if !request_text.is_empty() {
                     info!("RPC Request Received: {:?}", &request_text);
 
+                    let task_rpc_server = rpc_server.clone();
+                    let task_socket_active = socket_active.clone();
+                    let task_ws_sender = ws_sender.clone();
+
                     match serde_json::from_str(&request_text)
                         as Result<jsonrpc_v2::RequestObject, serde_json::Error>
                     {
-                        Ok(call) => match &*call.method_ref() {
-                            RPC_METHOD_CHAIN_NOTIFY => {
-                                let request_id = match call.id_ref() {
-                                    Some(id) => id.to_owned(),
-                                    None => jsonrpc_v2::Id::Null,
-                                };
-
-                                let (subscription_response, subscription_id) = call_rpc::<i64>(
-                                    rpc_server.clone(),
-                                    jsonrpc_v2::RequestObject::request()
-                                        .with_method(RPC_METHOD_CHAIN_HEAD_SUB)
-                                        .with_id(request_id.clone())
-                                        .finish(),
+                        Ok(rpc_call) => {
+                            async_std::task::spawn(async move {
+                                match rpc_ws_task::<DB, KS, B>(
+                                    rpc_call,
+                                    task_rpc_server,
+                                    task_socket_active,
+                                    task_ws_sender.clone(),
                                 )
-                                .await?;
-
-                                ws_sender
-                                    .lock()
-                                    .await
-                                    .send(Message::Text(subscription_response))
-                                    .await?;
-
-                                info!(
-                                    "RPC WS ChainNotify for subscription ID: {}",
-                                    subscription_id
-                                );
-
-                                let handler_rpc_server = rpc_server.clone();
-                                let handler_socket_active = socket_active.clone();
-                                let handler_ws_sender = ws_sender.clone();
-
-                                async_std::task::spawn(async move {
-                                    while handler_socket_active.load() {
-                                        let (_, event) = call_rpc::<SubscriptionHeadChange>(
-                                            handler_rpc_server.clone(),
-                                            jsonrpc_v2::RequestObject::request()
-                                                .with_method(RPC_METHOD_CHAIN_NOTIFY)
-                                                .with_id(subscription_id)
-                                                .finish(),
-                                        )
-                                        .await
-                                        .unwrap();
-
-                                        debug!("RPC WS ChainNotify event: {:?}", event);
-
-                                        let event_response = StreamingData {
-                                            json_rpc: "2.0",
-                                            method: "xrpc.ch.val",
-                                            params: event,
-                                        };
-
-                                        match handler_ws_sender
+                                .await
+                                {
+                                    Ok(_) => {
+                                        debug!("WS RPC task success.");
+                                    }
+                                    Err(e) => {
+                                        let msg = format!("WS RPC task error: {}", e);
+                                        error!("{}", msg);
+                                        task_ws_sender
                                             .lock()
                                             .await
-                                            .send(Message::Text(
-                                                serde_json::to_string(&event_response).unwrap(),
-                                            ))
+                                            .send(Message::Text(get_error_str(3, msg)))
                                             .await
-                                        {
-                                            Ok(_) => {
-                                                info!("New WS data sent.");
-                                            }
-                                            Err(msg) => {
-                                                warn!("WS connection closed. {:?}", msg);
-                                                handler_socket_active.store(false);
-                                            }
-                                        }
+                                            .unwrap();
                                     }
-                                });
-                            }
-                            _ => {
-                                info!("RPC WS called method: {}", call.method_ref());
-                                let response = call_rpc_str(rpc_server.clone(), call).await?;
-                                ws_sender.lock().await.send(Message::Text(response)).await?;
-                            }
-                        },
+                                }
+                            });
+                        }
                         Err(e) => {
-                            error!("Error deserializing WS request payload.");
-                            ws_sender
+                            let msg = format!("Error deserializing WS request payload: {}", e);
+                            error!("{}", msg);
+                            task_ws_sender
                                 .lock()
                                 .await
-                                .send(Message::Text(get_error_str(1, e.to_string())))
+                                .send(Message::Text(get_error_str(1, msg)))
                                 .await?;
                         }
                     }
                 }
             }
             Err(e) => {
-                error!("Error in WS socket stream. (Client possibly disconnected)");
+                let msg = format!(
+                    "Error in WS socket stream. (Client possibly disconnected): {}",
+                    e
+                );
+                error!("{}", msg);
                 ws_sender
                     .lock()
                     .await
-                    .send(Message::Text(get_error_str(2, e.to_string())))
+                    .send(Message::Text(get_error_str(2, msg)))
                     .await?;
             }
         }
