b0bb45caf81aa84c933b8bdd0da7fbb201c99bf6
connor mullett <connormullett@gmail.com>
2021-07-12 13:48:47 -0400

    validate FULLNODE_API_INFO is set and use value

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -4,3 +4,10 @@ if [ "$1" == "help" ]; then
     echo "Smoke Test\nRun forest node; set FULLNODE_API_INFO; smoke test CLI"
     exit
 fi
+
+API_MULTIADDR=$FULLNODE_API_INFO
+
+if [ -z "$API_MULTIADDR" ]; then
+    echo "FULLNODE_API_INFO is not set. Use $0 help for usage"
+    exit
+fi
