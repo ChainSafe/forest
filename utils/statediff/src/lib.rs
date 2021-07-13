5f01f9f612ebe32e4c34dcf0f072d06c8ae4b06c
eric tu <tu.eric@hotmail.com>
2021-07-13 14:36:08 -0400

    UNDO THIS COMMIT AS WELL

--- utils/statediff/src/lib.rs
+++ utils/statediff/src/lib.rs
@@ -13,6 +13,7 @@ use serde::{Deserialize, Serialize};
 use state_tree::StateTree;
 use std::collections::HashMap;
 use std::error::Error as StdError;
+use std::str::FromStr;
 use vm::ActorState;
 
 #[derive(Serialize, Deserialize)]
@@ -32,11 +33,11 @@ fn actor_to_resolved(
         resolve_cids_recursive(bs, &actor.state, depth).unwrap_or(Ipld::Link(actor.state));
     match resolved.clone() {
         Ipld::List(s) => {
-            let dlscid = match s[s.len() - 2]{
-                Ipld::Link(c) => c,
-                _ => {panic!("not a link we dun goofed");}
-            };
-            if s.len() >= 6 {
+            if actor.code == Cid::from_str("bafkqaetgnfwc6njpon2g64tbm5sw22lomvzas").unwrap() {
+                let dlscid = match s[s.len() - 3]{
+                    Ipld::Link(c) => c,
+                    _ => {panic!("not a link we dun goofed");}
+                };
                 let dls: forest_actor::miner::Deadlines = bs.get(&dlscid).unwrap().unwrap();
                 for dl in dls.due.iter() {
                     let d: forest_actor::miner::Deadline =
