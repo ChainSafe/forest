ac6269f87cf42961a4228e049bffc5cb95ea3a73
connor mullett <connormullett@gmail.com>
2021-07-13 14:09:41 -0400

    getting output; RPC returning 0 code and not hitting endpoint

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -7,11 +7,21 @@ fi
 
 ADDR=$FULLNODE_API_INFO
 
-if [ -z "$API_MULTIADDR" ]; then
+if [ -z "$ADDR" ]; then
     echo "FULLNODE_API_INFO is not set. Use $0 help for usage"
     exit
 fi
 
+SPLIT_ADDR=$(echo $ADDR | tr ":" "\n")
+TOKEN=$SPLIT_ADDR[0]
+
 echo "Smoke testing Wallet"
 
+# command
+# curl -s -X POST -H 'Content-Type: application/json-rpc' -H 'Authorization: Bearer <token>'
+# -d '{"jsonrpc": "2.0", "method":"Filecoin.<method>", "params":[], "id": 0}' http://127.0.0.1:1234/rpc/v0
 
+OUTPUT=$(curl -s -X POST -H 'Content-Type: application/json-rpc' -H 'Authorization: Bearer ${TOKEN}' -d '{"jsonrpc": "2.0", "method":"Filecoin.WalletDefault", "params":[], "id": 0}' http://127.0.0.1:1235/rpc/v0)
+
+echo $OUTPUT
+echo $?
