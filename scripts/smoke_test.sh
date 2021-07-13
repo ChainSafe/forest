762cb626e758d4031ca248649119fe03dc158700
connor mullett <connormullett@gmail.com>
2021-07-13 14:37:06 -0400

    attempted fixups

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -21,7 +21,7 @@ echo "Smoke testing Wallet"
 # curl -s -X POST -H 'Content-Type: application/json-rpc' -H 'Authorization: Bearer <token>'
 # -d '{"jsonrpc": "2.0", "method":"Filecoin.<method>", "params":[], "id": 0}' http://127.0.0.1:1234/rpc/v0
 
-OUTPUT=$(curl --write-out "WalletDefault %{http_code}\n" -s -X POST -H 'Content-Type: application/json-rpc' -H 'Authorization: Bearer ${TOKEN}' -d '{"jsonrpc": "2.0", "method":"Filecoin.WalletDefault", "params":[], "id": 0}' http://127.0.0.1:1235/rpc/v0)
+OUTPUT=$(curl --write-out "WalletDefault %{http_code}\n" -s -X POST -H 'Content-Type: application/json-rpc' -H 'Authorization: Bearer ${TOKEN}' -d '{"jsonrpc": "2.0", "method":"Filecoin.WalletList", "params":[], "id": 0}' http://127.0.0.1:1235/rpc/v0)
 
 echo $OUTPUT
 echo $?
