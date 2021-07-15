f14470e26490da773e202a4240a4f02927249482
connor mullett <connormullett@gmail.com>
2021-07-15 17:27:24 -0400

    color output and display results of tests

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -12,6 +12,10 @@ if [ -z "$ADDR" ]; then
     exit
 fi
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 TOKEN="$(cut -d':' -f1 <<< $ADDR)"
 
 AUTH_HEADER="Authorization: Bearer ${TOKEN}"
@@ -25,5 +29,10 @@ for endpoint in ${RPC_ENDPOINTS[@]}; do
 
     RESPONSE_CODE=$(curl -w "%{http_code}" -s -o /dev/null -X POST -H "$CONTENT_TYPE_HEADER" -H "$AUTH_HEADER" -d "$REQUEST_BODY" http://127.0.0.1:1235/rpc/v0)
 
-    echo $RESPONSE_CODE
+    if [ $RESPONSE_CODE = '200' ]; then
+        echo -e "${METHOD} ${GREEN}${RESPONSE_CODE}${NC}"
+    else
+        echo -e "${METHOD} ${RED}${RESPONSE_CODE}${NC}"
+    fi
+
 done
