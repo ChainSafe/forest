3490fee4ac26241955194cc963851799ef58e16f
connor mullett <connormullett@gmail.com>
2021-07-16 13:22:30 -0400

    add more endpoints

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -21,7 +21,36 @@ TOKEN="$(cut -d':' -f1 <<< $ADDR)"
 AUTH_HEADER="Authorization: Bearer ${TOKEN}"
 CONTENT_TYPE_HEADER="Content-Type: application/json-rpc"
 
-RPC_ENDPOINTS=("WalletList" "WalletBalance" "WalletDefaultAddress" "WalletExport" "WalletHas" "WalletImport" "WalletNew" "WalletSetDefault" "WalletSign" "WalletVerify")
+# Wallet
+RPC_ENDPOINTS=("WalletList" "WalletBalance" "WalletDefaultAddress" "WalletExport" "WalletHas" "WalletImport" "WalletNew")
+RPC_ENDPOINTS+=("WalletSetDefault" "WalletSign" "WalletVerify")
+
+# Sync
+RPC_ENDPOINTS+=("SyncCheckBad" "SyncMarkBad" "SyncState" "SyncSubmitBlock")
+
+# Message Pool
+RPC_ENDPOINTS+=("MpoolEstimateGasPrice" "MpoolGetNonce" "MpoolPending" "MpoolPush" "MpoolPushMessage" "MpoolSelect")
+
+# Chain
+RPC_ENDPOINTS+=("ChainGetMessage" "ChainReadObj" "ChainHasObj" "ChainGetBlockMessages" "ChainGetTipsetByHeight" "ChainGetGenesis")
+RPC_ENDPOINTS+=("ChainHead" "ChainHeadSubscription" "ChainNotify" "ChainTipSetWeight" "ChainGetBlock" "ChainGetTipSet")
+RPC_ENDPOINTS+=("ChainGetRandomnessFromTickets" "ChainGetRandomnessFromBeacon")
+
+# Auth
+RPC_ENDPOINTS+=("AuthNew" "AuthVerify")
+
+# Net
+RPC_ENDPOINTS+=("NetAddrsListen" "NetPeers" "NetConnect" "NetDisconnect")
+
+# Common
+RPC_ENDPOINTS+=("Version")
+
+# Gas
+RPC_ENDPOINTS+=("GasEstimateFeeCap" "GasEstimateGasPremium" "GasEstimateGasLimit" "GasEstimateMessageGas")
+
+# State
+# RPC_ENDPOINTS+=("")
+
 
 for endpoint in ${RPC_ENDPOINTS[@]}; do
     METHOD="Filecoin.${endpoint}"
