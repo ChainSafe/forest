166291c350aef2398e0031aa0c4c28f5a1bdcd17
creativcoder <creativcoders@gmail.com>
2021-07-20 11:16:46 +0000

    cleanup multisig actor

--- vm/state_migration/src/nv10/multisig.rs
+++ vm/state_migration/src/nv10/multisig.rs
@@ -1,58 +1,54 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use crate::{ActorMigration, ActorMigrationInput};
 use crate::nv10::util::migrate_hamt_raw;
 use crate::MigrationError;
 use crate::MigrationOutput;
 use crate::MigrationResult;
-use actor_interface::actorv2::multisig::State as V2MultiSigState;
-use actor_interface::actorv3::multisig::State as V3MultiSigState;
+use crate::{ActorMigration, ActorMigrationInput};
 use actor::multisig::Transaction;
+use actor_interface::actorv2::multisig::State as V2_MultiSigState;
+use actor_interface::actorv3;
+use actor_interface::actorv3::multisig::{State as V3_MultiSigState, TxnID as V3_TxnID};
 use async_std::sync::Arc;
 use cid::Code;
-use ipld_blockstore::BlockStore;
-
-use actor_interface::actorv3;
-use fil_types::ActorID;
 use fil_types::HAMT_BIT_WIDTH;
+use ipld_blockstore::BlockStore;
 
 pub struct MultisigMigrator;
 
-// each actor's state migration is read from blockstore, changes state tree, and writes back to the blocstore.
 impl<BS: BlockStore + Send + Sync> ActorMigration<BS> for MultisigMigrator {
     fn migrate_state(
         &self,
         store: Arc<BS>,
         input: ActorMigrationInput,
     ) -> MigrationResult<MigrationOutput> {
-        let v2_in_state: Option<V2MultiSigState> = store
+        let v2_in_state: V2_MultiSigState = store
             .get(&input.head)
-            .map_err(|e| MigrationError::BlockStoreRead(e.to_string()))?;
-
-        let v2_in_state = v2_in_state.unwrap();
+            .map_err(|e| MigrationError::BlockStoreRead(e.to_string()))?
+            .ok_or(MigrationError::StateNotFound)?;
 
         // HAMT<addr.Address, Transaction>
         let pending_txns_out =
             migrate_hamt_raw::<_, Transaction>(&*store, v2_in_state.pending_txs, HAMT_BIT_WIDTH)?;
 
-        let out_state = V2MultiSigState { // FIXME This should be v3. Getting an error about incompatible `next_tx_id` on using V3State.
+        let out_state = V3_MultiSigState {
             signers: v2_in_state.signers,
             num_approvals_threshold: v2_in_state.num_approvals_threshold,
-            next_tx_id: v2_in_state.next_tx_id,
-            
+            next_tx_id: V3_TxnID(v2_in_state.next_tx_id.0),
             initial_balance: v2_in_state.initial_balance,
             start_epoch: v2_in_state.start_epoch,
             unlock_duration: v2_in_state.unlock_duration,
-        
             pending_txs: pending_txns_out,
         };
 
-        let new_head = store.put(&out_state, Code::Blake2b256);
+        let new_head = store
+            .put(&out_state, Code::Blake2b256)
+            .map_err(|e| MigrationError::BlockStoreWrite(e.to_string()))?;
 
         Ok(MigrationOutput {
             new_code_cid: *actorv3::MULTISIG_ACTOR_CODE_ID,
-            new_head: new_head.unwrap(),
+            new_head,
         })
     }
 }
