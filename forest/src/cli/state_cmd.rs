f17274711db1b2aca1c0423fac1e9cf98cdcacde
connormullett <connormullett@gmail.com>
2021-08-05 19:03:46 -0400

    porting lotus -> forest

--- forest/src/cli/state_cmd.rs
+++ forest/src/cli/state_cmd.rs
@@ -1,7 +1,13 @@
 // Copyright 2020 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use std::str::FromStr;
+
+use address::Address;
+use rpc_client::{chain_head, state_miner_info};
 use structopt::StructOpt;
+
+use super::handle_rpc_err;
 
 #[derive(Debug, StructOpt)]
 pub enum StateCommands {
@@ -20,7 +26,15 @@ impl StateCommands {
             Self::Power { miner_address } => {
                 let miner_address = miner_address.to_owned();
 
-                println!("{:?}", miner_address);
+                match miner_address {
+                    Some(miner_addr) => {
+                        let address = Address::from_str(&miner_addr);
+                        let tipset = chain_head().await.map_err(handle_rpc_err).unwrap();
+
+                        let actor_state = state_get_actor(address, tipset);
+                    }
+                    None => {}
+                }
             }
             Self::ListMiners => {}
             Self::ListActors => {}
