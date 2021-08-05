97f1d9abe756545fd8a0c798549699759d374e71
connormullett <connormullett@gmail.com>
2021-08-05 19:20:39 -0400

    add state_ops

---
+++ node/rpc-client/src/state_ops.rs
@@ -0,0 +1,10 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use crate::call;
+use jsonrpc_v2::Error;
+use rpc_api::state_api::*;
+
+pub async fn state_get_actor(params: StateGetActorParams) -> Result<StateGetActorResult, Error> {
+    call(STATE_GET_ACTOR, params).await
+}
