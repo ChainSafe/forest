41a55a319c5204de67b837337fa1b3c06eba1ae2
connor mullett <connormullett@gmail.com>
2021-08-23 14:48:29 -0400

    fix conflicts and merge main

--- node/rpc-client/src/client.rs
+++ node/rpc-client/src/client.rs
@@ -171,38 +171,3 @@ where
 
     call(rpc_req).await.map_err(|e| e)
 }
-
-/// Filecoin RPC client interface methods
-pub mod filecoin_rpc {
-    use jsonrpc_v2::Error;
-
-    use crate::call_params;
-    use rpc_api::{auth_api::*, chain_api::*, mpool_api::*};
-
-    /// Auth
-    pub async fn auth_new(perm: AuthNewParams) -> Result<AuthNewResult, Error> {
-        call_params(AUTH_NEW, perm).await
-    }
-
-    pub async fn chain_get_block(cid: ChainGetBlockParams) -> Result<ChainGetBlockResult, Error> {
-        call_params(CHAIN_GET_BLOCK, cid).await
-    }
-
-    pub async fn chain_get_genesis() -> Result<ChainGetGenesisResult, Error> {
-        call_params(CHAIN_GET_GENESIS, ()).await
-    }
-
-    pub async fn chain_head() -> Result<ChainHeadResult, Error> {
-        call_params(CHAIN_HEAD, ()).await
-    }
-
-    pub async fn chain_get_message(
-        cid: ChainGetMessageParams,
-    ) -> Result<ChainGetMessageResult, Error> {
-        call_params(CHAIN_GET_MESSAGE, cid).await
-    }
-
-    pub async fn chain_read_obj(cid: ChainReadObjParams) -> Result<ChainReadObjResult, Error> {
-        call_params(CHAIN_READ_OBJ, cid).await
-    }
-}
