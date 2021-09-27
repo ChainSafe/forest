6065cb1999ec3bcd083bd0d92a61fbf9c2bcbaf0
Afr Schoe <58883403+q9f@users.noreply.github.com>
2021-09-27 18:06:40 +0200

    forest: add build script to generate version string with git hash

---
+++ forest/build.rs
@@ -0,0 +1,27 @@
+// Copyright 2019-2021 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use std::process::Command;
+
+// we reserve this space once we start releasing
+const RELEASE_TRACK: &'static str = "unstable";
+
+fn main(){
+  // expose environment variable FOREST_VERSON at build time
+  println!("cargo:rustc-env=FOREST_VERSION={}", version());
+}
+
+// returns version string at build time, e.g., `v0.1.0/unstable/7af2f5bf`
+fn version() -> String {
+  let git_cmd = Command::new("git")
+    .args(&["rev-parse", "--short", "HEAD"])
+    .output()
+    .expect("Git references should be available on a build system");
+  let git_hash = String::from_utf8(git_cmd.stdout).unwrap_or("".to_string());
+  format!(
+    "v{}/{}/{}",
+    env!("CARGO_PKG_VERSION"),
+    RELEASE_TRACK,
+    git_hash
+  )
+}
