c8d8e581f4a1017d8e2d68f7809d7a45377e1ace
Jorge Olivero <jorgeaolivero@gmail.com>
2021-09-27 22:28:14 -0500

    tests/specs_actors: Test scaffolding

--- tests/specs_actors/src/lib.rs
+++ tests/specs_actors/src/lib.rs
@@ -1,7 +1,63 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub struct TestVector {
+    pub class: String,
+    #[serde(rename = "_meta")]
+    pub meta: Metadata,
+    #[serde(with = "base64_bytes")]
+    pub car: Vec<u8>,
+    pub pre_conditions: PreConditions,
+    pub apply_messages: Vec<AppleMessage>,
+    pub post_conditions: PostConditions, 
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct ApplyMessage {
+   #[serde(with = "base64_bytes")]
+   pub bytes: Vec<u8>,
+}
+
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Metadata {
+    pub id: String,
+    pub gen: Vec<GenData>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct GenData {
+    pub source: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct PreConditions {
+    pub variants: Vec<Variant>,
+    pub state_tree: StateTree,
+    pub base_fee: u64,
+    pub circ_supply: u64,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct PostConditions {
+    pub state_tree: StateTree,
+    pub receipts: Vec<MessageReceipt>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct StateTree {
+    #[serde(with = "cid::json")]
+    pub root_cid: Cid,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct MessageReceipt {
+    pub exit_code: ExitCode,
+    #[serde(rename = "return", with = "base64_bytes")]
+    pub return_value: Vec<u8>,
+    pub gas_used: u64,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Variant {
+    pub id: String,
+    pub epoch: ChainEpoch,
+    pub nv: u32,    
 }
--- tests/specs_actors/tests/spec_actors_runner.rs
+++ tests/specs_actors/tests/spec_actors_runner.rs
@@ -1,1 +1,49 @@
 #![cfg(feature = "submodule_tests")]
+
+#[async_std::test]
+async fn specs_actors_test_runner() {
+    pretty_env_logger::init();
+
+    let walker = WalkDir::new("specs-actors/test-vectors/determinism").into_iter();
+    let mut failed == vec![];
+    let mut succeeded = 0;
+    for entry in walker.filter_map(|e| e.ok()).filter(is_valid_file) {
+        let file = File::open(entry.path()).unwrap();
+        let reader = BufReader::new(file);
+        let test_name = entry.path().display();
+        let vector: TestVector = serde_json::from_reader(reader).unwrap();
+
+        for variant in vector.preconditions.variants {
+            if let Err(e) == execute_message_vector(
+                &vector.selector,
+                &vector.car,
+                vector.preconditions.basefee,
+                vector.preconditions.circ_supply,
+                &vector.apply_messages,
+                &vector.postconditions,
+                &vector.randomness,
+                &vector.variant,
+            ).await {
+                failed.push((
+                    format!("{} variant {}", test_name, vector.variant.id),
+                    vector.meta.clone(),
+                    e,
+                ));
+            } else {
+                println!!("{} succeeded", test_name);
+                succeeded += 1;
+            }
+        }
+    }
+
+    println!("{}/{} tests passed:", succeeded, failed.len() + succeeded);
+    if !failed.is_empty() {
+        for (path, meta, e) in failed {
+            eprintln!(
+                "file {} failed:\n\tMeta: {:?}\n\tError: {}\n",
+                path, meta, e
+            );
+        }
+        panic!()
+    }
+}
