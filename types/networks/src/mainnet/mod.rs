e72f70e9d0905ceedd85c611594e78174aaf239a
Jorge Olivero <jorgeaolivero@gmail.com>
2021-10-19 14:59:18 -0500

    types: Remove edits to upgrade heights

--- types/networks/src/mainnet/mod.rs
+++ types/networks/src/mainnet/mod.rs
@@ -12,37 +12,37 @@ use fil_types::NetworkVersion;
 pub const DEFAULT_GENESIS: &[u8] = include_bytes!("genesis.car");
 
 /// V1 network upgrade
-pub const UPGRADE_BREEZE_HEIGHT: ChainEpoch = -13;
+pub const UPGRADE_BREEZE_HEIGHT: ChainEpoch = 41280;
 /// V2 network upgrade
-pub const UPGRADE_SMOKE_HEIGHT: ChainEpoch = -12;
+pub const UPGRADE_SMOKE_HEIGHT: ChainEpoch = 51000;
 /// V3 network upgrade
-pub const UPGRADE_IGNITION_HEIGHT: ChainEpoch = -11;
+pub const UPGRADE_IGNITION_HEIGHT: ChainEpoch = 94000;
 /// V4 network upgrade
-pub const UPGRADE_ACTORS_V2_HEIGHT: ChainEpoch = -10;
+pub const UPGRADE_ACTORS_V2_HEIGHT: ChainEpoch = 138720;
 /// V5 network upgrade
-pub const UPGRADE_TAPE_HEIGHT: ChainEpoch = -9;
+pub const UPGRADE_TAPE_HEIGHT: ChainEpoch = 140760;
 /// Switching to mainnet network name
-pub const UPGRADE_LIFTOFF_HEIGHT: i64 = -8;
+pub const UPGRADE_LIFTOFF_HEIGHT: i64 = 148888;
 /// V6 network upgrade
-pub const UPGRADE_KUMQUAT_HEIGHT: ChainEpoch = -7;
+pub const UPGRADE_KUMQUAT_HEIGHT: ChainEpoch = 170000;
 /// V7 network upgrade
-pub const UPGRADE_CALICO_HEIGHT: ChainEpoch = -6;
+pub const UPGRADE_CALICO_HEIGHT: ChainEpoch = 256200;
 /// V8 network upgrade
-pub const UPGRADE_PERSIAN_HEIGHT: ChainEpoch = -5;
+pub const UPGRADE_PERSIAN_HEIGHT: ChainEpoch = 272400;
 /// V9 network upgrade
-pub const UPGRADE_ORANGE_HEIGHT: ChainEpoch = -4;
+pub const UPGRADE_ORANGE_HEIGHT: ChainEpoch = 336458;
 /// Remove burn on window PoSt fork
-pub const UPGRADE_CLAUS_HEIGHT: ChainEpoch = -3;
+pub const UPGRADE_CLAUS_HEIGHT: ChainEpoch = 343200;
 /// V10 network upgrade
-pub const UPGRADE_ACTORS_V3_HEIGHT: ChainEpoch = -2;
+pub const UPGRADE_ACTORS_V3_HEIGHT: ChainEpoch = 550321;
 /// V11 network upgrade
-pub const UPGRADE_NORWEGIAN_HEIGHT: ChainEpoch = -100;
+pub const UPGRADE_NORWEGIAN_HEIGHT: ChainEpoch = 665200;
 /// V12 network upgrade
-pub const UPGRADE_ACTORS_V4_HEIGHT: ChainEpoch = -100;
+pub const UPGRADE_ACTORS_V4_HEIGHT: ChainEpoch = 712320;
 /// V13 network upgrade
-pub const UPGRADE_HYPERDRIVE_HEIGHT: ChainEpoch = -1;
+pub const UPGRADE_HYPERDRIVE_HEIGHT: ChainEpoch = 892800;
 
-pub const UPGRADE_PLACEHOLDER_HEIGHT: ChainEpoch = 3;
+pub const UPGRADE_PLACEHOLDER_HEIGHT: ChainEpoch = 9999999;
 
 /// Current network version for the network
 pub const NEWEST_NETWORK_VERSION: NetworkVersion = NetworkVersion::V11;
