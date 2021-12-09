02599040a6c2bd297115bed66b0cfd6122b3a631
noot <elizabethjbinks@gmail.com>
2021-12-09 12:00:41 -0500

    change SysErrIllegalArgument to ErrIllegalArgument

--- vm/interpreter/src/default_runtime.rs
+++ vm/interpreter/src/default_runtime.rs
@@ -507,7 +507,9 @@ where
         self.charge_gas(self.price_list().on_create_actor())?;
 
         if addr.is_bls_zero_address() && self.network_version() >= NetworkVersion::V10 {
-            return Err(actor_error!(SysErrIllegalArgument; "cannot create the bls zero address actor"));
+            return Err(
+                actor_error!(ErrIllegalArgument; "cannot create the bls zero address actor"),
+            );
         }
 
         let addr_id = (*self.state)
