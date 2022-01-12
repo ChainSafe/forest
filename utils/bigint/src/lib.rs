ae1600b878292fa0f668ac7fc3e24800124f18a5
elmattic <elmattic@users.noreply.github.com>
2022-01-12 16:32:28 +0100

    Fix encoding size constraints for BigInt and BigUint not enforced

--- utils/bigint/src/lib.rs
+++ utils/bigint/src/lib.rs
@@ -6,3 +6,7 @@ pub mod biguint_ser;
 
 pub use num_bigint::*;
 pub use num_integer::{self, Integer};
+
+/// MAX_ENCODED_SIZE is the max length of a byte slice representing a
+/// CBOR serialized BigInt or BigUint.
+const MAX_ENCODED_SIZE: usize = 128;
