207bde99ed2f1f3f3466da23e7850c6314a256bf
David Himmelstrup <lemmih@gmail.com>
2022-01-14 15:57:49 +0000

    Use pre-made action to deploy documentation to gh-pages. (#1380)

--- scripts/build-rust-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd documentation/book
-git init
-git add .
-git config --global -l
-git -c user.name='ci' -c user.email='ci' commit -m 'Deploy documentation'
-git push -f -q https://git:${GITHUB_TOKEN}@github.com/ChainSafe/forest HEAD:gh-pages
