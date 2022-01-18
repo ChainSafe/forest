6a8b2de7a86cc8d6efc857380fb940e153a5a151
c r <c@laudiacay.cool>
2022-01-18 11:10:33 -0600

    B4 fix: fixing by adding max index computation in bitfield validation (#1344)

--- vm/actor/src/builtin/miner/mod.rs
+++ vm/actor/src/builtin/miner/mod.rs
@@ -2508,14 +2508,16 @@ impl Actor {
             .validate()
             .map_err(|e| actor_error!(ErrIllegalArgument, "invalid mask bitfield: {}", e))?;
 
-        let last_sector_number = mask_sector_numbers
-            .iter()
-            .last()
-            .ok_or_else(|| actor_error!(ErrIllegalArgument, "invalid mask bitfield"))?
-            as SectorNumber;
+        let last_sector_number = mask_sector_numbers.last().map_err(|e| {
+            actor_error!(
+                ErrIllegalArgument,
+                "invalid mask bitfield, no sectors set: {}",
+                e
+            )
+        })?;
 
         #[allow(clippy::absurd_extreme_comparisons)]
-        if last_sector_number > MAX_SECTOR_NUMBER {
+        if (last_sector_number as u64) > MAX_SECTOR_NUMBER {
             return Err(actor_error!(
                 ErrIllegalArgument,
                 "masked sector number {} exceeded max sector number",
