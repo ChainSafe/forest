ae2935bbb7d4cd10adc24a7af2f57708f69ec33a
David Himmelstrup <lemmih@gmail.com>
2022-01-26 16:00:48 +0000

    Include network in forest version string. (#1401)

--- forest/build.rs
+++ forest/build.rs
@@ -9,12 +9,21 @@ const RELEASE_TRACK: &str = "unstable";
 #[cfg(feature = "release")]
 const RELEASE_TRACK: &str = "alpha";
 
+#[cfg(feature = "devnet")]
+const NETWORK: &str = "devnet";
+
+#[cfg(feature = "interopnet")]
+const NETWORK: &str = "interopnet";
+
+#[cfg(all(not(feature = "devnet"), not(feature = "interopnet")))]
+const NETWORK: &str = "mainnet";
+
 fn main() {
     // expose environment variable FOREST_VERSON at build time
     println!("cargo:rustc-env=FOREST_VERSION={}", version());
 }
 
-// returns version string at build time, e.g., `v0.1.0/unstable/7af2f5bf`
+// returns version string at build time, e.g., `v0.1.0/unstable/mainnet/7af2f5bf`
 fn version() -> String {
     let git_cmd = Command::new("git")
         .args(&["rev-parse", "--short", "HEAD"])
@@ -22,9 +31,10 @@ fn version() -> String {
         .expect("Git references should be available on a build system");
     let git_hash = String::from_utf8(git_cmd.stdout).unwrap_or_default();
     format!(
-        "v{}/{}/{}",
+        "v{}/{}/{}/{}",
         env!("CARGO_PKG_VERSION"),
         RELEASE_TRACK,
-        git_hash
+        NETWORK,
+        git_hash,
     )
 }
