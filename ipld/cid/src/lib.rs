573d63062b4b1d01e94f4d4e550ba93965791382
willesxm <willeslau@gmail.com>
2022-02-01 10:37:30 +0800

    match lotus implementation

--- ipld/cid/src/lib.rs
+++ ipld/cid/src/lib.rs
@@ -122,16 +122,25 @@ impl<'de> de::Deserialize<'de> for Cid {
             Some(CBOR_TAG_CID) | None => {
                 let mut bz = tagged.value.into_vec();
 
-                if bz.first() != Some(&MULTIBASE_IDENTITY) {
-                    Err(de::Error::custom(
-                        "cbor serialized CIDs must have binary multibase",
-                    ))
-                } else {
-                    bz.remove(0);
-                    Ok(Cid::try_from(bz).map_err(|e| {
-                        de::Error::custom(format!("Failed to deserialize Cid: {}", e))
-                    })?)
+                if bz.len() == 0 {
+                    return Err(de::Error::custom("undefined cid"));
                 }
+
+                if bz.len() < 2 {
+                    return Err(de::Error::custom(
+                        "cbor serialized CIDs must have at least two bytes",
+                    ));
+                }
+
+                if bz.first() != Some(&MULTIBASE_IDENTITY) {
+                    return Err(de::Error::custom(
+                        "cbor serialized CIDs must have binary multibase",
+                    ));
+                }
+
+                bz.remove(0);
+                Ok(Cid::try_from(bz)
+                    .map_err(|e| de::Error::custom(format!("Failed to deserialize Cid: {}", e)))?)
             }
             Some(_) => Err(de::Error::custom("unexpected tag")),
         }
