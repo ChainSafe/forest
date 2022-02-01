8e807a6bde85ac02e1374970be8186041f131987
David Himmelstrup <lemmih@gmail.com>
2022-02-01 13:55:55 +0100

    Update isahc (http-library) dependency. The new code makes it more clear that we're blocking in async code.

--- utils/net_utils/src/download.rs
+++ utils/net_utils/src/download.rs
@@ -3,8 +3,9 @@
 
 use async_std::fs::File;
 use async_std::io::BufReader;
+use async_std::task;
 use futures::prelude::*;
-use isahc::{Body, HttpClient};
+use isahc::{AsyncBody, HttpClient};
 use pbr::{ProgressBar, Units};
 use pin_project_lite::pin_project;
 use std::convert::TryFrom;
@@ -49,7 +50,7 @@ impl<R: AsyncRead + Unpin, W: Write> AsyncRead for FetchProgress<R, W> {
     }
 }
 
-impl TryFrom<Url> for FetchProgress<Body, Stdout> {
+impl TryFrom<Url> for FetchProgress<AsyncBody, Stdout> {
     type Error = Box<dyn std::error::Error>;
 
     fn try_from(url: Url) -> Result<Self, Self::Error> {
@@ -67,7 +68,7 @@ impl TryFrom<Url> for FetchProgress<Body, Stdout> {
             }
         };
 
-        let request = client.get(url.as_str())?;
+        let request = task::block_on(client.get_async(url.as_str()))?;
 
         let mut pb = ProgressBar::new(total_size);
         pb.set_units(Units::Bytes);
