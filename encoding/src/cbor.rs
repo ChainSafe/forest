39bffcaaec5ed02d632335896496d7cf82ef9931
Hubert Bugaj <hubert@chainsafe.io>
2022-02-11 15:12:06 +0100

    fix warnings in encoding/

--- encoding/src/cbor.rs
+++ encoding/src/cbor.rs
@@ -1,8 +1,4 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use super::errors::Error;
-use crate::{de::DeserializeOwned, from_slice, ser::Serialize, to_vec};
-use cid::{Cid, Code::Blake2b256};
-
 pub use fvm_shared::encoding::Cbor;
--- encoding/src/errors.rs
+++ encoding/src/errors.rs
@@ -1,10 +1,4 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use cid::Error as CidError;
-use serde_cbor::error::Error as CborError;
-use std::fmt;
-use std::io;
-use thiserror::Error;
-
 pub use fvm_shared::encoding::Error;
