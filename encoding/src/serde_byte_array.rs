fb1716ae1f93a9915ad836ac2af1a1a56cc019eb
clearloop <mercuryfletcher@gmail.com>
2022-02-15 19:36:13 +0800

    chore(serde_byte_array): rename checked_serde_bytes serde_byte_array

--- encoding/src/serde_byte_array.rs
+++ encoding/src/serde_byte_array.rs
@@ -5,42 +5,36 @@ use crate::BYTE_ARRAY_MAX_LEN;
 use serde::{de, ser, Deserializer, Serializer};
 use serde_bytes::{Deserialize, Serialize};
 
-/// serde_bytes with max length check
-pub mod serde_byte_array {
-    use super::*;
+/// checked if input > `crate::BYTE_ARRAY_MAX_LEN`
+pub fn serialize<T, S>(bytes: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: ?Sized + Serialize + AsRef<[u8]>,
+    S: Serializer,
+{
+    if bytes.as_ref().len() > BYTE_ARRAY_MAX_LEN {
+        return Err(ser::Error::custom::<String>(
+            "Array exceed max length".into(),
+        ));
+    }
 
-    /// checked if input > `crate::BYTE_ARRAY_MAX_LEN`
-    pub fn serialize<T, S>(bytes: &T, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        T: ?Sized + Serialize + AsRef<[u8]>,
-        S: Serializer,
-    {
-        let len = bytes.as_ref().len();
-        if len > BYTE_ARRAY_MAX_LEN {
-            return Err(ser::Error::custom::<String>(
+    Serialize::serialize(bytes, serializer)
+}
+
+/// checked if output > `crate::ByteArrayMaxLen`
+pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Deserialize<'de> + AsRef<[u8]>,
+    D: Deserializer<'de>,
+{
+    Deserialize::deserialize(deserializer).and_then(|bytes: T| {
+        if bytes.as_ref().len() > BYTE_ARRAY_MAX_LEN {
+            Err(de::Error::custom::<String>(
                 "Array exceed max length".into(),
-            ));
+            ))
+        } else {
+            Ok(bytes)
         }
-
-        Serialize::serialize(bytes, serializer)
-    }
-
-    /// checked if output > `crate::ByteArrayMaxLen`
-    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
-    where
-        T: Deserialize<'de> + AsRef<[u8]>,
-        D: Deserializer<'de>,
-    {
-        Deserialize::deserialize(deserializer).and_then(|bytes: T| {
-            if bytes.as_ref().len() > BYTE_ARRAY_MAX_LEN {
-                Err(de::Error::custom::<String>(
-                    "Array exceed max length".into(),
-                ))
-            } else {
-                Ok(bytes)
-            }
-        })
-    }
+    })
 }
 
 #[cfg(test)]
