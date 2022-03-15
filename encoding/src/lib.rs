2947ec629aa1f81e47be59c20992305efbf4038a
David Himmelstrup <david.himmelstrup@chainsafe.io>
2022-03-15 10:44:47 +0100

    Remove unused import.

--- encoding/src/lib.rs
+++ encoding/src/lib.rs
@@ -7,7 +7,6 @@ mod hash;
 
 pub use serde::{de, ser};
 pub use serde_bytes;
-//pub use serde_cbor::{error, from_reader, from_slice, tags, to_vec, to_writer};
 pub use serde_ipld_dagcbor::error;
 
 pub use self::bytes::*;
