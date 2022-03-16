84579fabbe97c30d2eff14ecd55ad27b36cfdab6
David Himmelstrup <david.himmelstrup@chainsafe.io>
2022-03-16 16:03:53 +0100

    Remove unused async CBOR encoder.

--- node/forest_libp2p/src/rpc/cbor_codec.rs
+++ node/forest_libp2p/src/rpc/cbor_codec.rs
@@ -3,21 +3,17 @@
 // Code originally from futures_cbor_codec. License: Apache-2.0/MIT
 
 use {
-    asynchronous_codec::{Decoder as IoDecoder, Encoder as IoEncoder},
+    asynchronous_codec::Decoder as IoDecoder,
     bytes::{Buf, BytesMut},
-    serde::{Deserialize, Serialize},
-    serde_ipld_dagcbor::de::{Deserializer, IoRead},
-    // serde_cbor::ser::{IoWrite, Serializer},
-    // serde_cbor::{
-    //     de::{Deserializer, IoRead},
-    //     error::Error as CborError,
-    // },
-    serde_ipld_dagcbor::ser::{IoWrite, Serializer},
-    serde_ipld_dagcbor::Error as CborError,
+    serde::Deserialize,
+    serde_ipld_dagcbor::{
+        de::{Deserializer, IoRead},
+        Error as CborError,
+    },
     std::{
         error::Error as ErrorTrait,
         fmt::{Display, Formatter, Result as FmtResult},
-        io::{Error as IoError, Read, Result as IoResult, Write},
+        io::{Error as IoError, Read, Result as IoResult},
         marker::PhantomData,
     },
 };
@@ -154,182 +150,6 @@ impl<'de, Item: Deserialize<'de>> IoDecoder for Decoder<Item> {
     }
 }
 
-/// Describes the behaviour of self-describe tags.
-///
-/// CBOR defines a tag which can be used to recognize a document as being CBOR (it's sometimes
-/// called „magic“). This specifies if it should be present when encoding.
-//
-#[derive(Clone, Debug, Eq, PartialEq)]
-//
-pub enum SdMode {
-    /// Places the tag in front of each encoded frame.
-    _Always,
-    /// Places the tag in front of the first encoded frame.
-    Once,
-    /// Doesn't place the tag at all.
-    Never,
-}
-
-/// CBOR based encoder.
-///
-/// This encoder can be used with `future_codec`'s `FramedWrite` to encode CBOR frames. Anything
-/// that is `serde`s `Serialize` can be encoded this way (at least in theory, some values return
-/// errors when attempted to serialize).
-//
-#[derive(Clone, Debug)]
-//
-pub struct Encoder<Item> {
-    _data: PhantomData<fn(Item)>,
-    sd: SdMode,
-    packed: bool,
-}
-
-impl<Item: Serialize> Encoder<Item> {
-    /// Creates a new encoder.
-    ///
-    /// By default, it doesn't do packed encoding (it includes struct field names) and it doesn't
-    /// prefix the frames with self-describe tag.
-    //
-    pub fn new() -> Self {
-        Self {
-            _data: PhantomData,
-            sd: SdMode::Never,
-            packed: false,
-        }
-    }
-
-    /// Turns the encoder into one with configured self-describe behaviour.
-    //
-    pub fn _sd(self, sd: SdMode) -> Self {
-        Self { sd, ..self }
-    }
-
-    /// Turns the encoder into one with configured packed encoding.
-    ///
-    /// If `packed` is true, it omits the field names from the encoded data. That makes it smaller,
-    /// but it also means the decoding end must know the exact order of fields and it can't be
-    /// something like python, which would want to get a dictionary out of it.
-    //
-    pub fn _packed(self, packed: bool) -> Self {
-        Self { packed, ..self }
-    }
-}
-
-impl<Item: Serialize> Default for Encoder<Item> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-/// The Cbor serializer wants a writer, we provide one by wrapping `BytesMut`.
-///
-/// As of writing this code, `BytesMut` doesn't know how to be a writer itself. This may change,
-/// there's an open issue for it: https://github.com/carllerche/bytes/issues/77.
-//
-struct BytesWriter<'a>(&'a mut BytesMut);
-
-impl Write for BytesWriter<'_> {
-    fn write(&mut self, buf: &[u8]) -> IoResult<usize> {
-        self.0.extend(buf);
-        Ok(buf.len())
-    }
-
-    fn flush(&mut self) -> IoResult<()> {
-        Ok(())
-    }
-}
-
-impl<Item: Serialize> IoEncoder for Encoder<Item> {
-    type Item = Item;
-    type Error = Error;
-
-    fn encode(&mut self, item: Item, dst: &mut BytesMut) -> Result<(), Error> {
-        let writer = BytesWriter(dst);
-
-        let mut serializer = if self.packed {
-            Serializer::new(IoWrite::new(writer)).packed_format()
-        } else {
-            Serializer::new(IoWrite::new(writer))
-        };
-
-        if self.sd != SdMode::Never {
-            serializer.self_describe()?;
-        }
-
-        if self.sd == SdMode::Once {
-            self.sd = SdMode::Never;
-        }
-
-        item.serialize(&mut serializer).map_err(Into::into)
-    }
-}
-
-/// Cbor serializer and deserializer.
-///
-/// This is just a combined [`Decoder`](struct.Decoder.html) and [`Encoder`](struct.Encoder.html).
-//
-#[derive(Clone, Debug)]
-//
-pub struct Codec<Dec, Enc> {
-    dec: Decoder<Dec>,
-    enc: Encoder<Enc>,
-}
-
-impl<'de, Dec: Deserialize<'de>, Enc: Serialize> Codec<Dec, Enc> {
-    /// Creates a new codec
-    //
-    pub fn new() -> Self {
-        Self {
-            dec: Decoder::new(),
-            enc: Encoder::new(),
-        }
-    }
-
-    /// Turns the internal encoder into one with confifured self-describe behaviour.
-    //
-    pub fn _sd(self, sd: SdMode) -> Self {
-        Self {
-            dec: self.dec,
-            enc: Encoder { sd, ..self.enc },
-        }
-    }
-
-    /// Turns the internal encoder into one with configured packed encoding.
-    ///
-    /// If `packed` is true, it omits the field names from the encoded data. That makes it smaller,
-    /// but it also means the decoding end must know the exact order of fields and it can't be
-    /// something like python, which would want to get a dictionary out of it.
-    //
-    pub fn _packed(self, packed: bool) -> Self {
-        Self {
-            dec: self.dec,
-            enc: Encoder { packed, ..self.enc },
-        }
-    }
-}
-
-impl<'de, Dec: Deserialize<'de>, Enc: Serialize> Default for Codec<Dec, Enc> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<'de, Dec: Deserialize<'de>, Enc: Serialize> IoDecoder for Codec<Dec, Enc> {
-    type Item = Dec;
-    type Error = Error;
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Dec>, Error> {
-        self.dec.decode(src)
-    }
-}
-
-impl<'de, Dec: Deserialize<'de>, Enc: Serialize> IoEncoder for Codec<Dec, Enc> {
-    type Item = Enc;
-    type Error = Error;
-    fn encode(&mut self, item: Enc, dst: &mut BytesMut) -> Result<(), Error> {
-        self.enc.encode(item, dst)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
@@ -386,76 +206,5 @@ mod tests {
     fn decode_only() {
         let decoder = Decoder::new();
         decode(decoder);
-    }
-
-    /// Run the decoding tests on the combined codec.
-    #[test]
-    fn decode_codec() {
-        let decoder: Codec<_, ()> = Codec::new();
-        decode(decoder);
-    }
-
-    /// Test encoding.
-    fn encode<Enc: IoEncoder<Item = TestData, Error = Error>>(enc: Enc) {
-        let mut encoder = enc;
-        let data = test_data();
-        let mut buffer = BytesMut::with_capacity(0);
-        encoder.encode(data.clone(), &mut buffer).unwrap();
-        let pos1 = buffer.len();
-        let decoded = serde_ipld_dagcbor::from_slice::<TestData>(&buffer).unwrap();
-        assert_eq!(data, decoded);
-        // Once more, this time without the self-describe (should be smaller)
-        encoder.encode(data.clone(), &mut buffer).unwrap();
-        let pos2 = buffer.len();
-        // More data arrived
-        assert!(pos2 > pos1);
-        // But not as much as twice as many
-        assert!(pos1 * 2 > pos2);
-        // We can still decode it
-        let decoded = serde_ipld_dagcbor::from_slice::<TestData>(&buffer[pos1..]).unwrap();
-        assert_eq!(data, decoded);
-        // Encoding once more the size stays the same
-        encoder.encode(data, &mut buffer).unwrap();
-        let pos3 = buffer.len();
-        assert_eq!(pos2 - pos1, pos3 - pos2);
-    }
-
-    /// Test encoding by the lone encoder.
-    #[test]
-    fn encode_only() {
-        let encoder = Encoder::new()._sd(SdMode::Once);
-        encode(encoder);
-    }
-
-    /// The same as `encode_only`, but with packed encoding.
-    #[test]
-    fn encode_packed() {
-        let encoder = Encoder::new()._packed(true)._sd(SdMode::Once);
-        encode(encoder);
-    }
-
-    /// Encoding with the combined `Codec`
-    #[test]
-    fn encode_codec() {
-        let encoder: Codec<(), _> = Codec::new()._sd(SdMode::Once);
-        encode(encoder);
-    }
-
-    /// Checks that the codec can be send
-    #[test]
-    fn is_send() {
-        let codec: Codec<(), ()> = Codec::new();
-        std::thread::spawn(move || {
-            let _c = codec;
-        });
-    }
-
-    /// Checks that the codec can be send
-    #[test]
-    fn is_sync() {
-        let codec: Arc<Codec<(), ()>> = Arc::new(Codec::new());
-        std::thread::spawn(move || {
-            let _c = codec;
-        });
     }
 }
