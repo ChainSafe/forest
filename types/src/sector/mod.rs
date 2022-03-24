1220f6a79a27cc53e7b9ed0b2dfede7501804a49
Hubert Bugaj <hubert@chainsafe.io>
2022-03-24 09:11:08 +0100

    obliterate even more code

--- types/src/sector/mod.rs
+++ types/src/sector/mod.rs
@@ -8,23 +8,6 @@ pub use fvm_shared::sector::{
     AggregateSealVerifyInfo, AggregateSealVerifyProofAndInfos, InteractiveSealRandomness,
     OnChainWindowPoStVerifyInfo, PoStProof, PoStRandomness, RegisteredAggregateProof,
     RegisteredPoStProof, RegisteredSealProof, SealRandomness, SealVerifyInfo, SealVerifyParams,
-    SectorID, SectorInfo, SectorSize, WindowPoStVerifyInfo, WinningPoStVerifyInfo,
+    SectorID, SectorInfo, SectorNumber, SectorQuality, SectorSize, Spacetime, StoragePower,
+    WindowPoStVerifyInfo, WinningPoStVerifyInfo, MAX_SECTOR_NUMBER,
 };
-use num_bigint::BigInt;
-pub const RANDOMNESS_LENGTH: usize = 32;
-
-/// SectorNumber is a numeric identifier for a sector. It is usually relative to a miner.
-pub type SectorNumber = u64;
-
-/// The maximum assignable sector number.
-/// Raising this would require modifying our AMT implementation.
-pub const MAX_SECTOR_NUMBER: SectorNumber = i64::MAX as u64;
-
-/// Unit of storage power (measured in bytes)
-pub type StoragePower = BigInt;
-
-/// The unit of spacetime committed to the network
-pub type Spacetime = BigInt;
-
-/// Unit of sector quality
-pub type SectorQuality = BigInt;
