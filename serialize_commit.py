4f53f9a2fd7abe10cda4d4dad4b4149974a8afa3
Bodo Hugo Barwich <b.barwich@hotmail.com>
2022-04-09 19:25:20 +0100

    script serialize CURRENT_COMMIT

---
+++ serialize_commit.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+import sys
+import os
+from pygit2 import Repository
+import toml
+
+
+# ------------------------
+# Script Environment
+
+module_file = ''
+module_path = os.path.abspath(__file__)
+work_dir = ''
+commit_file = 'config_forest_commit.toml'
+
+
+slash_pos = module_path.rfind('/', 0)
+
+if slash_pos != -1:
+    work_dir = module_path[0: slash_pos + 1]
+    module_file = module_path[slash_pos + 1: len(module_path)]
+else:
+    module_file = module_path
+
+
+# ------------------------
+# Script Parameter
+
+module_debug = False
+module_quiet = False
+module_rs = 0
+
+for arg in sys.argv:
+    if arg[0: 1] == '--':
+        arg = arg[2: len(arg)]
+        if arg == 'debug':
+            module_debug = True
+        elif arg == 'quiet':
+            module_quiet = True
+
+    elif arg[0] == '-':
+        arg = arg[1: len(arg)]
+        for idx in range(0, len(arg)):
+            if arg[idx] == 'd':
+                module_debug = True
+            elif arg[idx] == 'q':
+                module_quiet = True
+
+
+# ------------------------
+# Get the Commit ID of HEAD
+
+commit_reference = 'HEAD'
+commit_hash = ''
+commit_hash_short = ''
+
+try:
+    # ------------------------
+    # Execute the Git Command
+
+    repo = Repository(work_dir + '.git')
+
+    commit_hash = str(repo.revparse_single(commit_reference).id)
+    commit_hash_short = commit_hash[0: 8]
+except Exception as e:
+    if not module_quiet:
+        print("script '{}' - Git: Git Command failed!".format(module_file),
+              file=sys.stderr)
+        print("script '{}' - Git Exception Message: {}".format(
+            module_file, str(e)), file=sys.stderr)
+
+    module_rs = 1
+
+
+if commit_hash != '':
+    # ------------------------
+    # Serialize Git Commit
+
+    forest_commit = {'FOREST_VERSION': {'CURRENT_COMMIT': {
+        commit_reference: {'hash': commit_hash, 'short': commit_hash_short}}}}
+
+    if module_debug:
+        print("script '{}' - Forest Commit:\n{}".format(module_file, str(forest_commit)))
+
+    try:
+        with open(work_dir + commit_file, 'w') as f:
+            forest_commit_str = toml.dump(forest_commit, f)
+
+        if module_debug:
+            print(
+                "script '{}' - Forest Commit:\n{}".format(module_file, forest_commit_str))
+
+    except Exception as e:
+        if not module_quiet:
+            print("script '{}' - Commit File '{}': Save File failed!".format(
+                module_file, commit_file), file=sys.stderr)
+            print("script '{}' - Commit File Exception Message: {}".format(
+                module_file, str(e)), file=sys.stderr)
+
+        module_rs = 1
+
+
+sys.exit(module_rs)
