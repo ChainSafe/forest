b96b666c0d9488ef576d680d9539e4714183d3d7
tyshkor <tyshko1@gmail.com>
2022-06-27 09:56:42 +0200

    fmt

--- utils/bigint/src/lib.rs
+++ utils/bigint/src/lib.rs
@@ -4,8 +4,3 @@
 pub mod bigint_ser;
 
 pub use num_bigint::*;
-pub use num_integer::{self, Integer};
-
-/// MAX_ENCODED_SIZE is the max length of a byte slice representing a
-/// CBOR serialized BigInt or BigUint.
-const MAX_ENCODED_SIZE: usize = 128;
