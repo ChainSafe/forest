ee52c89a6588194a0091ba6331d83676a85174ad
Hubert Bugaj <hubert@chainsafe.io>
2022-07-06 13:27:46 +0200

    Revert "Revert "remove stray ipld_amt occurences""
    This reverts commit 0403f005d697980e3b25f5f86b338a6ed0a91c36.

--- blockchain/chain/src/store/errors.rs
+++ blockchain/chain/src/store/errors.rs
@@ -5,8 +5,7 @@ use blocks::Error as BlkErr;
 use cid::Error as CidErr;
 use db::Error as DbErr;
 use encoding::{error::Error as SerdeErr, Error as EncErr};
-use std::error::Error as StdError;
-use ipld_amt::Error as AmtErr;
+use fvm_ipld_amt::Error as AmtErr;
 use thiserror::Error;
 
 /// Chain error
