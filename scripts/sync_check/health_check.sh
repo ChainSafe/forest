908ce63bf8653c077424ea6aa79d48f75af54c66
Hubert <hubert@chainsafe.io>
2022-07-21 16:55:51 +0200

    sync check in docker (#1627)
    * open draft
    
    * sync check

--- scripts/sync_check/health_check.sh
+++ scripts/sync_check/health_check.sh
@@ -3,22 +3,26 @@
 # Script to check health status of a running node.
 # The only prerequisite here is that the `forest` process is running.
 # The script will wait till metrics endpoint becomes available.
+# Input: Forest hostname
+
+# Exit codes
+RET_OK=0
+RET_SYNC_TIPSET_STALE=1
+RET_SYNC_ERROR=2
+RET_SYNC_TIMEOUT=3
+RET_HOSTNAME_NOT_SET=4
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied. Need to provide Forest hostname, e.g. forest-mainnet."
+    exit "$RET_HOSTNAME_NOT_SET"
+fi
 
 # Governs how long the health check will run to assert Forest condition
 HEALTH_CHECK_DURATION_SECONDS=${HEALTH_CHECK_DURATION_SECONDS:-"120"}
 # Forest metrics endpoint path
-FOREST_METRICS_ENDPOINT=${FOREST_METRICS_ENDPOINT:-"http://localhost:6116/metrics"}
+FOREST_METRICS_ENDPOINT=${FOREST_METRICS_ENDPOINT:-"http://$1:6116/metrics"}
 # Initial sync timeout (in seconds) after which the health check will fail
 HEALTH_CHECK_SYNC_TIMEOUT_SECONDS=${HEALTH_CHECK_SYNC_TIMEOUT_SECONDS:-"7200"}
-
-SERVICE_NAME="forest"
-
-# Exit codes
-RET_OK=0
-RET_FOREST_NOT_RUNNING=1
-RET_SYNC_TIPSET_STALE=2
-RET_SYNC_ERROR=3
-RET_SYNC_TIMEOUT=4
 
 # Extracts metric value from the metric data
 # Arg: name of the metric
@@ -43,14 +47,6 @@ function assert_error() {
 }
 
 ##### Actual script
-
-# Check if forest is running
-if pgrep -x "$SERVICE_NAME" >/dev/null; then
-  echo "✅ $SERVICE_NAME is running"
-else
-  echo "❌ $SERVICE_NAME is not running. Please start the node before launching the script."
-  exit $RET_FOREST_NOT_RUNNING
-fi
 
 # Wait for Forest to start syncing
 timeout="$HEALTH_CHECK_SYNC_TIMEOUT_SECONDS"
