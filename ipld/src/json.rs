b3645cd536ace66736db8912f1154518206202b8
tyshkor <rostyslav@chainsafe.io>
2022-07-25 15:23:29 +0200

    fmt

--- ipld/src/json.rs
+++ ipld/src/json.rs
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 //! Differences between serializers
-//! 
-//! The serializer created here uses `multihash` and `libipld-json` uses plain `base64`. 
-//! That means one has an extra `m` in front of all the encoded byte values, using our serializer. 
-//! 
+//!
+//! The serializer created here uses `multihash` and `libipld-json` uses plain `base64`.
+//! That means one has an extra `m` in front of all the encoded byte values, using our serializer.
+//!
 //! For example:
-//! 
+//!
 //! |`forest_ipld`|`libipld-json`|
 //! | ----------- | ------------ |
 //! |```{ "/": { "bytes": "mVGhlIHF1aQ" } }```|```{ "/": { "bytes": "VGhlIHF1aQ" } }```|
-//! 
+//!
 //! As `Lotus` is using `multihash-base64` and our serializer is using it as well.
 use super::Ipld;
 use libipld_macro::ipld;
