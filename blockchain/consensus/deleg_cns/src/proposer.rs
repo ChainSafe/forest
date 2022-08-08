4437b1790aebd8f78ca40835f78189c5ffd414bf
Akosh Farkash <aakoshh@gmail.com>
2022-08-08 10:15:11 +0100

    Flip args to proposing log.

--- blockchain/consensus/deleg_cns/src/proposer.rs
+++ blockchain/consensus/deleg_cns/src/proposer.rs
@@ -114,8 +114,8 @@ impl Proposer for DelegatedProposer {
             if let Some(base) = chain_store.heaviest_tipset().await {
                 info!(
                     "Proposing a block on top {} in epoch {}",
+                    base.min_ticket_block().cid(),
                     base.epoch(),
-                    base.min_ticket_block().cid()
                 );
                 match self.create_block(mpool, &state_manager, &base).await {
                     Ok(block) => {
