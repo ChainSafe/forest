0ab79577e250dd37f6630710de88c4b17b758e08
elmattic <elmattic@users.noreply.github.com>
2022-08-10 15:51:08 +0200

    cargo fmt

--- node/rpc/src/common_api.rs
+++ node/rpc/src/common_api.rs
@@ -6,7 +6,10 @@ use jsonrpc_v2::Error as JsonRpcError;
 use fil_types::build_version::{APIVersion, Version};
 use rpc_api::common_api::*;
 
-pub(crate) async fn version(block_delay: u64, forest_version: &'static str) -> Result<VersionResult, JsonRpcError> {
+pub(crate) async fn version(
+    block_delay: u64,
+    forest_version: &'static str,
+) -> Result<VersionResult, JsonRpcError> {
     Ok(APIVersion {
         version: forest_version.to_string(),
         api_version: Version::new(0, 0, 0),
