cf813799822e063eef75c0bfb75621760e9061df
Hubert Bugaj <hubert@chainsafe.io>
2022-09-23 11:42:27 +0200

    snapshots prune script

--- scripts/daily_snapshot/daily_snapshot.rb
+++ scripts/daily_snapshot/daily_snapshot.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
-require_relative 'slack_client'
-require_relative 'docker_utils'
+
+require_relative 'ruby_common/slack_client'
+require_relative 'ruby_common/docker_utils'
+require_relative 'snapshots_prune'
+
 require 'date'
 require 'logger'
 require 'fileutils'
@@ -25,21 +28,23 @@ raise 'No chain name supplied. Please provide chain identifier, e.g. calibnet or
 DATE = Time.new.strftime '%FT%H:%M:%S'
 LOG_EXPORT = "#{CHAIN_NAME}_#{DATE}_export"
 
+SNAPSHOTS_DIR = File.join(BASE_FOLDER, 's3', CHAIN_NAME)
+
 loop do
   client = SlackClient.new CHANNEL, SLACK_TOKEN
 
   # Find the snapshot with the most recent modification date
-  LATEST = Dir.glob("#{BASE_FOLDER}/s3/#{CHAIN_NAME}/*").max_by {|f| File.mtime(f)}
+  latest = Dir.glob(File.join(SNAPSHOTS_DIR, '/*')).max_by { |f| File.mtime(f) }
 
   # Check if the date of the most recent snapshot is today
-  if Time.new.to_date() == File.stat(LATEST).mtime.to_date()
+  if Time.new.to_date == File.stat(latest).mtime.to_date
     # We already have a snapshot for today. Do nothing.
     puts "No snapshot required for #{CHAIN_NAME}"
   else
-    puts "New snapshot required"
+    puts 'New snapshot required'
 
     # Sync and export snapshot
-    snapshot_uploaded = system("bash upload_snapshot.sh #{CHAIN_NAME} #{LATEST} > #{LOG_EXPORT} 2>&1")
+    snapshot_uploaded = system("bash upload_snapshot.sh #{CHAIN_NAME} #{latest} > #{LOG_EXPORT} 2>&1")
 
     client = SlackClient.new CHANNEL, SLACK_TOKEN
 
@@ -49,6 +54,10 @@ loop do
       client.post_message "⛔ Snapshot failed for #{CHAIN_NAME}. 🔥🌲🔥 "
       client.attach_files(LOG_EXPORT)
     end
+
+    # Prune snapshots
+    pruned = prune_snapshots(SNAPSHOTS_DIR)
+    client.attach_comment("Pruned snapshots: `#{pruned.join(', ')}`") unless pruned.empty?
   end
 
   # Loop such that a new snapshot will be updated once per day.
