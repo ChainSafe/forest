e789ea2d6481c53d299345e3d5be0f861383b553
Hubert Bugaj <hubert@chainsafe.io>
2022-09-30 16:45:10 +0200

    allow export of sha256sum for snapshots

--- node/rpc/src/chain_api.rs
+++ node/rpc/src/chain_api.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use ::forest_message::message::json::MessageJson;
+use anyhow::Result;
 use cid::Cid;
 use forest_beacon::Beacon;
 use forest_blocks::{
@@ -17,10 +18,16 @@ use forest_rpc_api::{
     data_types::{BlockMessages, RPCState},
 };
 use fvm_shared::message::Message as FVMMessage;
+use hex::ToHex;
 use jsonrpc_v2::{Data, Error as JsonRpcError, Params};
 use log::{debug, error};
 use serde::{Deserialize, Serialize};
-use std::{path::PathBuf, sync::Arc};
+use sha2::{Digest, Sha256};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+use tokio::io::AsyncWriteExt;
 use tokio::{fs::File, io::BufWriter};
 use tokio_util::compat::TokioAsyncWriteCompatExt;
 
@@ -58,7 +65,7 @@ where
     DB: BlockStore + Send + Sync + 'static,
     B: Beacon + Send + Sync + 'static,
 {
-    let (epoch, recent_roots, include_olds_msgs, out, TipsetKeysJson(tsk)) = params;
+    let (epoch, recent_roots, include_olds_msgs, out, TipsetKeysJson(tsk), skip_checksum) = params;
     let skip_old_msgs = !include_olds_msgs;
 
     let chain_finality = data.state_manager.chain_config().policy.chain_finality;
@@ -87,15 +94,48 @@ where
         .await
     {
         if let Err(e) = std::fs::remove_file(&out) {
-            error!("failed to remove incomplete export file at {out}: {e}");
+            error!(
+                "failed to remove incomplete export file at {}: {e}",
+                out.display()
+            );
         } else {
-            debug!("incomplete export file at {out} removed");
+            debug!("incomplete export file at {} removed", out.display());
         }
 
         return Err(JsonRpcError::from(e));
     }
 
-    Ok(PathBuf::from(out))
+    if !skip_checksum {
+        calculate_and_save_checksum(&out).await?;
+    }
+
+    Ok(out)
+}
+
+/// Calculates the SHA256 of the provided file, prints its
+/// hex-encoded representation and saves it to a file with the same
+/// name but with a `.sha256sum` extension.
+async fn calculate_and_save_checksum(source: &Path) -> Result<()> {
+    let mut file = std::fs::File::open(source)?;
+
+    let mut hasher = Sha256::new();
+    std::io::copy(&mut file, &mut hasher)?;
+    let hash = hasher.finalize();
+
+    let encoded_hash = hash.encode_hex::<String>();
+
+    let mut checksum_path = PathBuf::from(source);
+    checksum_path.set_extension("sha256sum");
+
+    let mut checksum_file = File::create(&checksum_path).await?;
+    checksum_file.write_all(encoded_hash.as_bytes()).await?;
+    checksum_file.flush().await?;
+    log::info!(
+        "Snapshot checksum: {encoded_hash} saved to {}",
+        checksum_path.display()
+    );
+
+    Ok(())
 }
 
 pub(crate) async fn chain_read_obj<DB, B>(
