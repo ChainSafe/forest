83db96b9d4c6105bba0d2558a12ae7f842a1aca7
Hubert <hubert@chainsafe.io>
2022-10-06 12:00:49 +0200

    allow export of sha256sum for snapshots (#1979)

---
+++ node/utils/src/io/lib.rs
@@ -0,0 +1,3 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+pub use writer_checksum::*;
---
+++ node/utils/src/io/mod.rs
@@ -0,0 +1,5 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+mod writer_checksum;
+
+pub use writer_checksum::*;
