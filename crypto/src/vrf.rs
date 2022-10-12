af23881a4d91e244fd5f62a1eb8a9a2630107b36
connor mullett <connormullett@gmail.com>
2022-10-12 14:40:27 -0400

    remove vrfproof tests; serde cases are covered in checked_serde_bytes

--- crypto/src/vrf.rs
+++ crypto/src/vrf.rs
@@ -28,14 +28,6 @@ impl VRFProof {
     }
 }
 
-#[cfg(test)]
-impl quickcheck::Arbitrary for VRFProof {
-    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
-        let fmt_str = format!("===={}=====", u64::arbitrary(g));
-        VRFProof::new(fmt_str.into_bytes())
-    }
-}
-
 /// Verifies raw VRF proof. This VRF proof is a BLS signature.
 pub fn verify_vrf(worker: &Address, vrf_base: &[u8], vrf_proof: &[u8]) -> Result<(), String> {
     verify_bls_sig(vrf_proof, vrf_base, worker).map_err(|e| format!("VRF was invalid: {}", e))
@@ -61,19 +53,5 @@ pub mod json {
         Ok(VRFProof::new(
             base64::decode(s.as_ref()).map_err(de::Error::custom)?,
         ))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use quickcheck_macros::quickcheck;
-
-    #[quickcheck]
-    fn vrfproof_roundtrip(proof: VRFProof) {
-        let serialized = serde_json::to_string(&proof).unwrap();
-        println!("serialized {}", serialized);
-        let parsed = serde_json::from_str(&serialized).unwrap();
-        assert_eq!(proof, parsed);
     }
 }
