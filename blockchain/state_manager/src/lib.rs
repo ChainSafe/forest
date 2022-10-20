a92036cb4d467ef63a57bd2d1b524fd9a45c47cb
tyshkor <rostyslav@chainsafe.io>
2022-10-20 15:57:45 +0200

    fix doc

--- blockchain/state_manager/src/lib.rs
+++ blockchain/state_manager/src/lib.rs
@@ -168,12 +168,12 @@ where
         Ok(state.get_actor(addr)?)
     }
 
-    /// Returns the cloned [`Arc`] of the state manager's [`BlockStore`].
+    /// Returns the cloned [`Arc`] of the state manager's [`Blockstore`].
     pub fn blockstore_cloned(&self) -> DB {
         self.cs.blockstore_cloned()
     }
 
-    /// Returns a reference to the state manager's [`BlockStore`].
+    /// Returns a reference to the state manager's [`Blockstore`].
     pub fn blockstore(&self) -> &DB {
         self.cs.blockstore()
     }
