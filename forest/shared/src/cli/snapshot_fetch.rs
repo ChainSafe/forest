5abd9da6f525eb9bfe6d5c48e27b18eab5a779a3
hanabi1224 <harlowmoo@gmail.com>
2022-11-14 20:08:16 +0800

    resolve comments

--- forest/shared/src/cli/snapshot_fetch.rs
+++ forest/shared/src/cli/snapshot_fetch.rs
@@ -243,7 +243,9 @@ where
     if !checksum_response.status().is_success() {
         bail!("Unable to get the checksum file. Url: {checksum_url}");
     }
-    let checksum_bytes = hyper::body::to_bytes(checksum_response.into_body()).await?[..64].to_vec();
+    let checksum_bytes = hyper::body::to_bytes(checksum_response.into_body()).await?
+        [..Sha256::output_size() * 2]
+        .to_vec();
     let checksum_expected = String::from_utf8(checksum_bytes)?;
     info!("Expected sha256 checksum: {checksum_expected}");
     download_with_aria2(
