314738cdb556a82ac0f9e3ab18bf0168a013992d
tyshkor <tyshko1@gmail.com>
2022-11-21 13:22:18 +0100

    pover v8

---
+++ actors/power_v8/src/state.rs
@@ -0,0 +1,276 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use anyhow::{anyhow, Context};
+use cid::Cid;
+use fil_actors_runtime_v8::runtime::Policy;
+use fil_actors_runtime_v8::{
+    actor_error, make_empty_map, make_map_with_root, make_map_with_root_and_bitwidth,
+    ActorDowncast, Map, Multimap,
+};
+use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::tuple::*;
+use fvm_ipld_encoding::{Cbor, RawBytes};
+use fvm_ipld_hamt::BytesKey;
+use fvm_shared::address::Address;
+use fvm_shared::bigint::bigint_ser;
+use fvm_shared::clock::ChainEpoch;
+use fvm_shared::econ::TokenAmount;
+use fvm_shared::error::ExitCode;
+use fvm_shared::sector::{RegisteredPoStProof, StoragePower};
+use fvm_shared::smooth::FilterEstimate;
+use fvm_shared::HAMT_BIT_WIDTH;
+use integer_encoding::VarInt;
+use lazy_static::lazy_static;
+use num_traits::Signed;
+
+use super::{CONSENSUS_MINER_MIN_MINERS, CRON_QUEUE_AMT_BITWIDTH, CRON_QUEUE_HAMT_BITWIDTH};
+
+lazy_static! {
+    /// genesis power in bytes = 750,000 GiB
+    pub static ref INITIAL_QA_POWER_ESTIMATE_POSITION: StoragePower = StoragePower::from(750_000) * (1 << 30);
+    /// max chain throughput in bytes per epoch = 120 ProveCommits / epoch = 3,840 GiB
+    pub static ref INITIAL_QA_POWER_ESTIMATE_VELOCITY: StoragePower = StoragePower::from(3_840) * (1 << 30);
+}
+
+/// Storage power actor state
+#[derive(Default, Serialize_tuple, Deserialize_tuple)]
+pub struct State {
+    #[serde(with = "bigint_ser")]
+    pub total_raw_byte_power: StoragePower,
+    #[serde(with = "bigint_ser")]
+    pub total_bytes_committed: StoragePower,
+    #[serde(with = "bigint_ser")]
+    pub total_quality_adj_power: StoragePower,
+    #[serde(with = "bigint_ser")]
+    pub total_qa_bytes_committed: StoragePower,
+    pub total_pledge_collateral: TokenAmount,
+
+    #[serde(with = "bigint_ser")]
+    pub this_epoch_raw_byte_power: StoragePower,
+    #[serde(with = "bigint_ser")]
+    pub this_epoch_quality_adj_power: StoragePower,
+    pub this_epoch_pledge_collateral: TokenAmount,
+    pub this_epoch_qa_power_smoothed: FilterEstimate,
+
+    pub miner_count: i64,
+    /// Number of miners having proven the minimum consensus power.
+    pub miner_above_min_power_count: i64,
+
+    /// A queue of events to be triggered by cron, indexed by epoch.
+    pub cron_event_queue: Cid, // Multimap, (HAMT[ChainEpoch]AMT[CronEvent]
+
+    /// First epoch in which a cron task may be stored. Cron will iterate every epoch between this
+    /// and the current epoch inclusively to find tasks to execute.
+    pub first_cron_epoch: ChainEpoch,
+
+    /// Claimed power for each miner.
+    pub claims: Cid, // Map, HAMT[address]Claim
+
+    pub proof_validation_batch: Option<Cid>,
+}
+
+impl State {
+    pub fn new<BS: Blockstore>(store: &BS) -> anyhow::Result<State> {
+        let empty_map = make_empty_map::<_, ()>(store, HAMT_BIT_WIDTH)
+            .flush()
+            .map_err(|e| anyhow!("Failed to create empty map: {}", e))?;
+
+        let empty_mmap = Multimap::new(store, CRON_QUEUE_HAMT_BITWIDTH, CRON_QUEUE_AMT_BITWIDTH)
+            .root()
+            .map_err(|e| {
+                e.downcast_default(
+                    ExitCode::USR_ILLEGAL_STATE,
+                    "Failed to get empty multimap cid",
+                )
+            })?;
+        Ok(State {
+            cron_event_queue: empty_mmap,
+            claims: empty_map,
+            this_epoch_qa_power_smoothed: FilterEstimate::new(
+                INITIAL_QA_POWER_ESTIMATE_POSITION.clone(),
+                INITIAL_QA_POWER_ESTIMATE_VELOCITY.clone(),
+            ),
+            ..Default::default()
+        })
+    }
+
+    pub fn into_total_locked(self) -> TokenAmount {
+        self.total_pledge_collateral
+    }
+
+    /// Checks power actor state for if miner meets minimum consensus power.
+    pub fn miner_nominal_power_meets_consensus_minimum<BS: Blockstore>(
+        &self,
+        policy: &Policy,
+        s: &BS,
+        miner: &Address,
+    ) -> anyhow::Result<bool> {
+        let claims = make_map_with_root_and_bitwidth(&self.claims, s, HAMT_BIT_WIDTH)?;
+
+        let claim =
+            get_claim(&claims, miner)?.ok_or_else(|| anyhow!("no claim for actor: {}", miner))?;
+
+        let miner_nominal_power = &claim.raw_byte_power;
+        let miner_min_power = consensus_miner_min_power(policy, claim.window_post_proof_type)
+            .context("could not get miner min power from proof type: {}")?;
+
+        if miner_nominal_power >= &miner_min_power {
+            // If miner is larger than min power requirement, valid
+            Ok(true)
+        } else if self.miner_above_min_power_count >= CONSENSUS_MINER_MIN_MINERS {
+            // if min consensus miners requirement met, return false
+            Ok(false)
+        } else {
+            // if fewer miners than consensus minimum, return true if non-zero power
+            Ok(miner_nominal_power.is_positive())
+        }
+    }
+
+    pub fn miner_power<BS: Blockstore>(
+        &self,
+        s: &BS,
+        miner: &Address,
+    ) -> anyhow::Result<Option<Claim>> {
+        let claims = make_map_with_root(&self.claims, s)?;
+        get_claim(&claims, miner).map(|s| s.cloned())
+    }
+
+    pub fn current_total_power(&self) -> (StoragePower, StoragePower) {
+        if self.miner_above_min_power_count < CONSENSUS_MINER_MIN_MINERS {
+            (
+                self.total_bytes_committed.clone(),
+                self.total_qa_bytes_committed.clone(),
+            )
+        } else {
+            (
+                self.total_raw_byte_power.clone(),
+                self.total_quality_adj_power.clone(),
+            )
+        }
+    }
+
+    pub fn get_claim<BS: Blockstore>(
+        &self,
+        store: &BS,
+        miner: &Address,
+    ) -> anyhow::Result<Option<Claim>> {
+        let claims =
+            make_map_with_root_and_bitwidth::<_, Claim>(&self.claims, store, HAMT_BIT_WIDTH)
+                .map_err(|e| {
+                    e.downcast_default(ExitCode::USR_ILLEGAL_STATE, "failed to load claims")
+                })?;
+
+        let claim = get_claim(&claims, miner)?;
+        Ok(claim.cloned())
+    }
+}
+
+/// Gets claim from claims map by address
+fn get_claim<'m, BS: Blockstore>(
+    claims: &'m Map<BS, Claim>,
+    a: &Address,
+) -> anyhow::Result<Option<&'m Claim>> {
+    claims
+        .get(&a.to_bytes())
+        .map_err(|e| e.downcast_wrap(format!("failed to get claim for address {}", a)))
+}
+
+pub fn set_claim<BS: Blockstore>(
+    claims: &mut Map<BS, Claim>,
+    a: &Address,
+    claim: Claim,
+) -> anyhow::Result<()> {
+    if claim.raw_byte_power.is_negative() {
+        return Err(anyhow!(actor_error!(
+            illegal_state,
+            "negative claim raw power {}",
+            claim.raw_byte_power
+        )));
+    }
+    if claim.quality_adj_power.is_negative() {
+        return Err(anyhow!(actor_error!(
+            illegal_state,
+            "negative claim quality-adjusted power {}",
+            claim.quality_adj_power
+        )));
+    }
+
+    claims
+        .set(a.to_bytes().into(), claim)
+        .map_err(|e| e.downcast_wrap(format!("failed to set claim for address {}", a)))?;
+    Ok(())
+}
+
+pub fn epoch_key(e: ChainEpoch) -> BytesKey {
+    let bz = e.encode_var_vec();
+    bz.into()
+}
+
+impl Cbor for State {}
+
+#[derive(Debug, Serialize_tuple, Deserialize_tuple, Clone, PartialEq)]
+pub struct Claim {
+    /// Miner's proof type used to determine minimum miner size
+    pub window_post_proof_type: RegisteredPoStProof,
+    /// Sum of raw byte power for a miner's sectors.
+    #[serde(with = "bigint_ser")]
+    pub raw_byte_power: StoragePower,
+    /// Sum of quality adjusted power for a miner's sectors.
+    #[serde(with = "bigint_ser")]
+    pub quality_adj_power: StoragePower,
+}
+
+#[derive(Clone, Debug, Serialize_tuple, Deserialize_tuple)]
+pub struct CronEvent {
+    pub miner_addr: Address,
+    pub callback_payload: RawBytes,
+}
+
+impl Cbor for CronEvent {}
+
+/// Returns the minimum storage power required for each seal proof types.
+pub fn consensus_miner_min_power(
+    policy: &Policy,
+    p: RegisteredPoStProof,
+) -> anyhow::Result<StoragePower> {
+    use RegisteredPoStProof::*;
+    match p {
+        StackedDRGWinning2KiBV1
+        | StackedDRGWinning8MiBV1
+        | StackedDRGWinning512MiBV1
+        | StackedDRGWinning32GiBV1
+        | StackedDRGWinning64GiBV1
+        | StackedDRGWindow2KiBV1
+        | StackedDRGWindow8MiBV1
+        | StackedDRGWindow512MiBV1
+        | StackedDRGWindow32GiBV1
+        | StackedDRGWindow64GiBV1 => Ok(policy.minimum_consensus_power.clone()),
+        Invalid(i) => Err(anyhow::anyhow!("unsupported proof type: {}", i)),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use fvm_shared::clock::ChainEpoch;
+
+    use super::*;
+
+    #[test]
+    fn epoch_key_test() {
+        let e1: ChainEpoch = 101;
+        let e2: ChainEpoch = 102;
+        let e3: ChainEpoch = 103;
+        let e4: ChainEpoch = -1;
+
+        let b1: BytesKey = [0xca, 0x1].to_vec().into();
+        let b2: BytesKey = [0xcc, 0x1].to_vec().into();
+        let b3: BytesKey = [0xce, 0x1].to_vec().into();
+        let b4: BytesKey = [0x1].to_vec().into();
+
+        assert_eq!(b1, epoch_key(e1));
+        assert_eq!(b2, epoch_key(e2));
+        assert_eq!(b3, epoch_key(e3));
+        assert_eq!(b4, epoch_key(e4));
+    }
+}
---
+++ actors/power_v8/src/testing.rs
@@ -0,0 +1,284 @@
+use std::collections::HashMap;
+
+use fil_actors_runtime_v8::{parse_uint_key, runtime::Policy, Map, MessageAccumulator, Multimap};
+use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::RawBytes;
+use fvm_shared::{
+    address::Address,
+    clock::ChainEpoch,
+    sector::{SealVerifyInfo, StoragePower},
+    HAMT_BIT_WIDTH,
+};
+use num_traits::{Signed, Zero};
+
+use crate::{
+    consensus_miner_min_power, Claim, CronEvent, State, CRON_QUEUE_AMT_BITWIDTH,
+    CRON_QUEUE_HAMT_BITWIDTH, MAX_MINER_PROVE_COMMITS_PER_EPOCH,
+    PROOF_VALIDATION_BATCH_AMT_BITWIDTH,
+};
+
+pub struct MinerCronEvent {
+    pub epoch: ChainEpoch,
+    pub payload: RawBytes,
+}
+
+type CronEventsByAddress = HashMap<Address, Vec<MinerCronEvent>>;
+type ClaimsByAddress = HashMap<Address, Claim>;
+type ProofsByAddress = HashMap<Address, SealVerifyInfo>;
+
+pub struct StateSummary {
+    pub crons: CronEventsByAddress,
+    pub claims: ClaimsByAddress,
+    pub proofs: ProofsByAddress,
+}
+
+/// Checks internal invariants of power state
+pub fn check_state_invariants<BS: Blockstore>(
+    policy: &Policy,
+    state: &State,
+    store: &BS,
+) -> (StateSummary, MessageAccumulator) {
+    let acc = MessageAccumulator::default();
+
+    // basic invariants around recorded power
+    acc.require(
+        !state.total_raw_byte_power.is_negative(),
+        format!("total raw power is negative {}", state.total_raw_byte_power),
+    );
+    acc.require(
+        !state.total_quality_adj_power.is_negative(),
+        format!(
+            "total qa power is negative {}",
+            state.total_quality_adj_power
+        ),
+    );
+    acc.require(
+        !state.total_bytes_committed.is_negative(),
+        format!(
+            "total raw power committed is negative {}",
+            state.total_bytes_committed
+        ),
+    );
+    acc.require(
+        !state.total_qa_bytes_committed.is_negative(),
+        format!(
+            "total qa power committed is negative {}",
+            state.total_qa_bytes_committed
+        ),
+    );
+
+    acc.require(
+        state.total_raw_byte_power <= state.total_quality_adj_power,
+        format!(
+            "total raw power {} is greater than total quality adjusted power {}",
+            state.total_raw_byte_power, state.total_quality_adj_power
+        ),
+    );
+    acc.require(
+        state.total_bytes_committed <= state.total_qa_bytes_committed,
+        format!(
+            "committed raw power {} is greater than committed quality adjusted power {}",
+            state.total_bytes_committed, state.total_qa_bytes_committed
+        ),
+    );
+    acc.require(
+        state.total_raw_byte_power <= state.total_bytes_committed,
+        format!(
+            "total raw power {} is greater than raw power committed {}",
+            state.total_raw_byte_power, state.total_bytes_committed
+        ),
+    );
+    acc.require(
+        state.total_quality_adj_power <= state.total_qa_bytes_committed,
+        format!(
+            "total qa power {} is greater than qa power committed {}",
+            state.total_quality_adj_power, state.total_qa_bytes_committed
+        ),
+    );
+
+    let crons = check_cron_invariants(state, store, &acc);
+    let claims = check_claims_invariants(policy, state, store, &acc);
+    let proofs = check_proofs_invariants(state, store, &claims, &acc);
+
+    (
+        StateSummary {
+            crons,
+            claims,
+            proofs,
+        },
+        acc,
+    )
+}
+
+fn check_cron_invariants<BS: Blockstore>(
+    state: &State,
+    store: &BS,
+    acc: &MessageAccumulator,
+) -> CronEventsByAddress {
+    let mut cron_events_by_address = CronEventsByAddress::new();
+
+    match Multimap::from_root(
+        store,
+        &state.cron_event_queue,
+        CRON_QUEUE_HAMT_BITWIDTH,
+        CRON_QUEUE_AMT_BITWIDTH,
+    ) {
+        Ok(queue) => {
+            let ret = queue.for_all::<_, CronEvent>(|key, events| {
+                let epoch = match parse_uint_key(key) {
+                    Ok(key) => key,
+                    Err(e) => {
+                        acc.add(format!("non-int key in cron array: {e}"));
+                        return Ok(());
+                    }
+                } as i64;
+
+                acc.require(
+                    epoch >= state.first_cron_epoch,
+                    format!(
+                        "cron event at epoch {epoch} before first_cron_epoch {}",
+                        state.first_cron_epoch
+                    ),
+                );
+                events
+                    .for_each(|_, event| {
+                        cron_events_by_address
+                            .entry(event.miner_addr)
+                            .or_default()
+                            .push(MinerCronEvent {
+                                epoch,
+                                payload: event.callback_payload.clone(),
+                            });
+                        Ok(())
+                    })
+                    .map_err(|e| {
+                        anyhow::anyhow!("error iterating cron events for epoch {}: {}", epoch, e)
+                    })
+            });
+            acc.require_no_error(ret, "error iterating cron tasks");
+        }
+        Err(e) => acc.add(format!("error loading cron event queue: {e}")),
+    }
+
+    cron_events_by_address
+}
+fn check_claims_invariants<BS: Blockstore>(
+    policy: &Policy,
+    state: &State,
+    store: &BS,
+    acc: &MessageAccumulator,
+) -> ClaimsByAddress {
+    let mut claims_by_address = ClaimsByAddress::new();
+
+    let mut committed_raw_power = StoragePower::zero();
+    let mut committed_qa_power = StoragePower::zero();
+    let mut raw_power = StoragePower::zero();
+    let mut qa_power = StoragePower::zero();
+    let mut claims_with_sufficient_power_count = 0;
+
+    match Map::<_, Claim>::load(&state.claims, store) {
+        Ok(claims) => {
+            let ret = claims.for_each(|key, claim| {
+                let address = Address::from_bytes(key)?;
+                claims_by_address.insert(address, claim.clone());
+
+                committed_raw_power += &claim.raw_byte_power;
+                committed_qa_power += &claim.quality_adj_power;
+
+                let min_power =
+                    match consensus_miner_min_power(policy, claim.window_post_proof_type) {
+                        Ok(power) => power,
+                        Err(e) => {
+                            acc.add(format!(
+                                "could not get consensus miner min power for miner {address}: {e}"
+                            ));
+                            // noted above
+                            return Ok(());
+                        }
+                    };
+
+                if claim.raw_byte_power >= min_power {
+                    claims_with_sufficient_power_count += 1;
+                    raw_power += &claim.raw_byte_power;
+                    qa_power += &claim.quality_adj_power;
+                }
+
+                Ok(())
+            });
+            acc.require_no_error(ret, "error iterating power claims");
+        }
+        Err(e) => acc.add(format!("error loading power claims: {e}")),
+    };
+
+    acc.require(committed_raw_power == state.total_bytes_committed, format!("sum of raw power in claims {committed_raw_power} does not match recorded bytes committed {}", state.total_bytes_committed));
+    acc.require(committed_qa_power == state.total_qa_bytes_committed, format!("sum of qa power in claims {committed_qa_power} does not match recorded qa power committed {}", state.total_qa_bytes_committed));
+    acc.require(claims_with_sufficient_power_count == state.miner_above_min_power_count, format!("claims with sufficient power {claims_with_sufficient_power_count} does not match miner_above_min_power_count {}", state.miner_above_min_power_count));
+    acc.require(
+        state.total_raw_byte_power == raw_power,
+        format!(
+            "recorded raw power {} does not match raw power in claims {raw_power}",
+            state.total_raw_byte_power
+        ),
+    );
+    acc.require(
+        state.total_quality_adj_power == qa_power,
+        format!(
+            "recorded qa power {} does not match qa power in claims {qa_power}",
+            state.total_quality_adj_power
+        ),
+    );
+
+    claims_by_address
+}
+fn check_proofs_invariants<BS: Blockstore>(
+    state: &State,
+    store: &BS,
+    claims: &ClaimsByAddress,
+    acc: &MessageAccumulator,
+) -> ProofsByAddress {
+    if state.proof_validation_batch.is_none() {
+        return ProofsByAddress::default();
+    }
+
+    let mut proofs_by_address = ProofsByAddress::new();
+    match Multimap::from_root(
+        store,
+        &state.proof_validation_batch.unwrap(),
+        HAMT_BIT_WIDTH,
+        PROOF_VALIDATION_BATCH_AMT_BITWIDTH,
+    ) {
+        Ok(queue) => {
+            let ret = queue.for_all::<_, SealVerifyInfo>(|key, infos| {
+                let address = Address::from_bytes(key)?;
+                let claim = if let Some(claim) = claims.get(&address) {
+                    claim
+                } else {
+                    acc.add(format!("miner {address} has proofs awaiting validation but no claim"));
+                    return Ok(())
+                };
+
+                let ret = infos.for_each(|_, info| {
+                    match info.registered_proof.registered_window_post_proof() {
+                        Ok(sector_window_post_proof_type) => {
+                            acc.require(claim.window_post_proof_type == sector_window_post_proof_type, format!("miner submitted proof with proof type {:?} different from claim {:?}", sector_window_post_proof_type, claim.window_post_proof_type));
+                        },
+                        Err(e) => acc.add(format!("Invalid PoSt proof: {e}"))
+                    }
+                    proofs_by_address.insert(address, info.clone());
+                    Ok(())
+                });
+
+                if ret.is_err() {
+                    return ret.map_err(|e| anyhow::anyhow!("error iterating proof validation batch for address {}: {}", address, e));
+                }
+
+                acc.require(proofs_by_address.len() as u64 <= MAX_MINER_PROVE_COMMITS_PER_EPOCH, format!("miner {address} has submitted too many proofs ({}) for batch verification", proofs_by_address.len()));
+                Ok(())
+            });
+            acc.require_no_error(ret, "error iterating proof validation queue");
+        }
+        Err(e) => acc.add(format!("error loading proof validation queue: {e}")),
+    }
+
+    proofs_by_address
+}
