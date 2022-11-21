8c1df764eab50dc721c7ce05bbaef79b60b41867
Josh Jones <joshua.jones@chainsafe.io>
2022-11-21 16:56:28 -0600

    resolve redundant `find_config_path` logic

--- forest/shared/src/cli/mod.rs
+++ forest/shared/src/cli/mod.rs
@@ -66,7 +66,7 @@ possible_values = &["mainnet", "calibnet"],
 
 impl CliOpts {
     pub fn to_config(&self) -> Result<(Config, Option<ConfigPath>), anyhow::Error> {
-        let path = find_config_path(Opts::CliOpts(self));
+        let path = find_config_path(&self.config);
         let mut cfg: Config = match &path {
             Some(path) => {
                 // Read from config file
@@ -187,7 +187,7 @@ pub struct DaemonOpts {
 
 impl DaemonOpts {
     pub fn to_config(&self) -> Result<(Config, Option<ConfigPath>), anyhow::Error> {
-        let path = find_config_path(Opts::DaemonOpts(self));
+        let path = find_config_path(&self.config);
         let mut cfg: Config = match &path {
             Some(path) => {
                 // Read from config file
@@ -280,44 +280,20 @@ impl ConfigPath {
     }
 }
 
-pub enum Opts<'a> {
-    CliOpts(&'a CliOpts),
-    DaemonOpts(&'a DaemonOpts),
-}
-
-fn find_config_path(opts: Opts) -> Option<ConfigPath> {
-    match opts {
-        Opts::CliOpts(cli_opts) => {
-            if let Some(s) = &cli_opts.config {
-                return Some(ConfigPath::Cli(PathBuf::from(s)));
-            }
-            if let Ok(s) = std::env::var("FOREST_CONFIG_PATH") {
-                return Some(ConfigPath::Env(PathBuf::from(s)));
-            }
-            if let Some(dir) = ProjectDirs::from("com", "ChainSafe", "Forest") {
-                let path = dir.config_dir().join("config.toml");
-                if path.exists() {
-                    return Some(ConfigPath::Project(path));
-                }
-            }
-            None
-        }
-        Opts::DaemonOpts(daemon_opts) => {
-            if let Some(s) = &daemon_opts.config {
-                return Some(ConfigPath::Cli(PathBuf::from(s)));
-            }
-            if let Ok(s) = std::env::var("FOREST_CONFIG_PATH") {
-                return Some(ConfigPath::Env(PathBuf::from(s)));
-            }
-            if let Some(dir) = ProjectDirs::from("com", "ChainSafe", "Forest") {
-                let path = dir.config_dir().join("config.toml");
-                if path.exists() {
-                    return Some(ConfigPath::Project(path));
-                }
-            }
-            None
+fn find_config_path(opts: &Option<String>) -> Option<ConfigPath> {
+    if let Some(s) = &opts {
+        return Some(ConfigPath::Cli(PathBuf::from(s)));
+    }
+    if let Ok(s) = std::env::var("FOREST_CONFIG_PATH") {
+        return Some(ConfigPath::Env(PathBuf::from(s)));
+    }
+    if let Some(dir) = ProjectDirs::from("com", "ChainSafe", "Forest") {
+        let path = dir.config_dir().join("config.toml");
+        if path.exists() {
+            return Some(ConfigPath::Project(path));
         }
     }
+    None
 }
 
 fn find_unknown_keys<'a>(
