91d566dced68db343f018082e571b753f64ec6f0
sudo-shashank <shashank@chainsafe.io>
2022-11-22 16:14:07 +0530

    Revert system actors

--- actors/system_v9/src/lib.rs
+++ actors/system_v9/src/lib.rs
@@ -1,10 +1,11 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use cid::Cid;
+use fvm_ipld_encoding::tuple::*;
+use fvm_ipld_encoding::Cbor;
 use fvm_shared::METHOD_CONSTRUCTOR;
 use num_derive::FromPrimitive;
-
-mod state;
 
 #[cfg(feature = "fil-actor")]
 fil_actors_runtime::wasm_trampoline!(Actor);
@@ -15,3 +16,13 @@ fil_actors_runtime::wasm_trampoline!(Actor);
 pub enum Method {
     Constructor = METHOD_CONSTRUCTOR,
 }
+
+/// System actor state.
+#[derive(Default, Deserialize_tuple, Serialize_tuple, Debug, Clone)]
+pub struct State {
+    // builtin actor registry: Vec<(String, Cid)>
+    pub builtin_actors: Cid,
+}
+impl Cbor for State {}
+
+impl State {}
