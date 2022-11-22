8eb66c30aef20cb15f755c58dc832adbb9cd3b11
David Himmelstrup <david.himmelstrup@chainsafe.io>
2022-11-22 15:01:52 +0100

    Move vendored actors to separate repository.

--- actors/runtime_v8/src/builtin/shared.rs
+++
@@ -1,4 +0,0 @@
-// Copyright 2019-2022 ChainSafe Systems
-// SPDX-License-Identifier: Apache-2.0, MIT
-
-pub const HAMT_BIT_WIDTH: u32 = 5;
--- actors/runtime_v8/src/runtime/policy.rs
+++
@@ -1,6 +0,0 @@
-pub use fil_actors_runtime_v9::runtime::Policy;
-
-// A trait for runtime policy configuration
-pub trait RuntimePolicy {
-    fn policy(&self) -> &Policy;
-}
