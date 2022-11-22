86c6b38f2520a960f0a2d25fa02546b573666f8e
hanabi1224 <harlowmoo@gmail.com>
2022-11-23 00:38:21 +0800

    Update node/forest_libp2p/src/service.rs
    Co-authored-by: Hubert <lesny.rumcajs@gmail.com>

--- node/forest_libp2p/src/service.rs
+++ node/forest_libp2p/src/service.rs
@@ -781,7 +781,6 @@ async fn emit_event(sender: &flume::Sender<NetworkEvent>, event: NetworkEvent) {
 pub async fn build_transport(local_key: Keypair) -> Boxed<(PeerId, StreamMuxerBox)> {
     let tcp_transport =
         || libp2p::tcp::TokioTcpTransport::new(libp2p::tcp::GenTcpConfig::new().nodelay(true));
-    // let transport = libp2p::websocket::WsConfig::new(tcp_transport()).or_transport(tcp_transport());
     let transport = libp2p::dns::TokioDnsConfig::system(tcp_transport()).unwrap();
     let auth_config = {
         let dh_keys = noise::Keypair::<noise::X25519Spec>::new()
