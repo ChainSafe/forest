0a7f970bb9d7d21f4a5b247954b7e6b0e981f7fe
Hubert Bugaj <hubert@chainsafe.io>
2022-11-23 10:07:25 +0100

    use function name macro to reduce boilerplate

--- vm/interpreter/src/instrumented_kernel.rs
+++ vm/interpreter/src/instrumented_kernel.rs
@@ -21,6 +21,7 @@ use fvm_shared::randomness::RANDOMNESS_LENGTH;
 use fvm_shared::sector::*;
 use fvm_shared::{ActorID, MethodNum};
 use std::time;
+use stdext::function_name;
 
 use crate::metrics;
 use crate::ForestMachine;
@@ -28,16 +29,18 @@ use crate::ForestMachine;
 /// Calls the supplied lambda and updates the corresponding Prometheus metrics - call count and
 /// total call duration.
 macro_rules! forward_instrumented {
-    ($method_name:literal, $call:expr) => {{
+    ($call:expr) => {{
         let stopwatch = time::Instant::now();
         let result = $call();
         let elapsed = stopwatch.elapsed();
+        let fn_name = function_name!()
+            .rsplit_once(':')
+            .expect("could not parse function name")
+            .1;
 
-        metrics::KERNEL_OP_COUNT
-            .with_label_values(&[$method_name])
-            .inc();
+        metrics::KERNEL_OP_COUNT.with_label_values(&[fn_name]).inc();
         metrics::KERNEL_OP_DURATION
-            .with_label_values(&[$method_name])
+            .with_label_values(&[fn_name])
             .inc_by(elapsed.as_nanos() as u64);
 
         result
@@ -78,40 +81,36 @@ impl<DB: Blockstore> fvm::Kernel for ForestInstrumentedKernel<DB> {
 }
 impl<DB: Blockstore> fvm::kernel::ActorOps for ForestInstrumentedKernel<DB> {
     fn resolve_address(&self, address: &Address) -> fvm::kernel::Result<Option<ActorID>> {
-        forward_instrumented!("resolve_address", || self.0.resolve_address(address))
+        forward_instrumented!(|| self.0.resolve_address(address))
     }
 
     fn get_actor_code_cid(&self, id: ActorID) -> fvm::kernel::Result<Option<Cid>> {
-        forward_instrumented!("get_actor_code_cid", || self.0.get_actor_code_cid(id))
+        forward_instrumented!(|| self.0.get_actor_code_cid(id))
     }
 
     fn new_actor_address(&mut self) -> fvm::kernel::Result<Address> {
-        forward_instrumented!("new_actor_address", || self.0.new_actor_address())
+        forward_instrumented!(|| self.0.new_actor_address())
     }
 
     fn create_actor(&mut self, code_id: Cid, actor_id: ActorID) -> fvm::kernel::Result<()> {
-        forward_instrumented!("create_actor", || self.0.create_actor(code_id, actor_id))
+        forward_instrumented!(|| self.0.create_actor(code_id, actor_id))
     }
 
     fn get_builtin_actor_type(&self, code_cid: &Cid) -> u32 {
-        forward_instrumented!("get_builtin_actor_type", || self
-            .0
-            .get_builtin_actor_type(code_cid))
+        forward_instrumented!(|| self.0.get_builtin_actor_type(code_cid))
     }
 
     fn get_code_cid_for_type(&self, typ: u32) -> Result<Cid> {
-        forward_instrumented!("get_code_cid_for_type", || self
-            .0
-            .get_code_cid_for_type(typ))
+        forward_instrumented!(|| self.0.get_code_cid_for_type(typ))
     }
 }
 impl<DB: Blockstore> fvm::kernel::IpldBlockOps for ForestInstrumentedKernel<DB> {
     fn block_open(&mut self, cid: &Cid) -> fvm::kernel::Result<(BlockId, BlockStat)> {
-        forward_instrumented!("block_open", || self.0.block_open(cid))
+        forward_instrumented!(|| self.0.block_open(cid))
     }
 
     fn block_create(&mut self, codec: u64, data: &[u8]) -> fvm::kernel::Result<BlockId> {
-        forward_instrumented!("block_create", || self.0.block_create(codec, data))
+        forward_instrumented!(|| self.0.block_create(codec, data))
     }
 
     fn block_link(
@@ -120,15 +119,15 @@ impl<DB: Blockstore> fvm::kernel::IpldBlockOps for ForestInstrumentedKernel<DB> 
         hash_fun: u64,
         hash_len: u32,
     ) -> fvm::kernel::Result<Cid> {
-        forward_instrumented!("block_link", || self.0.block_link(id, hash_fun, hash_len))
+        forward_instrumented!(|| self.0.block_link(id, hash_fun, hash_len))
     }
 
     fn block_read(&mut self, id: BlockId, offset: u32, buf: &mut [u8]) -> fvm::kernel::Result<i32> {
-        forward_instrumented!("block_read", || self.0.block_read(id, offset, buf))
+        forward_instrumented!(|| self.0.block_read(id, offset, buf))
     }
 
     fn block_stat(&mut self, id: BlockId) -> fvm::kernel::Result<BlockStat> {
-        forward_instrumented!("block_stat", || self.0.block_stat(id))
+        forward_instrumented!(|| self.0.block_stat(id))
     }
 }
 impl<DB: Blockstore> fvm::kernel::CircSupplyOps for ForestInstrumentedKernel<DB> {
@@ -136,7 +135,7 @@ impl<DB: Blockstore> fvm::kernel::CircSupplyOps for ForestInstrumentedKernel<DB>
         match self.1.clone() {
             Some(supply) => Ok(supply),
             None => {
-                forward_instrumented!("total_fil_circ_supply", || self.0.total_fil_circ_supply())
+                forward_instrumented!(|| self.0.total_fil_circ_supply())
             }
         }
     }
@@ -144,7 +143,7 @@ impl<DB: Blockstore> fvm::kernel::CircSupplyOps for ForestInstrumentedKernel<DB>
 
 impl<DB: Blockstore> fvm::kernel::CryptoOps for ForestInstrumentedKernel<DB> {
     fn hash(&mut self, code: u64, data: &[u8]) -> Result<cid::multihash::MultihashGeneric<64>> {
-        forward_instrumented!("hash", || self.0.hash(code, data))
+        forward_instrumented!(|| self.0.hash(code, data))
     }
 
     fn compute_unsealed_sector_cid(
@@ -152,9 +151,7 @@ impl<DB: Blockstore> fvm::kernel::CryptoOps for ForestInstrumentedKernel<DB> {
         proof_type: RegisteredSealProof,
         pieces: &[PieceInfo],
     ) -> Result<Cid> {
-        forward_instrumented!("compute_unsealed_sector_cid", || self
-            .0
-            .compute_unsealed_sector_cid(proof_type, pieces))
+        forward_instrumented!(|| self.0.compute_unsealed_sector_cid(proof_type, pieces))
     }
 
     fn verify_signature(
@@ -164,21 +161,21 @@ impl<DB: Blockstore> fvm::kernel::CryptoOps for ForestInstrumentedKernel<DB> {
         signer: &Address,
         plaintext: &[u8],
     ) -> Result<bool> {
-        forward_instrumented!("verify_signature", || self
+        forward_instrumented!(|| self
             .0
             .verify_signature(sig_type, signature, signer, plaintext))
     }
 
     fn batch_verify_seals(&mut self, vis: &[SealVerifyInfo]) -> Result<Vec<bool>> {
-        forward_instrumented!("batch_verify_seals", || self.0.batch_verify_seals(vis))
+        forward_instrumented!(|| self.0.batch_verify_seals(vis))
     }
 
     fn verify_seal(&mut self, vi: &SealVerifyInfo) -> Result<bool> {
-        forward_instrumented!("verify_seal", || self.0.verify_seal(vi))
+        forward_instrumented!(|| self.0.verify_seal(vi))
     }
 
     fn verify_post(&mut self, vi: &WindowPoStVerifyInfo) -> Result<bool> {
-        forward_instrumented!("verify_post", || self.0.verify_post(vi))
+        forward_instrumented!(|| self.0.verify_post(vi))
     }
 
     fn verify_consensus_fault(
@@ -187,21 +184,15 @@ impl<DB: Blockstore> fvm::kernel::CryptoOps for ForestInstrumentedKernel<DB> {
         h2: &[u8],
         extra: &[u8],
     ) -> Result<Option<ConsensusFault>> {
-        forward_instrumented!("verify_consensus_fault", || self
-            .0
-            .verify_consensus_fault(h1, h2, extra))
+        forward_instrumented!(|| self.0.verify_consensus_fault(h1, h2, extra))
     }
 
     fn verify_aggregate_seals(&mut self, agg: &AggregateSealVerifyProofAndInfos) -> Result<bool> {
-        forward_instrumented!("verify_aggregate_seals", || self
-            .0
-            .verify_aggregate_seals(agg))
+        forward_instrumented!(|| self.0.verify_aggregate_seals(agg))
     }
 
     fn verify_replica_update(&mut self, replica: &ReplicaUpdateInfo) -> Result<bool> {
-        forward_instrumented!("verify_replica_update", || self
-            .0
-            .verify_replica_update(replica))
+        forward_instrumented!(|| self.0.verify_replica_update(replica))
     }
 
     fn recover_secp_public_key(
@@ -209,74 +200,72 @@ impl<DB: Blockstore> fvm::kernel::CryptoOps for ForestInstrumentedKernel<DB> {
         hash: &[u8; fvm_shared::crypto::signature::SECP_SIG_MESSAGE_HASH_SIZE],
         signature: &[u8; fvm_shared::crypto::signature::SECP_SIG_LEN],
     ) -> Result<[u8; fvm_shared::crypto::signature::SECP_PUB_LEN]> {
-        forward_instrumented!("recover_secp_public_key", || self
-            .0
-            .recover_secp_public_key(hash, signature))
+        forward_instrumented!(|| self.0.recover_secp_public_key(hash, signature))
     }
 }
 impl<DB: Blockstore> DebugOps for ForestInstrumentedKernel<DB> {
     fn log(&self, msg: String) {
-        forward_instrumented!("log", || self.0.log(msg))
+        forward_instrumented!(|| self.0.log(msg))
     }
 
     fn debug_enabled(&self) -> bool {
-        forward_instrumented!("debug_enabled", || self.0.debug_enabled())
+        forward_instrumented!(|| self.0.debug_enabled())
     }
 
     fn store_artifact(&self, name: &str, data: &[u8]) -> Result<()> {
-        forward_instrumented!("store_artifact", || self.0.store_artifact(name, data))
+        forward_instrumented!(|| self.0.store_artifact(name, data))
     }
 }
 impl<DB: Blockstore> GasOps for ForestInstrumentedKernel<DB> {
     /// Returns the gas used by the transaction so far.
     fn gas_used(&self) -> Gas {
-        forward_instrumented!("gas_used", || self.0.gas_used())
+        forward_instrumented!(|| self.0.gas_used())
     }
 
     /// Returns the remaining gas for the transaction.
     fn gas_available(&self) -> Gas {
-        forward_instrumented!("gas_available", || self.0.gas_available())
+        forward_instrumented!(|| self.0.gas_available())
     }
 
     /// `charge_gas` charges specified amount of `gas` for execution.
     /// `name` provides information about gas charging point.
     fn charge_gas(&mut self, name: &str, compute: Gas) -> Result<()> {
-        forward_instrumented!("charge_gas", || self.0.charge_gas(name, compute))
+        forward_instrumented!(|| self.0.charge_gas(name, compute))
     }
 
     /// Returns the currently active gas price list.
     fn price_list(&self) -> &PriceList {
-        forward_instrumented!("price_list", || self.0.price_list())
+        forward_instrumented!(|| self.0.price_list())
     }
 }
 impl<DB: Blockstore> MessageOps for ForestInstrumentedKernel<DB> {
     fn msg_caller(&self) -> ActorID {
-        forward_instrumented!("msg_caller", || self.0.msg_caller())
+        forward_instrumented!(|| self.0.msg_caller())
     }
 
     fn msg_receiver(&self) -> ActorID {
-        forward_instrumented!("msg_receiver", || self.0.msg_receiver())
+        forward_instrumented!(|| self.0.msg_receiver())
     }
 
     fn msg_method_number(&self) -> MethodNum {
-        forward_instrumented!("msg_method_number", || self.0.msg_method_number())
+        forward_instrumented!(|| self.0.msg_method_number())
     }
 
     fn msg_value_received(&self) -> TokenAmount {
-        forward_instrumented!("msg_value_received", || self.0.msg_value_received())
+        forward_instrumented!(|| self.0.msg_value_received())
     }
 }
 impl<DB: Blockstore> NetworkOps for ForestInstrumentedKernel<DB> {
     fn network_epoch(&self) -> ChainEpoch {
-        forward_instrumented!("network_epoch", || self.0.network_epoch())
+        forward_instrumented!(|| self.0.network_epoch())
     }
 
     fn network_version(&self) -> fvm_shared::version::NetworkVersion {
-        forward_instrumented!("network_version", || self.0.network_version())
+        forward_instrumented!(|| self.0.network_version())
     }
 
     fn network_base_fee(&self) -> &TokenAmount {
-        forward_instrumented!("network_base_fee", || self.0.network_base_fee())
+        forward_instrumented!(|| self.0.network_base_fee())
     }
 }
 impl<DB: Blockstore> RandomnessOps for ForestInstrumentedKernel<DB> {
@@ -286,9 +275,11 @@ impl<DB: Blockstore> RandomnessOps for ForestInstrumentedKernel<DB> {
         rand_epoch: ChainEpoch,
         entropy: &[u8],
     ) -> Result<[u8; RANDOMNESS_LENGTH]> {
-        forward_instrumented!("get_randomness_from_tickets", || self
-            .0
-            .get_randomness_from_tickets(personalization, rand_epoch, entropy))
+        forward_instrumented!(|| self.0.get_randomness_from_tickets(
+            personalization,
+            rand_epoch,
+            entropy
+        ))
     }
 
     fn get_randomness_from_beacon(
@@ -297,26 +288,28 @@ impl<DB: Blockstore> RandomnessOps for ForestInstrumentedKernel<DB> {
         rand_epoch: ChainEpoch,
         entropy: &[u8],
     ) -> Result<[u8; RANDOMNESS_LENGTH]> {
-        forward_instrumented!("get_randomness_from_beacon", || self
-            .0
-            .get_randomness_from_beacon(personalization, rand_epoch, entropy))
+        forward_instrumented!(|| self.0.get_randomness_from_beacon(
+            personalization,
+            rand_epoch,
+            entropy
+        ))
     }
 }
 impl<DB: Blockstore> SelfOps for ForestInstrumentedKernel<DB> {
     fn root(&self) -> Result<Cid> {
-        forward_instrumented!("root", || self.0.root())
+        forward_instrumented!(|| self.0.root())
     }
 
     fn set_root(&mut self, root: Cid) -> Result<()> {
-        forward_instrumented!("set_root", || self.0.set_root(root))
+        forward_instrumented!(|| self.0.set_root(root))
     }
 
     fn current_balance(&self) -> Result<TokenAmount> {
-        forward_instrumented!("current_balance", || self.0.current_balance())
+        forward_instrumented!(|| self.0.current_balance())
     }
 
     fn self_destruct(&mut self, beneficiary: &Address) -> Result<()> {
-        forward_instrumented!("self_destruct", || self.0.self_destruct(beneficiary))
+        forward_instrumented!(|| self.0.self_destruct(beneficiary))
     }
 }
 impl<DB: Blockstore> SendOps for ForestInstrumentedKernel<DB> {
@@ -327,6 +320,6 @@ impl<DB: Blockstore> SendOps for ForestInstrumentedKernel<DB> {
         params: BlockId,
         value: &TokenAmount,
     ) -> Result<SendResult> {
-        forward_instrumented!("send", || self.0.send(recipient, method, params, value))
+        forward_instrumented!(|| self.0.send(recipient, method, params, value))
     }
 }
