62e5c74b7c5d56b5020f9f0eb2fba2d02a8cc499
elmattic <elmattic@users.noreply.github.com>
2022-11-25 12:45:56 +0100

    rubocop

--- scripts/bench.rb
+++ scripts/bench.rb
@@ -86,10 +86,11 @@ class Numeric
   end
 end
 
+# Performs a simple deep merge for nested Hashes
 class ::Hash
   def deep_merge(second)
     merger = proc { |_key, v1, v2| v1.is_a?(Hash) && v2.is_a?(Hash) ? v1.merge(v2, &merger) : v2 }
-    self.merge(second, &merger)
+    merge(second, &merger)
   end
 end
 
