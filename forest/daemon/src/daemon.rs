d7139394b5d457a15ea5b62599ba862f74c0d948
tyshkor <rostyslav@chainsafe.io>
2022-12-06 16:28:40 +0100

    revert to old behaviour

--- forest/daemon/src/daemon.rs
+++ forest/daemon/src/daemon.rs
@@ -377,7 +377,7 @@ pub(super) async fn start(config: Config, detached: bool) -> Db {
 
     services.spawn(p2p_service.run());
 
-    while services.len() > 0 {
+    loop {
         select! {
             option = services.join_next().fuse() => {
                 if let Some(res) = option {
