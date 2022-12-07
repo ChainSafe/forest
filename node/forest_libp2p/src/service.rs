2e25e2c65e9f8cfc4846df74f52d5b97b8dcd0eb
tyshkor <rostyslav@chainsafe.io>
2022-12-07 13:24:33 +0100

    fix unwrap

--- node/forest_libp2p/src/service.rs
+++ node/forest_libp2p/src/service.rs
@@ -218,7 +218,8 @@ where
     /// Starts the libp2p service networking stack. This Future resolves when shutdown occurs.
     pub async fn run(mut self) -> Result<(), anyhow::Error> {
         info!("Running libp2p service");
-        Swarm::listen_on(&mut self.swarm, self.config.listening_multiaddr).unwrap();
+        Swarm::listen_on(&mut self.swarm, self.config.listening_multiaddr)
+            .map_err(|e| anyhow::anyhow!("{e}"))?;
         // Bootstrap with Kademlia
         if let Err(e) = self.swarm.behaviour_mut().bootstrap() {
             warn!("Failed to bootstrap with Kademlia: {e}");
