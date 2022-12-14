73210038c503dbfde03c59416a302e2b809976ef
creativcoder <rahul@chainsafe.io>
2022-12-14 13:29:47 +0000

    hope this works

--- forest/cli/src/cli/snapshot_cmd.rs
+++ forest/cli/src/cli/snapshot_cmd.rs
@@ -378,7 +378,11 @@ async fn validate(
 
         let db_path = tmp_db_path.path().join(&config.chain.name);
 
-        let db = forest_db::rocks::RocksDb::open(db_path, &config.rocks_db)?;
+        let db = open_db(
+            &db_path,
+            #[cfg(feature = "rocksdb")]
+            config,
+        )?;
 
         let chain_store = Arc::new(ChainStore::new(db).await);
 
@@ -485,5 +489,27 @@ fn delete_snapshot(snapshot_path: &PathBuf) {
                 println!("Deleted {}", path.display());
             }
         }
+    }
+}
+
+#[cfg(feature = "rocksdb")]
+type Database = forest_db::rocks::RocksDb;
+
+#[cfg(feature = "paritydb")]
+type Database = forest_db::parity_db::ParityDb;
+
+fn open_db(
+    path: &std::path::Path,
+    #[cfg(feature = "rocksdb")] config: &Config,
+) -> Result<Database, anyhow::Error> {
+    #[cfg(feature = "rocksdb")]
+    {
+        forest_db::rocks::RocksDb::open(path, &config.rocks_db)
+            .map_err(|e| anyhow::anyhow!("failed to open db: {}", e))
+    }
+    #[cfg(feature = "paritydb")]
+    {
+        let paritydb_config = forest_db::parity_db::ParityDbConfig::from_path(path);
+        forest_db::parity_db::ParityDb::open(&paritydb_config)
     }
 }
