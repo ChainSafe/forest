8ac8ebb4da340363bc6d65958a44838f11a1b11b
hanabi1224 <harlowmoo@gmail.com>
2023-01-11 19:02:18 +0800

    resolve review comments

--- scripts/find_unused_deps.rb
+++ scripts/find_unused_deps.rb
@@ -5,21 +5,21 @@ require 'set'
 
 exit_code = 0
 
-Dir.glob("#{File.dirname(File.expand_path($PROGRAM_NAME))}/../**/*.toml").each do |file|
+Dir.glob('**/*.toml').each do |file|
   crate_dir = File.dirname(file)
   toml = TomlRB.load_file(file)
   crates = Set.new
-  toml['dependencies']&.each do |pair|
-    crates.add pair[0]
+  toml['dependencies']&.each do |crate_name, _|
+    crates.add crate_name
   end
-  toml['dev-dependencies']&.each do |pair|
-    crates.add pair[0]
+  toml['dev-dependencies']&.each do |crate_name, _|
+    crates.add crate_name
   end
   crates.each do |crate_raw|
     crate = crate_raw.gsub(/-/, '_')
     used = false
     Dir.glob("#{crate_dir}/**/*.rs").each do |rs|
-      used = true if !used && File.read(rs).match?(Regexp.new("(\buse\\s#{crate}\\b)|(\\b#{crate}::)"))
+      used |= File.read(rs).match?(Regexp.new("(\\buse\\s#{crate}\\b)|(\\b#{crate}::)"))
     end
     unless used
       puts "Protentially unused: #{crate_raw} in #{crate_dir}"
