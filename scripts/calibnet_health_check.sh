1c591abb13da60b8fee8d6dbebc06619eb6d8da9
creativcoder <rahul@chainsafe.io>
2023-01-11 14:31:30 +0000

    add wallet checks in ci

--- scripts/calibnet_health_check.sh
+++ scripts/calibnet_health_check.sh
@@ -41,3 +41,32 @@ echo "--- Forest Prometheus metrics ---"; cat metrics.log
 # print forest log files
 ls -hl log
 cat log/*
+
+## Wallet tests
+
+FIL_AMT=500
+ADMIN_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXSwiZXhwIjoxNjc4NTMwODk3fQ.E0Glkq-Br5x3wLCes8HS20wAfzlSGqVX-mo0WKeYXRc
+# Wallet addresses: 
+ADDR_ONE=f1qmmbzfb3m6fijab4boagmkx72ouxhh7f2ylgzlq
+# ADDR_TWO=f1zy4ylxlqplz3ulqscmta4ay7aijphopmmmjcjqq
+
+# Show balance
+forest-cli --chain calibnet --token $ADMIN_TOKEN wallet list
+
+# # create new address
+# ADDR_TWO=`forest-cli --chain calibnet --token $ADMIN_TOKEN wallet new`
+
+# # send FIL to the above address
+# forest-cli --token send $ADDR_TWO $FIL_AMT
+
+# # Check balance of addr_two
+# timeout 30 min && ./target/release/forest-cli --chain calibnet --token $ADMIN_TOKEN wallet balance $ADDR_TWO
+# # Export wallet
+# forest-cli --chain calibnet --token $ADMIN_TOKEN wallet export $ADDR_ONE
+
+# Get and print metrics and logs and kill forest
+# wget -O metrics.log http://localhost:6116/metrics
+# pkill forest
+# echo "--- Forest STDOUT ---"; cat forest.out
+# echo "--- Forest STDERR ---"; cat forest.err
+# echo "--- Forest Prometheus metrics ---"; cat metrics.log
