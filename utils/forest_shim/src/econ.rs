2c919581f27c3b79e95eb0ac174fa4bef5e76c82
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-01-19 16:28:55 +0100

    Use the new TokenAmount in a few places.

--- utils/forest_shim/src/econ.rs
+++ utils/forest_shim/src/econ.rs
@@ -3,12 +3,27 @@
 
 use fvm_shared::econ::TokenAmount as TokenAmount_v2;
 use fvm_shared3::econ::TokenAmount as TokenAmount_v3;
+use num_bigint::BigInt;
 use serde::{Deserialize, Serialize};
-use std::ops::{Deref, DerefMut};
+use std::ops::{AddAssign, Deref, DerefMut, Mul};
 
-#[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+// FIXME: Transparent Debug trait impl
+#[derive(Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Debug, Default)]
 #[serde(transparent)]
 pub struct TokenAmount(TokenAmount_v3);
+
+impl std::fmt::Display for TokenAmount {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl TokenAmount {
+    pub fn from_atto(atto: impl Into<BigInt>) -> Self {
+        TokenAmount_v3::from_atto(atto).into()
+    }
+}
 
 impl Deref for TokenAmount {
     type Target = TokenAmount_v3;
@@ -44,5 +59,25 @@ impl From<TokenAmount> for TokenAmount_v3 {
 impl From<TokenAmount> for TokenAmount_v2 {
     fn from(other: TokenAmount) -> TokenAmount_v2 {
         TokenAmount_v2::from_atto(other.atto().clone())
+    }
+}
+
+impl Mul<BigInt> for TokenAmount {
+    type Output = TokenAmount;
+    fn mul(self, rhs: BigInt) -> Self::Output {
+        self.0.mul(rhs).into()
+    }
+}
+
+impl Mul<BigInt> for &TokenAmount {
+    type Output = TokenAmount;
+    fn mul(self, rhs: BigInt) -> Self::Output {
+        (&self.0).mul(rhs).into()
+    }
+}
+
+impl AddAssign for TokenAmount {
+    fn add_assign(&mut self, other: Self) {
+        self.0.add_assign(other.0)
     }
 }
