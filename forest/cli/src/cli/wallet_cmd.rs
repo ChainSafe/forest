0cc5336eb1fbc5e394531bdb9097ed40d3d8298d
tyshkor <rostyslav@chainsafe.io>
2023-01-24 11:54:21 +0100

    lint fixes

--- forest/cli/src/cli/wallet_cmd.rs
+++ forest/cli/src/cli/wallet_cmd.rs
@@ -86,19 +86,19 @@ pub enum WalletCommands {
     List {
         /// flag to force full accuracy,
         /// not just default 4 significant digits
-        /// E.g. 500.2367798 milli FIL instead of 500.2367 milli FIL
-        /// In compination with `--fixed-unit` flag
+        /// E.g. 500.2367798 `milli FIL` instead of 500.2367 `milli FIL`
+        /// In combination with `--fixed-unit` flag
         /// it will show exact data in `FIL` units
-        /// E.g. 0.0000002367798 FIL instead of 0 FIL
+        /// E.g. 0.0000002367798 `FIL` instead of 0 `FIL`
         #[structopt(short, long)]
         exact_balance: bool,
         /// flag to force the balance to be in `FIL`
         /// meaning one won't balance in `atto` or `micro`
         /// form even if it is appropriate
-        /// E.g. 0.5002 FIL instead of 500.2367 milli FIL
-        /// In compination with `--exact-balance` flag
+        /// E.g. 0.5002 `FIL` instead of 500.2367 `milli FIL`
+        /// In combination with `--exact-balance` flag
         /// it will show exact data in `FIL` units
-        /// E.g. 0.0000002367798 FIL instead of 0 FIL
+        /// E.g. 0.0000002367798 `FIL` instead of 0 `FIL`
         #[structopt(short, long)]
         fixed_unit: bool,
     },
