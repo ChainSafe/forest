0b498d11aafc78b15e5edc37260ec21d6f15a9a8
elmattic <elmattic@users.noreply.github.com>
2023-01-25 09:15:34 +0100

    Add bigint module to forest shim

---
+++ utils/forest_shim/src/bigint.rs
@@ -0,0 +1,29 @@
+// Copyright 2019-2023 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared3::bigint::BigInt as BigInt_v3;
+use serde::{Deserialize, Serialize};
+use std::ops::{Deref, DerefMut};
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct BigInt(BigInt_v3);
+
+impl Deref for BigInt {
+    type Target = BigInt_v3;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for BigInt {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl From<BigInt_v3> for BigInt {
+    fn from(other: BigInt_v3) -> Self {
+        BigInt(other)
+    }
+}
