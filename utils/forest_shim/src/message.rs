01259b6cac496d08b5b57e6dedbc0887f0139417
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-02-14 16:15:19 +0100

    add license header

--- utils/forest_shim/src/message.rs
+++ utils/forest_shim/src/message.rs
@@ -1,3 +1,5 @@
+// Copyright 2019-2023 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
 use fvm_shared::message::Message as Message_v2;
 // use fvm_shared3::message::Message as Message_v3;
 
