c80f0e923eafa592506fea04511dc63c20375316
hanabi1224 <harlowmoo@gmail.com>
2023-02-15 17:54:09 +0800

    try 64 bit hash

--- ipld/src/hashset.rs
+++ ipld/src/hashset.rs
@@ -1,22 +1,28 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use ahash::{HashSet, HashSetExt};
+use ahash::{HashSet, HashSetExt, RandomState};
 use cid::Cid;
 
 pub struct CidHashSet {
-    mem: HashSet<Vec<u8>>,
+    mem: HashSet<u64>,
+    hasher: RandomState,
 }
 
 impl CidHashSet {
     pub fn new() -> anyhow::Result<Self> {
         let mem = HashSet::new();
-        Ok(CidHashSet { mem })
+        Ok(CidHashSet {
+            mem,
+            hasher: RandomState::new(),
+        })
     }
 
     pub fn insert(&mut self, cid: &Cid) -> bool {
-        let hash = cid.hash().digest();
-        // self.mem.insert(hash[..hash.len().min(16)].to_vec())
-        self.mem.insert(hash.to_vec())
+        // let hash = cid.hash().digest();
+        // // self.mem.insert(hash[..hash.len().min(16)].to_vec())
+        // self.mem.insert(hash.to_vec())
+        let hash = self.hasher.hash_one(cid.to_bytes());
+        self.mem.insert(hash)
     }
 }
