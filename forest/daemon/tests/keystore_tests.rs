15420aa33bd1aa7c2f5b2347a6c6ff817bcfb6e2
Hubert Bugaj <hubert@chainsafe.io>
2023-02-21 12:22:06 +0100

    add new flags to the daemon

--- forest/daemon/tests/keystore_tests.rs
+++ forest/daemon/tests/keystore_tests.rs
@@ -5,13 +5,52 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use assert_cmd::Command;
+use forest_key_management::{ENCRYPTED_KEYSTORE_NAME, FOREST_KEYSTORE_PHRASE_ENV, KEYSTORE_NAME};
 use tempfile::TempDir;
 
 // https://github.com/ChainSafe/forest/issues/2499
 #[test]
 fn forest_headless_encrypt_keystore_no_passphrase_should_fail() -> Result<()> {
     let (config_file, _data_dir) = create_tmp_config()?;
-    cli()?.arg("--config").arg(config_file).assert().failure();
+    cli()?
+        .arg("--config")
+        .arg(config_file)
+        .arg("--exit-after-init")
+        .assert()
+        .failure();
+
+    Ok(())
+}
+
+#[test]
+fn forest_headless_no_encrypt_no_passphrase_should_succeed() -> Result<()> {
+    let (config_file, data_dir) = create_tmp_config()?;
+    cli()?
+        .arg("--config")
+        .arg(config_file)
+        .arg("--encrypt-keystore")
+        .arg("false")
+        .arg("--exit-after-init")
+        .assert()
+        .success();
+
+    assert!(data_dir.path().join(KEYSTORE_NAME).exists());
+
+    Ok(())
+}
+
+#[test]
+fn forest_headless_encrypt_keystore_with_passphrase_should_succeed() -> Result<()> {
+    let (config_file, data_dir) = create_tmp_config()?;
+    cli()?
+        .env(FOREST_KEYSTORE_PHRASE_ENV, "yuggoth")
+        .arg("--config")
+        .arg(config_file)
+        .arg("--exit-after-init")
+        .assert()
+        .success();
+
+    assert!(data_dir.path().join(ENCRYPTED_KEYSTORE_NAME).exists());
 
     Ok(())
 }
