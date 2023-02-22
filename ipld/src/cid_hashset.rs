3c9083c6a107d9317f1be2573b6c03bdbcb25358
hanabi1224 <harlowmoo@gmail.com>
2023-02-22 22:50:29 +0800

    simplify the code

--- ipld/src/cid_hashset.rs
+++ ipld/src/cid_hashset.rs
@@ -1,26 +1,15 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use ahash::{HashSet, HashSetExt, RandomState};
+use ahash::HashSet;
 use cid::Cid;
 
-pub struct CidHashSet {
-    set: HashSet<u64>,
-    hasher: RandomState,
-}
+#[derive(Default)]
+pub struct CidHashSet(HashSet<u64>);
 
 impl CidHashSet {
     pub fn insert(&mut self, cid: &Cid) -> bool {
-        let hash = self.hasher.hash_one(cid.to_bytes());
-        self.set.insert(hash)
-    }
-}
-
-impl Default for CidHashSet {
-    fn default() -> Self {
-        Self {
-            set: HashSet::new(),
-            hasher: RandomState::new(),
-        }
+        let hash = self.0.hasher().hash_one(cid.to_bytes());
+        self.0.insert(hash)
     }
 }
