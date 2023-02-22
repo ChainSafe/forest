91abb35074751f212898e134c7b03f458be7073b
sudo-shashank <shashank@chainsafe.io>
2023-02-22 12:53:44 +0530

    Added some retry macros test comments

--- forest/shared/tests/macros_test.rs
+++ forest/shared/tests/macros_test.rs
@@ -28,18 +28,26 @@ async fn retryable_function(counter: &mut i32) -> Result<(), ()> {
 }
 
 #[tokio::test]
+// Tests that the retry macro correctly handles a function that always fails.
+// This case should return Err after the maximum number of retries is exceeded.
 async fn test_retry_macro_failing_function() {
     let result = retry!(failing_function, 3, Duration::from_millis(100));
     assert!(result.is_err());
 }
 
 #[tokio::test]
+// Tests that the retry macro correctly handles a function that always succeeds.
+// This case should return Ok without retrying the function.
 async fn test_retry_macro_successful_function() {
     let result = retry!(successful_function, 3, Duration::from_millis(100));
     assert!(result.is_ok());
 }
 
 #[tokio::test]
+// Tests that the retry macro correctly handles a function that may fail a few
+// times before succeeding. This case should return Ok after retrying the
+// function a few times and modifying the argument that is passed to the
+// function.
 async fn test_retry_macro_retryable_function() {
     let mut counter = 3;
     let result = retry!(
