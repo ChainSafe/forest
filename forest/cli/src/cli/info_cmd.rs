69e6f6a195ed8488dd8c2ae61e679ccde4615010
creativcoder <rahul@chainsafe.io>
2023-02-28 15:44:34 +0000

    lints

--- forest/cli/src/cli/info_cmd.rs
+++ forest/cli/src/cli/info_cmd.rs
@@ -1,8 +1,8 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use std::time::Instant;
-use std::string::ToString;
+use std::{string::ToString, time::Instant};
+
 use clap::Subcommand;
 use colored::*;
 use forest_blocks::tipset_keys_json::TipsetKeysJson;
