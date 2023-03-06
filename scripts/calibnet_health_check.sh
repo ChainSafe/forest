46a9ac4d495c7db402bac9b6ec83be72fb41cde0
elmattic <elmattic@users.noreply.github.com>
2023-03-06 19:41:01 +0100

    Add tweakable path for forest binaries plus some fixes

--- scripts/calibnet_health_check.sh
+++ scripts/calibnet_health_check.sh
@@ -3,37 +3,40 @@
 SNAPSHOT_DIRECTORY="/tmp/snapshots"
 LOG_DIRECTORY="/tmp/log"
 
+FOREST_PATH="forest"
+FOREST_CLI_PATH="forest-cli"
+
 echo "Fetching params"
-forest-cli fetch-params --keys
+$FOREST_CLI_PATH fetch-params --keys
 echo "Downloading snapshot"
-forest-cli --chain calibnet snapshot fetch --aria2 -s $SNAPSHOT_DIRECTORY
+$FOREST_CLI_PATH --chain calibnet snapshot fetch --aria2 -s $SNAPSHOT_DIRECTORY
 
 echo "Importing snapshot and running Forest"
-forest --chain calibnet --encrypt-keystore false --halt-after-import --height=-200 --import-snapshot $SNAPSHOT_DIRECTORY/*.car
+$FOREST_PATH --chain calibnet --encrypt-keystore false --halt-after-import --height=-200 --import-snapshot $SNAPSHOT_DIRECTORY/*.car
 echo "Checking DB stats"
-forest-cli --chain calibnet db stats
+$FOREST_CLI_PATH --chain calibnet db stats
 echo "Running forest in detached mode"
-forest --chain calibnet --encrypt-keystore false --log-dir $LOG_DIRECTORY --detach --save-token ./admin_token
+$FOREST_PATH --chain calibnet --encrypt-keystore false --log-dir $LOG_DIRECTORY --detach --save-token ./admin_token
 
 echo "Validating checkpoint tipset hashes"
-forest-cli chain validate-tipset-checkpoints
+$FOREST_CLI_PATH chain validate-tipset-checkpoints
 
 echo "Waiting for sync and check health"
-timeout 30m forest-cli --chain calibnet sync wait && forest-cli --chain calibnet db stats
+timeout 30m $FOREST_CLI_PATH --chain calibnet sync wait && $FOREST_CLI_PATH --chain calibnet db stats
 echo "Exporting snapshot"
-forest-cli --chain calibnet snapshot export
+$FOREST_CLI_PATH --chain calibnet snapshot export
 
 echo "Verifing snapshot checksum"
 sha256sum -c ./*.sha256sum
 
 echo "Testing js console"
-forest-cli attach --exec 'showPeers()'
+$FOREST_CLI_PATH attach --exec 'showPeers()'
 
 echo "Validating as mainnet snapshot"
-forest-cli --chain mainnet snapshot validate $SNAPSHOT_DIRECTORY/*.car --force && \
+$FOREST_CLI_PATH --chain mainnet snapshot validate $SNAPSHOT_DIRECTORY/*.car --force && \
 { echo "mainnet snapshot validation with calibnet snapshot should fail"; return 1; }
 echo "Validating as calibnet snapshot"
-forest-cli --chain calibnet snapshot validate $SNAPSHOT_DIRECTORY/*.car --force
+$FOREST_CLI_PATH --chain calibnet snapshot validate $SNAPSHOT_DIRECTORY/*.car --force
 
 echo "--- Forest STDOUT ---"; cat forest.out
 echo "--- Forest STDERR ---"; cat forest.err
@@ -56,7 +59,7 @@ ADMIN_TOKEN=$(cat admin_token)
 ADDR_ONE=f1qmmbzfb3m6fijab4boagmkx72ouxhh7f2ylgzlq
 
 echo "Importing preloaded wallet key"
-forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet import scripts/preloaded_wallet.key
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet import scripts/preloaded_wallet.key
 sleep 5s
 
 echo "Fetching metrics"
@@ -66,31 +69,38 @@ sleep 5s
 
 # Show balances
 echo "Listing wallet balances"
-forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet list
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet list
 
-echo "Creating a new address to send FIL to"
-ADDR_TWO=$(forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet new)
+echo "Creating a new address to send FIL to:"
+ADDR_TWO=$($FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet new)
+echo $ADDR_TWO
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet set-default "t1ac6ndwj6nghqbmtbovvnwcqo577p6ox2pt52q2y"
 
-echo "Send FIL to the above address"
-forest-cli --token "$ADMIN_TOKEN" send "$ADDR_TWO" "$FIL_AMT"
+echo "Listing wallet balances"
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet list
 
-echo "Check balance of $ADDR_TWO"
+echo "Sending FIL to the above address"
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" send "$ADDR_TWO" "$FIL_AMT"
 
-sleep 3m
+echo "Checking balance of $ADDR_TWO..."
 
-forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet balance "$ADDR_TWO"
+sleep 4m
+
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet balance "$ADDR_TWO"
 
 echo "Exporting wallet with "
-forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet export "$ADDR_ONE" > addr_two_pkey.test.key
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet export "$ADDR_ONE" > addr_two_pkey.test.key
 echo "Importing wallet"
-forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet import addr_two_pkey.test.key || true
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet import addr_two_pkey.test.key || true
 
-# wallet list should contain address two will transfered FIL amount
-forest-cli --chain calibnet --token "$ADMIN_TOKEN" wallet list
+# wallet list should contain address two with transfered FIL amount
+$FOREST_CLI_PATH --chain calibnet --token "$ADMIN_TOKEN" wallet list
 
-Get and print metrics and logs and kill forest
+echo "Get and print metrics and logs and stop forest"
 wget -O metrics.log http://localhost:6116/metrics
-pkill forest
+
+$FOREST_CLI_PATH --token "$ADMIN_TOKEN" shutdown --force
+
 echo "--- Forest STDOUT ---"; cat forest.out
 echo "--- Forest STDERR ---"; cat forest.err
 echo "--- Forest Prometheus metrics ---"; cat metrics.log
