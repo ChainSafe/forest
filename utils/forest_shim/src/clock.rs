1a0bded36071b98553fa177e28c92ff3d6f86c55
sudo-shashank <shashank@chainsafe.io>
2023-03-09 17:20:39 +0530

    define EPOCHS_IN_DAY in forest

--- utils/forest_shim/src/clock.rs
+++ utils/forest_shim/src/clock.rs
@@ -1,5 +1,8 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use fil_actors_runtime_v9::builtin::network;
-pub const EPOCHS_IN_DAY: fvm_shared::clock::ChainEpoch = network::EPOCHS_IN_DAY;
+/// Duration of each tipset epoch.
+pub const EPOCH_DURATION_SECONDS: i64 = 30;
+
+pub const SECONDS_IN_DAY: i64 = 86400;
+pub const EPOCHS_IN_DAY: i64 = SECONDS_IN_DAY / EPOCH_DURATION_SECONDS;
