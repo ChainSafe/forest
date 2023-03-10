0da683b0c5630e663dd3f5c8d1b6f799b904ddab
sudo-shashank <shashank@chainsafe.io>
2023-03-10 12:29:16 +0530

    Use EPOCH_DURATION_SECONDS from fvm

--- utils/forest_shim/src/clock.rs
+++ utils/forest_shim/src/clock.rs
@@ -1,8 +1,7 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-/// Duration of each tipset epoch.
-pub const EPOCH_DURATION_SECONDS: i64 = 30;
+use fvm_shared::clock::EPOCH_DURATION_SECONDS;
 
 pub const SECONDS_IN_DAY: i64 = 86400;
 pub const EPOCHS_IN_DAY: i64 = SECONDS_IN_DAY / EPOCH_DURATION_SECONDS;
