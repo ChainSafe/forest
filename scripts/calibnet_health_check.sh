65b071e5de8c222cc129f1b37474a269b6cc7e68
elmattic <elmattic@users.noreply.github.com>
2023-03-16 15:24:17 +0100

    Remove old paths

--- scripts/calibnet_health_check.sh
+++ scripts/calibnet_health_check.sh
@@ -14,9 +14,6 @@ fi
 
 echo "$1" > preloaded_wallet.key
 
-SNAPSHOT_DIRECTORY="/tmp/snapshots"
-LOG_DIRECTORY="/tmp/log"
-
 TMP_DIR=$(mktemp --directory)
 SNAPSHOT_DIRECTORY=$TMP_DIR/snapshots
 LOG_DIRECTORY=$TMP_DIR/logs
