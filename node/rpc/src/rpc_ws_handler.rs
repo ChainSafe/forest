9e052edbcc79ea1b1bb79a2fe37b6a69ae0c7180
hanabi1224 <harlowmoo@gmail.com>
2023-03-16 08:27:15 +0800

    fix clippy

--- node/rpc/src/rpc_ws_handler.rs
+++ node/rpc/src/rpc_ws_handler.rs
@@ -50,7 +50,10 @@ pub async fn rpc_ws_handler(
     ws: WebSocketUpgrade,
 ) -> impl IntoResponse {
     let authorization_header = get_auth_header(headers);
-    ws.on_upgrade(move |socket| rpc_ws_handler_inner(socket, authorization_header, rpc_server))
+    #[allow(clippy::redundant_async_block)]
+    ws.on_upgrade(move |socket| async {
+        rpc_ws_handler_inner(socket, authorization_header, rpc_server).await
+    })
 }
 
 async fn rpc_ws_handler_inner(
