4f3cc2e56662ce95870e1644e8dfa16bed72e548
creativcoder <rahul@chainsafe.io>
2023-03-24 13:01:56 +0000

    spellcheck fix

--- utils/forest_utils/src/io/parser.rs
+++ utils/forest_utils/src/io/parser.rs
@@ -15,22 +15,22 @@ pub fn parse_duration(arg: &str) -> anyhow::Result<Duration> {
 }
 
 #[allow(clippy::enum_variant_names)]
-pub enum FormattingMode {
+enum FormattingMode {
     /// mode to show data in `FIL` units
     /// in full accuracy
-    /// E.g. 0.50023677980 `FIL`
+    /// E.g. `0.50023677980 FIL`
     ExactFixed,
     /// mode to show data in `FIL` units
     /// with 4 significant digits
-    /// E.g. 0.5002 `FIL`
+    /// E.g. `0.5002 FIL`
     NotExactFixed,
     /// mode to show data in SI units
     /// in full accuracy
-    /// E.g. 500.2367798 `milli FIL`
+    /// E.g. `500.2367798 milli FIL`
     ExactNotFixed,
     /// mode to show data in SI units
     /// with 4 significant digits
-    /// E.g. ~500.2 mill `FIL`
+    /// E.g. `~500.2 milli FIL`
     NotExactNotFixed,
 }
 
