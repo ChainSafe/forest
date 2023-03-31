6a10cfd9c479f262173c34eada36abbd17784f58
elmattic <elmattic@users.noreply.github.com>
2023-03-31 16:50:23 +0200

    Add local, to and from filters like in Lotus

--- forest/cli/src/cli/mpool_cmd.rs
+++ forest/cli/src/cli/mpool_cmd.rs
@@ -1,9 +1,11 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use ahash::HashSet;
 use clap::Subcommand;
 use forest_json::cid::vec::CidJsonVec;
-use forest_rpc_client::mpool_ops::*;
+use forest_rpc_client::{mpool_ops::mpool_pending, wallet_ops::wallet_list};
+use forest_shim::address::Address;
 use fvm_ipld_encoding::Cbor;
 
 use super::Config;
@@ -19,6 +21,12 @@ pub enum MpoolCommands {
         /// Only print cids of messages in output
         #[arg(long)]
         cids: bool,
+        /// Return messages to a given address
+        #[arg(long)]
+        to: Option<Address>,
+        /// Return messages from a given addres
+        #[arg(long)]
+        from: Option<Address>,
     },
     /// Print mempool stats
     Stat {
@@ -34,13 +42,40 @@ pub enum MpoolCommands {
 impl MpoolCommands {
     pub async fn run(&self, config: Config) -> anyhow::Result<()> {
         match *self {
-            Self::Pending { local, cids } => {
-                let response = mpool_pending((CidJsonVec(vec![]),), &config.client.rpc_token)
+            Self::Pending {
+                local,
+                cids,
+                to,
+                from,
+            } => {
+                let messages = mpool_pending((CidJsonVec(vec![]),), &config.client.rpc_token)
                     .await
                     .map_err(handle_rpc_err)?;
-                for msg in response {
+
+                let wallet_addrs = if local {
+                    let response = wallet_list((), &config.client.rpc_token)
+                        .await
+                        .map_err(handle_rpc_err)?;
+                    Some(HashSet::from_iter(
+                        response.iter().filter_map(|addr| addr.0.cid().ok()),
+                    ))
+                } else {
+                    None
+                };
+
+                let filtered_messages = messages.iter().filter(|msg| {
+                    wallet_addrs
+                        .as_ref()
+                        .map(|addrs| !addrs.contains(&msg.0.cid().unwrap()))
+                        .unwrap_or(true)
+                        && to.map(|addr| msg.0.message().to == *addr).unwrap_or(true)
+                        && from
+                            .map(|addr| msg.0.message().from == *addr)
+                            .unwrap_or(true)
+                });
+                for msg in filtered_messages {
                     if cids {
-                        println!("{}", msg.0.cid()?);
+                        println!("{}", msg.0.cid().unwrap());
                     } else {
                         println!("{}", serde_json::to_string_pretty(&msg)?);
                     }
