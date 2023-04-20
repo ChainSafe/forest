3e8e3d2df506831d2725679c4df1f0c17d8c1376
elmattic <elmattic@users.noreply.github.com>
2023-04-20 14:11:35 +0200

    Fix missing claim v11 import

--- vm/state_migration/src/nv19/power.rs
+++ vm/state_migration/src/nv19/power.rs
@@ -21,6 +21,7 @@ use forest_shim::{
 use forest_utils::db::BlockstoreExt;
 use fvm_ipld_blockstore::Blockstore;
 use fil_actor_miner_v11::convert_window_post_proof_v1p1_to_v1;
+use fil_actor_power_v11::Claim as ClaimV11;
 
 // TODO: get convert_window_post_proof_v1p1_to_v1 from v11 miner
 use crate::common::{ActorMigration, ActorMigrationInput, ActorMigrationOutput};
@@ -52,7 +53,7 @@ impl<BS: Blockstore + Clone + Send + Sync> ActorMigration<BS> for PowerMigrator 
         // TODO: should be v11
         let out_claims = make_map_with_root_and_bitwidth(&empty_claims, &store, HAMT_BIT_WIDTH)?;
 
-        in_claims.for_each(|key, claim| {
+        in_claims.for_each(|key, claim: &ClaimV11| {
             let address = Address::from_bytes(key)?;
             let new_proof_type = convert_window_post_proof_v1p1_to_v1(claim.window_post_proof_type);
             // TODO: use v11 Claim
