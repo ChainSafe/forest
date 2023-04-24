9c77aaa0a63e972a612cc9602f355993fc9e901b
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-04-24 13:12:50 +0200

    Include limited arbitrary implementation for TokenAmount

--- utils/forest_shim/src/econ.rs
+++ utils/forest_shim/src/econ.rs
@@ -15,6 +15,20 @@ use serde::{Deserialize, Serialize};
 #[serde(transparent)]
 pub struct TokenAmount(TokenAmount_v3);
 
+impl quickcheck::Arbitrary for TokenAmount {
+    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+        use fvm_shared3::bigint::MAX_BIGINT_SIZE;
+        // During serialization/deserialization, permissible length of the byte
+        // representation (plus a leading positive sign byte for non-zero
+        // values) of BigInts is currently set to a max of MAX_BIGINT_SIZE with
+        // a value of 128; need to constrain the corresponding length during
+        // `Arbitrary` generation of `BigInt` in `TokenAmount` to below
+        // MAX_BIGINT_SIZE.
+        let bigint_upper_limit = BigInt::from(1) << ((MAX_BIGINT_SIZE - 1) * 8);
+        TokenAmount::from_atto(BigInt::arbitrary(g) % bigint_upper_limit)
+    }
+}
+
 impl Zero for TokenAmount {
     fn zero() -> Self {
         TokenAmount(TokenAmount_v3::zero())
