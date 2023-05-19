f638ecb882f76831e2b82ef8a7b87b34c496d5e9
Josh Jones <joshua.jones@chainsafe.io>
2023-05-19 09:25:31 -0500

    add `time` crate ban

--- scripts/linters/find_banned_deps.rb
+++ scripts/linters/find_banned_deps.rb
@@ -4,12 +4,14 @@ require 'toml-rb'
 
 # Iterate over all TOML files in the repository
 Dir.glob('**/*.toml').each do |file|
-  # Create an exception for `forest_shim` (only place `fvm` & `fvm3` are allowed).
-  next unless file != 'utils/forest_shim/Cargo.toml'
-
   toml = TomlRB.load_file(file)
 
   toml['dependencies']&.each do |dep|
+    # Check all `Cargo.toml` files for `time` crate
+    puts "[#{dep[0]}] is being used in [#{file}], but this crate is banned!" if dep[0] == 'time'
+    # Create an exception for `forest_shim` (only place `fvm` & `fvm3` are allowed).
+    next unless file != 'utils/forest_shim/Cargo.toml'
+
     puts "[#{dep[0]}] is being used in [#{file}], but this crate is banned!" if dep[0] == 'fvm' || dep[0] == 'fvm3'
   end
 end
