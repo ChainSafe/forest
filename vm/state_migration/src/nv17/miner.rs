b475552031689a15b9590390757606b2aefdad3c
hanabi1224 <harlowmoo@gmail.com>
2023-05-24 23:08:03 +0800

    miner

--- vm/state_migration/src/nv17/miner.rs
+++ vm/state_migration/src/nv17/miner.rs
@@ -10,17 +10,30 @@ use cid::{multihash::Code::Blake2b256, Cid};
 use fil_actor_miner_state::{v8::State as MinerStateOld, v9::State as MinerStateNew};
 use forest_utils::db::BlockstoreExt;
 use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::CborStore;
 
 use crate::common::{
     ActorMigration, ActorMigrationInput, ActorMigrationOutput, TypeMigration, TypeMigrator,
 };
 
-pub struct MinerMigrator(Cid);
+pub struct MinerMigrator {
+    out_code: Cid,
+}
 
-pub(crate) fn init_migrator<BS: Blockstore + Clone + Send + Sync>(
-    cid: Cid,
-) -> Arc<dyn ActorMigration<BS> + Send + Sync> {
-    Arc::new(MinerMigrator(cid))
+pub(crate) fn miner_migrator<BS: Blockstore + Clone + Send + Sync>(
+    out_code: Cid,
+    store: &BS,
+    market_proposals: fil_actors_shared::v8::Array<fil_actor_market_state::v8::DealProposal, BS>,
+) -> anyhow::Result<Arc<dyn ActorMigration<BS> + Send + Sync>> {
+    let mut empty_precommit_map_cid = fil_actors_shared::v9::make_empty_map::<_, Cid>(
+        store,
+        fil_actors_shared::v9::builtin::HAMT_BIT_WIDTH,
+    );
+
+    let edv8 = fil_actor_miner_state::v8::Deadline::new(store)?;
+    let edv8_cid = store.put_cbor(&edv8, Blake2b256)?;
+
+    Ok(Arc::new(MinerMigrator { out_code }))
 }
 
 impl<BS: Blockstore + Clone + Send + Sync> ActorMigration<BS> for MinerMigrator {
@@ -38,7 +51,7 @@ impl<BS: Blockstore + Clone + Send + Sync> ActorMigration<BS> for MinerMigrator 
         let new_head = store.put_obj(&out_state, Blake2b256)?;
 
         Ok(ActorMigrationOutput {
-            new_code_cid: self.0,
+            new_code_cid: self.out_code,
             new_head,
         })
     }
