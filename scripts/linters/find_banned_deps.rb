186a46cdbae801a07c4d8d364b8fe878e34c95c6
Josh Jones <joshua.jones@chainsafe.io>
2023-05-24 17:16:25 -0500

    changes from code review

--- scripts/linters/find_banned_deps.rb
+++ scripts/linters/find_banned_deps.rb
@@ -2,16 +2,25 @@
 
 require 'toml-rb'
 
+whitelist = {
+  # Check all `Cargo.toml` files for `time` crate
+  ['time'] => []
+  # The following code will be uncommented as part of `PR#2880: https://github.com/ChainSafe/forest/pull/2880`
+  # # Create exceptions for `forest_shim` and `forest_interpreter` (only places `fvm` & `fvm3` are allowed).
+  # %w[fvm fvm3] => ['utils/forest_shim/Cargo.toml', 'vm/interpreter/Cargo.toml']
+}
+
 # Iterate over all TOML files in the repository
 Dir.glob('**/*.toml').each do |file|
   toml = TomlRB.load_file(file)
 
   toml['dependencies']&.each do |dep|
-    # Check all `Cargo.toml` files for `time` crate
-    puts "[#{dep[0]}] is being used in [#{file}], but this crate is banned!" if dep[0] == 'time'
-    # Create an exception for `forest_shim` (only place `fvm` & `fvm3` are allowed).
-    next unless file != 'utils/forest_shim/Cargo.toml'
-
-    puts "[#{dep[0]}] is being used in [#{file}], but this crate is banned!" if dep[0] == 'fvm' || dep[0] == 'fvm3'
+    whitelist.each_pair do |crates, exceptions|
+      crates.each do |crate|
+        if dep[0] == crate && !(exceptions.include? file)
+          puts "[#{dep[0]}] is being used in [#{file}], but this crate is banned!"
+        end
+      end
+    end
   end
 end
