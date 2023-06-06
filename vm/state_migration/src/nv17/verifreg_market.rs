23c28f075de8ea9ffb0429658c2f7ea383a22d2c
hanabi1224 <harlowmoo@gmail.com>
2023-06-06 23:39:46 +0800

    impl verifreg_market (partial)

--- vm/state_migration/src/nv17/verifreg_market.rs
+++ vm/state_migration/src/nv17/verifreg_market.rs
@@ -4,8 +4,10 @@
 //! This module contains the migration logic for the `NV17` upgrade for the verifreg and market
 //! actor.
 
+use anyhow::Context;
 use forest_shim::{deal::DealID, state_tree::StateTree};
 use fvm_ipld_blockstore::Blockstore;
+use fvm_shared::address::Address;
 
 use super::ChainEpoch;
 use crate::common::{ActorMigration, ActorMigrationInput, ActorMigrationOutput, PostMigrator};
@@ -20,6 +22,8 @@ pub(super) struct VerifregMarketPostMigrator {
 
 impl<BS: Blockstore> PostMigrator<BS> for VerifregMarketPostMigrator {
     fn post_migrate_state(&self, store: &BS, actors_out: &mut StateTree<BS>) -> anyhow::Result<()> {
+        const HAMT_BIT_WIDTH: u32 = fil_actors_shared::v9::builtin::HAMT_BIT_WIDTH;
+
         // FIXME: `DEFAULT_BIT_WIDTH` on rust side is 3 while it's 5 on go side. Revisit to make sure
         // it does not effect `load` API here. (Go API takes bit_width=5 for loading while Rust API does not)
         //
@@ -29,7 +33,48 @@ impl<BS: Blockstore> PostMigrator<BS> for VerifregMarketPostMigrator {
             _,
         >::load(&self.market_state_v8.proposals, &store)?;
 
-        let next_allocation_id: fil_actor_verifreg_state::v9::AllocationID = 1;
+        let mut next_allocation_id: fil_actor_verifreg_state::v9::AllocationID = 1;
+        let mut allocations_map_map = fil_actors_shared::v9::MapMap::<
+            BS,
+            fil_actor_verifreg_state::v9::Allocation,
+            Address,
+            fil_actor_verifreg_state::v9::AllocationID,
+        >::new(store, HAMT_BIT_WIDTH, HAMT_BIT_WIDTH);
+
+        for &deal_id in &self.pending_verified_deals {
+            let proposal = market_proposals
+                .get(deal_id)?
+                .context("Failed to get pending deal proposal")?;
+            let client_id_address = self
+                .init_state_v8
+                .resolve_address(store, &proposal.client)?
+                .context("Failed to find client in init actor map")?;
+            let provider_id_address = self
+                .init_state_v8
+                .resolve_address(store, &proposal.provider)?
+                .context("Failed to find provider in init actor map")?;
+
+            let mut expiration = fil_actors_shared::v9::runtime::policy_constants::MAXIMUM_VERIFIED_ALLOCATION_EXPIRATION + self.prior_epoch;
+            if expiration > proposal.start_epoch {
+                expiration = proposal.start_epoch;
+            }
+
+            allocations_map_map.put(
+                client_id_address,
+                next_allocation_id,
+                fil_actor_verifreg_state::v9::Allocation {
+                    client: client_id_address.id()?,
+                    provider: provider_id_address.id()?,
+                    data: proposal.piece_cid,
+                    size: proposal.piece_size,
+                    term_min: proposal.duration(),
+                    term_max: fil_actors_shared::v9::runtime::policy_constants::MAX_SECTOR_EXPIRATION_EXTENSION,
+                    expiration: expiration,
+                },
+            )?;
+
+            next_allocation_id += 1;
+        }
 
         todo!()
     }
