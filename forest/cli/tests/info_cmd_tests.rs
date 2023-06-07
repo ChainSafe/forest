5b67ada27cb130f9887f4e3f3d0b3bee0879eddd
Rahul Sharma <rahul@chainsafe.io>
2023-06-07 10:09:10 +0000

    simplify integration test and move invocation to forest_cli_check.sh

--- forest/cli/tests/info_cmd_tests.rs
+++ forest/cli/tests/info_cmd_tests.rs
@@ -3,34 +3,12 @@
 use assert_cmd::Command;
 
 #[test]
+#[ignore] // being run at scripts/forest_cli_check.sh for now as it needs the daemon to be running.
 fn test_forest_info_cmd() {
-    let cmd = Command::cargo_bin("forest-cli")
+    Command::cargo_bin("forest-cli")
         .unwrap()
         .arg("info")
         .arg("show")
         .assert()
         .success();
-
-    let output = std::str::from_utf8(&cmd.get_output().stdout).unwrap();
-    let lines = output.split("\n").filter(|e| !e.is_empty());
-    for info in lines {
-        if info.starts_with("Network") {
-            let info = info.split(":").skip(1).next().unwrap().trim_start();
-            assert!(info == "calibnet" || info == "mainnet" || info == "devnet");
-        }
-        if info.starts_with("Uptime")
-            || info.starts_with("Chain health")
-            || info.starts_with("Chain")
-            || info.starts_with("Default wallet address")
-        {
-            let info = info
-                .split(":")
-                .skip(1)
-                .next()
-                .expect(&format!("expecting line: {}", info))
-                .trim_start();
-
-            assert!(!info.is_empty());
-        }
-    }
 }
