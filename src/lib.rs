5fbf4fb1296c77a1bce6217a2c0f8b0a0a488106
Aatif Syed <aatifsyedyp@gmail.com>
2023-06-16 12:06:57 +0100

    run: ./one-crate.py

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,29 @@
-// Copyright 2019-2023 ChainSafe Systems
-// SPDX-License-Identifier: Apache-2.0, MIT
+mod auth;
+mod beacon;
+mod blocks;
+mod chain;
+mod chain_sync;
+mod cli_shared;
+mod db;
+mod deleg_cns;
+mod fil_cns;
+mod genesis;
+mod interpreter;
+mod ipld;
+mod json;
+mod key_management;
+mod libp2p;
+mod libp2p_bitswap;
+mod message;
+mod message_pool;
+mod metrics;
+mod networks;
+mod rpc;
+mod rpc_api;
+mod rpc_client;
+mod shim;
+mod state_manager;
+mod state_migration;
+mod statediff;
+mod test_utils;
+mod utils;
