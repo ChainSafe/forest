fb5343ef143425c12069438c130711f60e29a97b
Aatif Syed <aatifsyedyp@gmail.com>
2023-06-20 23:20:04 +0100

    fix: remove dead rpc entrypoints

--- src/rpc_client/mpool_ops.rs
+++ src/rpc_client/mpool_ops.rs
@@ -6,14 +6,6 @@ use jsonrpc_v2::Error;
 
 use crate::rpc_client::call;
 
-#[allow(unused)] // TODO(aatifsyed)
-pub async fn mpool_pending(
-    params: MpoolPendingParams,
-    auth_token: &Option<String>,
-) -> Result<MpoolPendingResult, Error> {
-    call(MPOOL_PENDING, params, auth_token).await
-}
-
 pub async fn mpool_push_message(
     params: MpoolPushMessageParams,
     auth_token: &Option<String>,
