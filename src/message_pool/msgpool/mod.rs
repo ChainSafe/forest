910fcb6805e3299bf27c7b8cab76b2ca9f9926d8
Josh Jones <joshua.jones@chainsafe.io>
2023-06-20 15:02:06 -0500

    remove additional `fvm_shared3` refs

--- src/message_pool/msgpool/mod.rs
+++ src/message_pool/msgpool/mod.rs
@@ -150,7 +150,7 @@ where
 
     chains.sort(false);
 
-    let mut gas_limit = fvm_shared3::BLOCK_GAS_LIMIT;
+    let mut gas_limit = crate::shim::econ::BLOCK_GAS_LIMIT;
     let mut i = 0;
     'l: while i < chains.len() {
         let chain = &mut chains[i];
