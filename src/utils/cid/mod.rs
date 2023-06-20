5e69532448e58230bfab1362da65bc6d283e4253
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-06-20 12:55:01 +0200

    update comments

--- src/utils/cid/mod.rs
+++ src/utils/cid/mod.rs
@@ -29,8 +29,14 @@ impl CidCborExt for Cid {}
 const BLAKE2B256: u64 = 0xb220;
 pub const BLAKE2B256_SIZE: usize = 32;
 
-// Nearly all Filecoin CIDs are V1, DagCbor encoded, and hashed with Blake2b265. If other types of
-// CID become popular, they can be added to the CidVariant structure.
+// `CidVariant` is an enumeration of known CID types that are used in the Filecoin blockchain. CIDs
+// contain a significant amount of static data (such as version, codec, hash identifier, hash
+// length). This static data represented by a single tag in the enum.
+//
+// Nearly all Filecoin CIDs are V1, DagCbor encoded, and hashed with Blake2b256 (which has a hash
+// length of 256bits). Naively representing such a CID requires 96 bytes but `CidVariant` does it in
+// only 40 bytes. If other types of CID become popular, they can be added to the CidVariant
+// structure.
 pub enum CidVariant {
     V1DagCborBlake2b([u8; BLAKE2B256_SIZE]),
 }
@@ -52,11 +58,18 @@ impl TryFrom<Cid> for CidVariant {
 
 #[cfg(test)]
 mod tests {
+    use super::CidVariant;
     use super::*;
     use crate::db::MemoryDB;
     use crate::utils::db::CborStoreExt;
     use anyhow::*;
+    use cid::{
+        multihash::{Code, MultihashDigest},
+        Cid,
+    };
+    use fvm_ipld_encoding::DAG_CBOR;
     use quickcheck_macros::quickcheck;
+    use std::mem::size_of;
 
     #[quickcheck]
     fn test_cid_cbor_ext(s: String) -> Result<()> {
@@ -69,14 +82,6 @@ mod tests {
 
         Ok(())
     }
-
-    use super::CidVariant;
-    use cid::{
-        multihash::{Code, MultihashDigest},
-        Cid,
-    };
-    use fvm_ipld_encoding::DAG_CBOR;
-    use std::mem::size_of;
 
     // If this stops being true, please update the documentation above.
     #[test]
@@ -113,10 +118,11 @@ mod tests {
         ));
     }
 
-    // If this test fails, the default encoding is no longer v1+dagcbor.
+    // If this test fails, the default encoding is no longer v1+dagcbor+blake2b. Add the new default
+    // CID type to `CidVariant`.
     #[test]
     fn default_is_v1_dagcbor() {
-        let cid = Cid::from_cbor_blake2b256(&()).unwrap();
+        let cid = MemoryDB::default().put_cbor_default(&()).unwrap();
         assert!(matches!(
             cid.try_into().unwrap(),
             CidVariant::V1DagCborBlake2b(_)
