0c7a764c75912799cdf8aaee650882b6294f33f8
Aatif Syed <aatifsyedyp@gmail.com>
2023-06-29 00:29:18 +0100

    refactor: utils::net is one file

--- src/utils/net/http.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2023 ChainSafe Systems
-// SPDX-License-Identifier: Apache-2.0, MIT
-
-use once_cell::sync::Lazy;
-
-pub fn global_http_client() -> reqwest::Client {
-    static CLIENT: Lazy<reqwest::Client> = Lazy::new(reqwest::Client::new);
-    CLIENT.clone()
-}
