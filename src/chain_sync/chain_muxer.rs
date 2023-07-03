6e0ce8c65fb960e1fa9251535b43342e33e76266
lemmih <david.himmelstrup@chainsafe.io>
2023-07-03 14:25:34 +0200

    apply clippy suggestion

--- src/chain_sync/chain_muxer.rs
+++ src/chain_sync/chain_muxer.rs
@@ -262,7 +262,7 @@ where
             let request = HelloRequest {
                 heaviest_tip_set: heaviest.cids().to_vec(),
                 heaviest_tipset_height: heaviest.epoch(),
-                heaviest_tipset_weight: heaviest.weight().clone().into(),
+                heaviest_tipset_weight: heaviest.weight().clone(),
                 genesis_cid: genesis_block_cid,
             };
             let (peer_id, moment_sent, response) =
