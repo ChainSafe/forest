1e9563806c03eac5c914d31b612f840d9193d5d2
lemmih <david.himmelstrup@chainsafe.io>
2023-07-04 08:34:51 +0200

    fix shell escapes

--- db_params_hyperfine.sh
+++ db_params_hyperfine.sh
@@ -5,9 +5,9 @@ SNAPSHOT=filecoin_full_calibnet_2023-04-07_450000.car
 
 hyperfine \
   --runs 2 \
-  --cleanup './target/release/forest-cli --chain ${CHAIN} db clean --force' \
+  --cleanup "./target/release/forest-cli --chain ${CHAIN} db clean --force" \
   --parameter-list CHUNK_SIZE 1000,5000,10000,20000,40000,200000 \
   --parameter-list BUFFER_CAPACITY 0,1,2,3 \
   --export-markdown db_tune_params.md \
   --command-name 'forest-import-{CHUNK_SIZE}-{BUFFER_CAPACITY}' \
-  'CHUNK_SIZE={CHUNK_SIZE} BUFFER_CAPACITY={BUFFER_CAPACITY} ./target/release/forest --chain ${CHAIN} --rpc false --no-gc --encrypt-keystore false --track-peak-rss --halt-after-import --import-snapshot ${SNAPSHOT}'
+  "CHUNK_SIZE={CHUNK_SIZE} BUFFER_CAPACITY={BUFFER_CAPACITY} ./target/release/forest --chain ${CHAIN} --rpc false --no-gc --encrypt-keystore false --track-peak-rss --halt-after-import --import-snapshot ${SNAPSHOT}"
