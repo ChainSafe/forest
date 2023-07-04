7e7b516311f18912f49201048d9bea2bcbff25dc
lemmih <david.himmelstrup@chainsafe.io>
2023-07-04 08:32:11 +0200

    parameterize hyperfine script

--- db_params_hyperfine.sh
+++ db_params_hyperfine.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+CHAIN=calibnet
+SNAPSHOT=filecoin_full_calibnet_2023-04-07_450000.car
+
 hyperfine \
-  --runs 3 \
-  --prepare 'sync; echo 1 | sudo tee /proc/sys/vm/drop_caches' \
-  --cleanup 'cargo cli db clean --force' \
+  --runs 2 \
+  --cleanup './target/release/forest-cli --chain ${CHAIN} db clean --force' \
   --parameter-list CHUNK_SIZE 1000,5000,10000,20000,40000,200000 \
   --parameter-list BUFFER_CAPACITY 0,1,2,3 \
   --export-markdown db_tune_params.md \
-  --command-name 'forest-import' \
-  'CHUNK_SIZE={CHUNK_SIZE} BUFFER_CAPACITY={BUFFER_CAPACITY} cargo run --bin forest --release -- --chain mainnet --rpc false --no-gc --encrypt-keystore false --track-peak-rss --halt-after-import --import-snapshot filops_snapshot_mainnet_2023-07-02_height_2998800.car.zst'
+  --command-name 'forest-import-{CHUNK_SIZE}-{BUFFER_CAPACITY}' \
+  'CHUNK_SIZE={CHUNK_SIZE} BUFFER_CAPACITY={BUFFER_CAPACITY} cargo run --bin forest --release -- --chain ${CHAIN} --rpc false --no-gc --encrypt-keystore false --track-peak-rss --halt-after-import --import-snapshot ${SNAPSHOT}'
