9ff0ba5b559841a5f9f232c05ac0bc60e7c67b71
elmattic <elmattic@users.noreply.github.com>
2023-07-05 12:25:05 +0200

    Add async read wrapper

--- src/utils/io/progress_log.rs
+++ src/utils/io/progress_log.rs
@@ -4,9 +4,18 @@
 use humantime::format_duration;
 use std::time::{Duration, Instant};
 
+//use std::borrow::Cow;
+//use std::convert::TryFrom;
+use std::io::{self};
+//use std::io::IoSliceMut;
+//use std::iter::FusedIterator;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tokio::io::{ReadBuf, SeekFrom};
+
 use log::info;
 
-const UPDATE_FREQUENCY: Duration = Duration::from_millis(1000);
+const UPDATE_FREQUENCY: Duration = Duration::from_millis(100);
 
 pub fn wrap_iter<Inner>(
     message: &str,
@@ -35,7 +44,7 @@ where
         match self.inner.next() {
             Some(item) => {
                 self.progress.emit_log_if_required(self.inner.size_hint());
-                self.progress.inc();
+                self.progress.inc(1);
                 Some(item)
             }
             None => {
@@ -54,11 +63,54 @@ pub fn wrap_stream<S: futures_core::Stream>(message: &str, stream: S) -> WithPro
     }
 }
 
+pub fn wrap_async_read<R: tokio::io::AsyncBufRead + Unpin + tokio::io::AsyncRead>(
+    message: &str,
+    read: R,
+) -> WithProgressStream<R> {
+    WithProgressStream {
+        stream: read,
+        progress: WithProgress::new(message),
+    }
+}
+
 /// Wraps an iterator to display its progress.
 #[derive(Debug)]
 pub struct WithProgressStream<S> {
     pub(crate) stream: S,
     progress: WithProgress,
+}
+
+impl<R: tokio::io::AsyncRead + Unpin> tokio::io::AsyncRead for WithProgressStream<R> {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        let prev_len = buf.filled().len() as u64;
+        if let Poll::Ready(e) = Pin::new(&mut self.stream).poll_read(cx, buf) {
+            self.progress.inc(buf.filled().len() as u64 - prev_len);
+            Poll::Ready(e)
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+impl<W: tokio::io::AsyncBufRead + Unpin + tokio::io::AsyncRead> tokio::io::AsyncBufRead
+    for WithProgressStream<W>
+{
+    fn poll_fill_buf(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<&[u8]>> {
+        let this = self.get_mut();
+        let result = Pin::new(&mut this.stream).poll_fill_buf(cx);
+        if let Poll::Ready(Ok(buf)) = &result {
+            this.progress.inc(buf.len() as u64);
+        }
+        result
+    }
+
+    fn consume(mut self: Pin<&mut Self>, amt: usize) {
+        Pin::new(&mut self.stream).consume(amt);
+    }
 }
 
 impl<S: futures_core::Stream + Unpin> futures_core::Stream for WithProgressStream<S> {
@@ -73,7 +125,7 @@ impl<S: futures_core::Stream + Unpin> futures_core::Stream for WithProgressStrea
         match &item {
             std::task::Poll::Ready(Some(_)) => {
                 this.progress.emit_log_if_required(this.stream.size_hint());
-                this.progress.inc();
+                this.progress.inc(1);
             }
             std::task::Poll::Ready(None) => this.progress.finish(),
             std::task::Poll::Pending => {}
@@ -103,8 +155,8 @@ impl WithProgress {
         }
     }
 
-    fn inc(&mut self) {
-        self.completed_items += 1;
+    fn inc(&mut self, value: u64) {
+        self.completed_items += value;
     }
 
     fn emit_log_if_required(&mut self, size_hint: (usize, Option<usize>)) {
@@ -116,8 +168,7 @@ impl WithProgress {
             let throughput = self.completed_items as f64 / elapsed_secs;
 
             let (lower_bound, upper_bound) = size_hint;
-            let total_items =
-                upper_bound.unwrap_or(lower_bound) as u64 + self.completed_items;
+            let total_items = upper_bound.unwrap_or(lower_bound) as u64 + self.completed_items;
             let eta_secs = (total_items - self.completed_items) as f64 / throughput;
             let eta_duration = format_duration(Duration::from_secs(eta_secs as u64));
 
