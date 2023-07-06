140a5e132f8cc4c948fba9ed51853a53c32082d6
elmattic <elmattic@users.noreply.github.com>
2023-07-06 18:26:56 +0200

    Refactor mpool stat subcommand

--- src/cli/subcommands/mpool_cmd.rs
+++ src/cli/subcommands/mpool_cmd.rs
@@ -3,6 +3,7 @@
 
 use std::str::FromStr;
 
+use crate::blocks::Tipset;
 use crate::json::cid::vec::CidJsonVec;
 use crate::json::signed_message::json::SignedMessageJson;
 use crate::message::{Message, SignedMessage};
@@ -14,6 +15,7 @@ use ahash::{HashMap, HashSet};
 use clap::Subcommand;
 use fvm_ipld_encoding::Cbor;
 use num::BigInt;
+use std::sync::Arc;
 
 use super::{handle_rpc_err, Config};
 
@@ -76,6 +78,150 @@ fn filter_messages(
     Ok(filtered)
 }
 
+async fn get_actor_sequences(
+    messages: &[SignedMessageJson],
+    tipset: Arc<Tipset>,
+    config: &Config,
+) -> HashMap<Address, u64> {
+    let mut actor_sequences = HashMap::<Address, u64>::default();
+
+    for msg in messages.iter() {
+        let address = msg.0.from();
+        let get_actor_result = state_get_actor(
+            (address.to_owned().into(), tipset.key().to_owned().into()),
+            &config.client.rpc_token,
+        )
+        .await;
+
+        let actor_state = match get_actor_result {
+            Ok(actor_json) => {
+                if let Some(state) = actor_json {
+                    state.0
+                } else {
+                    println!("{}, actor state not found", address);
+                    continue;
+                }
+            }
+            Err(err) => {
+                let error_message = match err {
+                    jsonrpc_v2::Error::Full { message, .. } => message,
+                    jsonrpc_v2::Error::Provided { message, .. } => message.to_string(),
+                };
+
+                println!("{}, err: {}", address, error_message);
+                continue;
+            }
+        };
+        actor_sequences.insert(address, actor_state.sequence);
+    }
+
+    actor_sequences
+}
+
+type StatBucket = HashMap<u64, SignedMessage>;
+
+#[derive(Default)]
+struct MpStat {
+    address: String,
+    past: u64,
+    current: u64,
+    future: u64,
+    below_current: u64,
+    below_past: u64,
+    gas_limit: BigInt,
+}
+
+fn compute_stats(
+    messages: &[SignedMessageJson],
+    actor_sequences: HashMap<Address, u64>,
+    curr_base_fee: TokenAmount,
+    min_base_fee: TokenAmount,
+) -> Vec<MpStat> {
+    let mut buckets = HashMap::<Address, StatBucket>::default();
+    for msg in messages {
+        buckets
+            .entry(msg.0.from())
+            .or_insert(StatBucket::default())
+            .insert(msg.0.sequence(), msg.0.to_owned());
+    }
+
+    let mut stats: Vec<MpStat> = Vec::new();
+
+    for (address, bucket) in buckets {
+        let actor_sequence = actor_sequences[&address];
+
+        let mut curr_sequence = actor_sequence;
+        while bucket.get(&curr_sequence).is_some() {
+            curr_sequence += 1;
+        }
+
+        let mut stat = MpStat {
+            address: address.to_string(),
+            ..Default::default()
+        };
+
+        for (_, msg) in bucket {
+            if msg.sequence() < actor_sequence {
+                stat.past += 1;
+            } else if msg.sequence() > curr_sequence {
+                stat.future += 1;
+            } else {
+                stat.current += 1;
+            }
+
+            if msg.gas_fee_cap() < curr_base_fee {
+                stat.below_current += 1;
+            }
+            if msg.gas_fee_cap() < min_base_fee {
+                stat.below_past += 1;
+            }
+
+            stat.gas_limit += msg.message().gas_limit;
+        }
+
+        stats.push(stat);
+    }
+
+    stats
+}
+
+fn print_stats(stats: &[MpStat], basefee_lookback: u32) {
+    let mut total = MpStat::default();
+
+    for stat in stats {
+        total.past += stat.past;
+        total.current += stat.current;
+        total.future += stat.future;
+        total.below_current += stat.below_current;
+        total.below_past += stat.below_past;
+        total.gas_limit += &stat.gas_limit;
+
+        println!(
+            "{}: Nonce past: {}, cur: {}, future: {}; FeeCap cur: {}, min-{}: {}, gasLimit: {}",
+            stat.address,
+            stat.past,
+            stat.current,
+            stat.future,
+            stat.below_current,
+            basefee_lookback,
+            stat.below_past,
+            stat.gas_limit
+        );
+    }
+
+    println!("-----");
+    println!(
+        "total: Nonce past: {}, cur: {}, future: {}; FeeCap cur: {}, min-{}: {}, gasLimit: {}",
+        total.past,
+        total.current,
+        total.future,
+        total.below_current,
+        basefee_lookback,
+        total.below_past,
+        total.gas_limit
+    );
+}
+
 impl MpoolCommands {
     pub async fn run(self, config: Config) -> anyhow::Result<()> {
         match self {
@@ -126,19 +272,6 @@ impl MpoolCommands {
                         .map_err(handle_rpc_err)?;
                 let min_base_fee = TokenAmount::from_atto(atto_str.parse::<BigInt>()?);
 
-                type StatBucket = HashMap<u64, SignedMessage>;
-
-                #[derive(Default)]
-                struct MpStat {
-                    address: String,
-                    past: u64,
-                    current: u64,
-                    future: u64,
-                    below_current: u64,
-                    below_past: u64,
-                    gas_limit: BigInt,
-                }
-
                 let messages = mpool_pending((CidJsonVec(vec![]),), &config.client.rpc_token)
                     .await
                     .map_err(handle_rpc_err)?;
@@ -154,112 +287,17 @@ impl MpoolCommands {
 
                 let filtered_messages = filter_messages(messages, local_addrs, &None, &None)?;
 
-                let mut buckets = HashMap::<Address, StatBucket>::default();
-                for msg in filtered_messages {
-                    buckets
-                        .entry(msg.0.from())
-                        .or_insert(StatBucket::default())
-                        .insert(msg.0.sequence(), msg.0.to_owned());
-                }
+                let actor_sequences =
+                    get_actor_sequences(&filtered_messages, tipset, &config).await;
 
-                let mut stats: Vec<MpStat> = Vec::new();
-
-                for (address, bucket) in buckets {
-                    let get_actor_result = state_get_actor(
-                        (address.to_owned().into(), tipset.key().to_owned().into()),
-                        &config.client.rpc_token,
-                    )
-                    .await;
-
-                    let actor_state = match get_actor_result {
-                        Ok(actor_json) => {
-                            if let Some(state) = actor_json {
-                                state.0
-                            } else {
-                                println!("{}, actor state not found", address);
-                                continue;
-                            }
-                        }
-                        Err(err) => {
-                            let error_message = match err {
-                                jsonrpc_v2::Error::Full { message, .. } => message,
-                                jsonrpc_v2::Error::Provided { message, .. } => message.to_string(),
-                            };
-
-                            println!("{}, err: {}", address, error_message);
-                            continue;
-                        }
-                    };
-
-                    let mut curr_sequence = actor_state.sequence;
-                    while bucket.get(&curr_sequence).is_some() {
-                        curr_sequence += 1;
-                    }
-
-                    let mut stat = MpStat {
-                        address: address.to_string(),
-                        ..Default::default()
-                    };
-
-                    for (_, msg) in bucket {
-                        if msg.sequence() < actor_state.sequence {
-                            stat.past += 1;
-                        } else if msg.sequence() > curr_sequence {
-                            stat.future += 1;
-                        } else {
-                            stat.current += 1;
-                        }
-
-                        if msg.gas_fee_cap() < curr_base_fee {
-                            stat.below_current += 1;
-                        }
-                        if msg.gas_fee_cap() < min_base_fee {
-                            stat.below_past += 1;
-                        }
-
-                        stat.gas_limit += msg.message().gas_limit;
-                    }
-
-                    stats.push(stat);
-                }
-
-                stats.sort_by(|a, b| a.address.cmp(&b.address));
-
-                let mut total = MpStat::default();
-
-                for stat in stats {
-                    total.past += stat.past;
-                    total.current += stat.current;
-                    total.future += stat.future;
-                    total.below_current += stat.below_current;
-                    total.below_past += stat.below_past;
-                    total.gas_limit += &stat.gas_limit;
-
-                    println!(
-                        "{}: Nonce past: {}, cur: {}, future: {}; FeeCap cur: {}, min-{}: {}, gasLimit: {}",
-                        stat.address,
-                        stat.past,
-                        stat.current,
-                        stat.future,
-                        stat.below_current,
-                        basefee_lookback,
-                        stat.below_past,
-                        stat.gas_limit
-                    );
-                }
-
-                println!("-----");
-                println!(
-                    "total: Nonce past: {}, cur: {}, future: {}; FeeCap cur: {}, min-{}: {}, gasLimit: {}",
-                    total.past,
-                    total.current,
-                    total.future,
-                    total.below_current,
-                    basefee_lookback,
-                    total.below_past,
-                    total.gas_limit
+                let stats = compute_stats(
+                    &filtered_messages,
+                    actor_sequences,
+                    curr_base_fee,
+                    min_base_fee,
                 );
 
+                print_stats(&stats, basefee_lookback);
                 Ok(())
             }
         }
