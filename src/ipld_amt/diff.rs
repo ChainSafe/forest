fa18f6ba765e38c4bb6d654e6684d6dd49eb6a0d
hanabi1224 <harlowmoo@gmail.com>
2023-07-06 10:58:54 +0800

    headers

--- src/ipld_amt/diff.rs
+++ src/ipld_amt/diff.rs
@@ -1,3 +1,5 @@
+// Copyright 2019-2023 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
 // Copyright 2021-2023 Protocol Labs
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
