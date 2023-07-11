6d4a67a700bc6f81e0a233a81f21ef77ae2668a9
lemmih <david.himmelstrup@chainsafe.io>
2023-07-11 13:13:50 +0200

    Document archive subcommands

--- src/cli/subcommands/archive_cmd.rs
+++ src/cli/subcommands/archive_cmd.rs
@@ -1,11 +1,39 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use super::*;
+//! Archives are key-value pairs encoded as
+//! [CAR files](https://ipld.io/specs/transport/car/carv1/). The key-value pairs
+//! represent a directed, acyclic graph (DAG). This graph is often a subset of a larger
+//! graph and references to missing keys are common.
+//!
+//! Each graph contains blocks, messages, state trees, and miscellaneous data
+//! such as compiled WASM code. The amount of data differs greatly in different
+//! kinds of archives. While there are no fixed definitions, there are three
+//! common kind of archives:
+//!   A full archive contains a complete graph with no missing nodes. These
+//!   archives are large (14TiB for Filecoin's mainnet) and only used in special
+//!   situations.
+//!   A lite-archive typically has ~3 million blocks, 2000 complete sets of
+//!   state-roots, and 2000 sets of messages. These archives usually take up
+//!   ~100GiB.
+//!   A diff-archive contains the subset of nodes that are _not_ shared by two
+//!   other archives. These archives are much smaller but can rarely be used on
+//!   their own. They are typically merged with other archives before use.
+//!
+//! Additional reading:
+//!     <https://github.com/ChainSafe/forest/blob/main/documentation/src/developer_documentation/filecoin_archive.md>
+//!     [`CarBackedBlockstore`]
+
+use super::Config;
+use crate::blocks::{Tipset, TipsetKeys};
 use crate::car_backed_blockstore::CarBackedBlockstore;
+use crate::networks::{calibnet, mainnet};
 use crate::shim::clock::ChainEpoch;
 use anyhow::{bail, Context as _};
 use clap::Subcommand;
+use fvm_ipld_blockstore::Blockstore;
+use indicatif::ProgressIterator;
+use itertools::Itertools;
 use std::io::{Read, Seek};
 use std::path::PathBuf;
 
