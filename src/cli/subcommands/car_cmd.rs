efd59bea6b88a3e7ff7dfeec39b4f3c195bf0313
hanabi1224 <harlowmoo@gmail.com>
2023-07-14 14:56:00 +0800

    quickcheck tests

--- src/cli/subcommands/car_cmd.rs
+++ src/cli/subcommands/car_cmd.rs
@@ -3,6 +3,7 @@
 
 use std::path::PathBuf;
 
+use async_trait::async_trait;
 use clap::Subcommand;
 use futures::{AsyncRead, StreamExt, TryStreamExt};
 use fvm_ipld_car::Block;
@@ -42,7 +43,6 @@ impl CarCommands {
                     for reader in &readers {
                         for &root in &reader.header.roots {
                             if seen.insert(root) {
-                                println!("roots.push {root}");
                                 roots.push(root);
                             }
                         }
@@ -75,20 +75,29 @@ impl CarCommands {
     }
 }
 
-struct MultiCarDedupReader<R>
+#[async_trait]
+trait CarBlockProvider {
+    async fn next_block(&mut self) -> Result<Option<Block>, fvm_ipld_car::Error>;
+}
+
+#[async_trait]
+impl<R> CarBlockProvider for CarReader<R>
 where
     R: AsyncRead + Send + Unpin,
 {
-    readers: Vec<CarReader<R>>,
+    async fn next_block(&mut self) -> Result<Option<Block>, fvm_ipld_car::Error> {
+        CarReader::next_block(self).await
+    }
+}
+
+struct MultiCarDedupReader<R: CarBlockProvider> {
+    readers: Vec<R>,
     index: usize,
     seen: CidHashSet,
 }
 
-impl<R> MultiCarDedupReader<R>
-where
-    R: AsyncRead + Send + Unpin,
-{
-    fn new(readers: Vec<CarReader<R>>) -> Self {
+impl<R: CarBlockProvider> MultiCarDedupReader<R> {
+    fn new(readers: Vec<R>) -> Self {
         Self {
             readers,
             index: 0,
@@ -109,5 +118,79 @@ where
                 self.index += 1;
             };
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec::IntoIter;
+
+    use super::*;
+    use ahash::HashSet;
+    use cid::multihash;
+    use cid::multihash::MultihashDigest;
+    use cid::Cid;
+    use pretty_assertions::assert_eq;
+    use quickcheck::Arbitrary;
+    use quickcheck_macros::quickcheck;
+
+    #[async_trait]
+    impl CarBlockProvider for IntoIter<Block> {
+        async fn next_block(&mut self) -> Result<Option<Block>, fvm_ipld_car::Error> {
+            Ok(self.next())
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    struct Blocks(Vec<Block>);
+
+    impl Arbitrary for Blocks {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let n = u16::arbitrary(g) as usize;
+            let mut blocks = Vec::with_capacity(n);
+            for _ in 0..n {
+                // use small len here to increase the chance of duplication
+                let data = [u8::arbitrary(g), u8::arbitrary(g)];
+                let cid = Cid::new_v0(multihash::Code::Sha2_256.digest(&data)).unwrap();
+                let block = Block {
+                    cid,
+                    data: data.to_vec(),
+                };
+                blocks.push(block);
+            }
+            Self(blocks)
+        }
+    }
+
+    #[quickcheck]
+    fn car_dedup_reader_tests(a: Blocks, b: Blocks) -> anyhow::Result<()> {
+        let unique_len = {
+            let mut set = HashSet::from_iter(a.0.iter().map(|b| b.cid).collect::<Vec<Cid>>());
+            for block in &b.0 {
+                set.insert(block.cid);
+            }
+            set.len()
+        };
+
+        println!(
+            "a.len: {}, b.len:{}, total: {}, unique: {unique_len}",
+            a.0.len(),
+            b.0.len(),
+            a.0.len() + b.0.len(),
+        );
+
+        let rt = tokio::runtime::Runtime::new()?;
+        let count = rt.block_on(async move {
+            let mut reader = MultiCarDedupReader::new(vec![a.0.into_iter(), b.0.into_iter()]);
+            let mut count = 0usize;
+            while let Ok(Some(_)) = reader.next_block().await {
+                count += 1;
+            }
+            count
+        });
+
+        assert_eq!(count, unique_len);
+
+        Ok(())
     }
 }
