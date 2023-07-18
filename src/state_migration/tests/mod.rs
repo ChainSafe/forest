50b9962dc91e49761e8d08415e0b924350fb8a1f
hanabi1224 <harlowmoo@gmail.com>
2023-07-18 15:35:44 +0800

    use compressed car

--- src/state_migration/tests/mod.rs
+++ src/state_migration/tests/mod.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use crate::{
-    car_backed_blockstore::UncompressedCarV1BackedBlockstore,
+    car_backed_blockstore::CompressedCarV1BackedBlockstore,
     daemon::bundle::get_actors_bundle,
     networks::{ChainConfig, Height, NetworkChain},
     state_migration::run_state_migrations,
@@ -14,6 +14,11 @@ use std::{str::FromStr, sync::Arc};
 
 #[tokio::test]
 async fn test_nv17_state_migration_calibnet() -> Result<()> {
+    // forest_filecoin::state_migration: State migration at height Shark(epoch 16800) was successful,
+    // Previous state: bafy2bzacedxtdhqjsrw2twioyaeomdk4z7umhgfv36vzrrotjb4woutphqgyg,
+    // new state: bafy2bzacecrejypa2rqdh3geg2u3qdqdrejrfqvh2ykqcrnyhleehpiynh4k4. Took: 1.4302154s.
+    //
+    // See <https://github.com/ChainSafe/forest/actions/runs/5579505385/jobs/10195488001#step:6:232>
     test_state_migration(
         Height::Shark,
         NetworkChain::Calibnet,
@@ -29,8 +34,10 @@ async fn test_state_migration(
     old_state: Cid,
     expected_new_state: Cid,
 ) -> Result<()> {
-    let store = Arc::new(UncompressedCarV1BackedBlockstore::new(
-        std::fs::File::open(format!("/home/me/fr/snapshots/calibnet/{old_state}.car"))?,
+    let store = Arc::new(CompressedCarV1BackedBlockstore::new(
+        std::io::BufReader::new(std::fs::File::open(format!(
+            "/home/me/fr/snapshots/calibnet/{old_state}.car.zst"
+        ))?),
     )?);
     let chain_config = Arc::new(match network {
         NetworkChain::Calibnet => ChainConfig::calibnet(),
