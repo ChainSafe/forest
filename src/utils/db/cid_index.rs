a6312a5c26152d51a0b1e39c7c38b295bb16b16d
lemmih <david.himmelstrup@chainsafe.io>
2023-07-18 14:07:10 +0200

    play around with linear probing hash tables

--- src/utils/db/cid_index.rs
+++ src/utils/db/cid_index.rs
@@ -1,25 +1,33 @@
+use crate::utils::cid::CidCborExt;
 use cid::Cid;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
 
 // Turn [(Cid, u64)]> into Vec<[u8]>
 
-type Table = Vec<Option<(Cid, u64)>>;
+type Table = Vec<Option<(usize, u64)>>;
 
-fn hash_cid(cid: Cid) -> u64 {
-    u64::from_le_bytes(cid.hash().digest().try_into().unwrap())
+fn hash_cid(cid: Cid) -> usize {
+    usize::from_le_bytes(cid.hash().digest()[0..8].try_into().unwrap())
 }
 
-fn insert(table: &mut Table, entry: (Cid, u64)) {
-    let start_offset = hash_cid(entry.0) as usize % table.len();
+fn insert(table: &mut Table, mut entry: (usize, u64)) {
+    // let entry_offset = hash_cid(entry.0) % table.len();
+    let entry_offset = entry.0 % table.len();
+    let mut at = entry_offset;
     loop {
-        match table[start_offset] {
+        match table[at] {
             None => {
-                table[start_offset] = Some(entry);
+                table[at] = Some(entry);
                 break;
             }
             Some(other_entry) => {
-
+                let other_offset = other_entry.0 % table.len();
+                if entry_offset < other_offset {
+                    table[at] = Some(entry);
+                    entry = other_entry;
+                }
+                at = (at + 1) % table.len();
             }
         }
     }
@@ -29,5 +37,23 @@ fn to_index(values: Vec<(Cid, u64)>) -> Table {
     let size = values.len() * 10 / 9;
     let mut vec = Vec::with_capacity(values.len() * 10 / 9);
     vec.resize(size, None);
+    for (cid, val) in values.into_iter() {
+        insert(&mut vec, (hash_cid(cid), val))
+    }
     vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_insert() {
+        let table = to_index(
+            (0..=50)
+                .map(|i| (Cid::from_cbor_blake2b256(&i).unwrap(), i))
+                .collect(),
+        );
+        dbg!(table);
+    }
 }
