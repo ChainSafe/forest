dd75feea005f4734366b0a977d5e3af3d74c322f
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-07-18 12:09:35 +0200

    fix: improve the ergonomics of `forest-cli snapshot validate` (#3196)

--- src/cli/subcommands/snapshot_cmd.rs
+++ src/cli/subcommands/snapshot_cmd.rs
@@ -6,19 +6,19 @@ use crate::blocks::{tipset_keys_json::TipsetKeysJson, Tipset, TipsetKeys};
 use crate::car_backed_blockstore::{
     self, CompressedCarV1BackedBlockstore, MaxFrameSizeExceeded, UncompressedCarV1BackedBlockstore,
 };
+use crate::chain::store::index::checkpoint_tipsets::genesis_from_checkpoint_tipset;
 use crate::chain::ChainStore;
 use crate::cli::subcommands::{cli_error_and_die, handle_rpc_err};
 use crate::cli_shared::snapshot::{self, TrustedVendor};
 use crate::fil_cns::composition as cns;
 use crate::genesis::read_genesis_header;
 use crate::ipld::{recurse_links_hash, CidHashSet};
-use crate::networks::NetworkChain;
+use crate::networks::{calibnet, mainnet, ChainConfig, NetworkChain};
 use crate::rpc_api::{chain_api::ChainExportParams, progress_api::GetProgressType};
 use crate::rpc_client::{chain_ops::*, progress_ops::get_progress};
-use crate::shim::clock::ChainEpoch;
 use crate::state_manager::StateManager;
 use crate::utils::{io::ProgressBar, proofs_api::paramfetch::ensure_params_downloaded};
-use anyhow::{bail, Context};
+use anyhow::{bail, Context, Result};
 use chrono::Utc;
 use clap::Subcommand;
 use fvm_ipld_blockstore::Blockstore;
@@ -55,13 +55,16 @@ pub enum SnapshotCommands {
 
     /// Validates the snapshot.
     Validate {
-        /// Number of block headers to validate from the tip
-        #[arg(long, default_value = "2000")]
-        recent_stateroots: i64,
-        /// Validate already computed tipsets at given EPOCH,
-        /// use a negative value -N to validate the last N EPOCH(s) starting at HEAD.
+        /// Number of recent epochs to scan for broken links
+        #[arg(long, default_value_t = 2000)]
+        check_links: u32,
+        /// Assert the snapshot belongs to this network. If left blank, the
+        /// network will be inferred before executing messages.
         #[arg(long)]
-        validate_tipsets: Option<i64>,
+        check_network: Option<crate::networks::NetworkChain>,
+        /// Number of recent epochs to scan for bad messages/transactions
+        #[arg(long, default_value_t = 60)]
+        check_stateroots: u32,
         /// Path to a snapshot CAR, which may be zstd compressed
         snapshot: PathBuf,
     },
@@ -79,7 +82,7 @@ pub enum SnapshotCommands {
 }
 
 impl SnapshotCommands {
-    pub async fn run(&self, config: Config) -> anyhow::Result<()> {
+    pub async fn run(self, config: Config) -> Result<()> {
         match self {
             Self::Export {
                 output_path,
@@ -112,8 +115,8 @@ impl SnapshotCommands {
                     recent_roots: config.chain.recent_state_roots,
                     output_path,
                     tipset_keys: TipsetKeysJson(chain_head.key().clone()),
-                    skip_checksum: *skip_checksum,
-                    dry_run: *dry_run,
+                    skip_checksum,
+                    dry_run,
                 };
 
                 let bar = Arc::new(tokio::sync::Mutex::new({
@@ -153,7 +156,7 @@ impl SnapshotCommands {
                 Ok(())
             }
             Self::Fetch { directory, vendor } => {
-                match snapshot::fetch(directory, &config.chain.network, *vendor).await {
+                match snapshot::fetch(&directory, &config.chain.network, vendor).await {
                     Ok(out) => {
                         println!("{}", out.display());
                         Ok(())
@@ -162,20 +165,21 @@ impl SnapshotCommands {
                 }
             }
             Self::Validate {
-                recent_stateroots,
-                validate_tipsets,
+                check_links,
+                check_network,
+                check_stateroots,
                 snapshot,
             } => {
                 // this is all blocking...
                 use std::fs::File;
-                match CompressedCarV1BackedBlockstore::new(BufReader::new(File::open(snapshot)?)) {
+                match CompressedCarV1BackedBlockstore::new(BufReader::new(File::open(&snapshot)?)) {
                     Ok(store) => {
                         validate_with_blockstore(
-                            &config,
                             store.roots(),
                             Arc::new(store),
-                            recent_stateroots,
-                            *validate_tipsets,
+                            check_links,
+                            check_network,
+                            check_stateroots,
                         )
                         .await
                     }
@@ -189,13 +193,13 @@ impl SnapshotCommands {
                     }
                     Err(error) => {
                         info!(%error, "file may be uncompressed, retrying as a plain CAR...");
-                        let store = UncompressedCarV1BackedBlockstore::new(File::open(snapshot)?)?;
+                        let store = UncompressedCarV1BackedBlockstore::new(File::open(&snapshot)?)?;
                         validate_with_blockstore(
-                            &config,
                             store.roots(),
                             Arc::new(store),
-                            recent_stateroots,
-                            *validate_tipsets,
+                            check_links,
+                            check_network,
+                            check_stateroots,
                         )
                         .await
                     }
@@ -218,11 +222,11 @@ impl SnapshotCommands {
                 use tokio::fs::File;
                 match zstd_compress_varint_manyframe(
                     async_compression::tokio::bufread::ZstdDecoder::new(tokio::io::BufReader::new(
-                        File::open(source).await?,
+                        File::open(&source).await?,
                     )),
-                    File::create(destination).await?,
-                    *frame_size,
-                    *compression_level,
+                    File::create(&destination).await?,
+                    frame_size,
+                    compression_level,
                 )
                 .await
                 {
@@ -230,10 +234,10 @@ impl SnapshotCommands {
                     Err(error) => {
                         info!(%error, "file may be uncompressed, retrying as a plain CAR...");
                         zstd_compress_varint_manyframe(
-                            File::open(source).await?,
-                            File::create(destination).await?,
-                            *frame_size,
-                            *compression_level,
+                            File::open(&source).await?,
+                            File::create(&destination).await?,
+                            frame_size,
+                            compression_level,
                         )
                         .await?;
                         Ok(())
@@ -244,135 +248,207 @@ impl SnapshotCommands {
     }
 }
 
+// Check the validity of a snapshot by looking at IPLD links, the genesis block,
+// and message output. More checks may be added in the future.
+//
+// If the snapshot is valid, the output should look like this:
+//     Checking IPLD integrity:       ✅ verified!
+//     Identifying genesis block:     ✅ found!
+//     Verifying network identity:    ✅ verified!
+//     Running tipset transactions:   ✅ verified!
+//   Snapshot is valid
+//
+// If we receive a mainnet snapshot but expect a calibnet snapshot, the output
+// should look like this:
+//     Checking IPLD integrity:       ✅ verified!
+//     Identifying genesis block:     ✅ found!
+//     Verifying network identity:    ❌ wrong!
+//   Error: Expected mainnet but found calibnet
 async fn validate_with_blockstore<BlockstoreT>(
-    config: &Config,
     roots: Vec<Cid>,
     store: Arc<BlockstoreT>,
-    recent_stateroots: &i64,
-    validate_tipsets: Option<i64>,
-) -> anyhow::Result<()>
+    check_links: u32,
+    check_network: Option<NetworkChain>,
+    check_stateroots: u32,
+) -> Result<()>
 where
     BlockstoreT: Blockstore + Send + Sync + 'static,
 {
-    let genesis = read_genesis_header(
-        config.client.genesis_file.as_ref(),
-        config.chain.genesis_bytes(),
-        &store,
-    )
-    .await?;
-    let chain_data_root = TempDir::new()?;
-    let chain_store = Arc::new(ChainStore::new(
-        Arc::clone(&store),
-        config.chain.clone(),
-        &genesis,
-        chain_data_root.path(),
-    )?);
+    let tipset_key = TipsetKeys::new(roots);
+    let store_clone = Arc::clone(&store);
+    let ts = Tipset::load(&store_clone, &tipset_key)?.context("missing root tipset")?;
 
-    let ts = Tipset::load(&store, &TipsetKeys::new(roots))?.context("missing root tipset")?;
+    if check_links != 0 {
+        validate_ipld_links(ts.clone(), &store, check_links).await?;
+    }
 
-    validate_links_and_genesis_traversal(
-        &chain_store,
-        &ts,
-        chain_store.blockstore(),
-        *recent_stateroots,
-        &Tipset::from(genesis),
-        &config.chain.network,
-    )
-    .await?;
-
-    if let Some(validate_from) = validate_tipsets {
-        let last_epoch = match validate_from.is_negative() {
-            true => ts.epoch() + validate_from,
-            false => validate_from,
-        };
-        // Set proof parameter data dir
-        if cns::FETCH_PARAMS {
-            crate::utils::proofs_api::paramfetch::set_proofs_parameter_cache_dir_env(
-                &config.client.data_dir,
-            );
+    if let Some(expected_network) = &check_network {
+        let actual_network = query_network(ts.clone(), &store)?;
+        // Somewhat silly use of a spinner but this makes the checks line up nicely.
+        let pb = validation_spinner("Verifying network identity:");
+        if expected_network != &actual_network {
+            pb.finish_with_message("❌ wrong!");
+            bail!("Expected {} but found {}", expected_network, actual_network);
+        } else {
+            pb.finish_with_message("✅ verified!");
         }
-        // Initialize StateManager
-        let state_manager = Arc::new(StateManager::new(chain_store, Arc::clone(&config.chain))?);
-        ensure_params_downloaded().await?;
-        // Prepare tipset stream to validate
-        let tipsets = ts
-            .chain(&store)
-            .map(|ts| Arc::clone(&Arc::new(ts)))
-            .take_while(|tipset| tipset.epoch() >= last_epoch);
+    }
 
-        state_manager.validate_tipsets(tipsets)?
+    if check_stateroots != 0 {
+        let network = check_network
+            .map(anyhow::Ok)
+            .unwrap_or_else(|| query_network(ts.clone(), &store))?;
+        validate_stateroots(ts, &store, network, check_stateroots).await?;
     }
 
     println!("Snapshot is valid");
     Ok(())
 }
 
-async fn validate_links_and_genesis_traversal<DB>(
-    chain_store: &ChainStore<DB>,
-    ts: &Tipset,
-    db: &DB,
-    recent_stateroots: ChainEpoch,
-    genesis_tipset: &Tipset,
-    network: &NetworkChain,
-) -> anyhow::Result<()>
+// The Filecoin block chain is a DAG of Ipld nodes. The complete graph isn't
+// required to sync to the network and snapshot files usually disgard data after
+// 2000 epochs. Validity can be verified by ensuring there are no bad IPLD or
+// broken links in the N most recent epochs.
+async fn validate_ipld_links<DB>(ts: Tipset, db: &DB, epochs: u32) -> Result<()>
 where
     DB: Blockstore + Send + Sync,
 {
+    let epoch_limit = ts.epoch() - epochs as i64;
     let mut seen = CidHashSet::default();
-    let upto = ts.epoch() - recent_stateroots;
 
-    let mut tsk = ts.parents().clone();
+    let pb = validation_spinner("Checking IPLD integrity:").with_finish(
+        indicatif::ProgressFinish::AbandonWithMessage("❌ Invalid IPLD data!".into()),
+    );
 
-    let total_size = ts.epoch();
-    let pb = crate::utils::io::ProgressBar::new(total_size as u64);
-    pb.message("Validating tipsets: ");
-    pb.set_max_refresh_rate(Some(std::time::Duration::from_millis(500)));
-
-    // Security: Recursive snapshots are difficult to create but not impossible.
-    // This limits the amount of recursion we do.
-    let mut prev_epoch = ts.epoch();
-    loop {
-        // if we reach 0 here, it means parent traversal didn't end up reaching genesis
-        // properly, bail with error.
-        if prev_epoch <= 0 {
-            bail!("Broken invariant: no genesis tipset in snapshot.");
-        }
-
-        let tipset = chain_store.tipset_from_keys(&tsk)?;
+    for tipset in ts
+        .chain(db)
+        .take_while(|tipset| tipset.epoch() > epoch_limit)
+    {
         let height = tipset.epoch();
-        // if parent tipset epoch is smaller than child, bail with error.
-        if height >= prev_epoch {
-            bail!("Broken tipset invariant: parent epoch larger than current epoch at: {height}");
-        }
-        // genesis is reachable, break with success
-        if height == 0 {
-            if tipset.as_ref() != genesis_tipset {
-                bail!("Invalid genesis tipset. Snapshot isn't valid for {network}. It may be valid for another network.");
-            }
+        pb.set_message(format!("{} remaining epochs", height - epoch_limit));
 
-            break;
-        }
-        // check for ipld links backwards till `upto`
-        if height > upto {
-            let mut assert_cid_exists = |cid: Cid| async move {
-                let data = db.get(&cid)?;
-                data.ok_or_else(|| anyhow::anyhow!("Broken IPLD link at epoch: {height}"))
-            };
+        let mut assert_cid_exists = |cid: Cid| async move {
+            let data = db.get(&cid)?;
+            data.ok_or_else(|| anyhow::anyhow!("Broken IPLD link at epoch: {height}"))
+        };
 
-            for h in tipset.blocks() {
-                recurse_links_hash(&mut seen, *h.state_root(), &mut assert_cid_exists, &|_| ())
-                    .await?;
-                recurse_links_hash(&mut seen, *h.messages(), &mut assert_cid_exists, &|_| ())
-                    .await?;
-            }
+        for h in tipset.blocks() {
+            recurse_links_hash(&mut seen, *h.state_root(), &mut assert_cid_exists, &|_| ()).await?;
+            recurse_links_hash(&mut seen, *h.messages(), &mut assert_cid_exists, &|_| ()).await?;
         }
-
-        tsk = tipset.parents().clone();
-        prev_epoch = tipset.epoch();
-        pb.set((ts.epoch() - tipset.epoch()) as u64);
     }
 
-    drop(pb);
-
+    pb.finish_with_message("✅ verified!");
     Ok(())
+}
+
+// The genesis block determines the network identity (e.g., mainnet or
+// calibnet). Scanning through the entire blockchain can be time-consuming, so
+// Forest keeps a list of known tipsets for each network. Finding a known tipset
+// short-circuits the search for the genesis block. If no genesis block can be
+// found or if the genesis block is unrecognizable, an error is returned.
+fn query_network<DB>(ts: Tipset, db: &DB) -> Result<NetworkChain>
+where
+    DB: Blockstore + Send + Sync + Clone + 'static,
+{
+    let pb = validation_spinner("Identifying genesis block:").with_finish(
+        indicatif::ProgressFinish::AbandonWithMessage("✅ found!".into()),
+    );
+
+    fn match_genesis_block(block_cid: Cid) -> Result<NetworkChain> {
+        if block_cid.to_string() == calibnet::GENESIS_CID {
+            Ok(NetworkChain::Calibnet)
+        } else if block_cid.to_string() == mainnet::GENESIS_CID {
+            Ok(NetworkChain::Mainnet)
+        } else {
+            bail!("Unrecognizable genesis block");
+        }
+    }
+
+    for tipset in pb.wrap_iter(ts.chain(db)) {
+        if tipset.epoch() == 0 {
+            return match_genesis_block(*tipset.min_ticket_block().cid());
+        }
+
+        if let Some(genesis_keys) = genesis_from_checkpoint_tipset(tipset.key()) {
+            let genesis_cid = genesis_keys.cids[0];
+            return match_genesis_block(genesis_cid);
+        }
+    }
+    pb.finish_with_message("❌ No valid genesis block!");
+    bail!("Snapshot does not contain a genesis block")
+}
+
+// Each tipset in the blockchain contains a set of messages. A message is a
+// transaction that manipulates a persistent state-tree. The hashes of these
+// state-trees are stored in the tipsets and can be used to verify if the
+// messages were correctly executed.
+// Note: Messages may access state-trees 900 epochs in the past. So, if a
+// snapshot has state-trees for 2000 epochs, one can only validate the messages
+// for the last 1100 epochs.
+async fn validate_stateroots<DB>(
+    ts: Tipset,
+    db: &DB,
+    network: NetworkChain,
+    epochs: u32,
+) -> Result<()>
+where
+    DB: Blockstore + Send + Sync + Clone + 'static,
+{
+    let chain_config = Arc::new(ChainConfig::from_chain(&network));
+    let genesis = read_genesis_header(None, chain_config.genesis_bytes(), db).await?;
+
+    let chain_data_root = TempDir::new()?;
+    let chain_store = Arc::new(ChainStore::new(
+        Arc::new(db.clone()),
+        Arc::clone(&chain_config),
+        &genesis,
+        chain_data_root.path(),
+    )?);
+
+    let pb = validation_spinner("Running tipset transactions:").with_finish(
+        indicatif::ProgressFinish::AbandonWithMessage(
+            "❌ Transaction result differs from Lotus!".into(),
+        ),
+    );
+
+    let last_epoch = ts.epoch() - epochs as i64;
+
+    // Set proof parameter data dir
+    if cns::FETCH_PARAMS {
+        crate::utils::proofs_api::paramfetch::set_proofs_parameter_cache_dir_env(
+            &Config::default().client.data_dir,
+        );
+    }
+    // Initialize StateManager
+    let state_manager = Arc::new(StateManager::new(chain_store, Arc::clone(&chain_config))?);
+    ensure_params_downloaded().await?;
+
+    // Prepare tipsets for validation
+    let tipsets = ts
+        .chain(db)
+        .map(|ts| Arc::clone(&Arc::new(ts)))
+        .take_while(|tipset| tipset.epoch() >= last_epoch)
+        .inspect(|tipset| {
+            pb.set_message(format!("epoch queue: {}", tipset.epoch() - last_epoch));
+        });
+
+    // ProgressBar::wrap_iter believes the progress has been abandoned once the
+    // iterator is consumed.
+    state_manager.validate_tipsets(tipsets)?;
+
+    pb.finish_with_message("✅ verified!");
+    drop(pb);
+    Ok(())
+}
+
+fn validation_spinner(prefix: &'static str) -> indicatif::ProgressBar {
+    let pb = indicatif::ProgressBar::new_spinner()
+        .with_style(
+            indicatif::ProgressStyle::with_template("{spinner} {prefix:<30} {msg}")
+                .expect("indicatif template must be valid"),
+        )
+        .with_prefix(prefix);
+    pb.enable_steady_tick(std::time::Duration::from_secs_f32(0.1));
+    pb
 }
