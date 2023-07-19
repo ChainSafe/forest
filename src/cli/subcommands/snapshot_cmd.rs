aa18d826129b2ac66404ea7ad4cb71b123127d64
lemmih <david.himmelstrup@chainsafe.io>
2023-07-19 12:13:39 +0200

    download bundles in `forest-cli snapshot validate`

--- src/cli/subcommands/snapshot_cmd.rs
+++ src/cli/subcommands/snapshot_cmd.rs
@@ -10,6 +10,7 @@ use crate::chain::store::index::checkpoint_tipsets::genesis_from_checkpoint_tips
 use crate::chain::ChainStore;
 use crate::cli::subcommands::{cli_error_and_die, handle_rpc_err};
 use crate::cli_shared::snapshot::{self, TrustedVendor};
+use crate::daemon::bundle::load_bundles;
 use crate::fil_cns::composition as cns;
 use crate::genesis::read_genesis_header;
 use crate::ipld::{recurse_links_hash, CidHashSet};
@@ -413,6 +414,18 @@ where
     );
 
     let last_epoch = ts.epoch() - epochs as i64;
+
+    // Bundles are required when doing state migrations. Download any bundles
+    // that may be necessary after `last_epoch`.
+    load_bundles(
+        last_epoch,
+        &Config {
+            chain: chain_config.clone(),
+            ..Default::default()
+        },
+        Arc::new(db.clone()),
+    )
+    .await?;
 
     // Set proof parameter data dir
     if cns::FETCH_PARAMS {
