01958ebc34bfb9109dfadc239583db7a324f884d
Aatif Syed <aatifsyedyp@gmail.com>
2023-07-20 18:49:19 +0100

    feat: zip_longest

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -22,9 +22,11 @@ use std::{
 
 use futures::{
     future::{pending, FusedFuture},
-    select, Future, FutureExt,
+    select,
+    stream::Fuse,
+    Future, FutureExt as _, Stream, StreamExt as _, TryStream,
 };
-use futures_util::{Stream, TryStream};
+use itertools::{Either, EitherOrBoth};
 use pin_project_lite::pin_project;
 use tokio::time::sleep;
 use tracing::error;
@@ -171,11 +173,95 @@ where
     }
 }
 
+/// Returns a stream of [`EitherOrBoth`], taking items from the inner two streams.
+/// The inner streams are fused - they're never polled again after the first time
+/// they return [`None`]
+pub fn zip_longest<LStream: Stream, RStream: Stream>(
+    left: LStream,
+    right: RStream,
+) -> ZipLongest<LStream, RStream, LStream::Item, RStream::Item> {
+    fn assert_stream<T: Stream>(t: T) -> T {
+        t
+    }
+
+    assert_stream(ZipLongest {
+        left: left.fuse(),
+        right: right.fuse(),
+        cache: None,
+    })
+}
+
+pin_project! {
+    /// Stream for [`zip_longest`], see that function for more
+    pub struct ZipLongest<LStream, RStream, L, R> {
+        #[pin]
+        left: Fuse<LStream>,
+        #[pin]
+        right: Fuse<RStream>,
+        // this could probably be rewritten to be Option<L>, but the symmetry is nice
+        cache: Option<Either<L, R>>,
+    }
+}
+
+impl<LStream, RStream, L, R> Stream for ZipLongest<LStream, RStream, L, R>
+where
+    LStream: Stream<Item = L>,
+    RStream: Stream<Item = R>,
+{
+    type Item = EitherOrBoth<L, R>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.project();
+        match this.cache.take() {
+            Some(Either::Left(left)) => match this.right.poll_next(cx) {
+                Poll::Ready(Some(right)) => Poll::Ready(Some(EitherOrBoth::Both(left, right))),
+                Poll::Ready(None) => Poll::Ready(Some(EitherOrBoth::Left(left))),
+                Poll::Pending => {
+                    *this.cache = Some(Either::Left(left));
+                    Poll::Pending
+                }
+            },
+            Some(Either::Right(right)) => match this.left.poll_next(cx) {
+                Poll::Ready(Some(left)) => Poll::Ready(Some(EitherOrBoth::Both(left, right))),
+                Poll::Ready(None) => Poll::Ready(Some(EitherOrBoth::Right(right))),
+                Poll::Pending => {
+                    *this.cache = Some(Either::Right(right));
+                    Poll::Pending
+                }
+            },
+            None => match (this.left.poll_next(cx), this.right.poll_next(cx)) {
+                (Poll::Ready(None), Poll::Ready(None)) => Poll::Ready(None),
+                (Poll::Ready(Some(left)), Poll::Ready(Some(right))) => {
+                    Poll::Ready(Some(EitherOrBoth::Both(left, right)))
+                }
+                (Poll::Ready(Some(left)), Poll::Ready(None)) => {
+                    Poll::Ready(Some(EitherOrBoth::Left(left)))
+                }
+                (Poll::Ready(None), Poll::Ready(Some(right))) => {
+                    Poll::Ready(Some(EitherOrBoth::Right(right)))
+                }
+                (Poll::Ready(Some(left)), Poll::Pending) => {
+                    *this.cache = Some(Either::Left(left));
+                    Poll::Pending
+                }
+                (Poll::Pending, Poll::Ready(Some(right))) => {
+                    *this.cache = Some(Either::Right(right));
+                    Poll::Pending
+                }
+                // the streams are fused, so they'll return none next time too
+                (Poll::Ready(None), Poll::Pending)
+                | (Poll::Pending, Poll::Ready(None))
+                | (Poll::Pending, Poll::Pending) => Poll::Pending,
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     mod files;
 
-    use futures::stream::StreamExt as _;
+    use futures::stream::{self, StreamExt as _};
     use std::{future::ready, sync::atomic::AtomicUsize};
     use RetryError::{RetriesExceeded, TimeoutExceeded};
 
@@ -232,7 +318,7 @@ mod tests {
     async fn test_try_collate() {
         let source = futures::stream::iter(["the", "cuttlefish", "is", "not", "a", "fish"])
             .map(Ok)
-            .chain(futures::stream::iter([Err(())]));
+            .chain(stream::iter([Err(())]));
 
         let mut collated = try_collate(
             source,
@@ -254,5 +340,28 @@ mod tests {
         assert_eq!(collated.next().await.unwrap().unwrap(), "afish");
         collated.next().await.unwrap().unwrap_err();
         assert!(collated.next().await.is_none());
+    }
+
+    #[tokio::test]
+    async fn test_zip_longest() {
+        let mut right_overhang = zip_longest(stream::iter([0]), stream::iter(0..=1));
+        assert_eq!(
+            right_overhang.next().await.unwrap(),
+            EitherOrBoth::Both(0, 0)
+        );
+        assert_eq!(right_overhang.next().await.unwrap(), EitherOrBoth::Right(1));
+        assert!(right_overhang.next().await.is_none());
+
+        let mut left_overhang = zip_longest(stream::iter(0..=1), stream::iter([0]));
+        assert_eq!(
+            left_overhang.next().await.unwrap(),
+            EitherOrBoth::Both(0, 0)
+        );
+        assert_eq!(left_overhang.next().await.unwrap(), EitherOrBoth::Left(1));
+        assert!(left_overhang.next().await.is_none());
+
+        let mut equal = zip_longest(stream::iter([0]), stream::iter([0]));
+        assert_eq!(equal.next().await.unwrap(), EitherOrBoth::Both(0, 0));
+        assert!(equal.next().await.is_none());
     }
 }
