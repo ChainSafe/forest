935426106638fd898a307901de068d23ba41fcdc
Aatif Syed <aatifsyedyp@gmail.com>
2023-07-21 14:43:14 +0100

    test: car_diff

--- tests/car_diff.rs
+++ tests/car_diff.rs
@@ -1,9 +1,60 @@
 use std::io::Write as _;
 
-use cid::Cid;
+use assert_cmd::{assert::Assert, Command};
+use cid::{multihash::Multihash, Cid};
 use futures::executor::block_on;
 use fvm_ipld_car::CarHeader;
 use tempfile::NamedTempFile;
+
+#[test]
+fn identical() {
+    let left = make_car([Cid::default()], [(Cid::default(), vec![])]);
+    let right = make_car([Cid::default()], [(Cid::default(), vec![])]);
+    diff(left, right).success();
+}
+
+#[test]
+fn different_cid() {
+    let left = make_car([Cid::default()], [(Cid::default(), vec![])]);
+    let right = make_car([Cid::default()], [(other_cid(), vec![])]);
+    diff(left, right).failure();
+}
+
+#[test]
+fn different_body() {
+    let left = make_car([Cid::default()], [(Cid::default(), vec![0])]);
+    let right = make_car([Cid::default()], [(Cid::default(), vec![1])]);
+    diff(left, right).failure();
+}
+
+#[test]
+fn longer() {
+    let left = make_car([Cid::default()], []);
+    let right = make_car([Cid::default()], [(Cid::default(), vec![])]);
+    diff(left, right).failure();
+}
+
+#[test]
+fn same_then_different() {
+    let left = make_car(
+        [Cid::default()],
+        [(Cid::default(), vec![]), (Cid::default(), vec![])],
+    );
+    let right = make_car(
+        [Cid::default()],
+        [(Cid::default(), vec![]), (other_cid(), vec![])],
+    );
+    diff(left, right).failure();
+}
+
+fn diff(left: NamedTempFile, right: NamedTempFile) -> Assert {
+    Command::cargo_bin("forest-cli")
+        .unwrap()
+        .args(["car", "diff"])
+        .arg(left.path())
+        .arg(right.path())
+        .assert()
+}
 
 fn make_car(
     roots: impl IntoIterator<Item = Cid>,
@@ -18,4 +69,11 @@ fn make_car(
     let mut file = NamedTempFile::new().unwrap();
     file.write_all(&buffer).unwrap();
     file
+}
+
+/// A non-default CID
+fn other_cid() -> Cid {
+    let cid = Cid::new_v1(1, Multihash::wrap(1, &[]).unwrap());
+    assert_ne!(cid, Cid::default());
+    cid
 }
