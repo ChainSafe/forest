eb6715b2e663058ff960e32d5cda59030fafcb86
lemmih <david.himmelstrup@chainsafe.io>
2023-07-21 13:24:09 +0200

    apply clippy suggestions

--- src/cli/subcommands/snapshot_cmd.rs
+++ src/cli/subcommands/snapshot_cmd.rs
@@ -163,8 +163,8 @@ impl SnapshotCommands {
                 let _ = handle.await;
 
                 std::fs::rename(
-                    &temp_path.with_extension("sha256sum"),
-                    &output_path.with_extension("sha256sum"),
+                    temp_path.with_extension("sha256sum"),
+                    output_path.with_extension("sha256sum"),
                 )?;
                 temp_path.persist(output_path)?;
 
