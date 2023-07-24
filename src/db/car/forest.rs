00e2dd648114caf6c2ed4945ca6e33488c35a98c
lemmih <david.himmelstrup@chainsafe.io>
2023-07-24 11:59:53 +0200

    draft ForestCAR writer

--- src/db/car/forest.rs
+++ src/db/car/forest.rs
@@ -10,8 +10,11 @@ use bytes::{buf::Writer, BufMut as _, Bytes, BytesMut};
 use cid::Cid;
 use futures::future::Either;
 use futures::{Stream, TryStream, TryStreamExt as _};
-use std::task::{Poll};
+use std::task::Poll;
 use std::{io, io::Write};
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use std::pin::{Pin, pin};
+use ahash::HashMapExt;
 
 // Input: stream of blocks
 // Output: (BlockPosition, Option<zst_frame>)
@@ -19,6 +22,27 @@ use std::{io, io::Write};
 struct ForestCAR {}
 
 impl ForestCAR {
+    pub async fn write(
+        sink: &mut (impl AsyncWrite + Unpin),
+        mut stream: impl TryStream<Ok = Either<(Cid, BlockPosition), Bytes>, Error = io::Error> + Unpin,
+    ) -> io::Result<()> {
+        let mut cid_map = ahash::HashMap::new();
+        // Write seekable zstd and collect a mapping of CIDs to frame_offset+data_offset.
+        while let Some(either) = stream.try_next().await? {
+            match either {
+                Either::Left((cid, position)) => {
+                    cid_map.insert(cid, position);
+                },
+                Either::Right(zstd_frame) => {
+                    sink.write_all(&zstd_frame).await?;
+                }
+            }
+        }
+        // Create index
+        // crate::car_backed_blockstore::write_skip_frame(&mut file, &index)?;
+        Ok(())
+    }
+
     pub fn compress_stream(
         zstd_frame_size_tripwire: usize,
         zstd_compression_level: u16,
@@ -31,7 +55,7 @@ impl ForestCAR {
         let mut stream = Box::pin(stream.into_stream());
         futures::stream::poll_fn(move |cx| {
             // Emit frame if compressed_len >= zstd_frame_size_tripwire OR uncompressed_len >= 2^16
-            if compressed_len(&mut encoder) >= zstd_frame_size_tripwire || frame_offset >= 1 << 16 {
+            if compressed_len(&encoder) >= zstd_frame_size_tripwire || frame_offset >= 1 << 16 {
                 let frame = finalize_frame(zstd_compression_level, &mut encoder)?;
                 emitted_bytes += frame.len();
                 frame_offset = 0;
@@ -43,7 +67,7 @@ impl ForestCAR {
                 // End-of-stream
                 None => {
                     // If there's anything in the zstd buffer, emit it.
-                    if compressed_len(&mut encoder) > 0 {
+                    if compressed_len(&encoder) > 0 {
                         let frame = finalize_frame(zstd_compression_level, &mut encoder)?;
                         Poll::Ready(Some(Ok(Either::Right(frame))))
                     } else {
@@ -75,7 +99,7 @@ impl ForestCAR {
     }
 }
 
-fn compressed_len(encoder: &mut zstd::Encoder<'static, Writer<BytesMut>>) -> usize {
+fn compressed_len(encoder: &zstd::Encoder<'static, Writer<BytesMut>>) -> usize {
     encoder.get_ref().get_ref().len()
 }
 
