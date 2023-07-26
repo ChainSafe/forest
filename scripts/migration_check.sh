cefd847b67b4441202f2e978ae8e7b6cb70db3dd
sudo-shashank <shashank@chainsafe.io>
2023-07-26 10:02:31 +0530

    fmt

--- scripts/migration_check.sh
+++ scripts/migration_check.sh
@@ -7,14 +7,14 @@ function sync_with_tag() {
     echo "Syncing using tag ($tag)..."
 
     # Write build and sync logic here
-    git checkout $tag
+    git checkout "$tag"
     make clean
     make install
 
     forest --chain calibnet --encrypt-keystore false --auto-download-snapshot --detach
     forest-cli --chain calibnet sync wait
     # Check if the sync succeeded for the tag
-    if [ $? -eq 0 ]; then
+    if forest-cli --chain calibnet sync wait; then
         echo "Sync successful for tag: $tag"
         pkill -9 forest
         sleep 5s
@@ -39,7 +39,7 @@ tags=$(git tag --sort=creatordate)
 # Database migration are not supported for forest version below `v0.11.1`
 is_tag_valid=false
 
-echo "Testing db migrations from "V0.11.1" to latest, one by one"
+echo "Testing db migrations from "v0.11.1" to latest, one by one"
 # Loop through each tag and sync with corresponding version
 for tag in $tags; do
   # Check if the current tag matches the start tag
@@ -52,13 +52,13 @@ for tag in $tags; do
   fi
 done
 
-echo "Testing db migration from "V0.11.1" to latest, at once"
+echo "Testing db migration from "v0.11.1" to latest, at once"
 # Get latest tag
 LATEST_TAG=$(git describe --tags --abbrev=0)
 
 # Sync calibnet with Forest `V0.11.1`
-sync_with_tag $START_TAG
+sync_with_tag "$START_TAG"
 # Sync calibnet with latest version of Forest
-sync_with_tag $LATEST_TAG
+sync_with_tag "$LATEST_TAG"
 
 echo "Migration check completed successfully."
