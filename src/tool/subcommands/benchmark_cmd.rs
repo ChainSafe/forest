2fa6aaba9f12507406b40d33443b32d86ce28a30
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-08-03 17:47:33 +0200

    feat: implement `forest-tool benchmark export` (#3318)

--- src/tool/subcommands/benchmark_cmd.rs
+++ src/tool/subcommands/benchmark_cmd.rs
@@ -1,14 +1,21 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use crate::chain::{
+    index::{ChainIndex, ResolveNullTipset},
+    ChainEpochDelta,
+};
 use crate::db::car::ManyCar;
-use crate::ipld::stream_graph;
+use crate::ipld::{stream_chain, stream_graph};
+use crate::shim::clock::ChainEpoch;
 use crate::utils::db::car_stream::CarStream;
 use anyhow::{Context as _, Result};
 use clap::Subcommand;
 use futures::{StreamExt, TryStreamExt};
 use indicatif::{ProgressBar, ProgressStyle};
+use std::ops::Deref;
 use std::path::PathBuf;
+use std::sync::Arc;
 use tokio::{
     fs::File,
     io::{AsyncWrite, AsyncWriteExt, BufReader},
@@ -38,6 +45,24 @@ pub enum BenchmarkCommands {
         #[arg(long, default_value_t = 8000usize.next_power_of_two())]
         frame_size: usize,
     },
+    /// Exporting a `.forest.car.zst` file from HEAD
+    Export {
+        /// Snapshot input files (`.car.`, `.car.zst`, `.forest.car.zst`)
+        #[arg(required = true)]
+        snapshot_files: Vec<PathBuf>,
+        #[arg(long, default_value_t = 3)]
+        compression_level: u16,
+        /// End zstd frames after they exceed this length
+        #[arg(long, default_value_t = 8000usize.next_power_of_two())]
+        frame_size: usize,
+        /// Latest epoch that has to be exported for this snapshot, the upper bound. This value
+        /// cannot be greater than the latest epoch available in the input snapshot.
+        #[arg(short, long)]
+        epoch: Option<ChainEpoch>,
+        /// How many state-roots to include. Lower limit is 900 for `calibnet` and `mainnet`.
+        #[arg(short, long, default_value_t = 2000)]
+        depth: ChainEpochDelta,
+    },
 }
 
 impl BenchmarkCommands {
@@ -52,6 +77,16 @@ impl BenchmarkCommands {
                 compression_level,
                 frame_size,
             } => benchmark_forest_encoding(snapshot_file, compression_level, frame_size).await,
+            Self::Export {
+                snapshot_files,
+                compression_level,
+                frame_size,
+                epoch,
+                depth,
+            } => {
+                benchmark_exporting(snapshot_files, compression_level, frame_size, epoch, depth)
+                    .await
+            }
         }
     }
 }
@@ -108,6 +143,46 @@ async fn benchmark_forest_encoding(
         block_stream.map_err(anyhow::Error::from),
     );
     crate::db::car::forest::Encoder::write(&mut dest, roots, frames).await?;
+    dest.flush().await?;
+    Ok(())
+}
+
+// Exporting combines a graph traversal with ForestCAR.zst encoding. Ideally, it
+// should be no lower than `min(benchmark_graph_traversal,
+// benchmark_forest_encoding)`.
+async fn benchmark_exporting(
+    input: Vec<PathBuf>,
+    compression_level: u16,
+    frame_size: usize,
+    epoch: Option<ChainEpoch>,
+    depth: ChainEpochDelta,
+) -> Result<()> {
+    let store = open_store(input)?;
+    let heaviest = store.heaviest_tipset()?;
+    let idx = ChainIndex::new(&store);
+    let ts = idx.tipset_by_height(
+        epoch.unwrap_or(heaviest.epoch()),
+        Arc::new(heaviest),
+        ResolveNullTipset::TakeOlder,
+    )?;
+    // We don't do any sanity checking for 'depth'. The output is discarded so
+    // there's no need.
+    let stateroot_lookup_limit = ts.epoch() - depth;
+
+    let mut dest = indicatif_sink("exported");
+
+    let blocks = stream_chain(
+        &store,
+        ts.deref().clone().chain(&store),
+        stateroot_lookup_limit,
+    );
+
+    let frames = crate::db::car::forest::Encoder::compress_stream(
+        frame_size,
+        compression_level,
+        blocks.map_err(anyhow::Error::from),
+    );
+    crate::db::car::forest::Encoder::write(&mut dest, ts.key().cids.clone(), frames).await?;
     dest.flush().await?;
     Ok(())
 }
