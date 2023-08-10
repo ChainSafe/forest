82c0285659b5f404a1b8d5e1ff6463ff04859f79
Aatif Syed <38045910+aatifsyed@users.noreply.github.com>
2023-08-10 11:07:00 +0100

    refactor: paritydb uses lz4 (#3236)

--- src/db/parity_db.rs
+++ src/db/parity_db.rs
@@ -77,32 +77,21 @@ pub struct ParityDb {
     statistics_enabled: bool,
 }
 
-/// Converts string to a compression `ParityDb` variant.
-fn compression_type_from_str(s: &str) -> anyhow::Result<CompressionType> {
-    match s.to_lowercase().as_str() {
-        "none" => Ok(CompressionType::NoCompression),
-        "lz4" => Ok(CompressionType::Lz4),
-        "snappy" => Ok(CompressionType::Snappy),
-        _ => Err(anyhow!("invalid compression option")),
-    }
-}
-
 impl ParityDb {
-    fn to_options(path: PathBuf, config: &ParityDbConfig) -> anyhow::Result<Options> {
-        let compression = compression_type_from_str(&config.compression_type)?;
-        Ok(Options {
+    fn to_options(path: PathBuf, config: &ParityDbConfig) -> Options {
+        Options {
             path,
             sync_wal: true,
             sync_data: true,
             stats: config.enable_statistics,
             salt: None,
-            columns: DbColumn::create_column_options(compression),
+            columns: DbColumn::create_column_options(CompressionType::Lz4),
             compression_threshold: [(0, 128)].into_iter().collect(),
-        })
+        }
     }
 
     pub fn open(path: impl Into<PathBuf>, config: &ParityDbConfig) -> anyhow::Result<Self> {
-        let opts = Self::to_options(path.into(), config)?;
+        let opts = Self::to_options(path.into(), config);
         Ok(Self {
             db: Arc::new(Db::open_or_create(&opts)?),
             statistics_enabled: opts.stats,
@@ -272,7 +261,6 @@ mod test {
     use cid::multihash::MultihashDigest;
     use fvm_ipld_encoding::IPLD_RAW;
     use nom::AsBytes;
-    use parity_db::CompressionType;
 
     use crate::db::tests::db_utils::parity::TempParityDB;
 
@@ -331,24 +319,6 @@ mod test {
         assert_eq!(b"bloop", actual.as_bytes());
 
         Ok(())
-    }
-
-    #[test]
-    fn compression_type_from_str_test() {
-        let test_cases = [
-            ("lz4", Ok(CompressionType::Lz4)),
-            ("SNAPPY", Ok(CompressionType::Snappy)),
-            ("none", Ok(CompressionType::NoCompression)),
-            ("cthulhu", Err(anyhow!("some error message"))),
-        ];
-        for (input, expected) in test_cases {
-            let actual = compression_type_from_str(input);
-            if let Ok(compression) = actual {
-                assert_eq!(expected.unwrap(), compression);
-            } else {
-                assert!(expected.is_err());
-            }
-        }
     }
 
     #[test]
