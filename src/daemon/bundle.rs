d1804ec056bac28761b2eb658b7bcb4a73620984
hanabi1224 <harlowmoo@gmail.com>
2023-08-16 18:46:26 +0800

    const

--- src/daemon/bundle.rs
+++ src/daemon/bundle.rs
@@ -7,17 +7,16 @@ use cid::Cid;
 use fvm_ipld_blockstore::Blockstore;
 
 pub async fn load_actor_bundles(db: &impl Blockstore) -> anyhow::Result<Vec<Cid>> {
-    pub const ACTOR_BUNDLES_CAR_ZST: &[u8] = include_bytes!("../../assets/actor_bundles.car.zst");
-    assert!(
-        ACTOR_BUNDLES_CAR_ZST.len() > 1024 * 1024,
-        "Actor bundles assets are not properly downloaded, make sure git-lfs is installed and run `git lfs pull` again. See <https://github.com/git-lfs/git-lfs/blob/main/INSTALLING.md>"
-    );
+    const ERROR_MESSAGE: &str = "Actor bundles assets are not properly downloaded, make sure git-lfs is installed and run `git lfs pull` again. See <https://github.com/git-lfs/git-lfs/blob/main/INSTALLING.md>";
+    const ACTOR_BUNDLES_CAR_ZST: &[u8] = include_bytes!("../../assets/actor_bundles.car.zst");
+    assert!(ACTOR_BUNDLES_CAR_ZST.len() > 1024 * 1024, "{ERROR_MESSAGE}");
 
     fvm_ipld_car::load_car(
         db,
         ZstdDecoder::new(futures::io::BufReader::new(ACTOR_BUNDLES_CAR_ZST)),
     )
-    .await.context("Actor bundles assets are not properly downloaded, make sure git-lfs is installed and run `git lfs pull` again. See <https://github.com/git-lfs/git-lfs/blob/main/INSTALLING.md>")
+    .await
+    .context(ERROR_MESSAGE)
 }
 
 #[cfg(test)]
