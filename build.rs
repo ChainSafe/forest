59941d0a7d01471b6898df48b6c15d39c26ef636
Hubert Bugaj <hubert@chainsafe.io>
2023-08-29 15:48:06 +0200

    move assertion to build script

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+use anyhow::{bail, Context};
 use protobuf_codegen::Customize;
 use std::path::PathBuf;
 use walkdir::WalkDir;
@@ -14,13 +15,39 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn ensure_required_bins_installed() -> anyhow::Result<()> {
-    which::which("git-lfs").map(|_| ()).map_err(|_| {
-        anyhow::anyhow!(
-            "git lfs is not installed. Please install git lfs and \
-            run `git lfs pull`. \
-            See https://github.com/git-lfs/git-lfs/blob/main/INSTALLING.md"
+    println!("cargo:rerun-if-changed=assets");
+
+    let is_bundle_size_correct = || -> anyhow::Result<bool> {
+        let bundle_size = std::fs::metadata("assets/actor_bundles.car.zst")?.len();
+        Ok(bundle_size == 2_438_387)
+    };
+
+    // If the bundle size is correct, we don't need to pull the bundle.
+    // Don't bail on failure, e.g., in case the file is not present.
+    // We can still get it.
+    if let Ok(true) = is_bundle_size_correct() {
+        return Ok(());
+    }
+
+    std::process::Command::new("git-lfs")
+        .arg("pull")
+        .status()
+        .with_context(|| {
+            anyhow::anyhow!(
+                "failed to run git lfs pull. \
+            Please ensure git-lfs is installed."
+            )
+        })?;
+
+    if is_bundle_size_correct()? {
+        Ok(())
+    } else {
+        bail!(
+            "actor bundle size is incorrect. \
+            Please run `git lfs pull` and try again. If this problem persists, \
+            please open an issue."
         )
-    })
+    }
 }
 
 fn generate_protobuf_code() -> anyhow::Result<()> {
