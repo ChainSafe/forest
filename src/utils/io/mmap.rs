9989c43ba03a53e4304a6c98d864a8d2564e45bf
lemmih <david.himmelstrup@chainsafe.io>
2023-09-01 16:24:37 +0200

    apply clippy suggestion

--- src/utils/io/mmap.rs
+++ src/utils/io/mmap.rs
@@ -23,7 +23,7 @@ impl ReadAt for Mmap {
     fn read_at(&self, pos: u64, buf: &mut [u8]) -> io::Result<usize> {
         let start = pos as usize;
         if start >= self.0.len() {
-            return Ok(0)
+            return Ok(0);
         }
         let end = start + buf.len();
         if end <= self.0.len() {
