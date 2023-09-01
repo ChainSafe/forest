c6c0d3a7fcccfaf9e57c76e4478d315084b378cc
lemmih <david.himmelstrup@chainsafe.io>
2023-09-01 16:23:41 +0200

    add export test with empty car file

--- scripts/tests/forest_tool_check.sh
+++ scripts/tests/forest_tool_check.sh
@@ -5,6 +5,12 @@
 
 set -euxo pipefail
 
-FOREST_TOOL_PATH="forest-tool"
+source "$(dirname "$0")/harness.sh"
 
 "$FOREST_TOOL_PATH" state-migration actor-bundle
+
+# Exporting with an empty car should fail but not panic
+touch empty.car
+ERROR=$($FOREST_CLI_PATH archive export empty.car 2>&1 || true)
+assert_eq "$ERROR" "Error: input not recognized as any kind of CAR data (.car, .car.zst, .forest.car)"
+rm empty.car
