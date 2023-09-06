0b028b5f71cb2cdc2115ed0586503be321f3ac78
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-06 22:26:04 +0100

    refactor: shim::ErrorNumber

--- src/shim/kernel.rs
+++ src/shim/kernel.rs
@@ -4,50 +4,80 @@ use fvm2::kernel::SyscallError as E2;
 use fvm3::kernel::SyscallError as E3;
 use fvm_shared2::error::ErrorNumber as N2;
 use fvm_shared3::error::ErrorNumber as N3;
+use itertools::Either;
 use std::fmt;
 
+macro_rules! error_number {
+    ($($variant:ident),* $(,)?) => {
+        #[derive(Debug, Clone)]
+        #[non_exhaustive]
+        pub enum ErrorNumber {
+            $($variant,)*
+            Unknown(UnknownErrorNumber),
+        }
+
+        $(
+            static_assertions::const_assert_eq!(N2::$variant as u32, N3::$variant as u32);
+        )*
+
+        impl From<N2> for ErrorNumber {
+            fn from(value: N2) -> Self {
+                match value {
+                    $(N2::$variant => Self::$variant,)*
+                    u => Self::Unknown(UnknownErrorNumber(Either::Left(u))),
+                }
+            }
+        }
+
+        impl From<N3> for ErrorNumber {
+            fn from(value: N3) -> Self {
+                match value {
+                    $(N3::$variant => Self::$variant,)*
+                    u => Self::Unknown(UnknownErrorNumber(Either::Right(u))),
+                }
+            }
+        }
+
+        impl ErrorNumber {
+            fn as_unshimmed(&self) -> Either<N2, N3> {
+                match self {
+                    $($variant => Either::Right(N3::$variant),)*
+                    Self::Unknown(UnknownErrorNumber(u)) => *u,
+                }
+            }
+        }
+    };
+}
+
 #[derive(Debug, Clone)]
-pub struct ErrorNumber(ErrorNumberInner);
+pub struct UnknownErrorNumber(Either<N2, N3>);
+
+error_number! {
+    IllegalArgument,
+    IllegalOperation,
+    LimitExceeded,
+    AssertionFailed,
+    InsufficientFunds,
+    NotFound,
+    InvalidHandle,
+    IllegalCid,
+    IllegalCodec,
+    Serialization,
+    Forbidden,
+    BufferTooSmall,
+}
+
+impl ErrorNumber {
+    pub fn as_u32(&self) -> u32 {
+        self.as_unshimmed()
+            .map_either(|n| n as u32, |n| n as u32)
+            .into_inner()
+    }
+}
 
 impl fmt::Display for ErrorNumber {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0 {
-            ErrorNumberInner::MappedOr3(it) => it.fmt(f),
-            ErrorNumberInner::Unknown2(it) => it.fmt(f),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-enum ErrorNumberInner {
-    MappedOr3(N3),
-    Unknown2(N2),
-}
-
-impl From<N2> for NShim {
-    fn from(value: N2) -> Self {
-        use ErrorNumberInner::*;
-        Self(match value {
-            N2::IllegalArgument => MappedOr3(N3::IllegalArgument),
-            N2::IllegalOperation => MappedOr3(N3::IllegalOperation),
-            N2::LimitExceeded => MappedOr3(N3::LimitExceeded),
-            N2::AssertionFailed => MappedOr3(N3::AssertionFailed),
-            N2::InsufficientFunds => MappedOr3(N3::InsufficientFunds),
-            N2::NotFound => MappedOr3(N3::NotFound),
-            N2::InvalidHandle => MappedOr3(N3::InvalidHandle),
-            N2::IllegalCid => MappedOr3(N3::IllegalCid),
-            N2::IllegalCodec => MappedOr3(N3::IllegalCodec),
-            N2::Serialization => MappedOr3(N3::Serialization),
-            N2::Forbidden => MappedOr3(N3::Forbidden),
-            N2::BufferTooSmall => MappedOr3(N3::BufferTooSmall),
-            o => Unknown2(o),
-        })
-    }
-}
-
-impl From<N3> for NShim {
-    fn from(value: N3) -> Self {
-        Self(ErrorNumberInner::MappedOr3(value))
+        self.as_unshimmed().fmt(f)
     }
 }
 
