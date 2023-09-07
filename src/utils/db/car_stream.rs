d31d8c91badef8b73155d10e86f95997f9bf4730
elmattic <elmattic@users.noreply.github.com>
2023-09-07 16:17:52 +0200

    Move write/flush inside poll_flush and add debug comments

--- src/utils/db/car_stream.rs
+++ src/utils/db/car_stream.rs
@@ -175,15 +175,8 @@ impl Sink<(Cid, Vec<u8>)> for CarWriter {
     type Error = io::Error;
 
     fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        let this = self.project();
-        this.inner.poll_write(cx, &this.buffer).map_ok(|s| {
-            if s == 0 {
-                tracing::debug!("No bytes were written");
-            }
-            if s < this.buffer.len() {
-                tracing::debug!("Less bytes than in the input buffer were written");
-            }
-        })
+        println!("poll_ready");
+        Poll::Ready(Ok(()))
     }
     fn start_send(self: Pin<&mut Self>, item: (Cid, Vec<u8>)) -> Result<(), Self::Error> {
         let mut this = self.project();
@@ -192,17 +185,47 @@ impl Sink<(Cid, Vec<u8>)> for CarWriter {
         let mut var_bytes = [0; 10];
         let len = payload.len().encode_var(&mut var_bytes);
 
-        this.buffer.clear();
         this.buffer.extend_from_slice(&var_bytes[0..len]);
         this.buffer.extend_from_slice(&payload);
 
+        println!("start_send {}", this.buffer.len());
+
         Ok(())
     }
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project().inner.poll_flush(cx)
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        // self.project().inner.poll_flush(cx)
+        
+        let this = self.as_mut().project();
+        if this.buffer.is_empty() {
+            println!("buffer is empty");
+            println!("poll_flush");
+            let y = this.inner.poll_flush(cx);
+            println!("poll_flush {:?}", y);
+            y
+        } else {
+            println!("buffer is not empty");
+
+            println!("poll_write");
+            let x = this.inner.poll_write(cx, &this.buffer);
+            println!("poll_write {:?}", x);
+            if let Poll::Ready(_s) = x {
+                let mut this = self.as_mut().project();
+                this.buffer.clear();
+
+                println!("poll_flush");
+                let y = this.inner.poll_flush(cx);
+                println!("poll_flush {:?}", y);
+                y
+            } else {
+                x.map_ok(|_s| ())
+            }
+        }
     }
     fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project().inner.poll_shutdown(cx)
+        let this = self.project();
+        println!("poll_close, buffer len: {}", this.inner.buffer().len());
+        
+        this.inner.poll_shutdown(cx)
     }
 }
 
