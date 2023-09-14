5427084ba8bd80f0df5d6188a0a4c421e7bf154a
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-15 00:44:39 +0100

    refactor: clearer ActorBundleInfo things

--- src/networks/mod.rs
+++ src/networks/mod.rs
@@ -360,54 +360,35 @@ pub struct ActorBundleInfo {
     pub url: Url,
 }
 
-impl ActorBundleInfo {
-    fn unwrap_parse(manifest: &str, url: &str) -> Self {
-        Self {
-            manifest: manifest.parse().unwrap(),
-            url: url.parse().unwrap(),
-        }
+macro_rules! actor_bundle_info {
+    ($($cid:literal @ $version:literal for $network:literal),* $(,)?) => {
+        [
+            $(
+                ActorBundleInfo {
+                    manifest: $cid.parse().unwrap(),
+                    url: concat!(
+                            "https://github.com/filecoin-project/builtin-actors/releases/download/",
+                            $version,
+                            "/builtin-actors-",
+                            $network,
+                            ".car"
+                        ).parse().unwrap()
+                },
+            )*
+        ]
     }
 }
 
 pub static ACTOR_BUNDLES: Lazy<Box<[ActorBundleInfo]>> = Lazy::new(|| {
-    Box::new([
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzacedbedgynklc4dgpyxippkxmba2mgtw7ecntoneclsvvl4klqwuyyy",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v9.0.3/builtin-actors-calibrationnet.car"
-        ),
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzaced25ta3j6ygs34roprilbtb3f6mxifyfnm7z7ndquaruxzdq3y7lo",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v10.0.0-rc.1/builtin-actors-calibrationnet.car"
-        ),
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzacedhuowetjy2h4cxnijz2l64h4mzpk5m256oywp4evarpono3cjhco",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v11.0.0-rc2/builtin-actors-calibrationnet.car"
-        ),
-
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzacedozk3jh2j4nobqotkbofodq4chbrabioxbfrygpldgoxs3zwgggk",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v9.0.3/builtin-actors-devnet.car"
-        ),
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzacebzz376j5kizfck56366kdz5aut6ktqrvqbi3efa2d4l2o2m653ts",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v10.0.0/builtin-actors-devnet.car"
-        ),
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzaceay35go4xbjb45km6o46e5bib3bi46panhovcbedrynzwmm3drr4i",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v11.0.0/builtin-actors-devnet.car"
-        ),
-
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzaceb6j6666h36xnhksu3ww4kxb6e25niayfgkdnifaqi6m6ooc66i6i",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v9.0.3/builtin-actors-mainnet.car"
-        ),
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzacecsuyf7mmvrhkx2evng5gnz5canlnz2fdlzu2lvcgptiq2pzuovos",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v10.0.0/builtin-actors-mainnet.car"
-        ),
-        ActorBundleInfo::unwrap_parse(
-            "bafy2bzacecnhaiwcrpyjvzl4uv4q3jzoif26okl3m66q3cijp3dfwlcxwztwo",
-            "https://github.com/filecoin-project/builtin-actors/releases/download/v11.0.0/builtin-actors-mainnet.car"
-        ),
+    Box::new(actor_bundle_info![
+        "bafy2bzacedbedgynklc4dgpyxippkxmba2mgtw7ecntoneclsvvl4klqwuyyy" @ "v9.0.3" for "calibrationnet",
+        "bafy2bzaced25ta3j6ygs34roprilbtb3f6mxifyfnm7z7ndquaruxzdq3y7lo" @ "v10.0.0-rc.1" for "calibrationnet",
+        "bafy2bzacedhuowetjy2h4cxnijz2l64h4mzpk5m256oywp4evarpono3cjhco" @ "v11.0.0-rc2" for "calibrationnet",
+        "bafy2bzacedozk3jh2j4nobqotkbofodq4chbrabioxbfrygpldgoxs3zwgggk" @ "v9.0.3" for "devnet",
+        "bafy2bzacebzz376j5kizfck56366kdz5aut6ktqrvqbi3efa2d4l2o2m653ts" @ "v10.0.0" for "devnet",
+        "bafy2bzaceay35go4xbjb45km6o46e5bib3bi46panhovcbedrynzwmm3drr4i" @ "v11.0.0" for "devnet",
+        "bafy2bzaceb6j6666h36xnhksu3ww4kxb6e25niayfgkdnifaqi6m6ooc66i6i" @ "v9.0.3" for "mainnet",
+        "bafy2bzacecsuyf7mmvrhkx2evng5gnz5canlnz2fdlzu2lvcgptiq2pzuovos" @ "v10.0.0" for "mainnet",
+        "bafy2bzacecnhaiwcrpyjvzl4uv4q3jzoif26okl3m66q3cijp3dfwlcxwztwo" @ "v11.0.0" for "mainnet",
     ])
 });
