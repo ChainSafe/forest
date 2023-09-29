a9e5e912f658d7efe085b8dc98428b6e310a0fe5
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-09-29 13:46:14 +0200

    baby, don't hurt me, don't hurt me, no more
    Co-authored-by: Hubert <hubert@chainsafe.io>

--- scripts/tests/calibnet_db_migration.sh
+++ scripts/tests/calibnet_db_migration.sh
@@ -46,7 +46,7 @@ echo "data_dir = \"${TMP_DIR}/data_dir\"" >> "${CONFIG_FILE}"
 echo 'encrypt_keystore = false' >> "${CONFIG_FILE}"
 
 # Run the current Forest with the old database. This should trigger a migration (or several ones).
-forest --chain calibnet --log-dir "$LOG_DIRECTORY" --halt-after-import --save-token ./admin_token --track-peak-rss --config "${CONFIG_FILE}"
+forest --chain calibnet --log-dir "$LOG_DIRECTORY" --halt-after-import --track-peak-rss --config "${CONFIG_FILE}"
 
 # Sync to HEAD. This might reveal migrations errors not caught above.
 forest --chain calibnet --log-dir "$LOG_DIRECTORY" --detach --save-token ./admin_token --track-peak-rss --config "${CONFIG_FILE}"
