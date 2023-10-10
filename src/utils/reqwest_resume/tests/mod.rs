51a07ff90863d527f9015a7c22d48e2cac8f440c
elmattic <elmattic@users.noreply.github.com>
2023-10-10 13:45:35 +0200

    Remove comment

--- src/utils/reqwest_resume/tests/mod.rs
+++ src/utils/reqwest_resume/tests/mod.rs
@@ -65,12 +65,7 @@ async fn create_flaky_server() -> (
     // For every connection, we must make a `Service` to handle all
     // incoming HTTP requests on said connection.
 
-    let make_svc = make_service_fn(|_conn| {
-        // This is the `Service` that will handle the connection.
-        // `service_fn` is a helper to convert a function that
-        // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
-    });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello)) });
 
     // A port number of 0 will request that the OS assigns a port.
     let addr = ([127, 0, 0, 1], 0).into();
