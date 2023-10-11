d4674183b33eea30293c9f878fb703a95f2f2dff
elmattic <elmattic@users.noreply.github.com>
2023-10-11 15:19:04 +0200

    Send only subsets of the buffer

--- src/utils/reqwest_resume/tests.rs
+++ src/utils/reqwest_resume/tests.rs
@@ -4,34 +4,41 @@ use crate::utils::reqwest_resume::get;
 use bytes::Bytes;
 use const_random::const_random;
 use futures::stream::StreamExt;
-use http_range_header::parse_range_header;
+use http_range_header::{parse_range_header, RangeUnsatisfiableError};
 use hyper::header::{self, HeaderValue};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
 use std::time::Duration;
 use tokio::time::sleep;
 
-const ABORT_COUNT: usize = 2;
 const CHUNK_LEN: usize = 2048;
 // `RANDOM_BYTES` size is arbitrarily chosen. We could use something smaller or bigger here.
-// The only constraint is that `(RANDOM_BYTES.len() - CHUNK_LEN * ABORT_COUNT) > 0`.
+// The only constraint is that `CHUNK_LEN <= RANDOM_BYTES.len()`.
 const RANDOM_BYTES: [u8; 8192] = const_random!([u8; 8192]);
 
-fn extract_range_start(value: &HeaderValue, total_len: usize) -> u64 {
+fn extract_range_start(value: &HeaderValue, total_len: usize) -> Option<usize> {
     let s = std::str::from_utf8(value.as_bytes()).unwrap();
     let parse_ranges = parse_range_header(s).unwrap();
-    let range = parse_ranges.validate(total_len as u64).unwrap();
-    *range[0].start()
+    match parse_ranges.validate(total_len as u64) {
+        Ok(range) => Some(*range[0].start() as usize),
+        Err(err) => {
+            assert_eq!(err, RangeUnsatisfiableError::RangeReversed);
+            None
+        }
+    }
 }
 
-async fn handle_request(req: Request<Body>, count: usize) -> Result<Response<Body>, Infallible> {
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let (mut sender, body) = Body::channel();
 
-    let body = if count < ABORT_COUNT {
-        let offset = count * CHUNK_LEN;
+    let start = if let Some(range) = req.headers().get(header::RANGE) {
+        extract_range_start(range, RANDOM_BYTES.len())
+    } else {
+        Some(0)
+    };
+
+    if let Some(offset) = start {
         tokio::task::spawn(async move {
             sender
                 .send_data(Bytes::copy_from_slice(
@@ -44,20 +51,7 @@ async fn handle_request(req: Request<Body>, count: usize) -> Result<Response<Bod
             // resume functionality
             sender.abort();
         });
-        body
-    } else {
-        let range = req.headers().get(header::RANGE);
-        let offset = extract_range_start(range.unwrap(), RANDOM_BYTES.len());
-
-        // Send the rest of the buffer
-        tokio::task::spawn(async move {
-            sender
-                .send_data(Bytes::copy_from_slice(&RANDOM_BYTES[offset as usize..]))
-                .await
-                .unwrap();
-        });
-        body
-    };
+    }
 
     let mut response: Response<_> = Response::new(body);
     response
@@ -67,12 +61,8 @@ async fn handle_request(req: Request<Body>, count: usize) -> Result<Response<Bod
 }
 
 async fn create_flaky_server() -> std::net::SocketAddr {
-    let counter = Arc::new(AtomicUsize::new(0));
-
-    let make_svc = make_service_fn(move |_conn| {
-        let count = counter.fetch_add(1, Ordering::AcqRel);
-        async move { Ok::<_, Infallible>(service_fn(move |req| handle_request(req, count))) }
-    });
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
 
     // A port number of 0 will request that the OS assigns a port.
     let addr = ([127, 0, 0, 1], 0).into();
