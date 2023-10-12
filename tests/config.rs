048d610732461997ab6cfa0c2715631c6b6e4bea
lemmih <david.himmelstrup@chainsafe.io>
2023-10-12 15:45:52 +0200

    remove outdated `config dump` tests

--- tests/config.rs
+++ tests/config.rs
@@ -11,8 +11,6 @@ use tempfile::TempDir;
 fn test_config_subcommand_produces_valid_toml_configuration_dump() {
     let cmd = Command::cargo_bin("forest-cli")
         .unwrap()
-        .arg("--rpc")
-        .arg("true")
         .arg("--token")
         .arg("Azazello")
         .arg("config")
@@ -22,105 +20,6 @@ fn test_config_subcommand_produces_valid_toml_configuration_dump() {
 
     let output = &cmd.get_output().stdout;
     toml::from_str::<Config>(std::str::from_utf8(output).unwrap()).expect("Invalid configuration!");
-}
-
-#[test]
-fn test_overrides_are_reflected_in_configuration_dump() {
-    let mut rng = rand::thread_rng();
-    let randomized_metrics_host = format!("127.0.0.1:{}", rng.gen::<u16>());
-
-    let cmd = Command::cargo_bin("forest-cli")
-        .unwrap()
-        .arg("--rpc")
-        .arg("true")
-        .arg("--token")
-        .arg("Azazello")
-        .arg("--metrics-address")
-        .arg(&randomized_metrics_host)
-        .arg("config")
-        .arg("dump")
-        .assert()
-        .success();
-
-    let output = &cmd.get_output().stdout;
-    let config = toml::from_str::<Config>(std::str::from_utf8(output).unwrap())
-        .expect("Invalid configuration!");
-
-    assert_eq!(
-        config.client.metrics_address,
-        SocketAddr::from_str(&randomized_metrics_host).unwrap()
-    );
-}
-
-#[test]
-fn test_reading_configuration_from_file() {
-    let mut rng = rand::thread_rng();
-
-    let expected_config = Config {
-        client: Client {
-            rpc_token: Some("Azazello".into()),
-            genesis_file: Some("cthulhu".into()),
-            encrypt_keystore: false,
-            metrics_address: SocketAddr::from_str(&format!("127.0.0.1:{}", rng.gen::<u16>()))
-                .unwrap(),
-            ..Client::default()
-        },
-        ..Config::default()
-    };
-
-    let mut config_file = tempfile::Builder::new().tempfile().unwrap();
-    config_file
-        .write_all(toml::to_string(&expected_config).unwrap().as_bytes())
-        .expect("Failed writing configuration!");
-
-    let cmd = Command::cargo_bin("forest-cli")
-        .unwrap()
-        .arg("--rpc")
-        .arg("true")
-        .arg("--token")
-        .arg("Azazello")
-        .arg("--config")
-        .arg(config_file.path())
-        .arg("config")
-        .arg("dump")
-        .assert()
-        .success();
-
-    let output = &cmd.get_output().stdout;
-    let actual_config = toml::from_str::<Config>(std::str::from_utf8(output).unwrap())
-        .expect("Invalid configuration!");
-
-    assert_eq!(expected_config, actual_config);
-}
-
-#[test]
-fn test_config_env_var() {
-    let expected_config = Config {
-        client: Client {
-            rpc_token: Some("some_rpc_token".into()),
-            data_dir: PathBuf::from("some_path_buf"),
-            ..Client::default()
-        },
-        ..Config::default()
-    };
-
-    let mut config_file = tempfile::Builder::new().tempfile().unwrap();
-    config_file
-        .write_all(toml::to_string(&expected_config).unwrap().as_bytes())
-        .unwrap();
-
-    let cmd = Command::cargo_bin("forest-cli")
-        .unwrap()
-        .env("FOREST_CONFIG_PATH", config_file.path())
-        .arg("config")
-        .arg("dump")
-        .assert()
-        .success();
-
-    let output = &cmd.get_output().stdout;
-    let actual_config = toml::from_str::<Config>(std::str::from_utf8(output).unwrap()).unwrap();
-
-    assert_eq!(expected_config, actual_config);
 }
 
 #[test]
