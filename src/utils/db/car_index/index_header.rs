67d6ba93e0669bf47ab55a8cc78fe6a33df33488
Aatif Syed <38045910+aatifsyed@users.noreply.github.com>
2023-10-12 11:51:07 +0100

    refactor: use zerocopy for `IndexHeader` instead of manually slicing (#3580)
    Co-authored-by: Hubert <hubert@chainsafe.io>

--- src/utils/db/car_index/index_header.rs
+++ src/utils/db/car_index/index_header.rs
@@ -1,59 +1,37 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 use positioned_io::ReadAt;
-use std::io::Result;
+use std::{io::Result, mem::size_of};
+use zerocopy::{little_endian::U64 as U64LE, AsBytes, FromBytes, FromZeroes};
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-#[cfg_attr(test, derive(derive_quickcheck_arbitrary::Arbitrary))]
+/// Layout of this struct is the same in-memory as on the wire, so it can be
+/// serialized using [`zerocopy::FromBytes::read_from`] and deserialized using
+/// [`zerocopy::AsBytes::as_bytes`].
+#[derive(AsBytes, Clone, Copy, Debug, Eq, FromBytes, FromZeroes, Hash, PartialEq)]
+#[repr(C)]
 pub struct IndexHeader {
     // Version number
-    pub magic_number: u64,
+    pub magic_number: U64LE,
     // Worst-case distance between an entry and its bucket.
-    pub longest_distance: u64,
+    pub longest_distance: U64LE,
     // Number of hash collisions. Reserved for future use.
-    pub collisions: u64,
+    pub collisions: U64LE,
     // Number of buckets. Note that the index includes padding after the last
     // bucket.
-    pub buckets: u64,
+    pub buckets: U64LE,
 }
 
+// There are no padding bytes
+static_assertions::const_assert_eq!(size_of::<IndexHeader>(), size_of::<u64>() * 4);
+
 impl IndexHeader {
-    pub const SIZE: usize = 32;
+    pub const SIZE: usize = size_of::<Self>();
     // 0xdeadbeef + 0 used a different hash algorithm
     pub const MAGIC_NUMBER: u64 = 0xdeadbeef + 1;
 
     pub fn read(reader: impl ReadAt, offset: u64) -> Result<IndexHeader> {
         let mut buffer = [0; Self::SIZE];
         reader.read_exact_at(offset, &mut buffer)?;
-        Ok(IndexHeader::from_le_bytes(buffer))
-    }
-
-    pub fn to_le_bytes(self) -> [u8; IndexHeader::SIZE] {
-        let mut bytes = [0; IndexHeader::SIZE];
-        bytes[0..8].copy_from_slice(&self.magic_number.to_le_bytes());
-        bytes[8..16].copy_from_slice(&self.longest_distance.to_le_bytes());
-        bytes[16..24].copy_from_slice(&self.collisions.to_le_bytes());
-        bytes[24..32].copy_from_slice(&self.buckets.to_le_bytes());
-        bytes
-    }
-
-    pub fn from_le_bytes(bytes: [u8; IndexHeader::SIZE]) -> Self {
-        IndexHeader {
-            magic_number: u64::from_le_bytes(bytes[0..8].try_into().expect("infallible")),
-            longest_distance: u64::from_le_bytes(bytes[8..16].try_into().expect("infallible")),
-            collisions: u64::from_le_bytes(bytes[16..24].try_into().expect("infallible")),
-            buckets: u64::from_le_bytes(bytes[24..32].try_into().expect("infallible")),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use quickcheck_macros::quickcheck;
-
-    #[quickcheck]
-    fn index_header_roundtrip(header: IndexHeader) {
-        assert_eq!(header, IndexHeader::from_le_bytes(header.to_le_bytes()))
+        Ok(IndexHeader::read_from(buffer.as_slice()).expect("`buffer` is the correct size"))
     }
 }
