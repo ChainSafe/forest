0c6d70ece1db753ded80f120c866e1b2057f2374
lemmih <david.himmelstrup@chainsafe.io>
2023-10-16 13:28:36 +0200

    Verify that we can read configuration files from parameters and env vars

--- tests/config.rs
+++ tests/config.rs
@@ -63,3 +63,64 @@ fn test_download_location_of_proof_parameter_files_default() {
         .stdout(tmp_param_dir.to_string_lossy().into_owned() + "\n")
         .success();
 }
+
+// Verify that a configuration path can be set with `--config` flag. We
+// assume 'data_dir' will be created iff the configuration is correctly parsed.
+#[test]
+fn test_config_parameter() {
+    let tmp_dir = TempDir::new().unwrap().into_path();
+    let config = Config {
+        client: Client {
+            data_dir: tmp_dir.clone(),
+            encrypt_keystore: false,
+            ..Client::default()
+        },
+        ..Config::default()
+    };
+
+    std::fs::remove_dir(&tmp_dir).unwrap();
+
+    let mut config_file = tempfile::Builder::new().tempfile().unwrap();
+    config_file
+        .write_all(toml::to_string(&config).unwrap().as_bytes())
+        .expect("Failed writing configuration!");
+
+    Command::cargo_bin("forest")
+        .unwrap()
+        .arg("--config")
+        .arg(config_file.path())
+        .arg("--exit-after-init")
+        .assert()
+        .success();
+    assert!(tmp_dir.is_dir());
+}
+
+// Verify that a configuration path can be set with FOREST_CONFIG_PATH. We
+// assume 'data_dir' will be created iff the configuration is correctly parsed.
+#[test]
+fn test_config_env_var() {
+    let tmp_dir = TempDir::new().unwrap().into_path();
+    let config = Config {
+        client: Client {
+            data_dir: tmp_dir.clone(),
+            encrypt_keystore: false,
+            ..Client::default()
+        },
+        ..Config::default()
+    };
+
+    std::fs::remove_dir(&tmp_dir).unwrap();
+
+    let mut config_file = tempfile::Builder::new().tempfile().unwrap();
+    config_file
+        .write_all(toml::to_string(&config).unwrap().as_bytes())
+        .expect("Failed writing configuration!");
+
+    Command::cargo_bin("forest")
+        .unwrap()
+        .env("FOREST_CONFIG_PATH", config_file.path())
+        .arg("--exit-after-init")
+        .assert()
+        .success();
+    assert!(tmp_dir.is_dir());
+}
