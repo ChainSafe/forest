77470cfa93cebc728ff4e9aaa524abfbb2a956fd
lemmih <david.himmelstrup@chainsafe.io>
2023-10-19 10:41:49 +0200

    update code coverage script with new commands

--- scripts/gen_coverage_report.sh
+++ scripts/gen_coverage_report.sh
@@ -18,6 +18,8 @@ TMP_DIR=$(mktemp --directory)
 TOKEN_PATH="$TMP_DIR/forest_admin_token"
 
 function cleanup {
+  # Kill any lingering forest node
+  timeout 15 killall --wait --signal SIGINT forest || true
   # echo Removing temporary directory $TMP_DIR
   rm -rf "$TMP_DIR"
 }
@@ -28,37 +30,42 @@ function cov {
     cargo llvm-cov --no-report run --bin="$1" -- "${@:2}"
 }
 
-cargo llvm-cov --workspace clean
-cargo llvm-cov --workspace --no-report
-cov forest-cli --chain calibnet db clean --force
-cov forest-tool snapshot fetch --chain calibnet --vendor filops -s "$TMP_DIR"
-SNAPSHOT_PATH=$(find "$TMP_DIR" -name \*.zst | head -n 1)
+MODE=${1:-""}
+
+if [ "$MODE" != "quick" ]
+then
+  cargo llvm-cov --workspace --no-report
+fi
+cov forest-tool db destroy --chain calibnet --force
+cov forest-tool snapshot fetch --chain calibnet --vendor filops --directory "$TMP_DIR"
+SNAPSHOT_PATH=$(find "$TMP_DIR" -name \*.car.zst | head -n 1)
 cov forest --chain calibnet --encrypt-keystore false --import-snapshot "$SNAPSHOT_PATH" --halt-after-import --height=-200 --track-peak-rss
-cov forest-cli --chain calibnet db clean --force
-cov forest-tool snapshot fetch --chain calibnet -s "$TMP_DIR"
-SNAPSHOT_PATH=$(find "$TMP_DIR" -name \*.car | head -n 1)
+rm "$SNAPSHOT_PATH"
+cov forest-tool db destroy --chain calibnet --force
+cov forest-tool snapshot fetch --chain calibnet --directory "$TMP_DIR"
+SNAPSHOT_PATH=$(find "$TMP_DIR" -name \*.forest.car.zst | head -n 1)
 cov forest --chain calibnet --encrypt-keystore false --import-snapshot "$SNAPSHOT_PATH" --height=-200 --detach --track-peak-rss --save-token "$TOKEN_PATH"
 cov forest-cli sync wait
 cov forest-cli sync status
-cov forest-cli --chain calibnet db gc
 cov forest-tool db stats --chain calibnet
-cov forest-cli snapshot export
-cov forest-cli snapshot export
+# cov forest-cli snapshot export
 cov forest-cli attach --exec 'showPeers()'
 cov forest-cli net listen
 cov forest-cli net peers
 cov forest-cli mpool pending
-cov forest-cli mpool stats
 cov forest-cli net info
 
 # Load the admin token
 TOKEN=$(cat "$TOKEN_PATH")
 
+# Make sure there's at least one account in the wallet
+cov forest-wallet --token "$TOKEN" new
+
 # Get default address
 DEFAULT_ADDR=$(cov forest-wallet --token "$TOKEN" default)
 
 # Check that the address exists
-cov forest-wallet --token "$TOKEN" has "$DEFAULT_ADDR" | grep "$DEFAULT_ADDR"
+cov forest-wallet --token "$TOKEN" has "$DEFAULT_ADDR"
 
 # Check that the address is listed
 cov forest-wallet --token "$TOKEN" list | grep "$DEFAULT_ADDR"
@@ -77,13 +84,13 @@ cov forest-wallet --token "$TOKEN" verify -a "$NEW_ADDR" -m deadbeef -s "$SIGNAT
 # Check balance
 cov forest-wallet --token "$TOKEN" balance "$NEW_ADDR" | grep 0
 
-# Send funds
-cov forest-cli --token "$TOKEN" send --from "$DEFAULT_ADDR" "$NEW_ADDR" 10attoFIL
-
 # Create a read-only token
 READ_TOKEN=$(cov forest-cli --token "$TOKEN" auth create-token --perm read)
 # Make sure that viewing the wallet fails with the read-only token
-cov forest-wallet --token "$READ_TOKEN" list && { echo "must fail"; return 1; }
+if cov forest-wallet --token "$READ_TOKEN" list; then
+  echo "expected failure"
+  exit 1
+fi
 # Verifying a message should still work with the read-only token
 cov forest-wallet --token "$READ_TOKEN" verify -a "$NEW_ADDR" -m deadbeef -s "$SIGNATURE" | grep true
 
