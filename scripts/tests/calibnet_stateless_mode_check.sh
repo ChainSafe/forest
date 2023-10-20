7faa669ab14580a383d7b89370faaaead4ca4162
hanabi1224 <harlowmoo@gmail.com>
2023-10-20 21:36:04 +0800

    test script

--- scripts/tests/calibnet_stateless_mode_check.sh
+++ scripts/tests/calibnet_stateless_mode_check.sh
@@ -10,3 +10,26 @@ forest_init_stateless
 echo "Verifying the heaviest tipset to be the genesis"
 MSG=$($FOREST_CLI_PATH chain head)
 assert_eq "$MSG" $'[\n  "bafy2bzacecyaggy24wol5ruvs6qm73gjibs2l2iyhcqmvi7r7a4ph7zx3yqd4"\n]'
+
+wait_util_p2p_is_ready
+ADDRESS=$($FOREST_CLI_PATH net listen | tail -n 1)
+echo "Stateless node address: $ADDRESS"
+PEER_ID=$(echo $ADDRESS | cut -d "/" -f7 -z)
+echo "Stateless node peer id: $PEER_ID"
+
+# Run a normal forest node that only connects to the stateless node
+CONFIG_PATH="./forest_config.toml"
+echo "${CONFIG_PATH}"
+echo "Running forest in stateless and detached mode"
+echo "[network]" > $CONFIG_PATH
+echo "listening_multiaddrs = [\"/ip4/127.0.0.1/tcp/0\"]" >> $CONFIG_PATH
+echo "bootstrap_peers = [\"$ADDRESS\"]" >> $CONFIG_PATH
+
+NODE_LOG_DIRECTORY=$(mktemp --directory)
+RUST_LOG="info,forest_filecoin::chain_sync::network_context=debug" $FOREST_PATH --chain calibnet --encrypt-keystore false --config "$CONFIG_PATH" --no-metrics --rpc false --log-dir "$NODE_LOG_DIRECTORY" &
+PID=$!
+# Verify that the stateless node can respond to chain exchange requests
+until grep -r -m 1 "non-empty ChainExchange response from $PEER_ID" "$NODE_LOG_DIRECTORY"; do
+    sleep 1s;
+done
+kill -9 $PID
