4808ac17481ad37059bc3e0bfa1c52f58bca332e
Hubert Bugaj <hubert@chainsafe.io>
2023-10-23 15:27:15 +0200

    runtime download for actor bundles with alt url

--- src/daemon/bundle.rs
+++ src/daemon/bundle.rs
@@ -1,33 +1,84 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use crate::utils::db::{car_stream::CarHeader, car_util::load_car};
+use crate::{
+    networks::{generate_actor_bundle, ActorBundleInfo},
+    utils::db::car_util::load_car,
+};
 use anyhow::Context as _;
 use fvm_ipld_blockstore::Blockstore;
+use tracing::info;
 
-pub async fn load_actor_bundles(db: &impl Blockstore) -> anyhow::Result<CarHeader> {
-    const ERROR_MESSAGE: &str = "Actor bundles assets are not properly downloaded, make sure git-lfs is installed and run `git lfs pull` again. See <https://github.com/git-lfs/git-lfs/blob/main/INSTALLING.md>";
+/// Tries to load the actor bundle from the specified location to the blockstore. If the bundle is
+/// not present, it will be generated.
+pub async fn load_actor_bundles(db: &impl Blockstore) -> anyhow::Result<()> {
+    if !are_bundles_present(db, &crate::networks::ACTOR_BUNDLES) {
+        info!("Generating actor bundle");
+        let bundle = tempfile::Builder::new()
+            .prefix("forest-actor-bundle")
+            .suffix(".car.zst")
+            .tempfile()?;
+        generate_actor_bundle(bundle.path()).await?;
 
-    const ACTOR_BUNDLES_CAR_ZST: &[u8] = include_bytes!("../../assets/actor_bundles.car.zst");
+        let bundle_reader = tokio::io::BufReader::new(tokio::fs::File::open(bundle.path()).await?);
 
-    load_car(db, ACTOR_BUNDLES_CAR_ZST)
-        .await
-        .context(ERROR_MESSAGE)
+        load_car(db, bundle_reader)
+            .await
+            .context("failed to load actor bundle")?;
+    } else {
+        info!("Actor bundles already present in the blockstore. Skipping generation");
+    }
+
+    Ok(())
+}
+
+/// Checks if all the actor bundles are present in the blockstore.
+fn are_bundles_present(db: &impl Blockstore, required_bundles: &[ActorBundleInfo]) -> bool {
+    required_bundles
+        .iter()
+        .map(|bundle| bundle.manifest)
+        .all(|manifest| db.has(&manifest).unwrap_or(false))
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::networks::ACTOR_BUNDLES;
-    use ahash::HashSet;
+    use crate::utils::cid::CidCborExt;
     use cid::Cid;
-    use pretty_assertions::assert_eq;
+    use url::Url;
 
-    #[tokio::test]
-    async fn test_load_actor_bundles() {
-        let db = fvm_ipld_blockstore::MemoryBlockstore::new();
-        let roots = HashSet::from_iter(load_actor_bundles(&db).await.unwrap().roots);
-        let roots_expected: HashSet<Cid> = ACTOR_BUNDLES.iter().map(|b| b.manifest).collect();
-        assert_eq!(roots, roots_expected);
+    use super::*;
+    #[test]
+    fn are_bundles_present_empty() {
+        let db = fvm_ipld_blockstore::MemoryBlockstore::default();
+        let required_bundles = vec![];
+
+        assert!(are_bundles_present(&db, &required_bundles));
+    }
+
+    #[test]
+    fn are_bundles_present_non_empty() {
+        let db = fvm_ipld_blockstore::MemoryBlockstore::default();
+        let required_bundles = vec![
+            ActorBundleInfo {
+                url: Url::parse("https://chainsafe.io").unwrap(),
+                alt_url: Url::parse("https://chainsafe.io").unwrap(),
+                manifest: Cid::from_cbor_blake2b256(&1).unwrap(),
+            },
+            ActorBundleInfo {
+                url: Url::parse("https://filecoin.io").unwrap(),
+                alt_url: Url::parse("https://filecoin.io").unwrap(),
+                manifest: Cid::from_cbor_blake2b256(&2).unwrap(),
+            },
+        ];
+
+        db.put_keyed(&Cid::from_cbor_blake2b256(&1).unwrap(), &[])
+            .unwrap();
+
+        assert!(!are_bundles_present(&db, &required_bundles));
+
+        db.put_keyed(&Cid::from_cbor_blake2b256(&2).unwrap(), &[])
+            .unwrap();
+
+        assert!(are_bundles_present(&db, &required_bundles));
     }
 }
---
+++ src/networks/actors_bundle.rs
@@ -0,0 +1,136 @@
+// Copyright 2019-2023 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use std::io::{self, Cursor};
+use std::path::Path;
+
+use anyhow::ensure;
+use async_compression::tokio::write::ZstdEncoder;
+use cid::Cid;
+use futures::{stream, StreamExt, TryStreamExt};
+use itertools::Itertools;
+use once_cell::sync::Lazy;
+use reqwest::{Response, Url};
+use tokio::fs::File;
+use tracing::{info, warn};
+
+use crate::utils::db::car_stream::{CarStream, CarWriter};
+use crate::utils::net::global_http_client;
+
+#[derive(Debug)]
+pub struct ActorBundleInfo {
+    pub manifest: Cid,
+    pub url: Url,
+    /// Alternative URL to download the bundle from if the primary URL fails.
+    /// Note that we host the bundles and so we need to update the bucket
+    /// ourselves when a new bundle is released.
+    pub alt_url: Url,
+}
+
+macro_rules! actor_bundle_info {
+    ($($cid:literal @ $version:literal for $network:literal),* $(,)?) => {
+        [
+            $(
+                ActorBundleInfo {
+                    manifest: $cid.parse().unwrap(),
+                    url: concat!(
+                            "https://github.com/filecoin-project/builtin-actors/releases/download/",
+                            $version,
+                            "/builtin-actors-",
+                            $network,
+                            ".car"
+                        ).parse().unwrap(),
+                    alt_url: concat!(
+                          "https://forest-snapshots.fra1.cdn.digitaloceanspaces.com/actors/",
+                          $version,
+                            "/builtin-actors-",
+                            $network,
+                            ".car"
+                        ).parse().unwrap()
+                },
+            )*
+        ]
+    }
+}
+
+pub static ACTOR_BUNDLES: Lazy<Box<[ActorBundleInfo]>> = Lazy::new(|| {
+    Box::new(actor_bundle_info![
+        "bafy2bzacedbedgynklc4dgpyxippkxmba2mgtw7ecntoneclsvvl4klqwuyyy" @ "v9.0.3" for "calibrationnet",
+        "bafy2bzaced25ta3j6ygs34roprilbtb3f6mxifyfnm7z7ndquaruxzdq3y7lo" @ "v10.0.0-rc.1" for "calibrationnet",
+        "bafy2bzacedhuowetjy2h4cxnijz2l64h4mzpk5m256oywp4evarpono3cjhco" @ "v11.0.0-rc2" for "calibrationnet",
+        "bafy2bzacedrunxfqta5skb7q7x32lnp4efz2oq7fn226ffm7fu5iqs62jkmvs" @ "v12.0.0-rc.1" for "calibrationnet",
+        "bafy2bzacedozk3jh2j4nobqotkbofodq4chbrabioxbfrygpldgoxs3zwgggk" @ "v9.0.3" for "devnet",
+        "bafy2bzacebzz376j5kizfck56366kdz5aut6ktqrvqbi3efa2d4l2o2m653ts" @ "v10.0.0" for "devnet",
+        "bafy2bzaceay35go4xbjb45km6o46e5bib3bi46panhovcbedrynzwmm3drr4i" @ "v11.0.0" for "devnet",
+        "bafy2bzacebk6yiirh4ennphzyka7b6g6jzn3lt4lr5ht7rjwulnrcthjihapo" @ "v12.0.0-rc.1" for "devnet",
+        "bafy2bzaceb6j6666h36xnhksu3ww4kxb6e25niayfgkdnifaqi6m6ooc66i6i" @ "v9.0.3" for "mainnet",
+        "bafy2bzacecsuyf7mmvrhkx2evng5gnz5canlnz2fdlzu2lvcgptiq2pzuovos" @ "v10.0.0" for "mainnet",
+        "bafy2bzacecnhaiwcrpyjvzl4uv4q3jzoif26okl3m66q3cijp3dfwlcxwztwo" @ "v11.0.0" for "mainnet",
+    ])
+});
+
+pub async fn try_download_bundle(url: &Url) -> anyhow::Result<Response> {
+    info!(%url, "downloading bundle");
+    Ok(global_http_client()
+        .get(url.clone())
+        .send()
+        .await?
+        .error_for_status()?)
+}
+
+pub async fn generate_actor_bundle(output: &Path) -> anyhow::Result<()> {
+    let (mut roots, blocks) = stream::iter(ACTOR_BUNDLES.iter())
+        .then(
+            |ActorBundleInfo {
+                 manifest: root,
+                 url,
+                 alt_url,
+             }| async move {
+                let response = if let Ok(response) = try_download_bundle(url).await {
+                    response
+                } else {
+                    warn!("failed to download bundle from primary URL, trying alternative URL");
+                    try_download_bundle(alt_url).await?
+                };
+                let bytes = response.bytes().await?;
+                let car = CarStream::new(Cursor::new(bytes)).await?;
+                ensure!(car.header.version == 1);
+                ensure!(car.header.roots.len() == 1);
+                ensure!(&car.header.roots[0] == root);
+                anyhow::Ok((*root, car.try_collect::<Vec<_>>().await?))
+            },
+        )
+        .try_collect::<Vec<_>>()
+        .await?
+        .into_iter()
+        .unzip::<_, _, Vec<_>, Vec<_>>();
+
+    ensure!(roots.iter().all_unique());
+
+    roots.sort(); // deterministic
+
+    let mut blocks = blocks.into_iter().flatten().collect::<Vec<_>>();
+    blocks.sort();
+    blocks.dedup();
+
+    for block in blocks.iter() {
+        ensure!(
+            block.valid(),
+            "sources contain an invalid block, cid {}",
+            block.cid
+        )
+    }
+
+    stream::iter(blocks)
+        .map(io::Result::Ok)
+        .forward(CarWriter::new_carv1(
+            roots.into_iter().collect(),
+            ZstdEncoder::with_quality(
+                File::create(&output).await?,
+                async_compression::Level::Precise(17),
+            ),
+        )?)
+        .await?;
+
+    Ok(())
+}
