f10c8d0cc51349ce84a7f92cf2094cdc305db79c
Roman Useinov <roman.useinov@gmail.com>
2023-10-23 10:41:11 +0200

    Update src/db/gc/mod.rs
    Co-authored-by: David Himmelstrup <david.himmelstrup@chainsafe.io>

--- src/db/gc/mod.rs
+++ src/db/gc/mod.rs
@@ -35,12 +35,10 @@
 //! after the run.
 //!
 //! ## Disk usage
-//! There's no additional disk space required to run this algorithm. However, removing the
-//! unreachable blocks from the database takes at least `chain finality`. The GC speed depends on
-//! the reachable graph size. Disk usage is also affected by the GC run interval. Additionally,
-//! since a truncated 4-byte hash is used - there's a slight possibility of a collision, which might
-//! result in an unreachable block being retained in the database. Still, the impact on the total
-//! disk size is negligible.
+//! The expected disk usage is slightly greater than the size of live data for three reasons:
+//! 1. Unreachable data is not removed until it is at least 7.5 hours old (see `chain finality`).
+//! 2. The garbage collector is conservative and is expected to leave a small (<1%) amount of unreachable data behind.
+//! 3. The blockstore backend may be fragmented and not relinquish the disk space back to the OS.
 //!
 //! ## Memory usage
 //! During the `mark` and up to the `sweep` stage, the algorithm requires `4 bytes` of memory for
