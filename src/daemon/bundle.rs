7ec098909bbd966d01c6abec3ecafb46ffd5ae47
Hubert Bugaj <hubert@chainsafe.io>
2023-10-24 15:09:57 +0200

    parallelize bundle download

--- src/daemon/bundle.rs
+++ src/daemon/bundle.rs
@@ -6,7 +6,7 @@ use crate::{
     utils::{db::car_util::load_car, net::http_get},
 };
 use anyhow::ensure;
-use futures::{stream, StreamExt, TryStreamExt};
+use futures::{stream::FuturesUnordered, TryStreamExt};
 use fvm_ipld_blockstore::Blockstore;
 use std::io::Cursor;
 use tracing::warn;
@@ -14,29 +14,29 @@ use tracing::warn;
 /// Tries to load the missing actor bundles to the blockstore. If the bundle is
 /// not present, it will be downloaded.
 pub async fn load_actor_bundles(db: &impl Blockstore) -> anyhow::Result<()> {
-    stream::iter(
+    FuturesUnordered::from_iter(
         ACTOR_BUNDLES
             .iter()
-            .filter(|bundle| !db.has(&bundle.manifest).unwrap_or(false)),
-    )
-    .then(
-        |ActorBundleInfo {
-             manifest: root,
-             url,
-             alt_url,
-         }| async move {
-            let response = if let Ok(response) = http_get(url).await {
-                response
-            } else {
-                warn!("failed to download bundle from primary URL, trying alternative URL");
-                http_get(alt_url).await?
-            };
-            let bytes = response.bytes().await?;
-            let header = load_car(db, Cursor::new(bytes)).await?;
-            ensure!(header.roots.len() == 1);
-            ensure!(&header.roots[0] == root);
-            Ok(())
-        },
+            .filter(|bundle| !db.has(&bundle.manifest).unwrap_or(false))
+            .map(
+                |ActorBundleInfo {
+                     manifest: root,
+                     url,
+                     alt_url,
+                 }| async move {
+                    let response = if let Ok(response) = http_get(url).await {
+                        response
+                    } else {
+                        warn!("failed to download bundle from primary URL, trying alternative URL");
+                        http_get(alt_url).await?
+                    };
+                    let bytes = response.bytes().await?;
+                    let header = load_car(db, Cursor::new(bytes)).await?;
+                    ensure!(header.roots.len() == 1);
+                    ensure!(&header.roots[0] == root);
+                    Ok(())
+                },
+            ),
     )
     .try_collect::<Vec<_>>()
     .await?;
