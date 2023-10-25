06f3520c20c60f98ad2a1722ce4a0f835b6aad70
lemmih <david.himmelstrup@chainsafe.io>
2023-10-25 12:55:12 +0200

    Use Lotus format for tokens

--- src/rpc/auth_api.rs
+++ src/rpc/auth_api.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use crate::auth::*;
+use crate::lotus_json::LotusJson;
 use crate::rpc_api::{auth_api::*, data_types::RPCState};
 use fvm_ipld_blockstore::Blockstore;
 use jsonrpc_v2::{Data, Error as JsonRpcError, Params};
@@ -10,12 +11,12 @@ use jsonrpc_v2::{Data, Error as JsonRpcError, Params};
 pub(in crate::rpc) async fn auth_new<DB: Blockstore>(
     data: Data<RPCState<DB>>,
     Params(params): Params<AuthNewParams>,
-) -> Result<Vec<u8>, JsonRpcError> {
+) -> Result<LotusJson<Vec<u8>>, JsonRpcError> {
     let auth_params: AuthNewParams = params;
     let ks = data.keystore.read().await;
     let ki = ks.get(JWT_IDENTIFIER)?;
     let token = create_token(auth_params.perms, ki.private_key(), auth_params.token_exp)?;
-    Ok(token.as_bytes().to_vec())
+    Ok(LotusJson(token.as_bytes().to_vec()))
 }
 
 /// RPC call to verify JWT Token and return the token's permissions
