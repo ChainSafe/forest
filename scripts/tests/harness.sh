ec8875140b094d197de9e1b9d2c36c3be4d4ebe9
elmattic <elmattic@users.noreply.github.com>
2023-10-25 14:41:19 +0200

    Remove message

--- scripts/tests/harness.sh
+++ scripts/tests/harness.sh
@@ -16,7 +16,6 @@ export LOG_DIRECTORY
 export FOREST_WALLET_PATH
 
 function glif_check_pending {
-  echo "Get pending messages"
   curl --silent --show-error --fail -X POST 'https://api.calibration.node.glif.io/rpc/v0' -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0","id":1,"method":"Filecoin.MpoolPending","params":[[]]}' | jq
 }
 
