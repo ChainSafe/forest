267f907d584af1f6dda756cc719e60440403d51f
elmattic <elmattic@users.noreply.github.com>
2023-10-25 16:39:13 +0200

    Fix compilation error

--- src/shim/kernel.rs
+++ src/shim/kernel.rs
@@ -1,5 +1,18 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
+//! We have three goals for our error shims:
+//! - preserve upstream error _numbers_.
+//! - preserve upstream error messages.
+//! - allow _matching_ on specific errors.
+//!
+//! There are a couple of things that make this difficult:
+//! - `fvm_shared*::error::ErrorNumber` is `#[non_exhaustive]`
+//! - ...
+//!
+//! We have designed with the following assumptions about the `fvm*` crates:
+//! - new error variants are append-only
+//! - error messages are consistent between crates
+
 use self::ErrorNumber as NShim;
 use self::SyscallError as EShim;
 use fvm2::kernel::SyscallError as E2;
@@ -13,18 +26,7 @@ use num_traits::FromPrimitive;
 use std::fmt;
 use std::fmt::Debug;
 
-//! We have three goals for our error shims:
-//! - preserve upstream error _numbers_.
-//! - preserve upstream error messages.
-//! - allow _matching_ on specific errors.
-//!
-//! There are a couple of things that make this difficult:
-//! - `fvm_shared*::error::ErrorNumber` is `#[non_exhaustive]`
-//! - ...
-//!
-//! We have designed with the following assumptions about the `fvm*` crates:
-//! - new error variants are append-only
-//! - error messages are consistent between crates
+macro_rules! error_number {
     ($($variant:ident),* $(,)?) => {
         #[derive(Debug, Clone)]
         pub enum ErrorNumber {
