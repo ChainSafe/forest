3cb21fa20ddc3caf5501505797cab248c2da9587
hanabi1224 <harlowmoo@gmail.com>
2023-10-26 17:41:53 +0800

    Remove wait_until_rpc_is_ready

--- scripts/tests/harness.sh
+++ scripts/tests/harness.sh
@@ -90,8 +90,6 @@ function forest_init_stateless {
 
   export ADMIN_TOKEN
   export FULLNODE_API_INFO
-
-  wait_util_rpc_is_ready
 }
 
 function forest_print_logs_and_metrics {
@@ -105,21 +103,6 @@ function forest_print_logs_and_metrics {
   echo "Print forest log files"
   ls -hl "$LOG_DIRECTORY"
   cat "$LOG_DIRECTORY"/*
-}
-
-function wait_util_rpc_is_ready {
-  # Wait for Forest to be ready. We can assume that it is ready when the
-  # RPC server is up. This checks if Forest's RPC endpoint is up.
-  function call_forest_chain_head {
-    curl --silent -X POST -H "Content-Type: application/json" \
-          --data '{"jsonrpc":"2.0","id":2,"method":"Filecoin.ChainHead","params":"null"}' \
-          "http://127.0.0.1:2345/rpc/v0"
-  }
-
-  until call_forest_chain_head; do
-      echo "Forest is unavailable - sleeping for 1s"
-      sleep 1s
-  done
 }
 
 function wait_until_p2p_is_ready {
