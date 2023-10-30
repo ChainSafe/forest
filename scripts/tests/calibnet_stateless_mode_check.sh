952a63ecfba77955c0a2abb6fe576cb98df9c813
hanabi1224 <harlowmoo@gmail.com>
2023-10-30 20:48:13 +0800

    feat: add `--stateless` flag to `forest` (#3593)
    Co-authored-by: David Himmelstrup <david.himmelstrup@chainsafe.io>
    Co-authored-by: Hubert <hubert@chainsafe.io>

---
+++ scripts/tests/calibnet_stateless_mode_check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euxo pipefail
+
+# This script tests the stateless mode of a forest node
+
+source "$(dirname "$0")/harness.sh"
+
+forest_init_stateless
+
+echo "Verifying the heaviest tipset to be the genesis"
+HEAD_CID=$($FOREST_CLI_PATH chain head | jq -r '.[0]')
+assert_eq "$HEAD_CID" "bafy2bzacecyaggy24wol5ruvs6qm73gjibs2l2iyhcqmvi7r7a4ph7zx3yqd4"
+
+# Example format: /ip4/127.0.0.1/tcp/41937/p2p/12D3KooWAB9z7vZ1x1v9t4BViVkX1Hy1ScoRnWV2GgGy5ec6pfUZ
+STATELESS_NODE_ADDRESS=$($FOREST_CLI_PATH net listen | tail -n 1)
+echo "Stateless node address: $STATELESS_NODE_ADDRESS"
+# Example format: 12D3KooWAB9z7vZ1x1v9t4BViVkX1Hy1ScoRnWV2GgGy5ec6pfUZ
+STATELESS_NODE_PEER_ID=$(echo "$STATELESS_NODE_ADDRESS" | cut --delimiter="/" --fields=7 --zero-terminated)
+echo "Stateless node peer id: $STATELESS_NODE_PEER_ID"
+
+# Run a normal forest node that only connects to the stateless node
+CONFIG_PATH="./forest_config.toml"
+cat <<- EOF > $CONFIG_PATH
+	[network]
+	listening_multiaddrs = ["/ip4/127.0.0.1/tcp/0"]
+	bootstrap_peers = ["$STATELESS_NODE_ADDRESS"]
+	mdns = false
+	kademlia = false
+EOF
+
+# Disable discovery to not connect to more nodes
+$FOREST_PATH --chain calibnet --encrypt-keystore false --auto-download-snapshot --config "$CONFIG_PATH" --rpc false --metrics-address 127.0.0.1:6117 &
+FOREST_NODE_PID=$!
+# Verify that the stateless node can respond to chain exchange requests
+until curl http://127.0.0.1:6117/metrics | grep "chain_exchange_response_in"; do
+    sleep 1s;
+done
+kill -KILL $FOREST_NODE_PID
