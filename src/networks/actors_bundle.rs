401fead7dcf724de601ac583942a50bc14153478
Hubert Bugaj <lesny.rumcajs@gmail.com>
2023-10-30 18:57:13 +0100

    increase timeout to 60s

--- src/networks/actors_bundle.rs
+++ src/networks/actors_bundle.rs
@@ -211,7 +211,7 @@ mod tests {
     pub async fn http_get(url: &Url) -> anyhow::Result<Response> {
         Ok(global_http_client()
             .get(url.clone())
-            .timeout(Duration::from_secs(30))
+            .timeout(Duration::from_secs(60))
             .send()
             .await?)
     }
