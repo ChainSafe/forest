6a46700a4897e59f11003296084208892510b4ec
Aatif Syed <aatifsyedyp@gmail.com>
2023-11-03 23:28:41 +0000

    feat: initial implementation

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,1 +1,207 @@
-fn main() {}
+#![allow(clippy::disallowed_types)] // I _hate_ this
+
+mod util;
+
+use std::{
+    collections::{
+        hash_map::{Entry, RandomState},
+        HashMap,
+    },
+    fmt,
+};
+
+use anyhow::{anyhow, bail, ensure, Context as _};
+use gosyn::ast::{Declaration, Expression, Field, FieldList, File, FuncType};
+use itertools::Itertools as _;
+
+use util::{expr, SetPos as _};
+
+fn main() -> anyhow::Result<()> {
+    let (mapped, could_not_map) = [
+        (include_str!("../api/api_common.go"), "Common"),
+        (include_str!("../api/api_net.go"), "Net"),
+        (include_str!("../api/api_full.go"), "FullNode"),
+    ]
+    .into_iter()
+    .map(|(text, interface)| gosyn::parse_source(text).and_then(|it| extract(it, interface)))
+    .collect::<Result<Vec<_>, _>>()?
+    .into_iter()
+    .flatten()
+    .map(|(name, (params, returns))| do_map(name, params, returns, map))
+    .sorted()
+    .partition_result::<Vec<_>, Vec<_>, _, _>();
+
+    for Method {
+        name,
+        params,
+        returns,
+    } in &mapped
+    {
+        println!(
+            "{}({}) -> ({})",
+            name,
+            params.join(", "),
+            returns.join(", ")
+        )
+    }
+
+    println!();
+
+    for it in &could_not_map {
+        println!("{}", it)
+    }
+
+    println!(
+        "processed {} methods. Additionally, there were {} methods that failed to process.",
+        mapped.len(),
+        could_not_map.len()
+    );
+
+    Ok(())
+}
+
+/// Get the methods defined in a given interface.
+///
+/// Returns a mapping from `method_name` -> `(param_types, return_types)`.
+pub fn extract(
+    file: File,
+    interface: &str,
+) -> anyhow::Result<HashMap<String, (FieldList, FieldList)>> {
+    let found = file
+        .decl
+        .into_iter()
+        .flat_map(|it| match it {
+            Declaration::Type(ty) => Some(ty.specs),
+            _ => None,
+        })
+        .flatten()
+        .filter(|it| it.name.name == interface)
+        .exactly_one()
+        .map_err(|too_many| anyhow!("{}", too_many))
+        .with_context(|| format!("couldn't get `type {interface} interface {{ .. }}`"))?
+        .typ;
+
+    let Expression::TypeInterface(interface) = found else {
+        bail!("expected Expression::TypeInterface, not {:?}", found)
+    };
+
+    let mut all_methods = HashMap::new();
+
+    for mut item in interface.methods.list {
+        if let Expression::TypeFunction(FuncType {
+            pos: _,
+            typ_params,
+            params,
+            result,
+        }) = item.typ
+        {
+            ensure!(
+                item.name.len() == 1,
+                "method `{}` must have a single name",
+                item.name.iter().map(|it| &it.name).join(", ")
+            );
+            ensure!(
+                typ_params.list.is_empty(),
+                "generic functions are not supported"
+            );
+            match all_methods.entry(item.name.remove(0).name) {
+                Entry::Occupied(it) => bail!("duplicate method definition {}", it.key()),
+                Entry::Vacant(it) => {
+                    it.insert((params, result));
+                }
+            }
+        }
+    }
+
+    Ok(all_methods)
+}
+
+/// A processed method definition.
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Method<T> {
+    name: String,
+    params: Vec<T>,
+    returns: Vec<T>,
+}
+
+/// Some types for this method could not be mapped.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct MapError {
+    name: String,
+    required: Vec<Expression>,
+}
+
+impl std::error::Error for MapError {}
+
+impl fmt::Display for MapError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!(
+            "{} depends on the following unmapped types:\n",
+            self.name
+        ))?;
+        for req in &self.required {
+            f.write_fmt(format_args!("- {}\n", util::Fmt(req)))?
+        }
+
+        Ok(())
+    }
+}
+
+/// Attempt to transform this method using the given `mapper`.
+fn do_map<T>(
+    name: String,
+    params: FieldList,
+    returns: FieldList,
+    mut mapper: impl FnMut(Expression) -> Option<T>,
+) -> Result<Method<T>, MapError> {
+    let mut required = vec![];
+    let mut mapped_params = vec![];
+    let mut mapped_returns = vec![];
+    for Field { typ, .. } in params.list {
+        match mapper(typ.clone()) {
+            Some(it) => mapped_params.push(it),
+            None => required.push(typ),
+        }
+    }
+    for Field { typ, .. } in returns.list {
+        match mapper(typ.clone()) {
+            Some(it) => mapped_returns.push(it),
+            None => required.push(typ),
+        }
+    }
+    match required.is_empty() {
+        true => Ok(Method {
+            name,
+            params: mapped_params,
+            returns: mapped_returns,
+        }),
+        false => Err(MapError { name, required }),
+    }
+}
+
+fn map(mut ty: Expression) -> Option<&'static str> {
+    ty.set_pos(0);
+    while let Expression::TypePointer(it) = ty {
+        ty = *it.typ;
+    }
+    let map = HashMap::<_, _, RandomState>::from_iter([
+        (expr::selector(expr::ident("context"), "Context"), "Context"),
+        (expr::ident("error"), "Error"),
+        (expr::selector(expr::ident("address"), "Address"), "Address"),
+        (
+            expr::selector(expr::ident("types"), "TipSetKey"),
+            "TipsetKey",
+        ),
+        (expr::selector(expr::ident("cid"), "Cid"), "Cid"),
+        (expr::selector(expr::ident("types"), "BigInt"), "BigInt"),
+        (expr::ident("bool"), "bool"),
+        (expr::ident("uint64"), "u64"),
+        (
+            expr::selector(expr::ident("abi"), "ChainEpoch"),
+            "ChainEpoch",
+        ),
+        (expr::slice(expr::ident("byte")), "Vec<u8>"),
+        (expr::pointer(expr::ident("MessagePrototype")), "Message"),
+    ]);
+    map.get(&ty).copied()
+}
---
+++ codegen/src/util.rs
@@ -0,0 +1,231 @@
+use std::{fmt, rc::Rc};
+
+use gosyn::ast::{
+    ChanMode, ChannelType, Comment, Expression, Field, FieldList, FuncType, Ident, InterfaceType,
+    MapType, PointerType, Selector, SliceType, StringLit, StructType,
+};
+
+/// Ergonomic constructors for [`Expression`].
+pub mod expr {
+    use super::*;
+
+    pub fn selector(left: Expression, right: impl Into<String>) -> Expression {
+        Expression::Selector(Selector {
+            pos: 0,
+            x: Box::new(left),
+            sel: Ident {
+                pos: 0,
+                name: right.into(),
+            },
+        })
+    }
+    pub fn ident(ident: impl Into<String>) -> Expression {
+        Expression::Ident(Ident {
+            pos: 0,
+            name: ident.into(),
+        })
+    }
+    pub fn slice(of: Expression) -> Expression {
+        Expression::TypeSlice(SliceType {
+            pos: (0, 0),
+            typ: Box::new(of),
+        })
+    }
+    pub fn pointer(to: Expression) -> Expression {
+        Expression::TypePointer(PointerType {
+            pos: 0,
+            typ: Box::new(to),
+        })
+    }
+}
+
+/// Set the `position` field on the given [`gosyn::ast`] struct recursively.
+///
+/// Useful for clearing that field for comparison (`==`) purposes.
+///
+/// # API Hazards
+/// - This is a fast and loose trait, and may set _any_ [`usize`]s in the tree.
+/// - Only partially implemented, and may panic
+pub trait SetPos {
+    fn set_pos(&mut self, new: usize);
+}
+
+impl<T> SetPos for &mut T
+where
+    T: SetPos,
+{
+    fn set_pos(&mut self, new: usize) {
+        T::set_pos(self, new)
+    }
+}
+
+impl<T> SetPos for Vec<T>
+where
+    T: SetPos,
+{
+    fn set_pos(&mut self, new: usize) {
+        for it in self {
+            it.set_pos(new)
+        }
+    }
+}
+
+impl<T> SetPos for Option<T>
+where
+    T: SetPos,
+{
+    fn set_pos(&mut self, new: usize) {
+        if let Some(it) = self {
+            it.set_pos(new)
+        }
+    }
+}
+
+impl<T> SetPos for Rc<T>
+where
+    T: SetPos + Clone,
+{
+    fn set_pos(&mut self, new: usize) {
+        Rc::make_mut(self).set_pos(new)
+    }
+}
+
+impl<T, U> SetPos for (T, U)
+where
+    T: SetPos,
+    U: SetPos,
+{
+    fn set_pos(&mut self, new: usize) {
+        let (a, b) = self;
+        a.set_pos(new);
+        b.set_pos(new)
+    }
+}
+
+impl SetPos for usize {
+    fn set_pos(&mut self, new: usize) {
+        *self = new; // naughty
+    }
+}
+
+impl SetPos for String {
+    fn set_pos(&mut self, _: usize) {}
+}
+
+impl SetPos for ChanMode {
+    fn set_pos(&mut self, _: usize) {}
+}
+
+macro_rules! set_pos {
+    ($(
+        $ty:ty {
+            $($ident:ident),* $(,)?
+        }
+    ),* $(,)?) => {
+        $(
+            impl SetPos for $ty {
+                fn set_pos(&mut self, new: usize) {
+                    let Self {
+                        $($ident,)*
+                    } = self;
+                    $($ident.set_pos(new);)*
+                }
+            }
+        )*
+    };
+}
+
+set_pos! {
+    Field { name, typ, tag, comments },
+    Ident { pos, name },
+    StringLit { pos, value },
+    Comment { pos, text },
+    Selector { pos, x, sel },
+    ChannelType { pos, dir, typ },
+    SliceType { pos, typ },
+    PointerType { pos, typ },
+    MapType { pos, key, val },
+    InterfaceType { pos, methods },
+    FieldList { pos, list },
+    FuncType { pos, typ_params, params, result },
+    StructType { pos, fields },
+}
+
+impl SetPos for Expression {
+    fn set_pos(&mut self, new: usize) {
+        match self {
+            Expression::Call(_) => todo!(),
+            Expression::Index(_) => todo!(),
+            Expression::IndexList(_) => todo!(),
+            Expression::Slice(_) => todo!(),
+            Expression::Ident(it) => it.set_pos(new),
+            Expression::FuncLit(_) => todo!(),
+            Expression::Ellipsis(_) => todo!(),
+            Expression::Selector(it) => it.set_pos(new),
+            Expression::BasicLit(_) => todo!(),
+            Expression::Range(_) => todo!(),
+            Expression::Star(_) => todo!(),
+            Expression::Paren(_) => todo!(),
+            Expression::TypeAssert(_) => todo!(),
+            Expression::CompositeLit(_) => todo!(),
+            Expression::List(_) => todo!(),
+            Expression::Operation(_) => todo!(),
+            Expression::TypeMap(it) => it.set_pos(new),
+            Expression::TypeArray(_) => todo!(),
+            Expression::TypeSlice(it) => it.set_pos(new),
+            Expression::TypeFunction(_) => todo!(),
+            Expression::TypeStruct(it) => it.set_pos(new),
+            Expression::TypeChannel(it) => it.set_pos(new),
+            Expression::TypePointer(it) => it.set_pos(new),
+            Expression::TypeInterface(it) => it.set_pos(new),
+        }
+    }
+}
+
+/// Custom formatter.
+pub struct Fmt<'a, T>(pub &'a T);
+
+impl fmt::Display for Fmt<'_, Expression> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.0 {
+            Expression::Call(_) => todo!(),
+            Expression::Index(_) => todo!(),
+            Expression::IndexList(_) => todo!(),
+            Expression::Slice(_) => todo!(),
+            Expression::Ident(it) => f.write_str(&it.name),
+            Expression::FuncLit(_) => todo!(),
+            Expression::Ellipsis(_) => todo!(),
+            Expression::Selector(it) => {
+                f.write_fmt(format_args!("{}.{}", Fmt(&*it.x), &it.sel.name))
+            }
+            Expression::BasicLit(_) => todo!(),
+            Expression::Range(_) => todo!(),
+            Expression::Star(_) => todo!(),
+            Expression::Paren(_) => todo!(),
+            Expression::TypeAssert(_) => todo!(),
+            Expression::CompositeLit(_) => todo!(),
+            Expression::List(_) => todo!(),
+            Expression::Operation(_) => todo!(),
+            Expression::TypeMap(it) => {
+                f.write_fmt(format_args!("map[{}]{}", Fmt(&*it.key), Fmt(&*it.val)))
+            }
+            Expression::TypeArray(_) => todo!(),
+            Expression::TypeSlice(it) => f.write_fmt(format_args!("[]{}", Fmt(&*it.typ))),
+            Expression::TypeFunction(_) => todo!(),
+            Expression::TypeStruct(it) => match it.fields.is_empty() {
+                true => f.write_fmt(format_args!("interface {{}}")),
+                false => todo!(),
+            },
+            Expression::TypeChannel(it) => match &it.dir {
+                Some(ChanMode::Recv) => f.write_fmt(format_args!("<-chan {}", Fmt(&*it.typ))),
+                Some(ChanMode::Send) => f.write_fmt(format_args!("chan<- {}", Fmt(&*it.typ))),
+                None => f.write_fmt(format_args!("chan {}", Fmt(&*it.typ))),
+            },
+            Expression::TypePointer(it) => f.write_fmt(format_args!("*{}", Fmt(&*it.typ))),
+            Expression::TypeInterface(it) => match it.methods.list.is_empty() {
+                true => f.write_fmt(format_args!("interface {{}}")),
+                false => todo!(),
+            },
+        }
+    }
+}
