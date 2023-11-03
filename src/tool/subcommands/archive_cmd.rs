cf8fc457a23e17f52f9034266f8ca286ba8eb949
elmattic <elmattic@users.noreply.github.com>
2023-11-03 13:41:16 +0100

    Move module doc comment

--- src/tool/subcommands/archive_cmd.rs
+++ src/tool/subcommands/archive_cmd.rs
@@ -1,6 +1,31 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
+//! Archives are key-value pairs encoded as
+//! [CAR files](https://ipld.io/specs/transport/car/carv1/). The key-value pairs
+//! represent a directed, acyclic graph (DAG). This graph is often a subset of a larger
+//! graph and references to missing keys are common.
+//!
+//! Each graph contains blocks, messages, state trees, and miscellaneous data
+//! such as compiled `WASM` code. The amount of data differs greatly in different
+//! kinds of archives. While there are no fixed definitions, there are three
+//! common kind of archives:
+//! - A full archive contains a complete graph with no missing nodes. These
+//!   archives are large (14 TiB for Filecoin's mainnet) and only used in special
+//!   situations.
+//! - A lite-archive typically has roughly 3 million blocks, 2000 complete sets of
+//!   state-roots, and 2000 sets of messages. These archives usually take up
+//!   roughly 100 GiB.
+//! - A diff-archive contains the subset of nodes that are _not_ shared by two
+//!   other archives. These archives are much smaller but can rarely be used on
+//!   their own. They are typically merged with other archives before use.
+//!
+//! The sub-commands in this module manipulate archive files without needing a
+//! running Forest-daemon or a separate database. Operations are carried out
+//! directly on CAR files.
+//!
+//! Additional reading: [`crate::db::car::plain`]
+
 use crate::blocks::Tipset;
 use crate::chain::{
     index::{ChainIndex, ResolveNullTipset},
