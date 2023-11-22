56a5109133b79e3bcec51dd71e3b6bd4c3ad0c13
lemmih <david.himmelstrup@chainsafe.io>
2023-11-22 10:26:32 +0100

    use perma links

--- src/lotus_json/signature_type.rs
+++ src/lotus_json/signature_type.rs
@@ -7,9 +7,9 @@ use crate::shim::crypto::SignatureType;
 // Lotus uses signature types under two names: `KeyType` and `SigType`.
 // `KeyType` can be deserialized from a string but `SigType` must always be an
 // integer. For more information, see
-// https://github.com/filecoin-project/go-state-types/blob/master/crypto/signature.go
+// https://github.com/filecoin-project/go-state-types/blob/a0445436230e221ab1828ad170623fcfe00c8263/crypto/signature.go
 // and
-// https://github.com/filecoin-project/lotus/blob/v1.23.3/chain/types/keystore.go#L47
+// https://github.com/filecoin-project/lotus/blob/7bb1f98ac6f5a6da2cc79afc26d8cd9fe323eb30/chain/types/keystore.go#L47
 
 #[derive(Deserialize, Serialize)]
 #[serde(untagged)] // try an int, then a string
