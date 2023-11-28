00008a651f963476bfad8b818f1e9ad0378e9151
Roman Useinov <roman.useinov@gmail.com>
2023-11-28 16:02:10 +0100

    fix a comment

--- src/rpc_api/data_types.rs
+++ src/rpc_api/data_types.rs
@@ -197,7 +197,8 @@ impl HasLotusJson for ApiMessage {
 
 const EMPTY_ADDRESS_VALUE: &str = "<empty>";
 
-/// This wrapper is needed, because of a
+/// This wrapper is needed, because of a bug in Lotus. It tries to serialize an empty address
+/// which results in `<empty>` string values.
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct AddressOrEmpty(pub Option<Address>);
 
