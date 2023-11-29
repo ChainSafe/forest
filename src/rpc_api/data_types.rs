aecd2bac23964b9965c5e84726237f3b0735298c
Roman Useinov <roman.useinov@gmail.com>
2023-11-29 11:13:07 +0100

    fix punctuation

--- src/rpc_api/data_types.rs
+++ src/rpc_api/data_types.rs
@@ -197,7 +197,7 @@ impl HasLotusJson for ApiMessage {
 
 const EMPTY_ADDRESS_VALUE: &str = "<empty>";
 
-/// This wrapper is needed, because of a bug in Lotus.
+/// This wrapper is needed because of a bug in Lotus.
 /// See: <https://github.com/filecoin-project/lotus/issues/11461>.
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct AddressOrEmpty(pub Option<Address>);
