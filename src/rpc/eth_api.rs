4c4b17fd05b3ca9bf5cc06ce5ac569bc374ac8d1
Josh Jones <joshua.jones@chainsafe.io>
2023-12-04 15:11:17 +0000

    rm duplicate method from merge conflict resolution

--- src/rpc/eth_api.rs
+++ src/rpc/eth_api.rs
@@ -6,15 +6,6 @@ use crate::rpc_api::data_types::RPCState;
 use fvm_ipld_blockstore::Blockstore;
 use jsonrpc_v2::{Data, Error as JsonRpcError};
 
-pub(in crate::rpc) async fn eth_chain_id<DB: Blockstore>(
-    data: Data<RPCState<DB>>,
-) -> Result<String, JsonRpcError> {
-    Ok(format!(
-        "0x{:x}",
-        data.state_manager.chain_config().eth_chain_id
-    ))
-}
-
 pub(in crate::rpc) async fn eth_block_number<DB: Blockstore>(
     data: Data<RPCState<DB>>,
 ) -> Result<String, JsonRpcError> {
