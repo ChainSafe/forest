0ea032e2cf5488182f7a1aa4688df007aee2dd42
sudo-shashank <shashank@chainsafe.io>
2024-01-29 17:52:53 +0530

    treat timeout as success

--- src/tool/subcommands/api_cmd.rs
+++ src/tool/subcommands/api_cmd.rs
@@ -939,7 +939,11 @@ async fn run_tests(
     let mut failed_results = HashMap::default();
     while let Some(Ok((method_name, forest_status, lotus_status))) = futures.next().await {
         let result_entry = (method_name, forest_status, lotus_status);
-        if forest_status == EndpointStatus::Valid && lotus_status == EndpointStatus::Valid {
+        if ((forest_status == EndpointStatus::Valid && lotus_status == EndpointStatus::Valid)
+            || (forest_status == EndpointStatus::Timeout
+                && lotus_status == EndpointStatus::Timeout))
+        {
+            // Your code here {
             success_results
                 .entry(result_entry)
                 .and_modify(|v| *v += 1)
