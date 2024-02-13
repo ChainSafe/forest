0d2303086d5963db48f8a0442de6d8a1fe22c5ab
elmattic <elmattic@users.noreply.github.com>
2024-02-13 11:18:46 +0100

    Try to log headers instead

--- src/rpc/auth_layer.rs
+++ src/rpc/auth_layer.rs
@@ -1,7 +1,12 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use std::task::{Context, Poll};
+use http::Request;
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
 use tower::{Layer, Service};
 
 pub struct LogLayer {
@@ -24,10 +29,28 @@ pub struct LogService<S> {
     service: S,
 }
 
-impl<S, Request> Service<Request> for LogService<S>
+// impl<S, Request> Service<Request> for LogService<S>
+// where
+//     S: Service<Request>,
+//     Request: std::fmt::Debug,
+// {
+//     type Response = S::Response;
+//     type Error = S::Error;
+//     type Future = S::Future;
+
+//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         self.service.poll_ready(cx)
+//     }
+
+//     fn call(&mut self, request: Request) -> Self::Future {
+//         println!("processing {:?}", request);
+//         self.service.call(request)
+//     }
+// }
+
+impl<S, B> Service<Request<B>> for LogService<S>
 where
-    S: Service<Request>,
-    Request: std::fmt::Debug,
+    S: Service<Request<B>> + Clone + Send,
 {
     type Response = S::Response;
     type Error = S::Error;
@@ -37,8 +60,8 @@ where
         self.service.poll_ready(cx)
     }
 
-    fn call(&mut self, request: Request) -> Self::Future {
-        println!("processing {:?}", request);
+    fn call(&mut self, request: Request<B>) -> Self::Future {
+        println!("processing {:?}", request.headers());
         self.service.call(request)
     }
 }
