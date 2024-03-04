bd438f78353d3ad1b62f54f58e0f1e185ad90f00
Aatif Syed <aatifsyedyp@gmail.com>
2024-03-04 15:01:05 +0000

    run: git show aatifsyed/spike-self-describing-rpc-endpoint:src/util.rs > src/rpc/reflect/util.rs

---
+++ src/rpc/reflect/util.rs
@@ -0,0 +1,105 @@
+use std::fmt::Display;
+
+use serde::{de::Visitor, forward_to_deserialize_any, Deserialize, Deserializer};
+
+/// "Introspection" by tracing a [`Deserialize`] to see if it's [`Option`]-like.
+///
+/// This allows us to smoothly operate between rust functions that take an optional
+/// paramater, and [`crate::openrpc_types::ContentDescriptor::required`].
+pub trait Optional<'de>: Deserialize<'de> {
+    fn optional() -> bool {
+        #[derive(Default)]
+        struct DummyDeserializer;
+
+        #[derive(thiserror::Error, Debug)]
+        #[error("")]
+        struct DeserializeOptionWasCalled(bool);
+
+        impl serde::de::Error for DeserializeOptionWasCalled {
+            fn custom<T: Display>(_: T) -> Self {
+                Self(false)
+            }
+        }
+
+        impl<'de> Deserializer<'de> for DummyDeserializer {
+            type Error = DeserializeOptionWasCalled;
+
+            fn deserialize_any<V: Visitor<'de>>(self, _: V) -> Result<V::Value, Self::Error> {
+                Err(DeserializeOptionWasCalled(false))
+            }
+
+            fn deserialize_option<V: Visitor<'de>>(self, _: V) -> Result<V::Value, Self::Error> {
+                Err(DeserializeOptionWasCalled(true))
+            }
+
+            forward_to_deserialize_any! {
+                bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
+                bytes byte_buf unit unit_struct newtype_struct seq tuple
+                tuple_struct map struct enum identifier ignored_any
+            }
+        }
+
+        let Err(DeserializeOptionWasCalled(optional)) = Self::deserialize(DummyDeserializer) else {
+            unreachable!("DummyDeserializer never returns Ok(..)")
+        };
+        optional
+    }
+    /// # Panics
+    /// - This is only safe to call if [`Optional::optional`] returns `true`.
+    fn unwrap_none() -> Self {
+        Self::deserialize(serde_json::Value::Null)
+            .expect("`null` json values should deserialize to a `None` for option-like types")
+    }
+}
+
+impl<'de, T> Optional<'de> for T where T: Deserialize<'de> {}
+
+#[cfg(test)]
+pub use test_utils::*;
+
+#[cfg(test)]
+mod test_utils {
+
+    macro_rules! from_value {
+        ($tt:tt) => {
+            serde_json::from_value(serde_json::json!($tt)).unwrap()
+        };
+    }
+    pub(crate) use from_value;
+
+    use tower::{Service, ServiceExt as _};
+
+    pub fn call<S, T, U, E>(svc: &mut S, request: T) -> Result<U, E>
+    where
+        S: Service<T, Response = U, Error = E>,
+    {
+        futures::executor::block_on(async { svc.ready().await?.call(request).await })
+    }
+
+    pub mod examples {
+        use schemars::JsonSchema;
+        use serde::Deserialize;
+
+        use crate::jsonrpc_types::Error;
+
+        #[derive(Deserialize, JsonSchema)]
+        #[serde(rename_all = "lowercase")]
+        pub enum LenMethod {
+            Bytes,
+            Chars,
+        }
+
+        /// Method with an optional trailing parameter
+        pub async fn len(s: String, method: Option<LenMethod>) -> Result<usize, Error> {
+            Ok(match method {
+                Some(LenMethod::Bytes) | None => s.len(),
+                Some(LenMethod::Chars) => s.chars().count(),
+            })
+        }
+
+        /// Method where optional parameter is too soon
+        pub async fn bad_len(method: Option<LenMethod>, s: String) -> Result<usize, Error> {
+            len(s, method).await
+        }
+    }
+}
