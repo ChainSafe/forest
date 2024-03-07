515753f9bd506ca345135dde2ab6b7bed3fee245
Hubert <hubert@chainsafe.io>
2024-03-07 12:17:02 +0100

    Butterflynet nv22 (#4028)

--- src/networks/butterflynet/mod.rs
+++ src/networks/butterflynet/mod.rs
@@ -10,7 +10,10 @@ use url::Url;
 
 use crate::{db::SettingsStore, utils::net::http_get};
 
-use super::{drand::DRAND_MAINNET, parse_bootstrap_peers, DrandPoint, Height, HeightInfo};
+use super::{
+    drand::{DRAND_MAINNET, DRAND_QUICKNET},
+    get_upgrade_height_from_env, parse_bootstrap_peers, DrandPoint, Height, HeightInfo,
+};
 
 /// Fetches the genesis CAR from the local database or downloads it if it does not exist.
 /// The result bytes may be compressed.
@@ -32,7 +35,7 @@ pub async fn fetch_genesis<DB: SettingsStore>(db: &DB) -> anyhow::Result<Vec<u8>
 
 /// Genesis CID
 pub static GENESIS_CID: Lazy<Cid> = Lazy::new(|| {
-    Cid::from_str("bafy2bzacecl7vdlut572ia64cskp3onngc5ii6co2vsdoshc6ehcx7bful5oo").unwrap()
+    Cid::from_str("bafy2bzaceca7iggnglnsntfmtqvpsmolzvbuyzhpoq55wzubq65jasnkqxuh6").unwrap()
 });
 
 /// Compressed genesis file. It is compressed with zstd and cuts the download size by 80% (from 10 MB to 2 MB).
@@ -47,7 +50,7 @@ static GENESIS_URL: Lazy<Url> = Lazy::new(|| {
 /// The genesis file does not live on the `master` branch, currently on a draft PR.
 /// `<https://github.com/filecoin-project/lotus/pull/11458>`
 static GENESIS_URL_ALT: Lazy<Url> = Lazy::new(|| {
-    "https://github.com/filecoin-project/lotus/raw/3e379c9997bf152639a593d3efee49b88fee27ec/build/genesis/butterflynet.car".parse().expect("hard-coded URL must parse")
+    "https://github.com/filecoin-project/lotus/raw/3331a7e52c97e5723fc0c613271a616bb3ccdb39/build/genesis/butterflynet.car".parse().expect("hard-coded URL must parse")
 });
 
 pub(crate) const MINIMUM_CONSENSUS_POWER: i64 = 2 << 30;
@@ -65,21 +68,21 @@ pub const ETH_CHAIN_ID: u64 = 3141592;
 pub static HEIGHT_INFOS: Lazy<HashMap<Height, HeightInfo>> = Lazy::new(|| {
     HashMap::from_iter([
         (
-            Height::Thunder,
+            Height::Watermelon,
             HeightInfo {
                 epoch: -1,
                 bundle: Some(
-                    Cid::try_from("bafy2bzaceaiy4dsxxus5xp5n5i4tjzkb7sc54mjz7qnk2efhgmsrobjesxnza")
+                    Cid::try_from("bafy2bzacectxvbk77ntedhztd6sszp2btrtvsmy7lp2ypnrk6yl74zb34t2cq")
                         .unwrap(),
                 ),
             },
         ),
         (
-            Height::Watermelon,
+            Height::Dragon,
             HeightInfo {
-                epoch: 400,
+                epoch: 2600,
                 bundle: Some(
-                    Cid::try_from("bafy2bzacectxvbk77ntedhztd6sszp2btrtvsmy7lp2ypnrk6yl74zb34t2cq")
+                    Cid::try_from("bafy2bzaceaqx5xa4cwso24rjiu2ketjlztrqlac6dkyol7tlyuhzrle3zfbos")
                         .unwrap(),
                 ),
             },
@@ -87,11 +90,18 @@ pub static HEIGHT_INFOS: Lazy<HashMap<Height, HeightInfo>> = Lazy::new(|| {
     ])
 });
 
-pub(super) static DRAND_SCHEDULE: Lazy<[DrandPoint<'static>; 1]> = Lazy::new(|| {
-    [DrandPoint {
-        height: 0,
-        config: &DRAND_MAINNET,
-    }]
+pub(super) static DRAND_SCHEDULE: Lazy<[DrandPoint<'static>; 2]> = Lazy::new(|| {
+    [
+        DrandPoint {
+            height: 0,
+            config: &DRAND_MAINNET,
+        },
+        DrandPoint {
+            height: get_upgrade_height_from_env("FOREST_DRAND_QUICKNET_HEIGHT")
+                .unwrap_or(HEIGHT_INFOS.get(&Height::Dragon).unwrap().epoch + 120),
+            config: &DRAND_QUICKNET,
+        },
+    ]
 });
 
 /// Creates a new butterfly policy with the given version.
@@ -161,10 +171,12 @@ mod tests {
         let v10 = make_butterfly_policy!(v10);
         let v11 = make_butterfly_policy!(v11);
         let v12 = make_butterfly_policy!(v12);
+        let v13 = make_butterfly_policy!(v13);
 
         // basic sanity checks
         assert_eq!(v10.minimum_consensus_power, MINIMUM_CONSENSUS_POWER.into());
         assert_eq!(v11.minimum_consensus_power, MINIMUM_CONSENSUS_POWER.into());
         assert_eq!(v12.minimum_consensus_power, MINIMUM_CONSENSUS_POWER.into());
+        assert_eq!(v13.minimum_consensus_power, MINIMUM_CONSENSUS_POWER.into());
     }
 }
