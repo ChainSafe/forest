22caf550f2ce07e79c4908cdb4722f88b177d9f0
elmattic <elmattic@users.noreply.github.com>
2024-03-19 15:51:52 +0100

    Remove useless const bindings

--- src/rpc_client/mod.rs
+++ src/rpc_client/mod.rs
@@ -35,8 +35,6 @@ pub const API_INFO_KEY: &str = "FULLNODE_API_INFO";
 pub const DEFAULT_HOST: &str = "127.0.0.1";
 pub const DEFAULT_MULTIADDRESS: &str = "/ip4/127.0.0.1/tcp/2345/http";
 pub const DEFAULT_PORT: u16 = 2345;
-pub const HTTP_PROTOCOL: &str = "http";
-pub const WS_PROTOCOL: &str = "ws";
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(60);
 
 #[derive(Clone, Debug)]
