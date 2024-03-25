0625ad13d660e4989e6e4209fb5b05205810de15
hanabi1224 <harlowmoo@gmail.com>
2024-03-25 22:53:58 +0800

    update calibnet_rpc_checks.sh

--- scripts/tests/calibnet_rpc_checks.sh
+++ scripts/tests/calibnet_rpc_checks.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # This script is used to test both online and offline RPC API server against lotus.
-# It's run in CI, and uses forest-tool api compare subcommand to test RPC endpoints.
+# It runs forest daemon, forest offline rpc server and lotus 
+# and uses forest-tool api compare subcommand to test RPC endpoints.
 
 set -euxo pipefail
 
@@ -9,7 +10,7 @@ LOTUS_IMAGE="filecoin/lotus-all-in-one:v1.26.0-rc3-calibnet"
 FOREST_PATH="forest"
 FOREST_CLI_PATH="forest-cli"
 FOREST_TOOL_PATH="forest-tool"
-PORTS=(1234 2345 2346)
+PORTS=(1234 2345 3456)
 
 # Function to stop services on specified ports
 stop_services() {
@@ -34,8 +35,8 @@ EXPORTED_SNAPSHOT="latest.forest.car.zst"
 old_snapshot=forest_diff_calibnet_2022-11-02_height_0+3000.forest.car.zst
 curl --location --remote-name "https://forest-archive.chainsafe.dev/calibnet/diff/$old_snapshot"
 
-# Run forest-tool RPC server at 2346
-"$FOREST_TOOL_PATH" api serve "$EXPORTED_SNAPSHOT" "$old_snapshot" --chain calibnet --port 2346 &
+# Run forest-tool RPC server at 3456
+"$FOREST_TOOL_PATH" api serve "$EXPORTED_SNAPSHOT" "$old_snapshot" --chain calibnet --port 3456 &
 FOREST_TOOL_PID=$!
 
 # Run lotus daemon RPC server at 1234
@@ -63,7 +64,7 @@ done
 # Assert an old block is present, given that the old snapshot is used.
 # https://calibration.filfox.info/en/block/bafy2bzacecpjvcld56dazyukvj35uzwvlh3tb4ga2lvbgbiua3mgbqaz45hbm
 temp_dir=$(mktemp -d)
-FULLNODE_API_INFO="/ip4/127.0.0.1/tcp/2346/http" forest-cli chain block -c bafy2bzacecpjvcld56dazyukvj35uzwvlh3tb4ga2lvbgbiua3mgbqaz45hbm | jq . > "$temp_dir/block.json"
+FULLNODE_API_INFO="/ip4/127.0.0.1/tcp/3456/http" forest-cli chain block -c bafy2bzacecpjvcld56dazyukvj35uzwvlh3tb4ga2lvbgbiua3mgbqaz45hbm | jq . > "$temp_dir/block.json"
 
 # assert block is as expected
 diff "test_data/calibnet_block_3000.json" "$temp_dir/block.json"
@@ -72,12 +73,12 @@ diff "test_data/calibnet_block_3000.json" "$temp_dir/block.json"
 # forest daemon vs lotus daemon
 $FOREST_TOOL_PATH api compare "$EXPORTED_SNAPSHOT" --forest /ip4/127.0.0.1/tcp/2345/http --lotus /ip4/127.0.0.1/tcp/1234/http --n-tipsets 5 --filter-file api_compare/filter-list
 # forest-tool vs lotus daemon
-$FOREST_TOOL_PATH api compare "$EXPORTED_SNAPSHOT" --forest /ip4/127.0.0.1/tcp/2346/http --lotus /ip4/127.0.0.1/tcp/1234/http --n-tipsets 5 --filter-file api_compare/filter-list || true
+$FOREST_TOOL_PATH api compare "$EXPORTED_SNAPSHOT" --forest /ip4/127.0.0.1/tcp/3456/http --lotus /ip4/127.0.0.1/tcp/1234/http --n-tipsets 5 --filter-file api_compare/filter-list || true
 
 # Compare the ws endpoints
 # forest daemon vs lotus daemon
 $FOREST_TOOL_PATH api compare "$EXPORTED_SNAPSHOT" --forest /ip4/127.0.0.1/tcp/2345/ws --lotus /ip4/127.0.0.1/tcp/1234/ws --n-tipsets 5 --filter-file api_compare/filter-list || true
 # forest-tool vs lotus daemon
-$FOREST_TOOL_PATH api compare "$EXPORTED_SNAPSHOT" --forest /ip4/127.0.0.1/tcp/2346/ws --lotus /ip4/127.0.0.1/tcp/1234/ws --n-tipsets 5 --filter-file api_compare/filter-list || true
+$FOREST_TOOL_PATH api compare "$EXPORTED_SNAPSHOT" --forest /ip4/127.0.0.1/tcp/3456/ws --lotus /ip4/127.0.0.1/tcp/1234/ws --n-tipsets 5 --filter-file api_compare/filter-list || true
 
 popd
