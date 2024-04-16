9d9b3d7a2b6902f1a96a1b148ff4f7f364a54996
Aatif Syed <38045910+aatifsyed@users.noreply.github.com>
2024-04-16 08:36:39 +0100

    refactor: migrate net to trait RpcEndpoint (#4200)

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -48,6 +48,7 @@ pub mod prelude {
     common::for_each_method!(export);
     gas::for_each_method!(export);
     wallet::for_each_method!(export);
+    net::for_each_method!(export);
 }
 
 /// All the methods live in their own folder
@@ -206,6 +207,7 @@ where
     common::for_each_method!(register);
     gas::for_each_method!(register);
     wallet::for_each_method!(register);
+    net::for_each_method!(register);
     module.finish()
 }
 
@@ -216,7 +218,6 @@ where
 {
     use eth::*;
     use gas::*;
-    use net::*;
     use node::*;
     use sync::*;
 
@@ -291,16 +292,6 @@ where
     module.register_async_method(GAS_ESTIMATE_FEE_CAP, gas_estimate_fee_cap::<DB>)?;
     module.register_async_method(GAS_ESTIMATE_GAS_PREMIUM, gas_estimate_gas_premium::<DB>)?;
     module.register_async_method(GAS_ESTIMATE_MESSAGE_GAS, gas_estimate_message_gas::<DB>)?;
-    // Net API
-    module.register_async_method(NET_ADDRS_LISTEN, |_, state| net_addrs_listen::<DB>(state))?;
-    module.register_async_method(NET_PEERS, |_, state| net_peers::<DB>(state))?;
-    module.register_async_method(NET_LISTENING, |_, _| net_listening())?;
-    module.register_async_method(NET_INFO, |_, state| net_info::<DB>(state))?;
-    module.register_async_method(NET_CONNECT, net_connect::<DB>)?;
-    module.register_async_method(NET_DISCONNECT, net_disconnect::<DB>)?;
-    module.register_async_method(NET_AGENT_VERSION, net_agent_version::<DB>)?;
-    module.register_async_method(NET_AUTO_NAT_STATUS, net_auto_nat_status::<DB>)?;
-    module.register_async_method(NET_VERSION, net_version::<DB>)?;
     // Node API
     module.register_async_method(NODE_STATUS, |_, state| node_status::<DB>(state))?;
     // Eth API
