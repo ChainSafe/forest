37c62d8cf2ad8d4c9f0bc59949f882d6e8599e2d
Aatif Syed <aatifsyedyp@gmail.com>
2024-05-06 09:14:44 +0100

    wip

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -13,7 +13,7 @@ pub use reflect::{ApiVersion, RpcMethod, RpcMethodExt};
 pub use request::Request;
 mod error;
 mod reflect;
-pub use reflect::openrpc_types::OpenRPC;
+pub use reflect::openrpc_types;
 pub mod types;
 pub use methods::*;
 use reflect::Permission;
@@ -250,7 +250,7 @@ where
     Ok(())
 }
 
-pub fn create_module<DB>(
+fn create_module<DB>(
     state: Arc<RPCState<DB>>,
 ) -> (RpcModule<RPCState<DB>>, reflect::openrpc_types::OpenRPC)
 where
--- src/tool/subcommands/shed_cmd.rs
+++ src/tool/subcommands/shed_cmd.rs
@@ -16,7 +16,6 @@ use anyhow::Context as _;
 use base64::{prelude::BASE64_STANDARD, Engine};
 use clap::{Parser, Subcommand};
 use futures::{StreamExt as _, TryFutureExt as _, TryStreamExt as _};
-use serde::Deserialize as _;
 
 #[derive(Subcommand)]
 pub enum ShedCommands {
@@ -64,8 +63,8 @@ pub enum Rpc {
 
 #[derive(Parser)]
 pub enum Dump {
-    OpenRpc,
-    JsonSchema,
+    // OpenRpc,
+    JsonSchemaDefinitions,
 }
 
 impl ShedCommands {
@@ -131,77 +130,39 @@ impl ShedCommands {
                 }
             }
             ShedCommands::Rpc(Rpc::Dump(what)) => {
-                // Creating an RPCState<...> here would required bringing in a bunch of `#[cfg(test)]` code,
-                // so just pull from the snapshot
-                let deserializer = serde_yaml::Deserializer::from_str(include_str!(concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/src/rpc/snapshots/forest_filecoin__rpc__tests__openrpc.snap"
-                )))
-                .nth(1)
-                .expect("yaml snapshot contains two documents");
-                let mut openrpc = crate::rpc::OpenRPC::deserialize(deserializer)?;
+                use crate::rpc::RpcMethodExt as _;
+                let mut gen =
+                    schemars::gen::SchemaGenerator::new(schemars::gen::SchemaSettings::draft07());
+                let mut methods = vec![];
+                macro_rules! register {
+                    ($ty:ty) => {
+                        let method = <$ty>::openrpc(
+                            &mut gen,
+                            crate::rpc::openrpc_types::ParamStructure::ByPosition,
+                        )
+                        .unwrap();
+                        methods.push(method);
+                    };
+                }
+                crate::rpc::auth::for_each_method!(register);
+                crate::rpc::beacon::for_each_method!(register);
+                crate::rpc::chain::for_each_method!(register);
+                crate::rpc::common::for_each_method!(register);
+                crate::rpc::gas::for_each_method!(register);
+                crate::rpc::mpool::for_each_method!(register);
+                crate::rpc::net::for_each_method!(register);
+                crate::rpc::state::for_each_method!(register);
+                crate::rpc::node::for_each_method!(register);
+                crate::rpc::sync::for_each_method!(register);
+                crate::rpc::wallet::for_each_method!(register);
+                crate::rpc::eth::for_each_method!(register);
                 match what {
-                    Dump::OpenRpc => serde_json::to_writer_pretty(io::stdout(), &openrpc)?,
-                    Dump::JsonSchema => {}
+                    Dump::JsonSchemaDefinitions => {
+                        serde_json::to_writer_pretty(io::stdout(), gen.definitions())?;
+                    }
                 }
             }
         }
         Ok(())
-    }
-}
-
-fn rewrite_links(schema: &mut schemars::schema::Schema) {
-    use schemars::schema::{
-        ArrayValidation, ObjectValidation, Schema, SchemaObject, SubschemaValidation,
-    };
-    match schema {
-        Schema::Bool(_) => {}
-        Schema::Object(SchemaObject {
-            metadata: _,
-            instance_type: _,
-            format: _,
-            enum_values: _,
-            const_value: _,
-            subschemas,
-            number: _,
-            string: _,
-            array,
-            object,
-            reference,
-            extensions: _,
-        }) => {
-            if let Some(SubschemaValidation {
-                all_of,
-                any_of,
-                one_of,
-                not,
-                if_schema,
-                then_schema,
-                else_schema,
-            }) = subschemas.as_deref_mut()
-            {
-                for it in all_of
-                    .iter_mut()
-                    .chain(any_of)
-                    .chain(one_of)
-                    .flatten()
-                    .chain(not.as_deref_mut())
-                    .chain(if_schema.as_deref_mut())
-                    .chain(then_schema.as_deref_mut())
-                    .chain(else_schema.as_deref_mut())
-                {
-                    rewrite_links(it)
-                }
-            }
-            if let Some(ArrayValidation {
-                items,
-                additional_items,
-                max_items,
-                min_items,
-                unique_items,
-                contains,
-            }) = array.as_deref_mut()
-            {}
-        }
     }
 }
