caf546d43d4bf28336ca2a61e0b430d586533663
hanabi1224 <harlowmoo@gmail.com>
2024-06-03 16:26:12 +0800

    fix(p2p): max concurrent streams in request_response protocol (#4367)

--- src/libp2p/chain_exchange/behaviour.rs
+++ src/libp2p/chain_exchange/behaviour.rs
@@ -25,6 +25,16 @@ pub struct ChainExchangeBehaviour {
 }
 
 impl ChainExchangeBehaviour {
+    pub fn new(cfg: request_response::Config) -> Self {
+        Self {
+            inner: InnerBehaviour::new(
+                [(CHAIN_EXCHANGE_PROTOCOL_NAME, ProtocolSupport::Full)],
+                cfg,
+            ),
+            response_channels: Default::default(),
+        }
+    }
+
     pub fn send_request(
         &mut self,
         peer: &PeerId,
@@ -78,18 +88,6 @@ impl ChainExchangeBehaviour {
         metrics::NETWORK_CONTAINER_CAPACITIES
             .get_or_create(&metrics::values::CHAIN_EXCHANGE_REQUEST_TABLE)
             .set(self.response_channels.capacity() as _);
-    }
-}
-
-impl Default for ChainExchangeBehaviour {
-    fn default() -> Self {
-        Self {
-            inner: InnerBehaviour::new(
-                [(CHAIN_EXCHANGE_PROTOCOL_NAME, ProtocolSupport::Full)],
-                Default::default(),
-            ),
-            response_channels: Default::default(),
-        }
     }
 }
 
