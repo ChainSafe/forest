8d3bc7ea01b8592d9afcf66f4b004cffbad5fb27
Hubert Bugaj <lesny.rumcajs@gmail.com>
2024-07-11 13:14:39 +0200

    feat: waffle logos

--- src/state_migration/nv23/migration.rs
+++ src/state_migration/nv23/migration.rs
@@ -13,6 +13,7 @@ use crate::shim::{
     state_tree::{StateTree, StateTreeVersion},
 };
 use crate::utils::db::CborStoreExt as _;
+use crate::utils::misc::reveal_waffle_upgrade;
 use anyhow::Context;
 use cid::Cid;
 
@@ -90,6 +91,8 @@ where
     let actors_in = StateTree::new_from_root(blockstore.clone(), state)?;
     let actors_out = StateTree::new(blockstore.clone(), StateTreeVersion::V5)?;
     let new_state = migration.migrate_state_tree(blockstore, epoch, actors_in, actors_out)?;
+
+    reveal_waffle_upgrade();
 
     Ok(new_state)
 }
