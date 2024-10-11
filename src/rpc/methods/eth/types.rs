04a2090b006a17506bc88fd75ab39558194c2667
elmattic <elmattic@users.noreply.github.com>
2024-10-11 10:19:30 +0200

    Rename Int64 and Uint64 with an Eth prefix

--- src/rpc/methods/eth/types.rs
+++ src/rpc/methods/eth/types.rs
@@ -222,7 +222,7 @@ impl TryFrom<EthAddress> for FilecoinAddress {
 pub enum BlockNumberOrPredefined {
     #[schemars(with = "String")]
     PredefinedBlock(Predefined),
-    BlockNumber(Int64),
+    BlockNumber(EthInt64),
 }
 lotus_json_with_self!(BlockNumberOrPredefined);
 
@@ -238,7 +238,7 @@ impl From<BlockNumberOrPredefined> for BlockNumberOrHash {
 #[derive(PartialEq, Debug, Clone, Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct EthFeeHistoryResult {
-    pub oldest_block: Uint64,
+    pub oldest_block: EthUint64,
     pub base_fee_per_gas: Vec<EthBigInt>,
     pub gas_used_ratio: Vec<f64>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -257,7 +257,7 @@ pub struct GasReward {
 pub struct EthCallMessage {
     pub from: Option<EthAddress>,
     pub to: Option<EthAddress>,
-    pub gas: Uint64,
+    pub gas: EthUint64,
     pub gas_price: EthBigInt,
     pub value: EthBigInt,
     pub data: EthBytes,
