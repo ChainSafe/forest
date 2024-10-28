ea011d7a30d17d27c9c2ca40fbd0e0898d0a8212
hanabi1224 <harlowmoo@gmail.com>
2024-10-28 21:41:39 +0800

    replace fancy_duration with humantime

--- src/lotus_json/duration.rs
+++ src/lotus_json/duration.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use super::*;
-use fancy_duration::FancyDuration;
 use std::time::Duration;
 
 impl HasLotusJson for Duration {
@@ -19,22 +18,5 @@ impl HasLotusJson for Duration {
 
     fn from_lotus_json(lotus_json: Self::LotusJson) -> Self {
         Self::from_nanos(lotus_json)
-    }
-}
-
-impl HasLotusJson for FancyDuration<Duration> {
-    type LotusJson = u64;
-
-    #[cfg(test)]
-    fn snapshots() -> Vec<(serde_json::Value, Self)> {
-        vec![(json!(15000000000_u64), Self(Duration::from_secs(15)))]
-    }
-
-    fn into_lotus_json(self) -> Self::LotusJson {
-        self.0.as_nanos() as _
-    }
-
-    fn from_lotus_json(lotus_json: Self::LotusJson) -> Self {
-        Self(Duration::from_nanos(lotus_json))
     }
 }
