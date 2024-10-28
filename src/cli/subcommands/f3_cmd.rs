e7cd520cf1a1d7cc8f77c1369dc4fa34f554b7fe
hanabi1224 <harlowmoo@gmail.com>
2024-10-28 11:28:45 +0800

    more tests

--- src/cli/subcommands/f3_cmd.rs
+++ src/cli/subcommands/f3_cmd.rs
@@ -65,3 +65,62 @@ impl ManifestTemplate {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_manifest_template() {
+        // lotus f3 manifest --output json
+        let lotus_json = serde_json::json!({
+          "Pause": false,
+          "ProtocolVersion": 4,
+          "InitialInstance": 0,
+          "BootstrapEpoch": 2081674,
+          "NetworkName": "calibrationnet",
+          "ExplicitPower": null,
+          "IgnoreECPower": false,
+          "InitialPowerTable": {
+            "/": "bafy2bzaceab236vmmb3n4q4tkvua2n4dphcbzzxerxuey3mot4g3cov5j3r2c"
+          },
+          "CommitteeLookback": 10,
+          "CatchUpAlignment": 15000000000_u64,
+          "Gpbft": {
+            "Delta": 6000000000_u64,
+            "DeltaBackOffExponent": 2_f64,
+            "MaxLookaheadRounds": 5,
+            "RebroadcastBackoffBase": 6000000000_u64,
+            "RebroadcastBackoffExponent": 1.3,
+            "RebroadcastBackoffSpread": 0.1,
+            "RebroadcastBackoffMax": 60000000000_u64
+          },
+          "EC": {
+            "Period": 30000000000_u64,
+            "Finality": 900,
+            "DelayMultiplier": 2_f64,
+            "BaseDecisionBackoffTable": [
+              1.3,
+              1.69,
+              2.2,
+              2.86,
+              3.71,
+              4.83,
+              6.27,
+              7.5
+            ],
+            "HeadLookback": 0,
+            "Finalize": true
+          },
+          "CertificateExchange": {
+            "ClientRequestTimeout": 10000000000_u64,
+            "ServerRequestTimeout": 60000000000_u64,
+            "MinimumPollInterval": 30000000000_u64,
+            "MaximumPollInterval": 120000000000_u64
+          }
+        });
+        let manifest: F3Manifest = serde_json::from_value(lotus_json.clone()).unwrap();
+        let template = ManifestTemplate::new(manifest);
+        println!("{}", template.render_once().unwrap());
+    }
+}
