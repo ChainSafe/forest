99ad649d02bd2d3f36e5cb10c6780aae3984f106
lemmih <david.himmelstrup@chainsafe.io>
2024-12-12 13:56:14 +0100

    get rid of even more code

--- src/libp2p/metrics.rs
+++ src/libp2p/metrics.rs
@@ -1,12 +1,8 @@
 // Copyright 2019-2024 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use libp2p::PeerId;
 use once_cell::sync::Lazy;
-use prometheus_client::{
-    encoding::{EncodeLabelKey, EncodeLabelSet, EncodeLabelValue, LabelSetEncoder},
-    metrics::{counter::Counter, gauge::Gauge},
-};
+use prometheus_client::metrics::{counter::Counter, gauge::Gauge};
 
 pub static PEER_FAILURE_TOTAL: Lazy<Counter> = Lazy::new(|| {
     let metric = Counter::default();
@@ -37,23 +33,3 @@ pub static BAD_PEERS: Lazy<Gauge> = Lazy::new(|| {
     );
     metric
 });
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-pub struct PeerLabel(PeerId);
-
-impl PeerLabel {
-    pub const fn new(peer: PeerId) -> Self {
-        Self(peer)
-    }
-}
-
-impl EncodeLabelSet for PeerLabel {
-    fn encode(&self, mut encoder: LabelSetEncoder) -> Result<(), std::fmt::Error> {
-        let mut label_encoder = encoder.encode_label();
-        let mut label_key_encoder = label_encoder.encode_label_key()?;
-        EncodeLabelKey::encode(&"PEER", &mut label_key_encoder)?;
-        let mut label_value_encoder = label_key_encoder.encode_label_value()?;
-        EncodeLabelValue::encode(&self.0.to_string(), &mut label_value_encoder)?;
-        label_value_encoder.finish()
-    }
-}
