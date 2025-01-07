ead0c547e0a853fff34d08733ea2b5164af5fd95
elmattic <elmattic@users.noreply.github.com>
2025-01-07 11:19:28 +0100

    Update license to 2025

--- src/tool/subcommands/net_cmd.rs
+++ src/tool/subcommands/net_cmd.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2024 ChainSafe Systems
+// Copyright 2019-2025 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use crate::libp2p::ping::p2p_ping;
--- src/utils/io/progress_log.rs
+++ src/utils/io/progress_log.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2024 ChainSafe Systems
+// Copyright 2019-2025 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 //! It can often take time to perform some operations in Forest and we would like to have a way for logging progress.
