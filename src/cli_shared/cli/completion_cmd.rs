2c0d868a62011cf69ec14b34d339cc5cc06fdf33
Aryan Tikarya <akaladarshi@gmail.com>
2025-02-26 20:34:44 +0530

    address comments

--- src/cli_shared/cli/completion_cmd.rs
+++ src/cli_shared/cli/completion_cmd.rs
@@ -1,8 +1,9 @@
 // Copyright 2019-2025 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
-use crate::cli::subcommands::Cli as CliSubCommand;
-use crate::tool::subcommands::Cli as ToolSubCommand;
-use crate::wallet::subcommands::Cli as WalletSubCommand;
+use crate::cli::subcommands::Cli as ForestCliSubCommand;
+use crate::tool::subcommands::Cli as ForestToolSubCommand;
+use crate::wallet::subcommands::Cli as ForestWalletSubCommand;
+use crate::daemon::main::Cli as ForestDaemonSubCommand;
 use ahash::HashMap;
 use clap::{Command, CommandFactory};
 use clap_complete::aot::{generate, Shell};
@@ -14,7 +15,7 @@ pub struct CompletionCommand {
     /// If omitted, completions for all known binaries will be generated.
     #[arg(value_delimiter = ',')]
     binaries: Option<Vec<String>>,
-    /// The Shell type to generate completions
+    /// The Shell type to generate completions for
     #[arg(long, default_value = "bash")]
     shell: Shell,
 }
@@ -22,17 +23,26 @@ pub struct CompletionCommand {
 impl CompletionCommand {
     pub fn run(self) -> anyhow::Result<()> {
         let mut bin_cmd_map: HashMap<String, Command> = HashMap::from_iter([
-            ("forest-cli".to_string(), CliSubCommand::command()),
-            ("forest-wallet".to_string(), WalletSubCommand::command()),
-            ("forest-tool".to_string(), ToolSubCommand::command()),
+            ("forest".to_string(), ForestDaemonSubCommand::command()),
+            ("forest-cli".to_string(), ForestCliSubCommand::command()),
+            ("forest-wallet".to_string(), ForestWalletSubCommand::command()),
+            ("forest-tool".to_string(), ForestToolSubCommand::command()),
         ]);
 
+        let valid_binaries = bin_cmd_map.keys().cloned().collect::<Vec<_>>();
         let binaries = self
             .binaries
-            .unwrap_or_else(|| bin_cmd_map.keys().cloned().collect());
+            .unwrap_or_else(|| valid_binaries.clone());
 
         for b in binaries {
-            let cmd = bin_cmd_map.get_mut(&b).unwrap();
+            let cmd = bin_cmd_map.get_mut(&b).ok_or_else(|| {
+                anyhow::anyhow!(
+                    "Unknown binary: '{}'. Valid binaries are: {:?}",
+                    b,
+                    valid_binaries.join(",")
+                )
+            })?;
+
             generate(
                 self.shell,
                 cmd,
