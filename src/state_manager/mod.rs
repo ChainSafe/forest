8b04076cdd0d921505e881778e760edf4b8d064d
elmattic <elmattic@users.noreply.github.com>
2025-02-27 17:34:20 +0100

    Add code to get events root cid

--- src/state_manager/mod.rs
+++ src/state_manager/mod.rs
@@ -49,6 +49,7 @@ use crate::shim::{
 };
 use crate::state_manager::chain_rand::draw_randomness;
 use crate::state_migration::run_state_migrations;
+use crate::utils::misc::env::is_env_set_and_truthy;
 use ahash::{HashMap, HashMapExt};
 use anyhow::{bail, Context as _};
 use bls_signatures::{PublicKey as BlsPublicKey, Serialize as _};
@@ -83,6 +84,8 @@ const DEFAULT_TIPSET_CACHE_SIZE: NonZeroUsize = nonzero!(1024usize);
 
 const DEFAULT_EVENT_CACHE_SIZE: NonZeroUsize = nonzero!(4096usize);
 
+const FOREST_CHAIN_INDEXER_ENABLED: &str = "FOREST_CHAIN_INDEXER_ENABLED";
+
 /// Intermediary for retrieving state objects and updating actor states.
 type CidPair = (Cid, Cid);
 
@@ -91,6 +94,7 @@ pub struct StateOutput {
     pub state_root: Cid,
     pub receipt_root: Cid,
     pub events: Vec<Vec<StampedEvent>>,
+    pub events_roots: Vec<Cid>,
 }
 
 #[derive(Clone)]
@@ -105,6 +109,7 @@ impl From<StateOutputValue> for StateOutput {
             state_root: value.state_root,
             receipt_root: value.receipt_root,
             events: vec![],
+            events_roots: vec![],
         }
     }
 }
@@ -488,18 +493,26 @@ where
         self: &Arc<Self>,
         tipset: &Arc<Tipset>,
     ) -> anyhow::Result<StateOutput> {
+        let vm_event = if is_env_set_and_truthy(FOREST_CHAIN_INDEXER_ENABLED).unwrap_or(false) {
+            VMEvent::PushedEventsRoot
+        } else {
+            VMEvent::NotPushed
+        };
         let key = tipset.key();
         self.cache
             .get_or_else(key, || async move {
-                let ts_state = self
+                let state_output = self
                     .compute_tipset_state(
                         Arc::clone(tipset),
                         NO_CALLBACK,
                         VMTrace::NotTraced,
-                        VMEvent::NotPushed,
+                        vm_event,
                     )
-                    .await?
-                    .into();
+                    .await?;
+                for events_root in state_output.events_roots.iter() {
+                    trace!("Indexing events root: {}", events_root);
+                }
+                let ts_state = state_output.into();
                 trace!("Completed tipset state calculation {:?}", tipset.cids());
                 Ok(ts_state)
             })
@@ -1765,6 +1778,7 @@ where
             state_root: *tipset.parent_state(),
             receipt_root: message_receipts,
             events: vec![],
+            events_roots: vec![],
         });
     }
 
@@ -1837,7 +1851,7 @@ where
         let mut vm = create_vm(parent_state, epoch, tipset.min_timestamp())?;
 
         // step 4: apply tipset messages
-        let (receipts, events) =
+        let (receipts, events, events_roots) =
             vm.apply_block_messages(&block_messages, epoch, callback, enable_event_pushing)?;
 
         // step 5: construct receipt root from receipts and flush the state-tree
@@ -1848,6 +1862,7 @@ where
             state_root,
             receipt_root,
             events,
+            events_roots,
         })
     })
 }
