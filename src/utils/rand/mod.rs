b75ea810d6420fcdd8205e283374140dc29fc25a
hanabi1224 <harlowmoo@gmail.com>
2025-03-05 18:45:53 +0800

    tests

--- src/utils/rand/mod.rs
+++ src/utils/rand/mod.rs
@@ -17,21 +17,21 @@ pub fn forest_os_rng() -> impl Rng + CryptoRng {
     forest_rng_internal(ForestRngMode::OsRng)
 }
 
+const FIXED_RNG_SEED_ENV: &str = "FOREST_TEST_RNG_FIXED_SEED";
+
 enum ForestRngMode {
     ThreadRng,
     OsRng,
 }
 
 fn forest_rng_internal(mode: ForestRngMode) -> impl Rng + CryptoRng {
-    const ENV_KEY: &str = "FOREST_TEST_RNG_FIXED_SEED";
-    if let Ok(v) = std::env::var(ENV_KEY) {
+    const ENV: &str = FIXED_RNG_SEED_ENV;
+    if let Ok(v) = std::env::var(ENV) {
         if let Ok(seed) = v.parse() {
-            tracing::warn!("[security] using test RNG with fixed seed {seed} set by {ENV_KEY}");
+            tracing::warn!("[security] using test RNG with fixed seed {seed} set by {ENV}");
             return Either::Left(rand_chacha::ChaChaRng::seed_from_u64(seed));
         } else {
-            tracing::warn!(
-                "invalid u64 seed set by {ENV_KEY}: {v}. Falling back to the default RNG."
-            );
+            tracing::warn!("invalid u64 seed set by {ENV}: {v}. Falling back to the default RNG.");
         }
     }
     match mode {
@@ -84,4 +84,51 @@ where
     A: RngCore,
     B: RngCore,
 {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serial_test::serial;
+
+    #[test]
+    #[serial]
+    fn test_fixed_seed_env() {
+        std::env::set_var(FIXED_RNG_SEED_ENV, "0");
+
+        let mut a = [0; 1024];
+        let mut b = [0; 1024];
+
+        forest_rng().fill(&mut a);
+        forest_rng().fill(&mut b);
+        assert_eq!(a, b);
+
+        forest_os_rng().fill(&mut a);
+        forest_os_rng().fill(&mut b);
+        assert_eq!(a, b);
+
+        std::env::remove_var(FIXED_RNG_SEED_ENV);
+    }
+
+    #[test]
+    #[serial]
+    fn test_thread_rng() {
+        std::env::remove_var(FIXED_RNG_SEED_ENV);
+        let mut a = [0; 1024];
+        forest_rng().fill(&mut a);
+        let mut b = [0; 1024];
+        forest_rng().fill(&mut b);
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    #[serial]
+    fn test_os_rng() {
+        std::env::remove_var(FIXED_RNG_SEED_ENV);
+        let mut a = [0; 1024];
+        forest_os_rng().fill(&mut a);
+        let mut b = [0; 1024];
+        forest_os_rng().fill(&mut b);
+        assert_ne!(a, b);
+    }
 }
