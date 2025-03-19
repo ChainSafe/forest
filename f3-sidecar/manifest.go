729f2d78afa620a6354477c697ce639b43d2be87
hanabi1224 <harlowmoo@gmail.com>
2025-03-19 17:51:07 +0800

    chore(f3): upgrade go-f3 to 0.8.3 (#5396)

--- f3-sidecar/manifest.go
+++ f3-sidecar/manifest.go
@@ -11,24 +11,22 @@ import (
 type ContractManifestProvider struct {
 	started                 *bool
 	pollInterval            time.Duration
-	initialManifest         *manifest.Manifest
 	currentManifest         *manifest.Manifest
 	staticInitialPowerTable cid.Cid
 	f3Api                   *F3Api
 	ch                      chan *manifest.Manifest
 }
 
-func NewContractManifestProvider(initialValue *manifest.Manifest, contract_manifest_poll_interval_seconds uint64, f3Api *F3Api) (*ContractManifestProvider, error) {
+func NewContractManifestProvider(staticManifest *manifest.Manifest, contract_manifest_poll_interval_seconds uint64, f3Api *F3Api) (*ContractManifestProvider, error) {
 	started := false
 	pollInterval := time.Duration(contract_manifest_poll_interval_seconds) * time.Second
 	var staticInitialPowerTable cid.Cid = cid.Undef
-	if initialValue != nil && isCidDefined(initialValue.InitialPowerTable) {
-		staticInitialPowerTable = initialValue.InitialPowerTable
+	if staticManifest != nil && isCidDefined(staticManifest.InitialPowerTable) {
+		staticInitialPowerTable = staticManifest.InitialPowerTable
 	}
 	p := ContractManifestProvider{
 		started:                 &started,
 		pollInterval:            pollInterval,
-		initialManifest:         initialValue,
 		currentManifest:         nil,
 		staticInitialPowerTable: staticInitialPowerTable,
 		f3Api:                   f3Api,
@@ -57,17 +55,12 @@ func (p *ContractManifestProvider) Start(ctx context.Context) error {
 	p.started = &started
 	go func() {
 		for started && ctx.Err() == nil {
-			if p.currentManifest == nil && p.initialManifest != nil {
-				p.Update(p.initialManifest)
-				p.initialManifest = nil
-			}
 			logger.Debugf("Polling manifest from contract...\n")
-			m, err := p.f3Api.GetManifestFromContract(ctx)
-			if m != nil && !isCidDefined(m.InitialPowerTable) {
-				m.InitialPowerTable = p.staticInitialPowerTable
-			}
-			if err == nil {
+			if m, err := p.f3Api.GetManifestFromContract(ctx); err == nil {
 				if m != nil {
+					if !isCidDefined(m.InitialPowerTable) {
+						m.InitialPowerTable = p.staticInitialPowerTable
+					}
 					if !m.Equal(p.currentManifest) {
 						logger.Infoln("Successfully polled manifest from contract, updating...")
 						p.Update(m)
@@ -75,6 +68,7 @@ func (p *ContractManifestProvider) Start(ctx context.Context) error {
 						logger.Infoln("Successfully polled unchanged manifest from contract")
 					}
 				}
+
 			} else {
 				logger.Warnf("failed to get manifest from contract: %s\n", err)
 			}
