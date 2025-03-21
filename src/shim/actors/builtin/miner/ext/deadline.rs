8aa56c5939e1ed821d6a2422aa69192df014a836
hanabi1224 <harlowmoo@gmail.com>
2025-03-21 19:11:33 +0800

    fix(rpc): update StateMinerDeadlines response to include daily_fee (#5435)

---
+++ src/shim/actors/builtin/miner/ext/deadline.rs
@@ -0,0 +1,22 @@
+// Copyright 2019-2025 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use super::*;
+use num::Zero;
+
+impl DeadlineExt for Deadline {
+    fn daily_fee(&self) -> TokenAmount {
+        use Deadline::*;
+        match self {
+            V8(_) => Zero::zero(),
+            V9(_) => Zero::zero(),
+            V10(_) => Zero::zero(),
+            V11(_) => Zero::zero(),
+            V12(_) => Zero::zero(),
+            V13(_) => Zero::zero(),
+            V14(_) => Zero::zero(),
+            V15(_) => Zero::zero(),
+            V16(d) => (&d.daily_fee).into(),
+        }
+    }
+}
