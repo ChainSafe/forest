0785abf654428dcf431ca6c66a7856f7fe48c104
hanabi1224 <harlowmoo@gmail.com>
2025-05-16 18:05:06 +0800

    chore(deps): bump go deps

--- src/libp2p_bitswap/message.rs
+++ src/libp2p_bitswap/message.rs
@@ -92,7 +92,10 @@ pub enum BitswapMessage {
 
 impl BitswapMessage {
     pub fn into_proto(self) -> IOResult<bitswap_pb::Message> {
-        let mut msg = bitswap_pb::Message::default();
+        let mut msg = bitswap_pb::Message {
+            wantlist: Some(Default::default()),
+            ..Default::default()
+        };
         match self {
             Self::Request(BitswapRequest {
                 ty,
