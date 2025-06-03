9f8d77e40803348aa827a1b76fbc46705dd38f1e
Aryan Tikarya <akaladarshi@gmail.com>
2025-06-03 15:03:36 +0530

    fix the comment in pop empty array err contract

--- src/tool/subcommands/api_cmd/contracts/pop_empty_array_err/pop_empty_array_err.sol
+++ src/tool/subcommands/api_cmd/contracts/pop_empty_array_err/pop_empty_array_err.sol
@@ -4,6 +4,6 @@ pragma solidity >=0.8.17;
 contract PopEmptyArrayFail {
     uint256[] public myArray;
     constructor() {
-        myArray.pop(); // Triggers panic code 0x22
+        myArray.pop(); // Triggers panic code 0x31
     }
 }
