31cd36731be66900e33c07542c41247bf5625ec8
hanabi1224 <harlowmoo@gmail.com>
2025-06-11 17:53:27 +0800

    fix

--- src/cli/subcommands/chain_cmd/list.rs
+++ src/cli/subcommands/chain_cmd/list.rs
@@ -10,7 +10,10 @@ use num::{BigInt, Zero as _};
 use crate::{
     rpc::{
         self, RpcMethodExt as _,
-        chain::{ChainGetBlockMessages, ChainGetTipSet, ChainGetTipSetByHeight, ChainHead},
+        chain::{
+            ChainGetBlockMessages, ChainGetParentMessages, ChainGetParentReceipts, ChainGetTipSet,
+            ChainGetTipSetByHeight, ChainHead,
+        },
     },
     shim::econ::{BLOCK_GAS_LIMIT, TokenAmount},
 };
@@ -44,8 +47,9 @@ impl ChainListCommand {
         } {
             ts = ChainGetTipSet::call(&client, (ts.parents().into(),)).await?;
         }
+        tipsets.reverse();
 
-        for ts in tipsets.iter().rev() {
+        for (i, ts) in tipsets.iter().enumerate() {
             if self.gas_stats {
                 let base_fee = &ts.block_headers().first().parent_base_fee;
                 let max_fee = base_fee * BLOCK_GAS_LIMIT;
@@ -77,6 +81,31 @@ impl ChainListCommand {
                         "\t{miner}: \t{len} msgs, gasLimit: {limit_sum} / {BLOCK_GAS_LIMIT} ({ratio:.2}), avgPremium: {premium_avg}",
                         miner = b.miner_address,
                         ratio = (limit_sum as f64) / (BLOCK_GAS_LIMIT as f64) * 100.0
+                    );
+                }
+                if i + 1 < tipsets.len() {
+                    #[allow(clippy::indexing_slicing)]
+                    let child_ts = &tipsets[i + 1];
+                    let msgs = ChainGetParentMessages::call(
+                        &client,
+                        (*child_ts.block_headers().first().cid(),),
+                    )
+                    .await?;
+                    let limit_sum: u64 = msgs.iter().map(|m| m.message.gas_limit).sum();
+                    let gas_used: u64 = {
+                        let receipts = ChainGetParentReceipts::call(
+                            &client,
+                            (*child_ts.block_headers().first().cid(),),
+                        )
+                        .await?;
+                        receipts.iter().map(|r| r.gas_used).sum()
+                    };
+                    let gas_efficiency = 100. * (gas_used as f64) / (limit_sum as f64);
+                    let gas_capacity = 100. * (limit_sum as f64) / (BLOCK_GAS_LIMIT as f64);
+
+                    println!(
+                        "\ttipset: \t{n} msgs, {gas_used} ({gas_efficiency:.2}%) / {limit_sum} ({gas_capacity:.2}%)",
+                        n = msgs.len()
                     );
                 }
             } else {
