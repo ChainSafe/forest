2e39ad06b79327fbdffd3da56212168bb865b676
Aryan Tikarya <akaladarshi@gmail.com>
2025-06-13 21:18:48 +0530

    add more versions for evm actor

--- src/rpc/registry/actors/evm.rs
+++ src/rpc/registry/actors/evm.rs
@@ -6,27 +6,24 @@ use crate::shim::message::MethodNum;
 use anyhow::Result;
 use cid::Cid;
 
+macro_rules! register_evm_version {
+    ($registry:expr, $code_cid:expr, $state_version:path) => {{
+        use $state_version::{ConstructorParams, Method};
+
+        register_actor_methods!(
+            $registry,
+            $code_cid,
+            [(Method::Constructor, ConstructorParams)]
+        );
+    }};
+}
+
 pub(crate) fn register_evm_actor_methods(registry: &mut MethodRegistry, cid: Cid) {
-    register_evm_v15_methods(registry, cid);
-    register_evm_v16_methods(registry, cid);
-}
-
-fn register_evm_v15_methods(registry: &mut MethodRegistry, code_cid: Cid) {
-    use fil_actor_evm_state::v15::{ConstructorParams, Method};
-
-    register_actor_methods!(
-        registry,
-        code_cid,
-        [(Method::Constructor, ConstructorParams)]
-    );
-}
-
-fn register_evm_v16_methods(registry: &mut MethodRegistry, code_cid: Cid) {
-    use fil_actor_evm_state::v16::{ConstructorParams, Method};
-
-    register_actor_methods!(
-        registry,
-        code_cid,
-        [(Method::Constructor, ConstructorParams)]
-    );
+    register_evm_version!(registry, cid, fil_actor_evm_state::v10);
+    register_evm_version!(registry, cid, fil_actor_evm_state::v11);
+    register_evm_version!(registry, cid, fil_actor_evm_state::v12);
+    register_evm_version!(registry, cid, fil_actor_evm_state::v13);
+    register_evm_version!(registry, cid, fil_actor_evm_state::v14);
+    register_evm_version!(registry, cid, fil_actor_evm_state::v15);
+    register_evm_version!(registry, cid, fil_actor_evm_state::v16);
 }
