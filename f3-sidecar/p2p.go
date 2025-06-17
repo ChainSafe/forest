60ecb96142e0179e65202b7858068340013bdfd0
hanabi1224 <harlowmoo@gmail.com>
2025-06-17 08:38:27 +0800

    chore: bump the pubsub validation queue length to 4096

--- f3-sidecar/p2p.go
+++ f3-sidecar/p2p.go
@@ -7,8 +7,10 @@ import (
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	pubsub_pb "github.com/libp2p/go-libp2p-pubsub/pb"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/protocol"
+	"golang.org/x/crypto/blake2b"
 )
 
 const ListenAddr = "/ip4/127.0.0.1/tcp/0"
@@ -51,10 +53,22 @@ func createP2PHost(ctx context.Context, networkName string) (*P2PHost, error) {
 	ps, err := pubsub.NewGossipSub(ctx, host,
 		pubsub.WithPeerExchange(true),
 		pubsub.WithFloodPublish(true),
+		pubsub.WithMessageIdFn(hashMsgId),
+		// Bump the validation queue to accommodate the increase in gossipsub message
+		// exchange rate as a result of f3. The size of 4096 should offer enough headroom
+		// for slower F3 validation while avoiding: 1) avoid excessive memory usage, 2)
+		// dropped consensus related messages and 3) cascading effect among other topics
+		// since this config isn't topic-specific.
+		pubsub.WithValidateQueueSize(4096),
 		pubsub.WithPeerScore(PubsubPeerScoreParams, PubsubPeerScoreThresholds))
 	if err != nil {
 		return nil, err
 	}
 
 	return &P2PHost{host, hostDHT, backupHostDHT, ps}, nil
+}
+
+func hashMsgId(m *pubsub_pb.Message) string {
+	hash := blake2b.Sum256(m.Data)
+	return string(hash[:])
 }
