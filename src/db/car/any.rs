e7b3fedf0eb7b7a8da30c249c9756a7ef9724fdb
hanabi1224 <harlowmoo@gmail.com>
2025-07-24 21:39:20 +0800

    fix: remove write_cache from ForestCar and PlainCar (#5860)

--- src/db/car/any.rs
+++ src/db/car/any.rs
@@ -10,7 +10,6 @@
 
 use super::{CacheKey, RandomAccessFileReader, ZstdFrameCache};
 use crate::blocks::{Tipset, TipsetKey};
-use crate::db::PersistentStore;
 use crate::utils::io::EitherMmapOrRandomAccessFile;
 use cid::Cid;
 use fvm_ipld_blockstore::Blockstore;
@@ -138,19 +137,6 @@ where
             AnyCar::Forest(forest) => forest.put_keyed(k, block),
             AnyCar::Plain(plain) => plain.put_keyed(k, block),
             AnyCar::Memory(mem) => mem.put_keyed(k, block),
-        }
-    }
-}
-
-impl<ReaderT> PersistentStore for AnyCar<ReaderT>
-where
-    ReaderT: ReadAt,
-{
-    fn put_keyed_persistent(&self, k: &Cid, block: &[u8]) -> anyhow::Result<()> {
-        match self {
-            AnyCar::Forest(forest) => forest.put_keyed_persistent(k, block),
-            AnyCar::Plain(plain) => plain.put_keyed_persistent(k, block),
-            AnyCar::Memory(mem) => mem.put_keyed_persistent(k, block),
         }
     }
 }
