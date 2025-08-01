f8f8d1cb1c7152dc4150a848f821a79f95919a2e
Aryan Tikarya <akaladarshi@gmail.com>
2025-08-01 20:46:36 +0530

    add remaining method and rename the macro

--- src/rpc/registry/actors/reward.rs
+++ src/rpc/registry/actors/reward.rs
@@ -27,7 +27,7 @@ macro_rules! register_reward_version_11_to_16 {
     }};
 }
 
-macro_rules! register_reward_version_8_to_9 {
+macro_rules! register_reward_version_8_to_10 {
     ($registry:expr, $code_cid:expr, $state_version:path, $fvm_shared_version:path) => {{
         use $state_version::{AwardBlockRewardParams, Method};
         use $fvm_shared_version::{bigint::bigint_ser::BigIntDe};
@@ -41,19 +41,22 @@ macro_rules! register_reward_version_8_to_9 {
                 (Method::UpdateNetworkKPI, Option<BigIntDe>),
             ]
         );
+
+        // Register methods without parameters
+        register_actor_methods!($registry, $code_cid, [(Method::ThisEpochReward, empty)]);
     }};
 }
 
 pub(crate) fn register_actor_methods(registry: &mut MethodRegistry, cid: Cid, version: u64) {
     match version {
         8 => {
-            register_reward_version_8_to_9!(registry, cid, fil_actor_reward_state::v8, fvm_shared2)
+            register_reward_version_8_to_10!(registry, cid, fil_actor_reward_state::v8, fvm_shared2)
         }
         9 => {
-            register_reward_version_8_to_9!(registry, cid, fil_actor_reward_state::v9, fvm_shared2)
+            register_reward_version_8_to_10!(registry, cid, fil_actor_reward_state::v9, fvm_shared2)
         }
         10 => {
-            register_reward_version_8_to_9!(registry, cid, fil_actor_reward_state::v10, fvm_shared3)
+            register_reward_version_8_to_10!(registry, cid, fil_actor_reward_state::v10, fvm_shared3)
         }
         11 => register_reward_version_11_to_16!(registry, cid, fil_actor_reward_state::v11),
         12 => register_reward_version_11_to_16!(registry, cid, fil_actor_reward_state::v12),
