1d210af7c24c15a52afc2d4216d7515994cdc7d1
Aryan Tikarya <akaladarshi@gmail.com>
2025-08-04 18:50:19 +0530

    feat: add support for multisig actor in state decode params API

--- src/rpc/registry/methods_reg.rs
+++ src/rpc/registry/methods_reg.rs
@@ -73,7 +73,9 @@ impl MethodRegistry {
     }
 
     fn register_known_methods(&mut self) {
-        use crate::rpc::registry::actors::{account, evm, init, miner, power, reward, system};
+        use crate::rpc::registry::actors::{
+            account, evm, init, miner, multisig, power, reward, system,
+        };
 
         for (&cid, &(actor_type, version)) in ACTOR_REGISTRY.iter() {
             match actor_type {
@@ -86,6 +88,7 @@ impl MethodRegistry {
                 BuiltinActor::System => system::register_actor_methods(self, cid, version),
                 BuiltinActor::Power => power::register_actor_methods(self, cid, version),
                 BuiltinActor::Reward => reward::register_actor_methods(self, cid, version),
+                BuiltinActor::Multisig => multisig::register_actor_methods(self, cid, version),
                 _ => {}
             }
         }
