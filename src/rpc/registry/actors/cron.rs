5faf793e59f457297acb9223d20cff3d7bd942b9
Shashank <shashank@chainsafe.io>
2025-08-06 16:05:23 +0530

    fix cron test

--- src/rpc/registry/actors/cron.rs
+++ src/rpc/registry/actors/cron.rs
@@ -5,28 +5,33 @@ use crate::rpc::registry::methods_reg::{MethodRegistry, register_actor_methods};
 use crate::shim::message::MethodNum;
 use anyhow::Result;
 use cid::Cid;
-use paste::paste;
 
-macro_rules! register_cron_versions_8_to_16 {
-    ($registry:expr, $code_cid:expr, $($version:literal),+) => {
-        $(
-            paste! {
-                {
-                    use fil_actor_cron_state::[<v $version>]::{ConstructorParams, Method};
+macro_rules! register_cron_version {
+    ($registry:expr, $code_cid:expr, $state_version:path) => {{
+        use $state_version::{ConstructorParams, Method};
 
-                    register_actor_methods!(
-                        $registry,
-                        $code_cid,
-                        [
-                            (Method::Constructor, ConstructorParams),
-                        ]
-                    );
-                }
-            }
-        )+
-    };
+        register_actor_methods!(
+            $registry,
+            $code_cid,
+            [(Method::Constructor, ConstructorParams),]
+        );
+
+        // Register methods with empty params
+        register_actor_methods!($registry, $code_cid, [(Method::EpochTick, empty),]);
+    }};
 }
 
-pub(crate) fn register_actor_methods(registry: &mut MethodRegistry, cid: Cid) {
-    register_cron_versions_8_to_16!(registry, cid, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+pub(crate) fn register_actor_methods(registry: &mut MethodRegistry, cid: Cid, version: u64) {
+    match version {
+        8 => register_cron_version!(registry, cid, fil_actor_cron_state::v8),
+        9 => register_cron_version!(registry, cid, fil_actor_cron_state::v9),
+        10 => register_cron_version!(registry, cid, fil_actor_cron_state::v10),
+        11 => register_cron_version!(registry, cid, fil_actor_cron_state::v11),
+        12 => register_cron_version!(registry, cid, fil_actor_cron_state::v12),
+        13 => register_cron_version!(registry, cid, fil_actor_cron_state::v13),
+        14 => register_cron_version!(registry, cid, fil_actor_cron_state::v14),
+        15 => register_cron_version!(registry, cid, fil_actor_cron_state::v15),
+        16 => register_cron_version!(registry, cid, fil_actor_cron_state::v16),
+        _ => {}
+    }
 }
