a82fdec8e0a35a80a662e558595e7c7b5312f664
hanabi1224 <harlowmoo@gmail.com>
2025-08-08 16:36:49 +0800

    resolve AI comments

--- interop-tests/tests/dhat_get_size_beacon_entry.rs
+++ interop-tests/tests/dhat_get_size_beacon_entry.rs
@@ -3,6 +3,7 @@
 
 use forest::interop_tests_private::beacon::BeaconEntry;
 use get_size2::GetSize;
+use std::mem::size_of;
 
 #[global_allocator]
 static ALLOC: dhat::Alloc = dhat::Alloc;
@@ -22,21 +23,33 @@ fn test_get_size_beacon_entry() {
         BeaconEntry::new(3, vec![0; 30]),
     ];
 
+    let inner_bytes = v.iter().map(GetSize::get_heap_size).sum::<usize>();
+
     let stats = dhat::HeapStats::get();
     assert!(v.capacity() >= v.len());
-    dhat::assert_eq!(stats.curr_bytes, 32 * v.capacity() + 60);
+    dhat::assert_eq!(
+        stats.curr_bytes,
+        size_of::<BeaconEntry>() * v.capacity() + inner_bytes
+    );
     dhat::assert_eq!(stats.curr_bytes, v.get_heap_size());
 
     v.reserve_exact(100);
 
     let stats = dhat::HeapStats::get();
     assert!(v.capacity() >= 100 + v.len());
-    dhat::assert_eq!(stats.curr_bytes, 32 * v.capacity() + 60);
+    dhat::assert_eq!(
+        stats.curr_bytes,
+        size_of::<BeaconEntry>() * v.capacity() + inner_bytes
+    );
     dhat::assert_eq!(stats.curr_bytes, v.get_heap_size());
 
     v.shrink_to_fit();
     let stats = dhat::HeapStats::get();
+    // `dhat::Alloc` works fine with `shrink_to_fit`
     assert_eq!(v.capacity(), v.len());
-    dhat::assert_eq!(stats.curr_bytes, 32 * v.capacity() + 60);
+    dhat::assert_eq!(
+        stats.curr_bytes,
+        size_of::<BeaconEntry>() * v.capacity() + 60
+    );
     dhat::assert_eq!(stats.curr_bytes, v.get_heap_size());
 }
