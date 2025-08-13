08828a1c3f6fe1cf12b2a9f30fa242a4976295a3
elmattic <elmattic@users.noreply.github.com>
2025-08-13 18:12:21 +0200

    Add market module to registry

---
+++ src/lotus_json/actor_states/methods/market_actor.rs
@@ -0,0 +1,58 @@
+// Copyright 2019-2025 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use super::*;
+use crate::shim::address::Address;
+use crate::shim::econ::TokenAmount;
+use ::cid::Cid;
+use fvm_ipld_encoding::RawBytes;
+use jsonrpsee::core::Serialize;
+use paste::paste;
+use schemars::JsonSchema;
+use serde::Deserialize;
+use std::fmt::Debug;
+
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct WithdrawBalanceParamsLotusJson {
+    #[schemars(with = "LotusJson<Address>")]
+    #[serde(with = "crate::lotus_json")]
+    pub provider_or_client: Address,
+    #[schemars(with = "LotusJson<TokenAmount>")]
+    #[serde(with = "crate::lotus_json")]
+    pub amount: TokenAmount,
+}
+
+macro_rules! impl_lotus_json_for_withdraw_balance_params {
+    ($($version:literal),+) => {
+        $(
+            paste! {
+                impl HasLotusJson for fil_actor_market_state::[<v $version>]::WithdrawBalanceParams {
+                    type LotusJson = WithdrawBalanceParamsLotusJson;
+
+                    #[cfg(test)]
+                    fn snapshots() -> Vec<(serde_json::Value, Self)> {
+                        vec![
+                        ]
+                    }
+
+                    fn into_lotus_json(self) -> Self::LotusJson {
+                        Self::LotusJson {
+                            provider_or_client: self.provider_or_client.into(),
+                            amount: self.amount.into(),
+                        }
+                    }
+
+                    fn from_lotus_json(json: Self::LotusJson) -> Self {
+                        Self {
+                            provider_or_client: json.provider_or_client.into(),
+                            amount: json.amount.into(),
+                        }
+                    }
+                }
+            }
+        )+
+    };
+}
+
+impl_lotus_json_for_withdraw_balance_params!(9, 10, 11, 12, 13, 14, 15, 16);
--- src/rpc/registry/actors/market.rs
+++ src/rpc/registry/actors/market.rs
@@ -6,34 +6,69 @@ use crate::shim::message::MethodNum;
 use anyhow::Result;
 use cid::Cid;
 
-// Macro for versions 8-10 that only have Exec method
-macro_rules! register_market_versions_8_to_10 {
+macro_rules! register_market_versions_8_to_9 {
     ($registry:expr, $code_cid:expr, $state_version:path) => {{
-        use $state_version::{ConstructorParams, ExecParams, Method};
+        use $state_version::{self, Method};
 
         register_actor_methods!(
             $registry,
             $code_cid,
             [
-                (Method::Constructor, ConstructorParams),
-                (Method::Exec, ExecParams),
+                (Method::Constructor, empty),
+                // (Method::AddBalance, AddBalanceParams),
+                // (Method::WithdrawBalance, WithdrawBalanceParams),
+                // (Method::PublishStorageDeals, PublishStorageDealsParams),
             ]
         );
     }};
 }
 
-// Macro for versions 11-16 that have Exec4
-macro_rules! register_market_versions_11_to_16 {
+macro_rules! register_market_versions_10_to_11 {
     ($registry:expr, $code_cid:expr, $state_version:path) => {{
-        use $state_version::{ConstructorParams, Exec4Params, ExecParams, Method};
+        use $state_version::{self, Method, PublishStorageDealsParams, WithdrawBalanceParams};
 
         register_actor_methods!(
             $registry,
             $code_cid,
             [
-                (Method::Constructor, ConstructorParams),
-                (Method::Exec, ExecParams),
-                (Method::Exec4, Exec4Params)
+                (Method::Constructor, empty),
+                // (Method::AddBalance, AddBalanceParams),
+                // (Method::WithdrawBalance, WithdrawBalanceParams),
+                // (Method::PublishStorageDeals, PublishStorageDealsParams),
+            ]
+        );
+    }};
+}
+
+macro_rules! register_market_versions_12 {
+    ($registry:expr, $code_cid:expr, $state_version:path) => {{
+        use $state_version::{Method, PublishStorageDealsParams, WithdrawBalanceParams};
+
+        register_actor_methods!(
+            $registry,
+            $code_cid,
+            [
+                (Method::Constructor, empty),
+                // (Method::AddBalance, AddBalanceParams),
+                // (Method::WithdrawBalance, WithdrawBalanceParams),
+                // (Method::PublishStorageDeals, PublishStorageDealsParams),
+            ]
+        );
+    }};
+}
+
+macro_rules! register_market_versions_13_to_16 {
+    ($registry:expr, $code_cid:expr, $state_version:path) => {{
+        use $state_version::{Method, PublishStorageDealsParams, WithdrawBalanceParams};
+
+        register_actor_methods!(
+            $registry,
+            $code_cid,
+            [
+                (Method::Constructor, empty),
+                // (Method::AddBalance, AddBalanceParams),
+                // (Method::WithdrawBalance, WithdrawBalanceParams),
+                // (Method::PublishStorageDeals, PublishStorageDealsParams),
             ]
         );
     }};
@@ -41,15 +76,15 @@ macro_rules! register_market_versions_11_to_16 {
 
 pub(crate) fn register_actor_methods(registry: &mut MethodRegistry, cid: Cid, version: u64) {
     match version {
-        8 => register_market_versions_8_to_10!(registry, cid, fil_actor_init_state::v8),
-        9 => register_market_versions_8_to_10!(registry, cid, fil_actor_init_state::v9),
-        10 => register_market_versions_8_to_10!(registry, cid, fil_actor_init_state::v10),
-        11 => register_market_versions_11_to_16!(registry, cid, fil_actor_init_state::v11),
-        12 => register_market_versions_11_to_16!(registry, cid, fil_actor_init_state::v12),
-        13 => register_market_versions_11_to_16!(registry, cid, fil_actor_init_state::v13),
-        14 => register_market_versions_11_to_16!(registry, cid, fil_actor_init_state::v14),
-        15 => register_market_versions_11_to_16!(registry, cid, fil_actor_init_state::v15),
-        16 => register_market_versions_11_to_16!(registry, cid, fil_actor_init_state::v16),
+        8 => register_market_versions_8_to_9!(registry, cid, fil_actor_market_state::v8),
+        9 => register_market_versions_8_to_9!(registry, cid, fil_actor_market_state::v9),
+        10 => register_market_versions_10_to_11!(registry, cid, fil_actor_market_state::v10),
+        11 => register_market_versions_10_to_11!(registry, cid, fil_actor_market_state::v11),
+        12 => register_market_versions_12!(registry, cid, fil_actor_market_state::v12),
+        13 => register_market_versions_13_to_16!(registry, cid, fil_actor_market_state::v13),
+        14 => register_market_versions_13_to_16!(registry, cid, fil_actor_market_state::v14),
+        15 => register_market_versions_13_to_16!(registry, cid, fil_actor_market_state::v15),
+        16 => register_market_versions_13_to_16!(registry, cid, fil_actor_market_state::v16),
         _ => {}
     }
 }
