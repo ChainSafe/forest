d06841377d3fee6e1061c8fc3fb4db4da032c5a4
elmattic <elmattic@users.noreply.github.com>
2025-08-22 12:50:35 +0200

    Add explanations on how we compute all parameters

--- scripts/tests/calibnet_api_test_stateful_check.sh
+++ scripts/tests/calibnet_api_test_stateful_check.sh
@@ -24,10 +24,23 @@ forest_init "$@"
 $FOREST_WALLET_PATH import preloaded_wallet.key
 $FOREST_WALLET_PATH --remote-wallet import preloaded_wallet.key
 
+# This is the address of a Calibnet pre-deployed simple ERC20 contract.
+# You can find the hex and source code in 'src/tool/subcommands/api_cmd/contracts/erc20'.
 TO_ADDRESS="t410fp6e7drelxau7nf76tcn6gva22t5jafefhevubwi"
+
 FROM_ADDRESS="t410f2avianksmit2cl2bqk53qant7nm7rdmk63twa5y"
+
+# This payload corresponds to minting new tokens for the 'FROM_ADDRESS' and will trigger a log event upon success.
+# To compute the calldata, you can use the 'cast calldata' subcommand with the following arguments:
+# cast calldata "mint(address,uint256)" 0x7f89f1c48bb829f697fe989be3541ad4fa901485 1000000000000000000
+#
+# Note that 0x7f89f1c48bb829f697fe989be3541ad4fa901485 is the Ethereum address corresponding to the contract f4 address.
 PAYLOAD="40c10f190000000000000000000000007f89f1c48bb829f697fe989be3541ad4fa9014850000000000000000000000000000000000000000000000000de0b6b3a7640000"
-TOPIC="0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+
+# This topic is derived using the keccak256 hash of the event signature 'Mint(address,uint256)'
+# To compute the topic, you can use the 'cast keccak256' subcommand with the following argument:
+# cast keccak256 "Mint(address,uint256)"
+TOPIC="0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885"
 
 $FOREST_TOOL_PATH api test-stateful "$FULLNODE_API_INFO" \
   --to "$TO_ADDRESS" \
