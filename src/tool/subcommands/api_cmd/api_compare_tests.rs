7c1e4dd2790317ace0d4b1c09aa7dbe512b00b8d
Aryan Tikarya <akaladarshi@gmail.com>
2025-08-25 22:11:47 +0530

    add snapshot tests for eam actor

--- src/tool/subcommands/api_cmd/api_compare_tests.rs
+++ src/tool/subcommands/api_cmd/api_compare_tests.rs
@@ -1876,8 +1876,52 @@ fn state_decode_params_api_tests(tipset: &Tipset) -> anyhow::Result<Vec<RpcTest>
     tests.extend(datacap_actor_state_decode_params_tests(tipset)?);
     tests.extend(multisig_actor_state_decode_params_tests(tipset)?);
     tests.extend(verified_reg_actor_state_decode_params_tests(tipset)?);
+    tests.extend(eam_actor_state_decode_params_tests(tipset)?);
 
     Ok(tests)
+}
+
+fn eam_actor_state_decode_params_tests(tipset: &Tipset) -> anyhow::Result<Vec<RpcTest>> {
+    let create_params = fil_actor_eam_state::v16::CreateParams {
+        initcode: vec![0x11, 0x22, 0x33, 0x44, 0x55], // dummy data
+        nonce: 2,
+    };
+
+    let create_params2 = fil_actor_eam_state::v16::Create2Params {
+        initcode: vec![0x11, 0x22, 0x33, 0x44, 0x55], // dummy data
+        salt: [0; 32],
+    };
+
+    let create_external_params = fil_actor_eam_state::v16::CreateExternalParams {
+        0: vec![0x11, 0x22, 0x33, 0x44, 0x55],
+    };
+
+    Ok(vec![
+        RpcTest::identity(StateDecodeParams::request((
+            Address::ETHEREUM_ACCOUNT_MANAGER_ACTOR,
+            fil_actor_eam_state::v16::Method::Constructor as u64,
+            vec![],
+            tipset.key().into(),
+        ))?),
+        RpcTest::identity(StateDecodeParams::request((
+            Address::ETHEREUM_ACCOUNT_MANAGER_ACTOR,
+            fil_actor_eam_state::v16::Method::Create as u64,
+            to_vec(&create_params)?,
+            tipset.key().into(),
+        ))?),
+        RpcTest::identity(StateDecodeParams::request((
+            Address::ETHEREUM_ACCOUNT_MANAGER_ACTOR,
+            fil_actor_eam_state::v16::Method::Create2 as u64,
+            to_vec(&create_params2)?,
+            tipset.key().into(),
+        ))?),
+        RpcTest::identity(StateDecodeParams::request((
+            Address::ETHEREUM_ACCOUNT_MANAGER_ACTOR,
+            fil_actor_eam_state::v16::Method::CreateExternal as u64,
+            to_vec(&create_external_params)?,
+            tipset.key().into(),
+        ))?),
+    ])
 }
 
 fn evm_actor_state_decode_params_tests(tipset: &Tipset) -> anyhow::Result<Vec<RpcTest>> {
