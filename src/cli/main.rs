3726d551804e9cc18596b39cc0d883ee935aeb07
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2025-08-28 14:32:50 +0200

    feat: strict checks for address args in `forest-cli`

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -11,44 +11,41 @@ use clap::Parser;
 use std::ffi::OsString;
 use std::str::FromStr as _;
 
-pub fn main<ArgT>(args: impl IntoIterator<Item = ArgT>) -> anyhow::Result<()>
+pub async fn main<ArgT>(args: impl IntoIterator<Item = ArgT>) -> anyhow::Result<()>
 where
     ArgT: Into<OsString> + Clone,
 {
+    // Preliminary client without the token to check network. This needs to occur before parsing to ensure the
+    // `StrictAddress` validation works correctly.
+    let client = rpc::Client::default_or_from_env(None)?;
+    if let Ok(name) = StateNetworkName::call(&client, ()).await
+        && !matches!(NetworkChain::from_str(&name), Ok(NetworkChain::Mainnet))
+    {
+        CurrentNetwork::set_global(Network::Testnet);
+    }
+
     // Capture Cli inputs
     let Cli { token, cmd } = Cli::parse_from(args);
 
     let client = rpc::Client::default_or_from_env(token.as_deref())?;
 
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            logger::setup_logger(&crate::cli_shared::cli::CliOpts::default());
+    logger::setup_logger(&crate::cli_shared::cli::CliOpts::default());
 
-            if let Ok(name) = StateNetworkName::call(&client, ()).await
-                && !matches!(NetworkChain::from_str(&name), Ok(NetworkChain::Mainnet))
-            {
-                CurrentNetwork::set_global(Network::Testnet);
-            }
-
-            // Run command
-            match cmd {
-                Subcommand::Chain(cmd) => cmd.run(client).await,
-                Subcommand::Auth(cmd) => cmd.run(client).await,
-                Subcommand::Net(cmd) => cmd.run(client).await,
-                Subcommand::Sync(cmd) => cmd.run(client).await,
-                Subcommand::Mpool(cmd) => cmd.run(client).await,
-                Subcommand::State(cmd) => cmd.run(client).await,
-                Subcommand::Config(cmd) => cmd.run(&mut std::io::stdout()),
-                Subcommand::Send(cmd) => cmd.run(client).await,
-                Subcommand::Info(cmd) => cmd.run(client).await,
-                Subcommand::Snapshot(cmd) => cmd.run(client).await,
-                Subcommand::Shutdown(cmd) => cmd.run(client).await,
-                Subcommand::Healthcheck(cmd) => cmd.run(client).await,
-                Subcommand::F3(cmd) => cmd.run(client).await,
-                Subcommand::WaitApi(cmd) => cmd.run(client).await,
-            }
-        })
+    // Run command
+    match cmd {
+        Subcommand::Chain(cmd) => cmd.run(client).await,
+        Subcommand::Auth(cmd) => cmd.run(client).await,
+        Subcommand::Net(cmd) => cmd.run(client).await,
+        Subcommand::Sync(cmd) => cmd.run(client).await,
+        Subcommand::Mpool(cmd) => cmd.run(client).await,
+        Subcommand::State(cmd) => cmd.run(client).await,
+        Subcommand::Config(cmd) => cmd.run(&mut std::io::stdout()),
+        Subcommand::Send(cmd) => cmd.run(client).await,
+        Subcommand::Info(cmd) => cmd.run(client).await,
+        Subcommand::Snapshot(cmd) => cmd.run(client).await,
+        Subcommand::Shutdown(cmd) => cmd.run(client).await,
+        Subcommand::Healthcheck(cmd) => cmd.run(client).await,
+        Subcommand::F3(cmd) => cmd.run(client).await,
+        Subcommand::WaitApi(cmd) => cmd.run(client).await,
+    }
 }
