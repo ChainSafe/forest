9965521836591e7a1542f32fa74f42f9c2504d41
hanabi1224 <harlowmoo@gmail.com>
2025-09-16 19:20:08 +0800

    revert metrics registry reset changes

--- src/daemon/mod.rs
+++ src/daemon/mod.rs
@@ -574,8 +574,6 @@ pub(super) async fn start_services(
     shutdown_send: mpsc::Sender<()>,
     on_app_context_and_db_initialized: impl Fn(&AppContext),
 ) -> anyhow::Result<()> {
-    // Cleanup the default prometheus metrics registry
-    *crate::metrics::default_registry() = Default::default();
     let mut services = JoinSet::new();
     let network = config.chain();
     let ctx = AppContext::init(opts, &config).await?;
