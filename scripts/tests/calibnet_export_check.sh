4751d1d5eb13ffc881eae62e4b76e0f132f9dd62
hanabi1224 <harlowmoo@gmail.com>
2025-09-17 17:53:50 +0800

    CI test

--- scripts/tests/calibnet_export_check.sh
+++ scripts/tests/calibnet_export_check.sh
@@ -20,7 +20,19 @@ $FOREST_CLI_PATH snapshot export --tipset 0 --format "$format"
 echo "Exporting zstd compressed snapshot in $format format"
 $FOREST_CLI_PATH snapshot export --format "$format"
 
+snapshot=$(ls *.car.zst | tail -n 1)
+snapshot_epoch=$(forest_query_epoch "$snapshot")
+
+echo "Exporting diff snapshot @ $snapshot_epoch with forest-cli snapshot export-diff"
+$FOREST_CLI_PATH snapshot export-diff --from "$snapshot_epoch" --to "$((snapshot_epoch - 900))" -d 900 -o diff1
+
 $FOREST_CLI_PATH shutdown --force
+
+echo "Exporting diff snapshot @ $snapshot_epoch with forest-tool archive export"
+$FOREST_TOOL_PATH archive export --epoch "$snapshot_epoch" --depth 900 --diff "$((snapshot_epoch - 900))" --diff-depth 900 -o diff2 "$snapshot"
+
+echo "Comparing diff snapshots"
+diff diff1 diff2
 
 for f in *.car.zst; do
   echo "Inspecting archive info $f"
