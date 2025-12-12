b7c6fed599204cab488119252b326906dd89e0f3
hanabi1224 <harlowmoo@gmail.com>
2025-12-12 23:53:33 +0800

    fix: setup tokio runtime with macro in forest-tool and forest-wallet

--- src/bin/forest-wallet.rs
+++ src/bin/forest-wallet.rs
@@ -1,6 +1,7 @@
 // Copyright 2019-2025 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-fn main() -> anyhow::Result<()> {
-    forest::forest_wallet_main(std::env::args_os())
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> anyhow::Result<()> {
+    forest::forest_wallet_main(std::env::args_os()).await
 }
