27439d27cd2e0e728aa878e312af140cb3e32e29
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2025-12-15 13:51:33 +0100

    chore: mpool regression test for message gas limit

--- src/utils/cache/lru.rs
+++ src/utils/cache/lru.rs
@@ -149,6 +149,11 @@ where
         }
         size
     }
+
+    #[cfg(test)]
+    pub(crate) fn new_mocked() -> Self {
+        Self::new_inner(Cow::Borrowed("mocked_cache"), NonZeroUsize::new(1))
+    }
 }
 
 impl<K, V> Collector for SizeTrackingLruCache<K, V>
