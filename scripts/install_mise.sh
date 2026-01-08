664739d60bf1389b60da9e221775dca514ebff0a
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2026-01-08 19:15:29 +0100

    harden install script

--- scripts/install_mise.sh
+++ scripts/install_mise.sh
@@ -6,8 +6,8 @@ export GPG_KEY=24853EC9F655CE80B48E6C3A8B81C9D17413A06D
 export MISE_VERSION=v2025.12.0
 export MISE_INSTALL_PATH=/bin/mise
 
-gpg --keyserver hkps://keys.openpgp.org --recv-keys 24853EC9F655CE80B48E6C3A8B81C9D17413A06D
-curl https://mise.jdx.dev/install.sh.sig | gpg --decrypt > install-mise.sh
+gpg --keyserver hkps://keys.openpgp.org --recv-keys ${GPG_KEY}
+curl --fail https://mise.jdx.dev/install.sh.sig | gpg --decrypt > install-mise.sh
 # ensure the above is signed with the mise release key
 sh ./install-mise.sh
 rm install-mise.sh
