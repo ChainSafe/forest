55479830a80c0ce9fb303b2f1bc4ca33686f4e53
Hubert <hubert@chainsafe.io>
2026-01-10 01:02:49 +0100

    chore: improve license check script (#6401)

--- mise-tasks/add_license.sh
+++ mise-tasks/add_license.sh
@@ -3,17 +3,23 @@
 
 # Returns 1 if there was a missing license, 0 otherwise.
 
-PAT_APA="^// Copyright 2019-2026 ChainSafe Systems// SPDX-License-Identifier: Apache-2.0, MIT$"
+COPYRIGHT="// Copyright 2019-2026 ChainSafe Systems\n// SPDX-License-Identifier: Apache-2.0, MIT"
 
-ret=0
-for file in $(git grep --cached -Il '' -- '*.rs' ':!*src/utils/encoding/fallback_de_ipld_dagcbor.rs'); do
-  header=$(head -2 "$file" | tr -d '\n')
-	if ! echo "$header" | grep -q "$PAT_APA"; then
-		echo "$file was missing header"
-		cat ./scripts/copyright.txt "$file" > temp
-		mv temp "$file"
-		ret=1
-	fi
+# Enable lastpipe option to allow while loop to modify variables in the parent shell. See https://www.shellcheck.net/wiki/SC2031
+shopt -s lastpipe
+
+missing_license=0
+git ls-files '*.rs' ':!src/utils/encoding/fallback_de_ipld_dagcbor.rs' | while read -r file; do
+  # Kind of contrived way of matching multiline text, but that's what grep supports. Note escaping dots
+  # to match literal dots.
+  if ! head -n 2 "$file" | grep -zPo "${COPYRIGHT//./\\.}" > /dev/null; then
+    echo "Adding missing license to $file"
+    # Adds the license to the top of the file
+    {
+      echo -e "$COPYRIGHT\n"
+      cat "$file"
+    } > "$file.tmp" && mv "$file.tmp" "$file"
+    missing_license=1
+  fi
 done
-
-exit $ret
+exit $missing_license
