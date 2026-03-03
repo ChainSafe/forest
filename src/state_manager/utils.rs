209f7e21956f306638383d262b936b0d71553dcb
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2026-03-03 13:39:15 +0100

    fix: move raw println to log for state compute

--- src/state_manager/utils.rs
+++ src/state_manager/utils.rs
@@ -317,7 +317,7 @@ pub mod state_compute {
         } = state_manager
             .compute_tipset_state(ts, crate::state_manager::NO_CALLBACK, VMTrace::NotTraced)
             .await?;
-        println!(
+        tracing::info!(
             "epoch: {epoch}, state_root: {state_root}, receipt_root: {receipt_root}, took {}.",
             humantime::format_duration(start.elapsed())
         );
