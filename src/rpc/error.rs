a5e858c69263bf36c23c5c378dcef57a1c53a594
hanabi1224 <harlowmoo@gmail.com>
2026-03-26 04:34:20 +0800

    resolve comments

--- src/rpc/error.rs
+++ src/rpc/error.rs
@@ -98,14 +98,14 @@ impl<E: std::error::Error + RpcErrorData + 'static> From<E> for ServerError {
 
 // Default implementation for anyhow::Error to handle downcasting once
 impl From<anyhow::Error> for ServerError {
-    fn from(e: anyhow::Error) -> Self {
+    fn from(error: anyhow::Error) -> Self {
         // Try to downcast to known RpcErrorData implementations
-        if let Some(eth_error) = e.downcast_ref::<EthErrors>() {
+        if let Some(eth_error) = error.downcast_ref::<EthErrors>() {
             return eth_error.clone().into();
         }
 
-        // Default fallback
-        Self::internal_error(format!("{e:#}"), None)
+        // Default fallback, not using `format!("{e:#}")` here to match Lotus error
+        Self::internal_error(error.to_string(), None)
     }
 }
 
