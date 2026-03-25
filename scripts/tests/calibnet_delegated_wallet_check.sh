53a5e8b0f28f7debbee76348db5aaef060b35aa8
Shashank <shashank@chainsafe.io>
2026-03-25 10:15:03 +0530

    enable wallet test

--- scripts/tests/calibnet_delegated_wallet_check.sh
+++ scripts/tests/calibnet_delegated_wallet_check.sh
@@ -15,7 +15,7 @@ FIL_AMT="500 atto FIL"
 FIL_ZERO="0 FIL"
 
 # The preloaded address
-ADDR_ONE=$($FOREST_WALLET_PATH list | tail -1 | cut -d ' ' -f1)
+ADDR_ONE=$($FOREST_WALLET_PATH list | tail -1 | cut -d ' ' -f2)
 
 sleep 5s
 
@@ -30,7 +30,7 @@ $FOREST_WALLET_PATH export "$DELEGATE_ADDR_ONE" > delegated_wallet.key
 $FOREST_WALLET_PATH --remote-wallet import delegated_wallet.key
 
 # Fund delegated wallet from preloaded wallet
-DELEGATE_FUND_AMT="2 micro FIL"
+DELEGATE_FUND_AMT="3 micro FIL"
 $FOREST_WALLET_PATH set-default "$ADDR_ONE"
 MSG_DELEGATE_FUND=$($FOREST_WALLET_PATH send "$DELEGATE_ADDR_ONE" "$DELEGATE_FUND_AMT")
 : "$MSG_DELEGATE_FUND"
@@ -79,6 +79,21 @@ while [[ $i != 20 && $DELEGATE_ADDR_THREE_BALANCE == "$FIL_ZERO" ]]; do
   : "Checking DELEGATE_ADDR_THREE balance $i/20"
   sleep 30s
   DELEGATE_ADDR_THREE_BALANCE=$($FOREST_WALLET_PATH --remote-wallet balance "$DELEGATE_ADDR_THREE" --exact-balance)
+done
+
+$FOREST_WALLET_PATH list
+$FOREST_WALLET_PATH --remote-wallet list
+
+MSG_DELEGATE_FOUR=$($FOREST_WALLET_PATH --remote-wallet send "$DELEGATE_ADDR_THREE" "$FIL_AMT")
+: "$MSG_DELEGATE_FOUR"
+
+DELEGATE_ADDR_REMOTE_THREE_BALANCE=$DELEGATE_ADDR_THREE_BALANCE
+i=0
+while [[ $i != 20 && $DELEGATE_ADDR_REMOTE_THREE_BALANCE == "$DELEGATE_ADDR_THREE_BALANCE" ]]; do
+  i=$((i+1))
+  : "Checking DELEGATE_ADDR_THREE balance $i/20"
+  sleep 30s
+  DELEGATE_ADDR_REMOTE_THREE_BALANCE=$($FOREST_WALLET_PATH --remote-wallet balance "$DELEGATE_ADDR_THREE" --exact-balance)
 done
 
 $FOREST_WALLET_PATH list
