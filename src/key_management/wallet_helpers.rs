08e4e263ec07df36367a3cbb02b3cfdb63e41cb5
hanabi1224 <harlowmoo@gmail.com>
2026-04-15 16:32:05 +0800

    chore: deprecate `libsecp256k1` and fix RUSTSEC-2025-0161 (#6907)

--- src/key_management/wallet_helpers.rs
+++ src/key_management/wallet_helpers.rs
@@ -13,26 +13,18 @@ use crate::shim::{
 };
 use crate::utils::encoding::{blake2b_256, keccak_256};
 use bls_signatures::{PrivateKey as BlsPrivate, Serialize};
-use libsecp256k1::{Message as SecpMessage, PublicKey as SecpPublic, SecretKey as SecpPrivate};
 
-/// Return the public key for a given private key and [`SignatureType`]
-pub fn to_public(sig_type: SignatureType, private_key: &[u8]) -> Result<Vec<u8>, Error> {
+/// Return the uncompressed public key for a given private key and [`SignatureType`]
+pub fn to_uncompressed_public_key(
+    sig_type: SignatureType,
+    private_key: &[u8],
+) -> Result<Vec<u8>, Error> {
     match sig_type {
-        SignatureType::Bls => Ok(BlsPrivate::from_bytes(private_key)
-            .map_err(|err| Error::Other(err.to_string()))?
-            .public_key()
-            .as_bytes()),
-        SignatureType::Secp256k1 => {
-            let private_key = SecpPrivate::parse_slice(private_key)
-                .map_err(|err| Error::Other(err.to_string()))?;
-            let public_key = SecpPublic::from_secret_key(&private_key);
-            Ok(public_key.serialize().to_vec())
-        }
-        SignatureType::Delegated => {
-            let private_key = SecpPrivate::parse_slice(private_key)
-                .map_err(|err| Error::Other(err.to_string()))?;
-            let public_key = SecpPublic::from_secret_key(&private_key);
-            Ok(public_key.serialize().to_vec())
+        SignatureType::Bls => Ok(BlsPrivate::from_bytes(private_key)?.public_key().as_bytes()),
+        SignatureType::Secp256k1 | SignatureType::Delegated => {
+            let private_key = k256::ecdsa::SigningKey::from_slice(private_key)?;
+            let public_key = private_key.verifying_key();
+            Ok(public_key.to_encoded_point(false).as_bytes().to_vec())
         }
     }
 }
@@ -51,11 +43,8 @@ pub fn new_address(sig_type: SignatureType, public_key: &[u8]) -> Result<Address
             Ok(addr)
         }
         SignatureType::Delegated => {
-            let eth_addr = EthAddress::eth_address_from_pub_key(public_key)
-                .map_err(|err| Error::Other(err.to_string()))?;
-            let addr = eth_addr
-                .to_filecoin_address()
-                .map_err(|err| Error::Other(err.to_string()))?;
+            let eth_addr = EthAddress::eth_address_from_uncompressed_public_key(public_key)?;
+            let addr = eth_addr.to_filecoin_address()?;
             Ok(addr)
         }
     }
@@ -64,41 +53,29 @@ pub fn new_address(sig_type: SignatureType, public_key: &[u8]) -> Result<Address
 /// Sign takes in [`SignatureType`], private key and message. Returns a Signature
 /// for that message
 pub fn sign(sig_type: SignatureType, private_key: &[u8], msg: &[u8]) -> Result<Signature, Error> {
+    let sign_k256 = |msg_hash, sig_type| -> Result<Signature, Error> {
+        let priv_key = k256::ecdsa::SigningKey::from_slice(private_key)?;
+        let (sig, recovery_id) = priv_key.sign_prehash_recoverable(msg_hash)?;
+        let mut new_bytes = [0; 65];
+        new_bytes[..64].copy_from_slice(&sig.to_bytes());
+        new_bytes[64] = recovery_id.to_byte();
+        Ok(Signature {
+            sig_type,
+            bytes: new_bytes.to_vec(),
+        })
+    };
+
     match sig_type {
         SignatureType::Bls => {
-            let priv_key =
-                BlsPrivate::from_bytes(private_key).map_err(|err| Error::Other(err.to_string()))?;
+            let priv_key = BlsPrivate::from_bytes(private_key)?;
             // this returns a signature from bls-signatures, so we need to convert this to a
             // crypto signature
             let sig = priv_key.sign(msg);
             let crypto_sig = Signature::new_bls(sig.as_bytes());
             Ok(crypto_sig)
         }
-        SignatureType::Secp256k1 => {
-            let priv_key = SecpPrivate::parse_slice(private_key)
-                .map_err(|err| Error::Other(err.to_string()))?;
-            let msg_hash = blake2b_256(msg);
-            let message = SecpMessage::parse(&msg_hash);
-            let (sig, recovery_id) = libsecp256k1::sign(&message, &priv_key);
-            let mut new_bytes = [0; 65];
-            new_bytes[..64].copy_from_slice(&sig.serialize());
-            new_bytes[64] = recovery_id.serialize();
-            let crypto_sig = Signature::new_secp256k1(new_bytes.to_vec());
-            Ok(crypto_sig)
-        }
-        SignatureType::Delegated => {
-            let priv_key = SecpPrivate::parse_slice(private_key)
-                .map_err(|err| Error::Other(err.to_string()))?;
-
-            let msg_hash = keccak_256(msg);
-            let message = SecpMessage::parse(&msg_hash);
-            let (sig, recovery_id) = libsecp256k1::sign(&message, &priv_key);
-            let mut new_bytes = [0; 65];
-            new_bytes[..64].copy_from_slice(&sig.serialize());
-            new_bytes[64] = recovery_id.serialize();
-            let crypto_sig = Signature::new_delegated(new_bytes.to_vec());
-            Ok(crypto_sig)
-        }
+        SignatureType::Secp256k1 => sign_k256(&blake2b_256(msg), sig_type),
+        SignatureType::Delegated => sign_k256(&keccak_256(msg), sig_type),
     }
 }
 
@@ -110,13 +87,9 @@ pub fn generate(sig_type: SignatureType) -> Result<Vec<u8>, Error> {
             let key = BlsPrivate::generate(rng);
             Ok(key.as_bytes())
         }
-        SignatureType::Secp256k1 => {
-            let key = SecpPrivate::random(rng);
-            Ok(key.serialize().to_vec())
-        }
-        SignatureType::Delegated => {
-            let key = SecpPrivate::random(rng);
-            Ok(key.serialize().to_vec())
+        SignatureType::Secp256k1 | SignatureType::Delegated => {
+            let key = k256::ecdsa::SigningKey::random(rng);
+            Ok(key.to_bytes().to_vec())
         }
     }
 }
