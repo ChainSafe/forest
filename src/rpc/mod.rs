2089aaddc3f476ce7524c40d9bd944799afd35d3
hanabi1224 <harlowmoo@gmail.com>
2026-05-25 21:07:55 +0800

    feat(rpc): internal method `Forest.BaseFeeByHeight` for troubleshooting

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -107,6 +107,7 @@ macro_rules! for_each_rpc_method {
         $callback!($crate::rpc::eth::EthAddressToFilecoinAddress);
         $callback!($crate::rpc::eth::FilecoinAddressToEthAddress);
         $callback!($crate::rpc::eth::EthBaseFee);
+        $callback!($crate::rpc::eth::BaseFeeByHeight);
         $callback!($crate::rpc::eth::EthBlockNumber);
         $callback!($crate::rpc::eth::EthCall);
         $callback!($crate::rpc::eth::EthChainId);
