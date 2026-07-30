d26ef25914ef538a141a72768daca87e1e99b11a
hanabi1224 <harlowmoo@gmail.com>
2026-07-30 15:42:31 +0800

    feat: forest-tool db import-tipset-lookup

--- docs/docs/users/reference/cli.sh
+++ docs/docs/users/reference/cli.sh
@@ -138,6 +138,7 @@ generate_markdown_section "forest-tool" "db"
 generate_markdown_section "forest-tool" "db stats"
 generate_markdown_section "forest-tool" "db destroy"
 generate_markdown_section "forest-tool" "db import"
+generate_markdown_section "forest-tool" "db import-tipset-lookup"
 
 generate_markdown_section "forest-tool" "car"
 generate_markdown_section "forest-tool" "car concat"
--- src/tool/subcommands/db_cmd.rs
+++ src/tool/subcommands/db_cmd.rs
@@ -2,18 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use std::path::PathBuf;
+use std::time::Instant;
 
+use crate::blocks::{Tipset, TipsetKey};
+use crate::chain::TIPSET_LOOKUP_HAMT_BIT_WIDTH;
 use crate::cli::subcommands::prompt_confirm;
 use crate::cli_shared::{chain_path, delete_chain_data, read_config};
-use crate::db::BlockstoreWithWriteBuffer;
+use crate::daemon::db_util::load_all_forest_cars;
+use crate::db::car::{AnyCar, ManyCar};
 use crate::db::db_engine::{db_root, open_db};
+use crate::db::{BlockstoreWithWriteBuffer, CAR_DB_DIR_NAME, EthMappingsStore};
 use crate::networks::NetworkChain;
+use crate::prelude::*;
 use crate::utils::db::car_stream::CarStream;
 use clap::Subcommand;
-use fvm_ipld_blockstore::Blockstore;
+use fil_actors_shared::fvm_ipld_hamt::Hamt;
 use indicatif::{ProgressBar, ProgressStyle};
 use tokio_stream::StreamExt;
-use tracing::error;
 
 #[derive(Debug, Subcommand)]
 pub enum DBCommands {
@@ -52,6 +57,18 @@ pub enum DBCommands {
         /// Skip block validation
         #[arg(long)]
         skip_validation: bool,
+    },
+    /// Import a tipset lookup HAMT file into the key-value store
+    ImportTipsetLookup {
+        /// Snapshot input paths. Supports `.car`, `.car.zst`, and `.forest.car.zst`.
+        #[arg(required = true)]
+        snapshot: PathBuf,
+        /// Filecoin network chain
+        #[arg(long, required = true)]
+        chain: NetworkChain,
+        /// Optional path to the database folder that powers a Forest node
+        #[arg(long)]
+        db: Option<PathBuf>,
     },
 }
 
@@ -141,6 +158,61 @@ impl DBCommands {
                 }
                 drop(db_writer);
                 pb.finish();
+                Ok(())
+            }
+            Self::ImportTipsetLookup {
+                snapshot,
+                chain,
+                db,
+            } => {
+                let start = Instant::now();
+                println!("Loading tipset lookup hamt...");
+                let hamt_car = AnyCar::try_from(snapshot.as_path())?;
+                anyhow::ensure!(
+                    hamt_car.header_v1().roots.len() == 1,
+                    "hamt car should have a single root"
+                );
+                let hamt_root = *hamt_car.header_v1().roots.first();
+                let hamt: Hamt<_, TipsetKey, ChainEpoch> =
+                    Hamt::load_with_bit_width(&hamt_root, hamt_car, TIPSET_LOOKUP_HAMT_BIT_WIDTH)?;
+                println!("Loaded tipset lookup hamt...");
+
+                let db_root_path = if let Some(db) = db {
+                    db
+                } else {
+                    let (_, config) = read_config(None, Some(chain.clone()))?;
+                    db_root(&chain_path(&config))?
+                };
+                println!("Opening parity-db at {}", db_root_path.display());
+                let db = {
+                    let forest_car_db_dir = db_root_path.join(CAR_DB_DIR_NAME);
+                    let db = ManyCar::new(open_db(db_root_path, &Default::default())?);
+                    load_all_forest_cars(&db, &forest_car_db_dir)?;
+                    db
+                };
+                let pb = ProgressBar::new_spinner().with_style(
+                    ProgressStyle::with_template("{spinner} {pos} blocks imported")
+                        .expect("indicatif template must be valid"),
+                );
+                pb.enable_steady_tick(std::time::Duration::from_millis(100));
+                hamt.for_each_cacheless(|&epoch, tsk| {
+                    let ts = Tipset::load_required(&db, tsk)?;
+                    anyhow::ensure!(
+                        ts.epoch() == epoch,
+                        "epochs do not match, {epoch} in hamt, {} in database",
+                        ts.epoch()
+                    );
+                    db.set_tipset_key_at_epoch(&ts)?;
+                    pb.inc(1);
+                    anyhow::Ok(())
+                })?;
+                drop(db);
+                let total = pb.position();
+                pb.finish();
+                println!(
+                    "Imported {total} tipset lookup entries, took {}",
+                    humantime::format_duration(start.elapsed())
+                );
                 Ok(())
             }
         }
