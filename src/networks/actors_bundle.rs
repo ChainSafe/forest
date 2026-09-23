fd679dc9e6243b62b55f8354b9a67393845be480
Aryan Tikarya <akaladarshi@gmail.com>
2026-09-23 13:16:52 +0530

    update the calibrationnet actor bundle to v19.0.1

--- src/networks/actors_bundle.rs
+++ src/networks/actors_bundle.rs
@@ -86,6 +86,7 @@ pub static ACTOR_BUNDLES: LazyLock<Box<[ActorBundleInfo]>> = LazyLock::new(|| {
         "bafy2bzacecqtwq6hjhj2zy5gwjp76a4tpcg2lt7dps5ycenvynk2ijqqyo65e" @ "v16.0.1" for "calibrationnet",
         "bafy2bzacecn64rlb52rjsvgopnidz6w42z3zobmjxqek5s4xqjh3ly47rcurg" @ "v17.0.0" for "calibrationnet",
         "bafy2bzacebkfatnbe6w4rj7lf6gkjh7mywlrpdh2dj6hu2dl4rmtwksszm2hs" @ "v18.0.0" for "calibrationnet",
+        "bafy2bzaceastk5qjmpnaqeeq6whogrlcymyurzyq6jawkntdzwedaznw7amvy" @ "v19.0.1" for "calibrationnet",
         "bafy2bzacedg3y3ylp3w4bqmfgyiasrikg3bgfibblc7o4gri3b7j7ftgjhyuu" @ "v18.0.0" for "butterflynet",
         "bafy2bzacedcghtji2u4mill4el2wbssvwfe2hyhjxp3l6oho57uqc537iq3x2" @ "v19.0.1" for "butterflynet",
         "bafy2bzacedozk3jh2j4nobqotkbofodq4chbrabioxbfrygpldgoxs3zwgggk" @ "v9.0.3" for "devnet",
